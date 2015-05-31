7a6473a8ddf9d359762270038c66b030b5dd5aed
Amit Aryeh Levy <amit@amitlevy.com>
2015-05-31 16:24:21 -0400

    move sam4l loader and target to appropriate folder

--- src/chips/sam4l/Makefile.mk
+++ src/chips/sam4l/Makefile.mk
@@ -1,10 +1,10 @@
 RUSTC_FLAGS += -C opt-level=2 -Z no-landing-pads
-RUSTC_FLAGS += --target src/platform/storm/target.json
+RUSTC_FLAGS += --target src/chips/sam4l/target.json
 RUSTC_FLAGS += -Ctarget-cpu=cortex-m4 -C relocation_model=static
 RUSTC_FLAGS += -g -C no-stack-check
 
 CFLAGS += -g -O3 -std=gnu99 -mcpu=cortex-m4 -mthumb -nostdlib
-LDFLAGS += -Tsrc/platform/storm/loader.ld
+LDFLAGS += -Tsrc/chips/sam4l/loader.ld
 
 $(BUILD_DIR)/libsam4l.rlib: $(call rwildcard,src/chips/sam4l,*.rs) $(BUILD_DIR)/libcore.rlib $(BUILD_DIR)/libhil.rlib $(BUILD_DIR)/libcommon.rlib
 	@echo "Building $@"
