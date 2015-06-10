c557f384d7e7f7bbfed73d849060472d630317b1
Amit Aryeh Levy <amit@amitlevy.com>
2015-06-10 15:17:43 -0400

    Pull out test app from main.rs

--- src/main/apps.rs
+++ src/main/apps.rs
@@ -1,4 +1,28 @@
-pub fn app1_init() {
-    loop {
+#[allow(improper_ctypes)]
+extern {
+    fn __subscribe(driver_num: usize, arg1: usize, arg2: fn());
+    fn __command(driver_num: usize, subnum: usize, arg1: usize);
+    fn __wait(a: usize, b: usize, c: usize);
+}
+
+fn command(driver_num: usize, subnum: usize, arg1: usize) {
+    unsafe {
+        __command(driver_num, subnum, arg1);
     }
 }
+
+fn wait() {
+    unsafe {
+        __wait(0, 0, 0);
+    }
+}
+
+pub fn app1_init() {
+    command(1, 0, 'c' as usize);
+    command(1, 0, 'm' as usize);
+    loop {
+        command(1, 0, 'd' as usize);
+        //wait();
+    }
+}
+
--- src/main/main.rs
+++ src/main/main.rs
@@ -26,52 +26,63 @@ pub extern fn main() {
 
     let mut process = app1;
 
-    loop {
-        unsafe {
-            platform.service_pending_interrupts();
-
-            match process.state {
-                process::State::Running => {
-                    process.switch_to();
-                }
-                process::State::Waiting => {
-                    match process.callbacks.dequeue() {
-                        None => { continue },
-                        Some(cb) => {
-                            process.state = process::State::Running;
-                            process.switch_to_callback(cb);
-                        }
+    unsafe {
+        match process.state {
+            process::State::Running => {
+                process.switch_to();
+            }
+            process::State::Waiting => {
+                match process.callbacks.dequeue() {
+                    None => { },
+                    Some(cb) => {
+                        process.state = process::State::Running;
+                        process.switch_to_callback(cb);
                     }
                 }
             }
-            match process.svc_number() {
-                Some(syscall::WAIT) => {
-                    process.state = process::State::Waiting;
-                    process.pop_syscall_stack();
-                    // TODO(alevy): iterate `process` to next available app
-                },
-                Some(syscall::SUBSCRIBE) => {
-                    let res = platform.with_driver(process.r0(), |driver| {
-                        match driver {
-                            Some(d) => d.subscribe(process.r1(),
-                                                        process.r2()),
-                            None => -1
-                        }
-                    });
-                    process.set_r0(res);
-                },
-                Some(syscall::COMMAND) => {
-                    let res = platform.with_driver(process.r0(), |driver| {
-                        match driver {
-                            Some(d) => d.command(process.r1(),
-                                                 process.r2()),
-                            None => -1
-                        }
-                    });
-                    process.set_r0(res);
-                },
-                _ => {}
-            }
+        }
+        match process.svc_number() {
+            Some(syscall::WAIT) => {
+                platform.with_driver(1, |console| {
+                    console.map(|c| c. command(0, 'w' as usize));
+                });
+                process.state = process::State::Waiting;
+                process.pop_syscall_stack();
+                // TODO(alevy): iterate `process` to next available app
+            },
+            Some(syscall::SUBSCRIBE) => {
+                platform.with_driver(1, |console| {
+                    console.map(|c| c. command(0, 's' as usize));
+                });
+                let res = platform.with_driver(process.r0(), |driver| {
+                    match driver {
+                        Some(d) => d.subscribe(process.r1(),
+                                                    process.r2()),
+                        None => -1
+                    }
+                });
+                process.set_r0(res);
+            },
+            Some(syscall::COMMAND) => {
+                platform.with_driver(1, |console| {
+                    console.map(|c| c. command(0, 'c' as usize));
+                });
+                let res = platform.with_driver(process.r0(), |driver| {
+                    match driver {
+                        Some(d) => d.command(process.r1(),
+                                             process.r2()),
+                        None => -1
+                    }
+                });
+                process.set_r0(res);
+            },
+            _ => {}
+        }
+    }
+
+    loop {
+        unsafe {
+            platform.service_pending_interrupts();
 
             support::atomic(|| {
                 if !platform.has_pending_interrupts() {
