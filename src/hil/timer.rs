6b4c0d91f4964150f250c43f0fa05e23ec927003
Philip Levis <pal@cs.stanford.edu>
2015-07-17 19:59:06 -0700

    Linked list implementation -- still needs proper equality test.

--- src/hil/timer.rs
+++ src/hil/timer.rs
@@ -49,7 +49,6 @@ impl RequestInternal {
 
 pub struct TimerMux {
   request: Option<&'static mut RequestInternal>,
-  last: Option<&'static mut RequestInternal>,
   internal: Option<&'static mut alarm::Alarm>
 }
 
@@ -57,7 +56,6 @@ impl TimerMux {
   pub fn new(internal: &'static mut alarm::Alarm) -> TimerMux {
     TimerMux {
       request: None,
-      last: None,
       internal: Some(internal)
     }
   }
@@ -76,6 +74,82 @@ impl TimerMux {
     self.request = Some(request);
 
   }
+
+  // Returns whether hardware clock has to be recalculated (inserted
+  // timer is now first timer)
+  fn insert(&'static mut self, request: &'static mut RequestInternal) -> bool {
+    if request.next.is_some() { // Already on a list, this is an error!
+      false
+    }
+    else if self.request.is_none() {
+      self.request = Some(request);
+      true
+    } else {
+      let mut first = true; 
+      let mut done = false;
+      let mut copt = &mut self.request;
+      while !done {
+        let mut mycopt = copt.take();
+        let mut curr = mycopt.unwrap();
+        // 'request' is earlier than current element, insert here by making
+        // the current Option point to 'request' and have 'request''s next
+        // Option point to the element the current Option held.
+        if request.when < curr.when {
+           request.next = Some(curr);
+           *copt = Some(request);
+           done = true;
+        } else {
+           // We need to insert later. We therefore are not inserting in
+           // the first element. There are two cases:
+           //   1. last element and we need to insert at the end, or
+           //   2. we need to traverse the next hop.
+           first = false;
+           if curr.next.is_none() {
+             // Reached end of list, insert here
+             curr.next = Some(request);
+             *copt = Some(curr);
+             done = true;
+           } else {
+             let mut nopt = &mut curr.next;
+             let mut mynopt = nopt.take();
+             *copt = Some(curr);
+             copt = nopt;
+             mycopt = mynopt;
+           }
+
+        }
+      }
+      first
+    }
+  }
+
+  // Returns whether hardware clock has to be recalculated (removed first
+  // timer)
+  fn remove(&'static mut self, request: &'static mut RequestInternal) -> bool {
+    if self.request.is_none() {return false;}
+    
+    let mut done = false;
+    let mut copt = &mut self.request;
+    let mut first = true;
+    while !done {
+      let mycopt = copt.take();
+      let mut curr = mycopt.unwrap();
+      if false {
+        *copt = curr.next.take();
+        done = true;
+      } else if curr.next.is_none() {
+        *copt = Some(curr);
+        first = false;
+        done = true;
+      } else {
+        let mut nopt = &mut curr.next;
+        *copt = Some(curr);
+        copt = nopt;
+        first = false;
+      }
+    }
+    first
+  }
 }
 
 impl alarm::Request for TimerMux {
@@ -86,7 +160,7 @@ impl alarm::Request for TimerMux {
     let request: &'static mut RequestInternal = ropt.unwrap();
 
     if request.is_repeat {
-      let t = self as &'static mut Timer;
+    //  let t = self as &'static mut Timer;
     }
   }
 }
