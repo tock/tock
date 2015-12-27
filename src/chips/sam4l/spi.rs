1483cecb19530d31701ce182908b0d2b8b9ca970
Philip Levis <pal@cs.stanford.edu>
2015-12-27 11:07:39 -0800

    Removed warnings.

--- src/chips/sam4l/spi.rs
+++ src/chips/sam4l/spi.rs
@@ -1,17 +1,20 @@
 use helpers::*;
-use core::intrinsics;
+use core::cell::Cell;
 use core::cmp;
 
 use pm;
 use hil::spi_master;
-use hil::spi_master::Reader;
+use hil::spi_master::SpiCallback;
+use hil::spi_master::ClockPolarity;
+use hil::spi_master::ClockPhase;
+use hil::spi_master::DataOrder;
 
 // Driver for the SPI hardware (seperate from the USARTS, described in chapter 26 of the
 // datasheet)
 
 /// The registers used to interface with the hardware
 #[repr(C, packed)]
-struct SPIRegisters {
+struct SpiRegisters {
     cr: u32, // 0x0
     mr: u32, // 0x4
     rdr: u32, // 0x8
@@ -33,7 +36,7 @@ struct SPIRegisters {
     version: u32, // 0xFC
 }
 
-const BASE_ADDRESS: u32 = 0x40008000;
+const SPI_BASE: u32 = 0x40008000;
 
 /// Values for selected peripherals
 #[derive(Copy,Clone)]
@@ -56,21 +59,21 @@ pub enum Peripheral {
 ///     spi.set_active_peripheral(Peripheral::Peripheral1);
 ///     spi.init(/* Parameters for peripheral 1 */);
 ///
-pub struct SPI {
+pub struct Spi {
     /// Registers
-    regs: &'static mut SPIRegisters,
+    regs: *mut SpiRegisters,
     /// Client
-    client: Option<&'static mut Reader>,
+    callback: Cell<Option<&'static SpiCallback>>,
 }
 
-impl SPI {
+pub static mut SPI: Spi = Spi::new();
+
+impl Spi {
     /// Creates a new SPI object, with peripheral 0 selected
-    pub fn new() -> SPI {
-        // Enable clock
-        unsafe { pm::enable_clock(pm::Clock::PBA(pm::PBAClock::SPI)); }
-        SPI {
-            regs: unsafe{ intrinsics::transmute(BASE_ADDRESS) },
-            client: None,
+   const fn new() -> Spi {
+        Spi {
+            regs: SPI_BASE as *mut SpiRegisters,
+            callback: Cell::new(None)
         }
     }
 
@@ -82,19 +85,20 @@ impl SPI {
     ///
     /// The lowest available baud rate is 188235 baud. If the requested rate is lower,
     /// 188235 baud will be selected.
-    pub fn set_baud_rate(&mut self, mut rate: u32) {
+    pub fn set_baud_rate(&'static self, rate: u32) {
         // Main clock frequency
+        let mut real_rate = rate;
         let clock = 48000000;
 
-        if rate < 188235 {
-            rate = 188235;
+        if real_rate < 188235 {
+            real_rate = 188235;
         }
-        if rate > clock {
-            rate = clock;
+        if real_rate > clock {
+            real_rate = clock;
         }
 
         // Divide and truncate, resulting in a n value that might be too low
-        let mut scbr = clock / rate;
+        let mut scbr = clock / real_rate;
         // If the division was not exact, increase the n to get a slower baud rate
         if clock % rate != 0 {
             scbr += 1;
@@ -109,7 +113,7 @@ impl SPI {
 
     /// Returns the currently active peripheral
     pub fn get_active_peripheral(&self) -> Peripheral {
-        let mr = volatile_load(&self.regs.mr);
+        let mr = unsafe {volatile_load(&(*self.regs).mr)};
         let pcs = (mr >> 16) & 0xF;
         // Split into bits for matching
         let pcs_bits = ((pcs >> 3) & 1, (pcs >> 2) & 1, (pcs >> 1) & 1, pcs & 1);
@@ -125,160 +129,146 @@ impl SPI {
             }
         }
     }
-    /// Sets the active peripheral
-    pub fn set_active_peripheral(&mut self, peripheral: Peripheral) {
-        let peripheral_number: u32 = match peripheral {
-            Peripheral::Peripheral0 => 0b0000,
-            Peripheral::Peripheral1 => 0b0001,
-            Peripheral::Peripheral2 => 0b0011,
-            Peripheral::Peripheral3 => 0b0111,
-        };
-
-        let mut mr = volatile_load(&self.regs.mr);
-        // Clear and set MR.PCS
-        let pcs_mask: u32 = 0xFFF0FFFF;
-        mr &= pcs_mask;
-        mr |= peripheral_number << 16;
-        volatile_store(&mut self.regs.mr, mr);
-    }
 
     /// Returns the value of CSR0, CSR1, CSR2, or CSR3, whichever corresponds to the active
     /// peripheral
-    fn read_active_csr(&self) -> u32 {
+    fn read_active_csr(&'static self) -> u32 {
         match self.get_active_peripheral() {
-            Peripheral::Peripheral0 => volatile_load(&self.regs.csr0),
-            Peripheral::Peripheral1 => volatile_load(&self.regs.csr1),
-            Peripheral::Peripheral2 => volatile_load(&self.regs.csr2),
-            Peripheral::Peripheral3 => volatile_load(&self.regs.csr3),
+            Peripheral::Peripheral0 => unsafe {volatile_load(&(*self.regs).csr0)},
+            Peripheral::Peripheral1 => unsafe {volatile_load(&(*self.regs).csr1)},
+            Peripheral::Peripheral2 => unsafe {volatile_load(&(*self.regs).csr2)},
+            Peripheral::Peripheral3 => unsafe {volatile_load(&(*self.regs).csr3)},
         }
     }
     /// Sets the value of CSR0, CSR1, CSR2, or CSR3, whichever corresponds to the active
     /// peripheral
-    fn write_active_csr(&mut self, value: u32) {
+    fn write_active_csr(&'static self, value: u32) {
         match self.get_active_peripheral() {
-            Peripheral::Peripheral0 => volatile_store(&mut self.regs.csr0, value),
-            Peripheral::Peripheral1 => volatile_store(&mut self.regs.csr1, value),
-            Peripheral::Peripheral2 => volatile_store(&mut self.regs.csr2, value),
-            Peripheral::Peripheral3 => volatile_store(&mut self.regs.csr3, value),
+            Peripheral::Peripheral0 => unsafe {volatile_store(&mut (*self.regs).csr0, value)},
+            Peripheral::Peripheral1 => unsafe {volatile_store(&mut (*self.regs).csr1, value)},
+            Peripheral::Peripheral2 => unsafe {volatile_store(&mut (*self.regs).csr2, value)},
+            Peripheral::Peripheral3 => unsafe {volatile_store(&mut (*self.regs).csr3, value)},
         };
-    }
-
-
-    /// Ends the SPI transaction by setting the slave select high
-    fn end_transaction(&mut self) {
-        volatile_store(&mut self.regs.cr, 1 << 24);
     }
 }
 
-impl spi_master::SPI for SPI {
-    fn init(&mut self, params: spi_master::SPIParams) {
-        self.client = params.client;
-        self.set_baud_rate(params.baud_rate);
+impl spi_master::SpiMaster for Spi {
+    fn init(&self, callback: &'static SpiCallback) {
+        // Enable clock
+        unsafe { pm::enable_clock(pm::Clock::PBA(pm::PBAClock::SPI)); }
 
-        let mut csr = self.read_active_csr();
-        // Clock polarity
-        match params.clock_polarity {
-            spi_master::ClockPolarity::IdleHigh => csr |= 1, // Set bit 0
-            spi_master::ClockPolarity::IdleLow => csr &= 0xFFFFFFFE, // Clear bit 0
-        };
-        // Clock phase
-        match params.clock_phase {
-            spi_master::ClockPhase::SampleLeading => csr |= 1 << 1, // Set bit 1
-            spi_master::ClockPhase::SampleTrailing => csr &= 0xFFFFFFFD, // Clear bit 1
-        }
-        // Keep slave select active until a last transfer bit is set
-        csr |= 1 << 3;
-        self.write_active_csr(csr);
+        self.callback.set(Some(callback));
+        self.set_rate(8000000); // Set initial baud rate to 8MHz
+        self.set_clock(ClockPolarity::IdleLow);
+        self.set_phase(ClockPhase::SampleLeading);
+        self.set_order(DataOrder::MSBFirst);
 
-        // Indicate the last transfer, so that the slave select will be disabled
-        volatile_store(&mut self.regs.cr, 1 << 24);
-
-        let mut mode = volatile_load(&self.regs.mr);
-        // Enable master mode
-        mode |= 1;
-        // Disable mode fault detection (open drain outputs do not seem to be supported)
-        mode |= 1 << 4;
-        volatile_store(&mut self.regs.mr, mode);
+        // Indicate the last transfer to disable slave select 
+        unsafe {volatile_store(&mut (*self.regs).cr, 1 << 24)};
+        
+        let mut mode = unsafe {volatile_load(&(*self.regs).mr)};
+        mode |= 1; // Enable master mode
+        mode |= 1 << 4; // Disable mode fault detection (open drain outputs not supported)
+        unsafe {volatile_store(&mut (*self.regs).mr, mode)};
     }
 
-    fn write_byte(&mut self, out_byte: u8, last_transfer: bool) -> u8 {
-        let tdr = out_byte as u32;
-        volatile_store(&mut self.regs.tdr, tdr);
-        if last_transfer {
-            self.end_transaction();
-        }
+    fn read_write_byte(&'static self, val: u8) -> u8 {
+        let tdr = val as u32;
+        unsafe {volatile_store(&mut (*self.regs).tdr, tdr)};
         // Wait for receive data register full
-        while (volatile_load(&self.regs.sr) & 1) != 1 {}
+        while (unsafe {volatile_load(&(*self.regs).sr)} & 1) != 1 {}
         // Return read value
-        volatile_load(&self.regs.rdr) as u8
+        unsafe {volatile_load(&(*self.regs).rdr) as u8}
     }
 
-    fn read_byte(&mut self, last_transfer: bool) -> u8 {
-        self.write_byte(0, last_transfer)
+    fn write_byte(&'static self, out_byte: u8) {
+        let tdr = out_byte as u32;
+        unsafe {volatile_store(&mut (*self.regs).tdr, tdr)};
     }
 
-    fn read(&mut self, buffer: &mut [u8], last_transfer: bool) {
+    fn read_byte(&'static self) -> u8 {
+        self.read_write_byte(0)
+    }
+
+    /// The write buffer has to be mutable because it's passed back to
+    /// the caller, and the caller may want to be able write into it.
+    fn read_write_bytes(&'static self, 
+                        mut read_buffer:  Option<&'static mut [u8]>, 
+                        write_buffer: Option<&'static mut [u8]>) -> bool {
+        // If both are Some, read/write minimum of lengths
+        // If only read is Some, read length and write zeroes
+        // If only write is Some, write length and discard reads
+        // If both are None, return false
         // TODO: Asynchronous
-        for i in 0..buffer.len() {
-            // Write 0
-            let tdr: u32 = 0;
-            volatile_store(&mut self.regs.tdr, tdr);
-            if last_transfer && i == buffer.len() - 1 {
-                self.end_transaction();
-            }
-            // Wait for receive data register full
-            while (volatile_load(&self.regs.sr) & 1) != 1 {}
-
-            buffer[i] = volatile_load(&self.regs.rdr) as u8;
+        if read_buffer.is_none() && write_buffer.is_none() {
+            return false
         }
-        if let Some(ref mut client) = self.client {
-            client.read_done();
-        }
-    }
-
-    fn write(&mut self, buffer: &[u8], last_transfer: bool) {
-        // TODO: Asynchronous
-        for i in 0..buffer.len() {
-            let tdr: u32 = buffer[i] as u32;
-            // Write the value
-            volatile_store(&mut self.regs.tdr, tdr);
-            if last_transfer && i == buffer.len() - 1 {
-                self.end_transaction();
-            }
-            // Wait for transmit data register empty
-            while ((volatile_load(&self.regs.sr) >> 1) & 1) != 1 {}
-        }
-        if let Some(ref mut client) = self.client {
-            client.write_done();
-        }
-    }
-
-    fn read_and_write(&mut self, read_buffer: &mut [u8], write_buffer: &[u8], last_transfer: bool) {
-        // TODO: Asynchronous
-        let count = cmp::min(read_buffer.len(), write_buffer.len());
+        let reading = read_buffer.is_some();
+        let writing = write_buffer.is_some();
+        let read_len = match read_buffer {
+            Some(ref buf) => {buf.len()},
+            None          => 0
+        };
+        let write_len = match write_buffer {
+            Some(ref buf) => {buf.len()},
+            None          => 0
+        };
+        let count = if reading && writing {cmp::min(read_len, write_len)}
+                    else                  {cmp::max(read_len, write_len)};
         for i in 0..count {
-            let tdr: u32 = write_buffer[i] as u32;
-            // Write the value
-            volatile_store(&mut self.regs.tdr, tdr);
-            if last_transfer && i == count - 1 {
-                self.end_transaction();
+            let mut txbyte: u8 = 0;
+            match write_buffer {
+                Some(ref buf) => {txbyte = buf[i];}
+                None          => {}
             }
-            // Wait for receive data register full
-            while (volatile_load(&self.regs.sr) & 1) != 1 {}
-            // Read the received value
-            let read_byte = volatile_load(&self.regs.rdr) as u8;
-            read_buffer[i] = read_byte;
+            // Write the value
+            let rxbyte = self.read_write_byte(txbyte);
+            match read_buffer.take() {
+                Some(ref mut buf) => {buf[i] = rxbyte;}
+                None          => {}
+            }
         }
-        if let Some(ref mut client) = self.client {
-            client.read_write_done();
-        }
+        self.callback.get().map(|cb| {
+            cb.read_write_done(read_buffer, write_buffer);
+        });
+        true
     }
 
-    fn enable(&mut self) {
-        volatile_store(&mut self.regs.cr, 0b1);
+#[allow(unused_variables)]
+    fn set_rate(&self, rate: u32) -> u32 { 0 }
+    fn get_rate(&self) -> u32 { 0 }
+            
+#[allow(unused_variables)]
+    fn set_order(&self, order: DataOrder) { }
+    fn get_order(&self) -> DataOrder { DataOrder::LSBFirst }
+
+#[allow(unused_variables)]
+    fn set_clock(&self, polarity: ClockPolarity) { }
+    fn get_clock(&self) -> ClockPolarity { ClockPolarity::IdleLow }
+
+#[allow(unused_variables)]
+    fn set_phase(&self, phase: ClockPhase) { }
+    fn get_phase(&self) -> ClockPhase { ClockPhase::SampleTrailing }
+
+    /// Sets the active peripheral
+    fn set_chip_select(&self, cs: u8) {
+        let peripheral_number: u32 = match cs {
+            0 => 0b0000,
+            1 => 0b0001,
+            2 => 0b0011,
+            3 => 0b0111,
+            _ => 0b0000,
+        };
+
+        let mut mr = unsafe {volatile_load(&(*self.regs).mr)};
+        // Clear and set MR.PCS
+        let pcs_mask: u32 = 0xFFF0FFFF;
+        mr &= pcs_mask;
+        mr |= peripheral_number << 16;
+        unsafe {volatile_store(&mut (*self.regs).mr, mr);}
     }
 
-    fn disable(&mut self) {
-        volatile_store(&mut self.regs.cr, 0b10);
+    fn clear_chip_select(&self) {
+       unsafe {volatile_store(&mut (*self.regs).cr, 1 << 24)};
     }
 }
