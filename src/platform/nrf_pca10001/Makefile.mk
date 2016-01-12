1715c66384ec25b5e19b8861a9e2cb15ed117be2
Anderson Lizardo <anderson.lizardo@gmail.com>
2016-01-11 20:03:51 -0400

    nrf51822: Implement JLink flashing target

--- src/platform/nrf_pca10001/Makefile.mk
+++ src/platform/nrf_pca10001/Makefile.mk
@@ -1,3 +1,8 @@
+JLINK_OPTIONS = -device nrf51822 -if swd -speed 1000
+JLINK = JLinkExe
+
+all: $(BUILD_DIR)/main.bin
+
 $(BUILD_DIR)/libplatform.rlib: $(call rwildcard,$(SRC_DIR)platform/$(PLATFORM),*.rs) $(BUILD_DIR)/libcore.rlib $(BUILD_DIR)/libhil.rlib $(BUILD_DIR)/lib$(CHIP).rlib $(BUILD_DIR)/libdrivers.rlib
 	@echo "Building $@"
 	@$(RUSTC) $(RUSTC_FLAGS) --out-dir $(BUILD_DIR) $(SRC_DIR)platform/$(PLATFORM)/lib.rs
@@ -7,14 +12,42 @@ $(BUILD_DIR)/main.elf: $(BUILD_DIR)/crt1.o $(BUILD_DIR)/arch.o $(BUILD_DIR)/main
 	@$(CC) $(LDFLAGS) -T$(LOADER) $^ -o $@ -ffreestanding -nostdlib -lc -lgcc
 	@$(TOOLCHAIN)size $@
 
-all: $(BUILD_DIR)/main.elf
+$(BUILD_DIR)/main.bin: $(BUILD_DIR)/main.elf
+	@echo "Generating $@"
+	@$(OBJCOPY) -Obinary $< $@
 
 .PHONY: rebuild-apps
 rebuild-apps: $(BUILD_DIR)/crt1.o $(BUILD_DIR)/arch.o $(BUILD_DIR)/main.o $(APP_BINS)
 	@echo "Relinking with APPS=\"$(APPS)\""
 	@$(CC) $(LDFLAGS) -T$(LOADER) $^ -o $(BUILD_DIR)/main.elf -ffreestanding -nostdlib -lc -lgcc
 
-# FIXME: implement "program" target using J-Link
-#.PHONY: program
-#program: $(BUILD_DIR)/main.hex
-#	...
+# "Flash" process:
+# 1) set NVMC.CONFIG to 1 (Write enabled)
+# 2) write firmware at address 0
+# 3) set NVMC.CONFIG to 0 (Read only access)
+.PHONY: program
+program: $(BUILD_DIR)/main.bin
+	echo \
+	connect\\n\
+	w4 4001e504 1\\n\
+	loadbin $< 0\\n\
+	w4 4001e504 0\\n\
+	r\\n\
+	g\\n\
+	exit | $(JLINK) $(JLINK_OPTIONS)
+
+# "Erase all" process:
+# 1) set NVMC.CONFIG to 2 (Erase enabled)
+# 2) set NVMC.ERASEALL to 1 (Start chip erase)
+# 3) wait some time for erase to finish
+# 4) set NVMC.CONFIG to 0 (Read only access)
+.PHONY: erase-all
+erase-all:
+	echo \
+	connect\\n\
+	w4 4001e504 2\\n\
+	w4 4001e50c 1\\n\
+	sleep 100\\n\
+	w4 4001e504 0\\n\
+	r\\n\
+	exit | $(JLINK) $(JLINK_OPTIONS)
