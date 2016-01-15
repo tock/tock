852b757ba5e3617c39204bcca0db26ae617e9186
Anderson Lizardo <anderson.lizardo@indt.org.br>
2016-01-15 14:04:30 -0400

    nrf51822: Add initial GPIO support
    Minimum support for output pins.

--- src/chips/nrf51822/lib.rs
+++ src/chips/nrf51822/lib.rs
@@ -1,4 +1,7 @@
 #![crate_name = "nrf51822"]
 #![crate_type = "rlib"]
-#![feature(asm,concat_idents,const_fn)]
+#![feature(asm,core_intrinsics,concat_idents,const_fn)]
 #![no_std]
+
+extern crate hil;
+pub mod gpio;
