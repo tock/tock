efb4831d90805958fa760ec5bb41c1cc15b9a873
Anderson Lizardo <anderson.lizardo@indt.org.br>
2016-01-15 13:38:53 -0400

    Add sample "c_blinky" app
    This application alternately blinks LEDs #0 and #1 of the nRF51822
    PCA10001 board. The implementation is currently specific to this
    platform.

---
+++ src/apps/c_blinky/main.c
@@ -0,0 +1,64 @@
+#include <firestorm.h>
+
+#define LED_0 18
+#define LED_1 19
+
+/* FIXME: These delay functions are Cortex-M0 specific (and calibrated for a
+ * 16MHz CPU clock), therefore should be moved to platform specific location.
+ * */
+
+/* Delay for for the given microseconds (approximately).
+ *
+ * For a 16 MHz CPU, 1us == 16 instructions (assuming each instruction takes
+ * one cycle). */
+static void delay_us(int duration)
+{
+	// The inner loop instructions are: 14 NOPs + 1 SUBS/ADDS + 1 CMP
+	while (duration-- != 0) {
+		__asm volatile (
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+			"nop\n"
+		);
+	}
+}
+
+/* Delay for for the given milliseconds (approximately).
+ *
+ * Note that this is not precise as there are 2 extra instructions on the inner
+ * loop. Therefore, there is 1us added every 8 iterations. */
+static void delay_ms(int duration)
+{
+	while (duration-- != 0) {
+		delay_us(1000);
+	}
+}
+
+void main(void)
+{
+	gpio_enable(LED_0);
+	gpio_enable(LED_1);
+
+	while (1) {
+		gpio_clear(LED_0);
+		gpio_set(LED_1);
+
+		delay_ms(500);
+
+		gpio_clear(LED_1);
+		gpio_set(LED_0);
+
+		delay_ms(500);
+	}
+}
