2225989889b2fdd683cbb2061a7e55447a845bc6
Pat Pannuto <pat.pannuto@gmail.com>
2016-01-25 20:52:31 -0500

    Only issue `--force-local` for tar on Windows
    The default version of tar on mac doesn't support `--force-local`.
    
    Resolves the issue introduced by #19.

--- Common.mk
+++ Common.mk
@@ -8,13 +8,6 @@ CC = $(TOOLCHAIN)gcc
 CPP = $(TOOLCHAIN)g++
 LD = $(TOOLCHAIN)ld
 
-UNAME = $(shell uname)
-ifeq ($(UNAME),Linux)
-DYLIB=so
-else
-DYLIB=dylib
-endif
-
 # Recursive wildcard function
 # http://blog.jgc.org/2011/07/gnu-make-recursive-wildcard-function.html
 rwildcard=$(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2) \
@@ -26,4 +19,24 @@ $(BUILD_DIR)/lib%.rlib: $$(call rwildcard,$(SRC_DIR)$$**/,*.rs) $(BUILD_DIR)/lib
 	@echo "Building $@"
 	@$(RUSTC) $(RUSTC_FLAGS) --out-dir $(BUILD_DIR) $(SRC_DIR)$*/lib.rs
 
+
+# Detect currently running OS
+# http://stackoverflow.com/questions/714100/os-detecting-makefile
+ifeq ($(OS),Windows_NT)
+HOST_OS := Windows
+else
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+HOST_OS := Linux
+endif
+ifeq ($(UNAME_S),Darwin)
+HOST_OS := Darwin
+endif
+endif
+
+ifeq ($(HOST_OS),Windows)
+TAR ?= tar --force-local
+else
+TAR ?= tar
+endif
 
