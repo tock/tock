637381cd05d2d04a641b91aa7996069969bd63cb
Anderson Lizardo <anderson.lizardo@gmail.com>
2016-02-08 20:20:21 -0400

    Move VolatileCell to libcommon
    Fixes Issue #33.

--- src/chips/nrf51822/gpio.rs
+++ src/chips/nrf51822/gpio.rs
@@ -1,30 +1,7 @@
 use core::mem;
 use core::ops::{Index, IndexMut};
+use common::VolatileCell;
 use hil;
-
-// Source: https://github.com/hackndev/zinc/tree/master/volatile_cell
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct VolatileCell<T> {
-    value: T,
-}
-
-#[allow(dead_code)]
-impl<T> VolatileCell<T> {
-    #[inline]
-    pub fn get(&self) -> T {
-        unsafe {
-            ::core::intrinsics::volatile_load(&self.value)
-        }
-    }
-
-    #[inline]
-    pub fn set(&self, value: T) {
-        unsafe {
-            ::core::intrinsics::volatile_store(&self.value as *const T as *mut T, value)
-        }
-    }
-}
 
 #[allow(non_snake_case)]
 struct GPIO {
--- src/chips/nrf51822/lib.rs
+++ src/chips/nrf51822/lib.rs
@@ -1,7 +1,9 @@
 #![crate_name = "nrf51822"]
 #![crate_type = "rlib"]
-#![feature(asm,core_intrinsics,concat_idents,const_fn)]
+#![feature(asm,concat_idents,const_fn)]
 #![no_std]
 
+extern crate common;
 extern crate hil;
+
 pub mod gpio;
