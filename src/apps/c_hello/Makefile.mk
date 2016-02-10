9b81aa8c664cfc4f04392d66e0320a79dbdaaa5f
Amit Aryeh Levy <amit@amitlevy.com>
2016-02-10 11:06:47 -0500

    Fix compile app errors after merge

--- src/apps/c_hello/Makefile.mk
+++ src/apps/c_hello/Makefile.mk
@@ -1,4 +1,4 @@
-$(BUILD_APP_DIR)/c_hello.elf: $(call rwildcard,$(SRC_DIR)apps/c_hello/,*.c) $(BUILD_DIR)/arch.o $(BUILD_APP_DIR)/firestorm.o $(BUILD_APP_DIR)/tock.o $(BUILD_APP_DIR)/crt1.o $(BUILD_APP_DIR)/sys.o $(BUILD_APP_DIR)/arch.o $(APP_LIBC)
+$(BUILD_APP_DIR)/c_hello.elf: $(call rwildcard,$(SRC_DIR)apps/c_hello/,*.c) $(BUILD_APP_DIR)/firestorm.o $(BUILD_APP_DIR)/tock.o $(BUILD_APP_DIR)/crt1.o $(BUILD_APP_DIR)/sys.o $(BUILD_APP_DIR)/arch.o $(APP_LIBC)
 	@echo "Building $@"
 	@$(CC) $(LDFLAGS) $(CFLAGS_APPS) -g -Os -T $(SRC_DIR)apps/c_hello/loader.ld -o $@ -ffreestanding -nostdlib $^
 
--- src/apps/tmp006/Makefile.mk
+++ src/apps/tmp006/Makefile.mk
@@ -1,4 +1,4 @@
-$(BUILD_APP_DIR)/tmp006.elf: $(call rwildcard,$(SRC_DIR)apps/tmp006/,*.c) $(BUILD_DIR)/arch.o $(BUILD_APP_DIR)/firestorm.o $(BUILD_APP_DIR)/tock.o $(BUILD_APP_DIR)/crt1.o $(BUILD_APP_DIR)/sys.o $(BUILD_APP_DIR)/tmp006.o $(APP_LIBC)
+$(BUILD_APP_DIR)/tmp006.elf: $(call rwildcard,$(SRC_DIR)apps/tmp006/,*.c) $(BUILD_APP_DIR)/firestorm.o $(BUILD_APP_DIR)/tock.o $(BUILD_APP_DIR)/crt1.o $(BUILD_APP_DIR)/sys.o $(BUILD_APP_DIR)/arch.o $(BUILD_APP_DIR)/tmp006.o $(APP_LIBC)
 	@echo "Building $@"
 	@$(CC) $(LDFLAGS) $(CFLAGS_APPS) -g -Os -T $(SRC_DIR)apps/tmp006/loader.ld -o $@ -ffreestanding -nostdlib -Wl,-Map=$(BUILD_APP_DIR)/app.Map $^
 	@$(OBJDUMP) $(OBJDUMP_FLAGS) $@ > $(BUILD_APP_DIR)/app.lst
