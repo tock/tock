a9cc4427ab6e51dd56fec512833bbd74b8a517b8
Branden Ghena <brghena@umich.edu>
2016-02-27 22:11:52 -0500

    GPIO test application

--- apps/libs/gpio.h
+++ apps/libs/gpio.h
@@ -48,9 +48,9 @@ int gpio_clear(GPIO_Pin_t pin);
 int gpio_toggle(GPIO_Pin_t pin);
 int gpio_enable_input(GPIO_Pin_t pin, GPIO_InputMode_t pin_config);
 int gpio_read(GPIO_Pin_t pin);
-int gpio_enable_interrupts(GPIO_Pin_t pin, GPIO_InputMode_t pin_config,
+int gpio_enable_interrupt(GPIO_Pin_t pin, GPIO_InputMode_t pin_config,
     GPIO_InterruptMode_t irq_config);
-int gpio_disable_interrupts(GPIO_Pin_t pin);
+int gpio_disable_interrupt(GPIO_Pin_t pin);
 int gpio_disable(GPIO_Pin_t pin);
 int gpio_interrupt_callback(subscribe_cb callback, void* callback_args);
 
