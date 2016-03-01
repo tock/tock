c5aeeda3104a22f6ca6f8ca3014106280034c25c
Brad Campbell <bradjc5@gmail.com>
2016-02-29 19:47:20 -0500

    refactoring ble app

---
+++ apps/ble-serialization/delay.c
@@ -0,0 +1,51 @@
+
+#include "delay.h"
+
+/******************************************************************************
+ * Helper functions that should be somewhere more generic
+ ******************************************************************************/
+
+
+// copied from spi_byte app
+
+/* FIXME: These delay functions are Cortex-M0 specific (and calibrated for a
+ * 16MHz CPU clock), therefore should be moved to platform specific location.
+ * */
+
+/* Delay for for the given microseconds (approximately).
+ *
+ * For a 16 MHz CPU, 1us == 16 instructions (assuming each instruction takes
+ * one cycle). */
+void delay_us(int duration) {
+    // The inner loop instructions are: 14 NOPs + 1 SUBS/ADDS + 1 CMP
+    while (duration-- != 0) {
+        __asm volatile (
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+        );
+    }
+}
+
+/* Delay for for the given milliseconds (approximately).
+ *
+ * Note that this is not precise as there are 2 extra instructions on the inner
+ * loop. Therefore, there is 1us added every 8 iterations. */
+void delay_ms(int duration) {
+    while (duration-- != 0) {
+        delay_us(1000);
+    }
+}
+
+
---
+++ apps/ble-serialization/delay.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void delay_us(int duration);
+void delay_ms(int duration);
