e1517897e2bd202f48f44aa857cc3282b57ad94a
Branden Ghena <brghena@umich.edu>
2016-03-02 20:26:39 -0500

    Indirection to .data section works just fine too

--- apps/app_load_error/main.c
+++ apps/app_load_error/main.c
@@ -2,18 +2,34 @@
 
 #include <firestorm.h>
 
+// pointer to read-only data
 char* dev_name = "Read-only data works\n";
 
+// function pointers
 void testFn () {
   putstr("Function pointers work\n");
 }
-
 void (*testFnPtr)() = testFn;
 
-void main() {
+// indirection to global variable in .data
+char global_string[] = "I should print three times\n";
+char* global_string_ptr = global_string;
+char** global_string_ptr_ptr = &global_string_ptr;
+
+int main() {
   putstr("Testing for app loading errors\n");
 
+  // read-only data
   putstr(dev_name);
+
+  // function pointers
   (*testFnPtr)();
+
+  // indirection to data section
+  putstr(global_string);
+  putstr(global_string_ptr);
+  putstr(*global_string_ptr_ptr);
+
+  return 0;
 }
 
