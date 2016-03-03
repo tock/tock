b1bf8d3983e0618e1b8b83f27349f4eb44a4a59e
Branden Ghena <brghena@umich.edu>
2016-03-03 16:48:04 -0500

    App loading failure. Structs in reldata are bad...

--- apps/app_load_error/main.c
+++ apps/app_load_error/main.c
@@ -16,6 +16,21 @@ char global_string[] = "I should print three times\n";
 char* global_string_ptr = global_string;
 char** global_string_ptr_ptr = &global_string_ptr;
 
+// what about directly setting a pointer?
+int* my_ptr = 0xFEEDBEEF;
+
+// what about a pointer in a struct
+struct test_struct_t {
+  uint32_t  data_1;
+  int*      ptr_1;
+  uint32_t  data_2;
+};
+struct test_struct_t my_struct = {
+  .data_1 = 0x00000001,
+  .ptr_1  = "String in a struct worked\n",
+  .data_1 = 0x80000001,
+};
+
 int main() {
   putstr("Testing for app loading errors\n");
 
@@ -29,6 +44,27 @@ int main() {
   putstr(global_string);
   putstr(global_string_ptr);
   putstr(*global_string_ptr_ptr);
+
+  // directly setting a pointer
+  {
+    char buf[64];
+    snprintf(buf, 64, "Directly set pointer (should be 0xFEEDBEEF) = 0x%X\n", my_ptr);
+    putstr(buf);
+  }
+
+  // structures
+  {
+    char buf[64];
+    snprintf(buf, 64, "Structure data1 (should be 0x00000001) = 0x%X\n", my_struct.data_1);
+    putstr(buf);
+  }
+  putstr(my_struct.ptr_1);
+  {
+    char buf[64];
+    snprintf(buf, 64, "Structure data2 (should be 0x80000001) = 0x%X\n", my_struct.data_2);
+    putstr(buf);
+  }
+
 
   return 0;
 }
