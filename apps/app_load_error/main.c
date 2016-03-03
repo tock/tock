29b5876bc401810233982a89f09ce0741c717b48
Branden Ghena <brghena@umich.edu>
2016-03-02 20:13:18 -0500

    The fix works on function pointers

--- apps/app_load_error/main.c
+++ apps/app_load_error/main.c
@@ -2,10 +2,18 @@
 
 #include <firestorm.h>
 
-char* dev_name = "TEST\n";
+char* dev_name = "Read-only data works\n";
+
+void testFn () {
+  putstr("Function pointers work\n");
+}
+
+void (*testFnPtr)() = testFn;
 
 void main() {
-  putstr("hello\n");
+  putstr("Testing for app loading errors\n");
+
   putstr(dev_name);
+  (*testFnPtr)();
 }
 
