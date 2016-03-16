26f5297cd1fad3ab5c460a8a638c0c404d34850a
Amit Aryeh Levy <amit@amitlevy.com>
2016-03-16 15:44:14 -0400

    Add system calls for controlling app memory break
    The new memory layout of an app is:
    
    0xFF--> /--------\
            | KHEAP  |
            |   ||   |
            |   \/   |
            +--------+
             ////////
             ////////
            +--------+
            |   /\   |
            |   ||   |
            |  HEAP  |
            |________|
            | STACK  |
            |  BSS   |
            |  DATA  |
            |  GOT   |
    0x0 --> \--------/
    
    When the kernel allocates in the applications memory space, it moves the
    kernel break down. The app needs to be able to move the memory break up
    and down (e.g. in order to implement _sbrk).
    
    We add the MEMOP system call with, currently, two operations:
    
    0 is brk, which changes the application break to a given address
    1 is sbrk, which moves the application break by a given diff
    
    We will probably want to add operations that provide the app information
    about current memory stats (how much heap the kernel is using, for
    example)

--- apps/libs/tock.h
+++ apps/libs/tock.h
@@ -19,6 +19,11 @@ int subscribe(uint32_t driver, uint32_t subscribe,
               subscribe_cb cb, void* userdata);
 int allow(uint32_t driver, uint32_t allow, void* ptr, size_t size);
 
+// op_type can be:
+// 0: brk, arg1 is pointer to new memory break
+// 1: sbrk, arg1 is increment to increase/decrease memory break
+int memop(uint32_t op_type, int arg1);
+
 #ifdef __cplusplus
 }
 #endif
