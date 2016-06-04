1737a33c37dd7b088ea7d403367a3a2c3faa7caa
Anderson Lizardo <anderson.lizardo@gmail.com>
2016-06-04 10:42:42 -0400

    DO NOT MERGE: nRF51822: blink LEDs 1 and 2 alternately

--- apps/c_blinky/main.c
+++ apps/c_blinky/main.c
@@ -1,11 +1,18 @@
 #include <firestorm.h>
 #include <gpio.h>
 
+#define LED_1 1
+
 void main(void) {
     gpio_enable_output(LED_0);
+    gpio_enable_output(LED_1);
 
     while(1) {
-      gpio_toggle(LED_0);
+      gpio_set(LED_0);
+      gpio_clear(LED_1);
+      delay_ms(500);
+      gpio_set(LED_1);
+      gpio_clear(LED_0);
       delay_ms(500);
     }
 }
