d7a9f483e1b6aba0c5004fa98dfe178117bfa701
Utkarsha <utkarshapets@yahoo.co.in>
2016-07-29 17:13:21 -0700

    uart.rs file added, hex file causes fail.txt

--- src/chips/nrf51822/uart.rs
+++ src/chips/nrf51822/uart.rs
@@ -1,173 +1,95 @@
-use helpers::*;
 use core::mem;
-use hil::{uart, Controller};
-use hil::uart::{Parity, Mode};
+use helpers::*;
+//use hil::uart;
 use nvic;
 use chip;
 
-#[repr(C, packed)]
 struct Registers {
     startrx: u32,
     stoprx: u32,
     starttx: u32,
     stoptx: u32,
-    rxdrdy: u32,
+    reserved0: [u32; 62],
+    rxdrdy: u32,    
+    reserved1: [u32; 4],
     txdrdy: u32,
     error: u32,
+    reserved2: [u32; 119],
     inten: u32,
     intenset: u32,
     intenclr: u32,
+    reserved3: [u32; 93],
     errorsrc: u32,
+    reserved4: [u32; 31],
     enable: u32,
+    reserved5: u32,
     pselrts: u32,
     pseltxd: u32,
     pselcts: u32,
     pselrxd: u32,
     rxd: u32,
     txd: u32,
+    reserved6: u32,
     baudrate: u32,
+    reserved7: [u32; 71],
     config: u32
 }
 
-const SIZE: usize = 0x4000;
-const BASE_ADDRESS: usize = 0x40002000;
-
-#[derive(Copy,Clone)]
-pub enum Location {
-    UART0
-}
+const UART_BASE: u32 = 0x40002000;
+const UART_SIZE: u32 = 0x4;
 
 pub struct UART {
-    regs: *mut Registers,
-    client: Option<&'static uart::Client>,
-    //clock: Clock,
-    nvic: nvic::NvicIdx,
+    regs: *mut Registers
 }
 
+
+
+
+
+#[derive(Copy, Clone)]
 pub struct UARTParams {
-    //pub client: &'static Shared<uart::Client>,
     pub baud_rate: u32,
-    pub data_bits: u8,
-    pub parity: Parity,
 }
-
-impl Controller for UART {
-    type Config = UARTParams;
-
-    fn configure(&self, params: UARTParams) {
-        self.set_baud_rate(params.baud_rate);         
-        let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        
-        self.enable_rx_interrupts();
-    }
-}
-
-pub static mut UART0 : UART =
-    UART::new(Location::UART0, nvic::NvicIdx::UART0); //clock??
-
 
 impl UART {
-    const fn new(location: Location, nvic: nvic::NvicIdx) //clock??
-            -> UART {
+
+    pub const fn new() -> UART {
         UART {
-            regs: (BASE_ADDRESS + (location as usize) * SIZE)
-                as *mut Registers,
-            //clock: Clock::PBA(clock),
-            nvic: nvic,
-            client: None,
+            regs: UART_BASE as *mut Registers
         }
     }
-
-    pub fn set_client<C: uart::Client>(&mut self, client: &'static C) {
-        self.client = Some(client);
-    }
-
-    //removed set_dma
-
 
     fn set_baud_rate(&self, baud_rate: u32) {
-        //let cd = 48000000 / (8 * baud_rate);
         let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.baudrate, 0x00275000);
-    }
-
-    //removed set_mode
-
-    /*fn enable_clock(&self) {
-        unsafe {
-            pm::enable_clock(self.clock);  //what clock to use??
-        }
-    }*/
-
-    fn enable_nvic(&self) {
-        unsafe {
-            nvic::enable(self.nvic);
-        }
-    }
-
-    fn disable_nvic(&self) {
-        unsafe {
-            nvic::disable(self.nvic);
+        match baud_rate {
+            1200 => { volatile_store(&mut regs.baudrate, 0x0004F000);}
+            2400 => { volatile_store(&mut regs.baudrate, 0x0009D000);}
+            4800 => { volatile_store(&mut regs.baudrate, 0x0013B000);}
+            9600 => { volatile_store(&mut regs.baudrate, 0x00275000);}
+            14400 =>{ volatile_store(&mut regs.baudrate, 0x003B0000);}
+            19200 =>{ volatile_store(&mut regs.baudrate, 0x004EA000);}
+            28800 =>{ volatile_store(&mut regs.baudrate, 0x0075F000);}
+            38400 =>{ volatile_store(&mut regs.baudrate, 0x009D5000);}
+            57600 =>{ volatile_store(&mut regs.baudrate, 0x00EBF000);}
+            76800 =>{ volatile_store(&mut regs.baudrate, 0x013A9000);}
+            115200 =>{ volatile_store(&mut regs.baudrate, 0x01D7E000);}
+            230400 =>{ volatile_store(&mut regs.baudrate, 0x03AFB000);}
+            250000 =>{ volatile_store(&mut regs.baudrate, 0x04000000);}
+            460800 =>{ volatile_store(&mut regs.baudrate, 0x075F7000);}
+            1000000 =>{ volatile_store(&mut regs.baudrate, 0x10000000);}
+            _ => { volatile_store(&mut regs.baudrate, 0x01D7E000);} //setting default to 115200
         }
     }
-
-    pub fn enable_rx_interrupts(&self) {
-        self.enable_nvic();
-        let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.inten, 1 as u32);
-    }
-
-    pub fn enable_tx_interrupts(&mut self) {
-        self.enable_nvic();
-        let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.inten, 1 as u32); 
-    }
-
-    pub fn disable_rx_interrupts(&mut self) {
-        self.disable_nvic();
-        let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.inten, 0 as u32);
-    }
-
-    pub fn handle_interrupt(&mut self) {
-        use hil::uart::UART;
-        if self.rx_ready() {
-            let regs : &Registers = unsafe { mem::transmute(self.regs) };
-            let c = volatile_load(&regs.rxd) as u8;
-            match self.client {
-                Some(ref client) => {client.read_done(c)},
-                None => {}
-            }
-        }
-    }
-
-  /*  pub fn reset_rx(&mut self) {
-        let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.cr, 1 << 2); //to reset receiver, no analogue in nrf??
-    }*/
-}
-
-//removed DMAclient
-
-impl uart::UART for UART {
-    fn init(&mut self, params: uart::UARTParams) {
-        self.set_baud_rate(params.baud_rate);
-        let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
+   
+    pub fn init(&mut self, baud_rate: u32) {
+		let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
+		volatile_store(&mut regs.enable, 100);
+        self.set_baud_rate(baud_rate);
         volatile_store(&mut regs.pselrts, 8);
         volatile_store(&mut regs.pseltxd, 9);
         volatile_store(&mut regs.pselcts, 10);
         volatile_store(&mut regs.pselrxd, 11);
-    }
-
-    fn send_byte(&self, byte: u8) {
-        while !self.tx_ready() {}
-        let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.txd, byte as u32);
-    }
-
-    fn send_bytes(&self, bytes: &'static mut [u8], len: usize) {
-        unimplemented!();
-    }
+	}
 
     fn rx_ready(&self) -> bool {
         let regs : &Registers = unsafe { mem::transmute(self.regs) };
@@ -179,10 +101,21 @@ impl uart::UART for UART {
         volatile_load(&regs.txdrdy) & 0b1 != 0
     }
 
+    pub fn send_byte(&self, byte: u8) {
+        while !self.tx_ready() {}
+        let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
+        volatile_store(&mut regs.starttx, 1 as u32);
+        volatile_store(&mut regs.txd, byte as u32);
+    }
+
+    fn send_bytes(&self, bytes: &'static mut [u8], len: usize) {
+        unimplemented!();
+    }
 
     fn read_byte(&self) -> u8 {
         while !self.rx_ready() {}
         let regs : &Registers = unsafe { mem::transmute(self.regs) };
+        //volatile_store(&mut regs.startrx, 1 as u32);
         volatile_load(&regs.rxd) as u8
     }
 
@@ -206,13 +139,4 @@ impl uart::UART for UART {
         volatile_store(&mut regs.stoptx, 1);
     }
 
-}
-
-#[no_mangle]
-#[allow(non_snake_case)]
-pub unsafe extern fn UART0_Handler() {
-    use common::Queue;
-
-    nvic::disable(nvic::NvicIdx::UART0);
-    chip::INTERRUPT_QUEUE.as_mut().unwrap().enqueue(nvic::NvicIdx::UART0);
 }
--- src/platform/nrf_pca10001/lib.rs
+++ src/platform/nrf_pca10001/lib.rs
@@ -2,6 +2,7 @@
 #![crate_type = "rlib"]
 #![no_std]
 #![feature(lang_items)]
+#![feature(core_intrinsics)]
 
 extern crate drivers;
 extern crate hil;
@@ -10,10 +11,13 @@ extern crate support;
 extern crate process;
 extern crate common;
 
+use core::intrinsics::{volatile_load, volatile_store};
+
 use hil::Controller;
 use drivers::virtual_alarm::{MuxAlarm, VirtualMuxAlarm};
 use hil::gpio::GPIOPin;
-
+use nrf51822::gpio::PORT;
+use nrf51822::uart;
 pub mod systick;
 
 pub struct Firestorm {
@@ -58,24 +62,40 @@ macro_rules! static_init {
    }
 }
 
-let nrf_uart = nrf51822::uart::UART::new()
-nrf_uart.init();
+//let nrf_uart = nrf51822::uart::UART::new()
+//nrf_uart.init();
 
-nrf_uart.enable_tx();
-nrf_uart.set_baud_rate(9600); //doesnt matter. I have hard coded the reg value in init
-while(nrf_uart.tx_ready()) {
+//nrf_uart.enable_tx();
+//nrf_uart.set_baud_rate(9600); //doesnt matter. I have hard coded the reg value in init
+//while(nrf_uart.tx_ready()) {
 
-    loop{
-    nrf_uart.send_byte('+' as u8);
-    }
-}
+//    loop{
+//    nrf_uart.send_byte('+' as u8);
+//    }
+//}
 
-
+#[inline(never)]
+	
 pub unsafe fn init<'a>() -> &'a mut Firestorm {
+
     use core::mem;
 
-    use nrf51822::gpio::PORT;
+//	let mut nrf_uart = uart::UART::new();
 
+    //let mut uart_params = hiluart::UARTParams {baud_rate : 115200, data_bits:1, parity: Odd, mode: Normal};
+//	nrf_uart.init(9600);
+//    nrf_uart.send_byte('h' as u8);
+    //let reg : uart::Registers = uart::Registers {starttx : 0x40002008};
+	//volatile_store(reg.starttx as *mut usize, 1); //starttx
+	//volatile_store(0x40002500 as *mut usize, 100); //enable
+	//volatile_store(0x40002524 as *mut usize, 0x00275000); //baudrate
+	//volatile_store(0x40002508 as *mut usize, 8); //pselrts
+	//volatile_store(0x4000250C as *mut usize, 9); //pseltxd
+	//volatile_store(0x40002510 as *mut usize, 10); //pselcts
+	//volatile_store(0x40002514 as *mut usize, 11); //pselrxd
+	//volatile_store(0x4000251C as *mut usize, 0111); //txd
+
+	
     static mut FIRESTORM_BUF : [u8; 1024] = [0; 1024];
 
     static_init!(gpio_pins : [&'static nrf51822::gpio::GPIOPin; 10] = [
