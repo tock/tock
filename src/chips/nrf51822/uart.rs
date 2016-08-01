78006c8b6fecd372ce93aff3dba6b07eb3bd9299
Utkarsha Agwan <utkarshapets@gmail.com>
2016-08-01 16:05:27 -0700

    Add files via upload

--- src/chips/nrf51822/uart.rs
+++ src/chips/nrf51822/uart.rs
@@ -1,5 +1,7 @@
+#![feature(core_intrinsics)]
+
 use core::mem;
-use helpers::*;
+
 //use hil::uart;
 //use nvic;
 //use chip;
@@ -63,50 +65,50 @@ impl UART {
     fn set_baud_rate(&self, baud_rate: u32) {
         let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
         match baud_rate {
-            1200 => { volatile_store(&mut regs.baudrate, 0x0004F000);}
-            2400 => { volatile_store(&mut regs.baudrate, 0x0009D000);}
-            4800 => { volatile_store(&mut regs.baudrate, 0x0013B000);}
-            9600 => { volatile_store(&mut regs.baudrate, 0x00275000);}
-            14400 =>{ volatile_store(&mut regs.baudrate, 0x003B0000);}
-            19200 =>{ volatile_store(&mut regs.baudrate, 0x004EA000);}
-            28800 =>{ volatile_store(&mut regs.baudrate, 0x0075F000);}
-            38400 =>{ volatile_store(&mut regs.baudrate, 0x009D5000);}
-            57600 =>{ volatile_store(&mut regs.baudrate, 0x00EBF000);}
-            76800 =>{ volatile_store(&mut regs.baudrate, 0x013A9000);}
-            115200 =>{ volatile_store(&mut regs.baudrate, 0x01D7E000);}
-            230400 =>{ volatile_store(&mut regs.baudrate, 0x03AFB000);}
-            250000 =>{ volatile_store(&mut regs.baudrate, 0x04000000);}
-            460800 =>{ volatile_store(&mut regs.baudrate, 0x075F7000);}
-            1000000 =>{ volatile_store(&mut regs.baudrate, 0x10000000);}
-            _ => { volatile_store(&mut regs.baudrate, 0x01D7E000);} //setting default to 115200
+            1200 => { unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x0004F000);}}
+            2400 => { unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x0009D000);}}
+            4800 => { unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x0013B000);}}
+            9600 => { unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x00275000);}}
+            14400 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x003B0000);}}
+            19200 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x004EA000);}}
+            28800 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x0075F000);}}
+            38400 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x009D5000);}}
+            57600 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x00EBF000);}}
+            76800 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x013A9000);}}
+            115200 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x01D7E000);}}
+            230400 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x03AFB000);}}
+            250000 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x04000000);}}
+            460800 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x075F7000);}}
+            1000000 =>{ unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x10000000);}}
+            _ => { unsafe{intrinsics::volatile_store(&mut regs.baudrate, 0x01D7E000);}} //setting default to 115200
         }
     }
    
     pub fn init(&mut self, baud_rate: u32) {
 		let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-		volatile_store(&mut regs.enable, 100);
+		unsafe{intrinsics::volatile_store(&mut regs.enable, 100);}
         self.set_baud_rate(baud_rate);
-        volatile_store(&mut regs.pselrts, 8);
-        volatile_store(&mut regs.pseltxd, 9);
-        volatile_store(&mut regs.pselcts, 10);
-        volatile_store(&mut regs.pselrxd, 11);
+        unsafe{intrinsics::volatile_store(&mut regs.pselrts, 8);}
+        unsafe{intrinsics::volatile_store(&mut regs.pseltxd, 9);}
+        unsafe{intrinsics::volatile_store(&mut regs.pselcts, 10);}
+        unsafe{intrinsics::volatile_store(&mut regs.pselrxd, 11);}
 	}
 
     fn rx_ready(&self) -> bool {
         let regs : &Registers = unsafe { mem::transmute(self.regs) };
-        volatile_load(&regs.rxdrdy) & 0b1 != 0
+        unsafe{intrinsics::volatile_load(&regs.rxdrdy) & 0b1 != 0 }
     }
 
     fn tx_ready(&self) -> bool {
         let regs : &Registers = unsafe { mem::transmute(self.regs) };
-        volatile_load(&regs.txdrdy) & 0b1 != 0
+        unsafe{intrinsics::volatile_load(&regs.txdrdy) & 0b1 != 0}
     }
 
     pub fn send_byte(&self, byte: u8) {
         while !self.tx_ready() {}
         let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.starttx, 1 as u32);
-        volatile_store(&mut regs.txd, byte as u32);
+        unsafe{intrinsics::volatile_store(&mut regs.starttx, 1 as u32);}
+        unsafe{intrinsics::volatile_store(&mut regs.txd, byte as u32);}
     }
 
     fn send_bytes(&self, bytes: &'static mut [u8], len: usize) {
@@ -116,28 +118,28 @@ impl UART {
     fn read_byte(&self) -> u8 {
         while !self.rx_ready() {}
         let regs : &Registers = unsafe { mem::transmute(self.regs) };
-        //volatile_store(&mut regs.startrx, 1 as u32);
-        volatile_load(&regs.rxd) as u8
+        //intrinsics::volatile_store(&mut regs.startrx, 1 as u32);
+        unsafe{intrinsics::volatile_load(&regs.rxd) as u8}
     }
 
     fn enable_rx(&self) {
         let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.startrx, 1);
+        unsafe{intrinsics::volatile_store(&mut regs.startrx, 1);}
     }
 
     fn disable_rx(&mut self) {
         let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.stoprx, 1);
+        unsafe{intrinsics::volatile_store(&mut regs.stoprx, 1);}
     }
 
     fn enable_tx(&self) {
         let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.starttx, 1);
+        unsafe{intrinsics::volatile_store(&mut regs.starttx, 1);}
     }
 
     fn disable_tx(&mut self) {
         let regs : &mut Registers = unsafe { mem::transmute(self.regs) };
-        volatile_store(&mut regs.stoptx, 1);
+        unsafe{intrinsics::volatile_store(&mut regs.stoptx, 1);}
     }
 
 }
