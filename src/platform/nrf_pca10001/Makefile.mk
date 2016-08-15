de1414bc41c06392557e8e6aa2a6b615bae89766
Amit Aryeh Levy <amit@amitlevy.com>
2016-08-15 18:47:21 -0400

    Funge NRF51 port for Cargo to work with Makefile
    Mostly little changes. The major difference is we have to compile
    kernel.elf directly with Rust otherwise the symbols that used to be in
    ctx_switch.S are not visible. This, in turn, affects how we incorporate
    apps into the a final product -- specifically by using objcopy instead
    of compiling.

--- src/platform/nrf_pca10001/Makefile.mk
+++ src/platform/nrf_pca10001/Makefile.mk
@@ -2,18 +2,14 @@ CHIP=nrf51822
 
 PLATFORM_DEPS=$(BUILD_PLATFORM_DIR)/libcore.rlib $(BUILD_PLATFORM_DIR)/libsupport.rlib
 PLATFORM_DEPS+=$(BUILD_PLATFORM_DIR)/libhil.rlib $(BUILD_PLATFORM_DIR)/libdrivers.rlib
-PLATFORM_DEPS+=$(BUILD_PLATFORM_DIR)/libmain.rlib
+PLATFORM_DEPS+=$(BUILD_PLATFORM_DIR)/libmain.rlib $(BUILD_PLATFORM_DIR)/libnrf51822.rlib
+PLATFORM_DEPS+=$(BUILD_PLATFORM_DIR)/libcortexm0.rlib
 
 all: $(BUILD_PLATFORM_DIR)/kernel.elf $(BUILD_PLATFORM_DIR)/crt1.o
 
-$(BUILD_PLATFORM_DIR)/kernel.o: $(call rwildcard,$(SRC_DIR)platform/nrf_pca10001,*.rs) $(BUILD_PLATFORM_DIR)/libnrf51822.rlib $(PLATFORM_DEPS) | $(BUILD_PLATFORM_DIR)
+$(BUILD_PLATFORM_DIR)/kernel.elf: $(call rwildcard,$(SRC_DIR)platform/nrf_pca10001/src,*.rs) $(PLATFORM_DEPS) | $(BUILD_PLATFORM_DIR)
 	@echo "Building $@"
-	@$(RUSTC) $(RUSTC_FLAGS) -C lto --emit obj -o $@ $(SRC_DIR)platform/nrf_pca10001/main.rs
+	@$(RUSTC) $(RUSTC_FLAGS) -C lto -o $@ $(SRC_DIR)platform/nrf_pca10001/src/main.rs -L native=$(BUILD_PLATFORM_DIR)
 	@$(OBJDUMP) $(OBJDUMP_FLAGS) $@ > $(BUILD_PLATFORM_DIR)/kernel.lst
-
-$(BUILD_PLATFORM_DIR)/kernel.elf: $(BUILD_PLATFORM_DIR)/ctx_switch.o $(BUILD_PLATFORM_DIR)/kernel.o | $(BUILD_PLATFORM_DIR)
-	@tput bold ; echo "Linking $@" ; tput sgr0
-	@$(CC) $(CFLAGS) -Wl,-gc-sections $^ $(LDFLAGS) -Wl,-Map=$(BUILD_PLATFORM_DIR)/kernel.Map -o $@
-	@$(OBJDUMP) $(OBJDUMP_FLAGS) $@ > $(BUILD_PLATFORM_DIR)/kernel_post-link.lst
 	@$(SIZE) $@
 
