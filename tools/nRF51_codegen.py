bd0d816e9f35c3f9fda6a0d6430da345551b43f6
Amit Aryeh Levy <amit@amitlevy.com>
2016-08-16 17:38:38 -0400

    Generate UART0 registers for the NRF51822

--- tools/nRF51_codegen.py
+++ tools/nRF51_codegen.py
@@ -140,7 +140,7 @@ def main():
     interrupts = get_peripheral_interrupts(parser)
     dump_macros(interrupts,
             open("src/chips/nrf51822/src/peripheral_interrupts.h", "w"))
-    peripherals = get_peripheral_registers(parser, ["GPIO"])
+    peripherals = get_peripheral_registers(parser, ["GPIO", "UART0"])
 
     env = Environment(loader=FileSystemLoader('src/chips/nrf51822/src'))
     template = env.get_template('peripheral_registers.rs.jinja')
