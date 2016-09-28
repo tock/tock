467003e5e856e6d7e011cad6af8e2110508304e7
Amit Levy <aalevy@gmail.com>
2016-09-27 21:35:31 -0400

    Move linker script and target spec to boards

--- chips/sam4l/build.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-use std::fs;
-use std::path::Path;
-
-fn main() {
-    let src = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("layout.ld");
-    let dst = Path::new(&env::var("OUT_DIR").unwrap()).join("../../layout.ld");
-    fs::copy(src, dst).unwrap();
-}
