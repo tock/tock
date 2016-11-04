4570ec4c473b26eb29f58cb0800f235ff0fd91e5
Pat Pannuto <pat.pannuto@gmail.com>
2016-11-04 16:27:33 -0400

    travis: Run fmt on all platforms

--- tools/run_cargo_fmt.sh
+++ tools/run_cargo_fmt.sh
@@ -2,4 +2,11 @@
 
 # Find folders with Cargo.toml files in them and run `cargo fmt`.
 
-for f in $(find . | grep Cargo.toml); do pushd $(dirname $f); cargo fmt -- --write-mode=overwrite; popd; done
+if [ "$1" == "diff" ]; then
+	# Just print out diffs and count errors, used by Travis
+	let FAIL=0
+	for f in $(find . | grep Cargo.toml); do pushd $(dirname $f); cargo fmt -- --write-mode=diff || let FAIL=FAIL+1; popd; done
+	exit $FAIL
+else
+	for f in $(find . | grep Cargo.toml); do pushd $(dirname $f); cargo fmt -- --write-mode=overwrite; popd; done
+fi
