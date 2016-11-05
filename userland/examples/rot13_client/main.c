85ce574ea958ae71824047873b9c922fe9a6c5e9
Amit Aryeh Levy <amit@amitlevy.com>
2016-11-05 10:09:30 -0400

    Adapt rot13 svc & client exmaple to IPC discovery
    Uses service discovery instead of hard-coded IPC service ids in the
    rot13 service/client example processes.
    
    Currently relies on the service's package name's length being a multiple
    of eight so that the actual binary (which appears directly after it)
    remains eight-byte aligned. One solution could be to pad the package
    name in the binary, but probably easiest just to put it at the end of
    the binary instead. This fix will come in the next commit.

--- userland/examples/rot13_client/main.c
+++ userland/examples/rot13_client/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <timer.h>
+#include <string.h>
 
 #define IPC_DRIVER 0x4c
+
+static char* rot13_service_name = "org.tockos.examples.rot1";
+int rot13_svc_num = 0;
 
 char buf[64] __attribute__((aligned(64)));
 
@@ -14,16 +18,20 @@ static void rot13_callback(int pid, int len, int arg2, void* ud) {
   struct rot13_buf *rb = (struct rot13_buf*)ud;
   printf("%d: %.*s\n", rb->length, rb->length, rb->buf);
   delay_ms(500);
-  command(IPC_DRIVER, 0, 0);
+  command(IPC_DRIVER, rot13_svc_num, 0);
 }
 
 int main(void) {
+  char svc_name[32];
+  memcpy(svc_name, rot13_service_name, strlen(rot13_service_name));
+  rot13_svc_num = allow(IPC_DRIVER, 0, svc_name, strlen(rot13_service_name));
+
   struct rot13_buf *rb = (struct rot13_buf*)buf;
   subscribe(IPC_DRIVER, 0, rot13_callback, rb);
   rb->length = snprintf(rb->buf, sizeof(rb->buf), "Hello World!");
-  allow(IPC_DRIVER, 0, rb, 64);
+  allow(IPC_DRIVER, rot13_svc_num, rb, 64);
 
-  command(IPC_DRIVER, 0, 0);
+  command(IPC_DRIVER, rot13_svc_num, 0);
   return 0;
 }
 
