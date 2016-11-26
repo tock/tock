f8fdd7faed9bba67b48da8cdfaca0ee1298f0f5b
Andrew <milichab@gmail.com>
2016-11-25 20:30:23 -0500

    files for detection app

---
+++ userland/examples/tests/FXOS8700CQ/main.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <tock.h>
+#include <console.h>
+
+// Step counter/fall detector
+
+int main() {
+	while (1) {
+		// read and calculate acceleration 
+	}
+}
