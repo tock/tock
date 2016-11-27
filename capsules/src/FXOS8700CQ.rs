4f59a4357aed85c72e68844e1b9f3594047f9a61
Andrew <milichab@gmail.com>
2016-11-27 14:19:25 -0800

    untested app code for step counter

--- capsules/src/FXOS8700CQ.rs
+++ capsules/src/FXOS8700CQ.rs
@@ -1,9 +1,7 @@
 use core::cell::Cell;
 use kernel::{AppId, Callback, Driver};
-use kernel::common::math::{sqrtf32, get_errno};
 use kernel::common::take_cell::TakeCell;
-use kernel::hil::gpio::{Pin, InterruptMode, Client};
-use kernel::hil::i2c;
+use kernel::hil::i2c::{I2CDevice, I2CClient, Error};
 
 pub static mut BUFFER: [u8; 6] = [0; 6];
 
@@ -12,182 +10,124 @@ const DEFAULT_SCALE: u8 = 0x0;
 #[allow(dead_code)]
 enum Registers {
     SensorStatus = 0x00,
-    Out_X_MSB = 0x01,
-    Out_X_LSB = 0x02,
-    Out_Y_MSB = 0x03,
-    Out_Y_LSB = 0x04,
-    Out_Z_MSB = 0x05,
-    Out_Z_LSB = 0x06,
-    XYZ_Data_CFG = 0x0e,
-    Ctrl_Reg1 = 0x2a,
+    OutXMSB = 0x01,
+    OutXLSB = 0x02,
+    OutYMSB = 0x03,
+    OutYLSB = 0x04,
+    OutZMSB = 0x05,
+    OutZLSB = 0x06,
+    XyzDataCfg = 0x0e,
+    CtrlReg1 = 0x2a,
 }
 
 #[derive(Clone,Copy,PartialEq)]
-enum ProtocolState {
-    Idle,
+enum State {
+    Disabled,
 
     /// Enable sensor
     Active,
 
-    /// Set accel register
-    SetRegAcceleration,
-
     /// Reading acceleration
     ReadingAcceleration,
+
+    /// Enabling
+    Enabling,
+
+    /// Disabling
+    Disabling(usize, usize, usize),
 }
 
 pub struct FXOS8700CQ<'a> {
-    i2c: &'a i2c::I2CDevice,
+    i2c: &'a I2CDevice,
     scale: Cell<u8>,
-    repeated_mode: Cell<bool>,
-    callback: Cell<Option<Callback>>,
-    protocol_state: Cell<ProtocolState>,
+    state: Cell<State>,
     buffer: TakeCell<&'static mut [u8]>,
+    callback: Cell<Option<Callback>>,
 }
 
 impl<'a> FXOS8700CQ<'a> {
-    pub fn new(i2c: &'a i2c::I2CDevice, buffer: &'static mut [u8]) -> FXOS8700CQ<'a> {
+    pub fn new(i2c: &'a I2CDevice, buffer: &'static mut [u8]) -> FXOS8700CQ<'a> {
         // setup and return struct
         FXOS8700CQ {
             i2c: i2c,
             scale: Cell::new(DEFAULT_SCALE),
-            repeated_mode: Cell::new(false),
-            callback: Cell::new(None),
-            protocol_state: Cell::new(ProtocolState::Idle),
+            state: Cell::new(State::Disabled),
             buffer: TakeCell::new(buffer),
+            callback: Cell::new(None),
         }
     }
 
-    fn enable_sensor(&self, scale: u8) {
+    fn start_read_accel(&self, scale: u8) {
         // enable and configure FXOS8700CQ
-        self.buffer.take().map(|buf| {
-            // turn on i2c
-            self.i2c.enable();
-            // configure accelerometer scale
-            // TODO
-            buf[0] = Registers::XYZ_Data_CFG as u8;
-            buf[1] = scale as u8;
-            self.i2c.write(buf, 2);
+        if self.state.get() == State::Disabled {
+            self.buffer.take().map(|buf| {
+                // turn on i2c
+                self.i2c.enable();
+                // configure accelerometer scale
+                // TODO
+                // buf[0] = Registers::XYZ_Data_CFG as u8;
+                // buf[1] = scale as u8;
+                // self.i2c.write(buf, 2);
 
-            // TODO configure magnetometer
+                // TODO configure magnetometer
 
-            // set to active mode
-            buf[0] = Registers::Ctrl_Reg1 as u8;
-            self.i2c.read(buf, 2);
-            buf[1] = buf[1] | 0x01;
-            self.i2c.write(buf, 2);
-
-            self.protocol_state.set(ProtocolState::Active);
-        });
-    }
-
-    fn disable_sensor(&self, temperature: Option<f32>) {
-        // TODO set to inactive
-    }
-
-    fn enable_interrupts(&self) {
-        // ???
-    }
-
-    fn disable_interrupts(&self) {
-        // ???
+                // set to active mode
+                buf[0] = Registers::CtrlReg1 as u8;
+                // self.i2c.read(buf, 2);
+                // buf[1] = buf[1] | 0x01;
+                buf[1] = 0x01;
+                self.i2c.write(buf, 2);
+                self.state.set(State::Active);
+            });
+        }
     }
 }
 
-fn calculate_acceleration() -> f32 {
-    0 as f32
-}
-
-impl<'a> i2c::I2CClient for FXOS8700CQ<'a> {
-    fn command_complete(&self, buffer: &'static mut [u8], _error: i2c::Error) {
-        match self.protocol_state.get() { 
-            ProtocolState::SetRegAcceleration => {
-                buffer[0] = Registers::Out_X_LSB as u8;
-                self.i2c.write(buffer, 1); // write byte of register we want to read from
-                self.protocol_state.set(ProtocolState::ReadingAcceleration);
+impl<'a> I2CClient for FXOS8700CQ<'a> {
+    fn command_complete(&self, buffer: &'static mut [u8], _error: Error) {
+        match self.state.get() { 
+            State::Enabling => {
+                buffer[0] = Registers::OutXLSB as u8;
+                self.i2c.write_read(buffer, 1, 6); // write byte of register we want to read from
+                self.state.set(State::ReadingAcceleration);
             }
-            ProtocolState::ReadingAcceleration => {
-                self.i2c.read(buffer, 6); // read 6 bytes for accel
+            State::ReadingAcceleration => {
+                // self.i2c.read(buffer, 6); // read 6 bytes for accel
+                // let x = (((buffer[0] as usize) << 8) + buffer[1]) as usize;
+                // let y = (((buffer[2] as usize) << 8) + buffer[3]) as usize;
+                // let z = (((buffer[4] as usize) << 8) + buffer[5]) as usize;
+                self.state.set(State::Disabling(buffer[0] as usize,
+                                                buffer[2] as usize,
+                                                buffer[4] as usize));
+            }
+            State::Disabling(x, y, z) => {
+                self.i2c.disable();
+                self.state.set(State::Disabled);
+                self.buffer.replace(buffer);
+                self.callback.get().map(|mut cb| cb.schedule(x, y, z));
             }
             _ => {}
         }
     }
 }
 
-impl<'a> Client for FXOS8700CQ<'a> {
-    fn fired(&self, _: usize) {
-        self.buffer.take().map(|buf| {
-            // do we need to do an i2c write here?
-            // self.i2c.enable();
-            self.protocol_state.set(ProtocolState::SetRegAcceleration);
-        });
-    }
-}
-
 impl<'a> Driver for FXOS8700CQ<'a> {
     fn subscribe(&self, subscribe_num: usize, callback: Callback) -> isize {
         match subscribe_num {
-            // single temperature reading with callback
             0 => {
-                // single sample mode
-                self.repeated_mode.set(false);
-
-                // set callback function
                 self.callback.set(Some(callback));
-
-                // enable sensor
-                self.enable_sensor(self.scale.get());
-
                 0
             }
-
-            // periodic acceleration reading subscription
-            1 => {
-                // periodic sampling mode
-                self.repeated_mode.set(true);
-
-                // set callback function
-                self.callback.set(Some(callback));
-
-                // enable sensor
-                self.enable_sensor(self.scale.get());
-
-                0
-            }
-
-            // default
             _ => -1,
         }
     }
 
-    fn command(&self, command_num: usize, data: usize, _: AppId) -> isize {
+    fn command(&self, command_num: usize, _arg1: usize, _: AppId) -> isize {
         match command_num {
-            // set period for sensing
             0 => {
-                // bounds check on the scale
-                // scale can be 0, 1, or 2?
-                if (data & 0xFFFFFFF8) != 0 {
-                    // TODO redo this
-                    return -1; // ERR_BAD_VAL
-                }
-                // TODO
-                // self.scale.set((data & 0x7) as u8);
-
+                self.start_read_accel(DEFAULT_SCALE);
                 0
             }
-
-            // unsubscribe callback
-            1 => {
-                // clear callback function
-                self.callback.set(None);
-
-                // disable sensor
-                self.disable_sensor(None);
-
-                0
-            }
-
-            // default
             _ => -1,
         }
     }
--- userland/examples/tests/FXOS8700CQ/main.c
+++ userland/examples/tests/FXOS8700CQ/main.c
@@ -4,14 +4,38 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include <tock.h>
 #include <console.h>
 
-// Step counter/fall detector
+#include "FXOS8700CQ.h"
 
+const double g = -9.8; 
+
+// Step counter/fall detector
 int main() {
-	while (1) {
+	printf("Step counter init\n"); 
+	unsigned num_measurements = 100; 
+	double accel_mags[num_measurements]; 
+
+	for (unsigned ii = 0; ii < num_measurements; ii ++) {
 		// read and calculate acceleration 
+		// uint16_t accel_x = 1; 
+		// uint16_t accel_y = 1; 
+		// uint16_t accel_z = 1; 
+
+		double accel_mag = FXOS8700CQ_read_accel_mag(); //sqrt(accel_x * accel_x + accel_y * accel_y + accel_z * accel_z); 
+		accel_mags[ii] = accel_mag + g;
 	}
+
+	unsigned steps = 0; 
+	for (unsigned ii = 0; ii < num_measurements - 1; ii ++) {
+		if (accel_mags[ii] < 0 && accel_mags[ii + 1] > 0) {
+			// step occurred 
+			steps ++; 
+		}
+	}
+
+	printf("%u steps occurred.\n", steps); 
 }
