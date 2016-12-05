3b646c0a32c1f7e1c105ac8c2fa9599dc841e0e5
Amit Aryeh Levy <amit@amitlevy.com>
2016-12-05 15:52:52 -0500

    Use opaque pinmux type for configuring nrf51 pins

--- boards/nrf51dk/src/main.rs
+++ boards/nrf51dk/src/main.rs
@@ -47,6 +47,7 @@ use capsules::timer::TimerDriver;
 use capsules::virtual_alarm::{MuxAlarm, VirtualMuxAlarm};
 use kernel::{Chip, SysTick};
 use kernel::hil::uart::UART;
+use nrf51::pinmux::Pinmux;
 use nrf51::rtc::{RTC, Rtc};
 
 // The nRF51 DK LEDs (see back of board)
@@ -171,6 +172,10 @@ pub unsafe fn reset_handler() {
         pin.set_client(gpio);
     }
 
+    nrf51::uart::UART0.configure(Pinmux::new(9),
+                                 Pinmux::new(11),
+                                 Pinmux::new(10),
+                                 Pinmux::new(8));
     let console = static_init!(
         capsules::console::Console<nrf51::uart::UART>,
         capsules::console::Console::new(&nrf51::uart::UART0,
