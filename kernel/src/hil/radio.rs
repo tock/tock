a9d35853ea08ebf543d226424855968fb740328b
Philip Levis <pal@cs.stanford.edu>
2017-01-05 17:07:19 -0800

    Add radio HIL.

---
+++ kernel/src/hil/radio.rs
@@ -0,0 +1,42 @@
+/// 802.15.4 radio interface
+
+use returncode::ReturnCode;
+pub trait TxClient {
+    fn send_done(&self, buf: &'static mut [u8], result: ReturnCode);
+}
+
+pub trait RxClient {
+    fn receive(&self, buf: &'static mut [u8]);
+}
+
+pub trait Radio {
+    fn initialize(&self) -> ReturnCode;
+    fn start(&self) -> ReturnCode;
+    fn stop(&self) -> ReturnCode;
+    fn reset(&self) -> ReturnCode;
+
+    fn set_transmit_client(&self, client: &'static TxClient);
+    fn set_receive_client(&self, client: &'static RxClient);
+
+    fn set_address(&self, addr: u16);
+    fn set_pan(&self, addr: u16);
+    fn payload_offset(&self) -> u8;
+    fn header_size(&self) -> u8;
+
+    fn transmit(&self,
+                dest: u16,
+                tx_data: &'static mut [u8],
+                tx_len: u8) -> ReturnCode;
+}
+
+pub const HEADER_SIZE: u8 = 10;
+
+#[repr(C, packed)]
+pub struct Header {
+    len: u8,
+    fcf: u16,
+    dsn: u8,
+    pan: u16,
+    src: u16,
+    dst: u16,
+}
