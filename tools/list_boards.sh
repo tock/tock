378ca9fab73707570861c2bae1729167d2470a20
Pat Pannuto <pat.pannuto@gmail.com>
2017-01-06 20:02:40 -0500

    Add `make allboards` target to root
    Update travis to use this so that it will automatically pick up
    new targets.

--- tools/list_boards.sh
+++ tools/list_boards.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 
+# http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+while getopts "h?1" opt; do
+    case "$opt" in
+    h|\?)
+        echo Prints supported tock boards
+        echo ""
+        echo "    -1  One entry per line, for scripting"
+        exit 0
+        ;;
+    1)  oneline=1
+        ;;
+    esac
+done
+
 boards=`ls -l boards | egrep '^d' | awk '{print $9}'`
+
+if [ $oneline -eq 1 ]; then
+    for board in $boards; do
+        echo $board
+    done
+    exit 0
+fi
+
 echo Supported Tock boards: $boards
 echo ""
 echo To build the kernel for a particular board, run:
