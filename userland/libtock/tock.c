11a5df121ddf27c3c7b1611a0ef14be8331fb7ea
Pat Pannuto <pat.pannuto@gmail.com>
2017-01-09 19:04:59 -0500

    Resolve 'control reaches end of non-void function'
    Following http://stackoverflow.com/questions/15927583/

--- userland/libtock/tock.c
+++ userland/libtock/tock.c
@@ -18,20 +18,28 @@ void yield() {
 
 int subscribe(uint32_t driver, uint32_t subscribe,
               subscribe_cb cb, void* userdata) {
-  asm volatile("svc 1\nbx lr" ::: "memory", "r0");
+  register int ret __asm__ ("r0");
+  asm volatile("svc 1" ::: "memory", "r0");
+  return ret;
 }
 
 
 int command(uint32_t driver, uint32_t command, int data) {
+  register int ret __asm__ ("r0");
   asm volatile("svc 2\nbx lr" ::: "memory", "r0");
+  return ret;
 }
 
 int allow(uint32_t driver, uint32_t allow, void* ptr, size_t size) {
+  register int ret __asm__ ("r0");
   asm volatile("svc 3\nbx lr" ::: "memory", "r0");
+  return ret;
 }
 
 int memop(uint32_t op_type, int arg1) {
+  register int ret __asm__ ("r0");
   asm volatile("svc 4\nbx lr" ::: "memory", "r0");
+  return ret;
 }
 
 bool driver_exists(uint32_t driver) {
