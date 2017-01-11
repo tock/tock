0177fa567b55296137a280e933beddff16aeae99
Pat Pannuto <pat.pannuto@gmail.com>
2017-01-11 14:51:35 -0500

    x-platform mac/linux xargs invocation

--- userland/libnrfserialization/create_libnrfserialization.sh
+++ userland/libnrfserialization/create_libnrfserialization.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Commit hash of the https://github.com/lab11/nrf5x-base repository
 # to use to make the library.
 NRF5X_BASE_SHA=6fb0730f2e976d9b85f8dddc6a873cb58d85d810
@@ -19,5 +21,5 @@ make headers NRF_BASE_PATH=nrf5x-base-$NRF5X_BASE_SHA
 rm -rf headers
 rm -f headers.tar.gz
 mkdir -p headers
-cat build/cortex-m4/*.headers | awk '{$1=$1};1' | awk '{print $1}' | sort | grep '\.h' | grep -v libtock | uniq | xargs -i cp {} headers/
+cat build/cortex-m4/*.headers | awk '{$1=$1};1' | awk '{print $1}' | sort | grep '\.h' | grep -v libtock | uniq | xargs -IFOO cp FOO headers/
 tar czf headers.tar.gz headers
