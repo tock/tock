4dd92ade654ac1efc175aee235986f3970f231f1
Pat Pannuto <pat.pannuto@gmail.com>
2017-01-10 23:07:11 -0500

    Mark inputs and outputs for extended ASM correctly
    LTO optimizations breaking things continues to teach me more about
    how these are supposed to be written. I believe these are now completely
    correct.

--- userland/libtock/tock.c
+++ userland/libtock/tock.c
@@ -4,8 +4,6 @@
 #include <stdbool.h>
 #include "tock.h"
 
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-
 void yield_for(bool *cond) {
   while(!*cond) {
     yield();
@@ -13,32 +11,69 @@ void yield_for(bool *cond) {
 }
 
 void yield() {
-  asm volatile("push {lr}\nsvc 0\npop {pc}" ::: "memory", "r0");
+  asm volatile(
+      "svc 0"
+      :
+      :
+      : "memory", "r0"
+      );
 }
 
 int subscribe(uint32_t driver, uint32_t subscribe,
               subscribe_cb cb, void* userdata) {
-  register int ret __asm__ ("r0");
-  asm volatile("svc 1" ::: "memory", "r0");
+  register uint32_t r0 asm("r0") = driver;
+  register uint32_t r1 asm("r1") = subscribe;
+  register void*    r2 asm("r2") = cb;
+  register void*    r3 asm("r3") = userdata;
+  register int ret asm ("r0");
+  asm volatile(
+      "svc 1"
+      : "=r" (ret)
+      : "r" (r0), "r" (r1), "r" (r2), "r" (r3)
+      : "memory");
   return ret;
 }
 
 
 int command(uint32_t driver, uint32_t command, int data) {
-  register int ret __asm__ ("r0");
-  asm volatile("svc 2\nbx lr" ::: "memory", "r0");
+  register uint32_t r0 asm("r0") = driver;
+  register uint32_t r1 asm("r1") = command;
+  register uint32_t r2 asm("r2") = data;
+  register int ret asm ("r0");
+  asm volatile(
+      "svc 2"
+      : "=r" (ret)
+      : "r" (r0), "r" (r1), "r" (r2)
+      : "memory"
+      );
   return ret;
 }
 
 int allow(uint32_t driver, uint32_t allow, void* ptr, size_t size) {
-  register int ret __asm__ ("r0");
-  asm volatile("svc 3\nbx lr" ::: "memory", "r0");
+  register uint32_t r0 asm("r0") = driver;
+  register uint32_t r1 asm("r1") = allow;
+  register void*    r2 asm("r2") = ptr;
+  register size_t   r3 asm("r3") = size;
+  register int ret asm ("r0");
+  asm volatile(
+      "svc 3"
+      : "=r" (ret)
+      : "r" (r0), "r" (r1), "r" (r2), "r" (r3)
+      : "memory"
+      );
   return ret;
 }
 
 int memop(uint32_t op_type, int arg1) {
-  register int ret __asm__ ("r0");
-  asm volatile("svc 4\nbx lr" ::: "memory", "r0");
+  register uint32_t r0 asm("r0") = op_type;
+  register int      r1 asm("r1") = arg1;
+  register int ret asm ("r0");
+  asm volatile(
+      "svc 4"
+      : "=r" (ret)
+      : "r" (r0), "r" (r1)
+      : "memory"
+      );
   return ret;
 }
 
