a2dac7622593a0d22d40f35ff778aec34a320d71
Pat Pannuto <pat.pannuto@gmail.com>
2017-01-15 15:53:29 -0500

    XXX: newlib disable io float altogether
    newlib has a nano-io trick that nominally allows for a user to
    compile-time decide whether to include the floating point IO routines,
    but that doesn't seem to work out-of-the-box with LTO, instead they're
    just always included, which was bloating application size
    
    Disabling floating point IO completely gets application sizes back
    down where we're expecting them. Puzzling out how to get the fleixble
    IO routines to play nicely with LTO is challenge for another day.

--- userland/newlib/build.sh
+++ userland/newlib/build.sh
@@ -13,6 +13,8 @@ $NEWLIB_SRC_DIR/configure --target=arm-none-eabi \
   --disable-newlib-unbuf-stream-opt \
   --enable-lite-exit \
   --enable-newlib-global-atexit \
-  --enable-newlib-nano-formatted-io
+  --disable-newlib-io-float
+
+# --enable-newlib-nano-formatted-io
 
 make -j CFLAGS_FOR_TARGET='-g -Os -ffunction-sections -fdata-sections -fPIC -flto -ffat-lto-objects -msingle-pic-base -mno-pic-data-is-text-relative'
