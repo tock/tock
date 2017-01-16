4432a821cb55383a61c9bce107fe01716e783688
Pat Pannuto <pat.pannuto@gmail.com>
2017-01-15 23:33:22 -0500

    XXX: use build.sh to pass --plugin to ar/ranlib for newlib
    [ci skip]

--- userland/newlib/build.sh
+++ userland/newlib/build.sh
@@ -17,4 +17,25 @@ $NEWLIB_SRC_DIR/configure --target=arm-none-eabi \
 
 # --enable-newlib-nano-formatted-io
 
-make -j CFLAGS_FOR_TARGET='-g -Os -ffunction-sections -fdata-sections -fPIC -flto -ffat-lto-objects -msingle-pic-base -mno-pic-data-is-text-relative'
+# Okay.. I cannot puzzle out a good way to add a flag to newlib's `ar` or `ranlib` invocation,
+# so we're going to hack around this in a somewhat creative fashion:
+ar=$(which arm-none-eabi-ar)
+ranlib=$(which arm-none-eabi-ranlib)
+
+rm -rf binhack
+mkdir binhack
+
+cat > binhack/arm-none-eabi-ar << EOF
+#!/usr/bin/env sh
+touch /tmp/hack-worked
+$ar "\$@" --plugin=$(arm-none-eabi-gcc --print-file-name=liblto_plugin.so)
+EOF
+chmod +x binhack/arm-none-eabi-ar
+
+cat > binhack/arm-none-eabi-ranlib << EOF
+#!/usr/bin/env sh
+$ranlib "\$@" --plugin=$(arm-none-eabi-gcc --print-file-name=liblto_plugin.so)
+EOF
+chmod +x binhack/arm-none-eabi-ranlib
+
+PATH="$PWD/binhack:$PATH" make -j CFLAGS_FOR_TARGET='-g -Os -ffunction-sections -fdata-sections -fPIC -flto -ffat-lto-objects -msingle-pic-base -mno-pic-data-is-text-relative'
