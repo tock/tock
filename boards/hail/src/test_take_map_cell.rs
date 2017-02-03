b8cef7f1bedfa2659302851fea2d8e3ecd555ff1
Pat Pannuto <pat.pannuto@gmail.com>
2017-02-02 23:54:03 -0500

    nit: fix compile warnings for lowercase globals

--- boards/hail/src/test_take_map_cell.rs
+++ boards/hail/src/test_take_map_cell.rs
@@ -3,29 +3,29 @@ use kernel::common::take_cell::MapCell;
 pub unsafe fn test_take_map_cell() {
     static FOO: u32 = 1234;
 
-    static mut mc_ref: MapCell<&'static u32> = MapCell::new(&FOO);
-    test_map_cell(&mc_ref);
+    static mut MC_REF: MapCell<&'static u32> = MapCell::new(&FOO);
+    test_map_cell(&MC_REF);
 
-    static mut mc1: MapCell<[[u8; 256]; 1]> = MapCell::new([[125; 256]; 1]);
-    test_map_cell(&mc1);
+    static mut MC1: MapCell<[[u8; 256]; 1]> = MapCell::new([[125; 256]; 1]);
+    test_map_cell(&MC1);
 
-    static mut mc2: MapCell<[[u8; 256]; 2]> = MapCell::new([[125; 256]; 2]);
-    test_map_cell(&mc2);
+    static mut MC2: MapCell<[[u8; 256]; 2]> = MapCell::new([[125; 256]; 2]);
+    test_map_cell(&MC2);
 
-    static mut mc3: MapCell<[[u8; 256]; 3]> = MapCell::new([[125; 256]; 3]);
-    test_map_cell(&mc3);
+    static mut MC3: MapCell<[[u8; 256]; 3]> = MapCell::new([[125; 256]; 3]);
+    test_map_cell(&MC3);
 
-    static mut mc4: MapCell<[[u8; 256]; 4]> = MapCell::new([[125; 256]; 4]);
-    test_map_cell(&mc4);
+    static mut MC4: MapCell<[[u8; 256]; 4]> = MapCell::new([[125; 256]; 4]);
+    test_map_cell(&MC4);
 
-    static mut mc5: MapCell<[[u8; 256]; 5]> = MapCell::new([[125; 256]; 5]);
-    test_map_cell(&mc5);
+    static mut MC5: MapCell<[[u8; 256]; 5]> = MapCell::new([[125; 256]; 5]);
+    test_map_cell(&MC5);
 
-    static mut mc6: MapCell<[[u8; 256]; 6]> = MapCell::new([[125; 256]; 6]);
-    test_map_cell(&mc6);
+    static mut MC6: MapCell<[[u8; 256]; 6]> = MapCell::new([[125; 256]; 6]);
+    test_map_cell(&MC6);
 
-    static mut mc7: MapCell<[[u8; 256]; 7]> = MapCell::new([[125; 256]; 7]);
-    test_map_cell(&mc7);
+    static mut MC7: MapCell<[[u8; 256]; 7]> = MapCell::new([[125; 256]; 7]);
+    test_map_cell(&MC7);
 }
 
 #[inline(never)]
