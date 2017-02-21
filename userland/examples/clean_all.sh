37a5b8d4e25f432d94ea778857e172c7939d60f7
Pat Pannuto <pat.pannuto@gmail.com>
2017-02-21 06:51:31 -0500

    userland: steal some shell prettiness from signpost
    Mostly motivated by having a re-print of what folder failed at the
    bottom of the terminal for when warning/error floods happen.

--- userland/examples/clean_all.sh
+++ userland/examples/clean_all.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 set -e
 
 for mkfile in `find . -maxdepth 3 -name Makefile`; do
 	dir=`dirname $mkfile`
 	if [ $dir == "." ]; then continue; fi
 	pushd $dir > /dev/null
-	make clean > /dev/null
+	make clean > /dev/null || echo "${bold} ⤤ $dir${normal}"
 	popd > /dev/null
 done
 
-echo ""
-echo "Done"
+echo "${bold}All Clean.${normal}"
