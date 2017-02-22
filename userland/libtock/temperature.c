6f927f664e4bb6df5eb265182269e002864fa6a8
niklasad1 <niklasadolfsson1@gmail.com>
2017-02-22 22:37:31 +0100

    Removed old files

--- userland/libtock/temperature.c
+++
@@ -1,9 +0,0 @@
-#include "temperature.h"
-
-int temp_init(subscribe_cb callback, void *ud) {
-  return subscribe(DRIVER_TEMP, 0, callback, NULL);
-}
-
-int temp_measure() {
-  return command(DRIVER_TEMP, 0, 0);
-}
