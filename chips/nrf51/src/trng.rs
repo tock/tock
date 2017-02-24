cb2536de4f29b1119058397d7ee4833ecd44a74e
niklasad1 <niklasadolfsson1@gmail.com>
2017-02-24 16:10:19 +0100

    some progress with the following bugs: * randomness_availabile() - always return done * randomness with many request is buggy works only for every second generation

--- chips/nrf51/src/trng.rs
+++ chips/nrf51/src/trng.rs
@@ -8,9 +8,13 @@ use nvic;
 use peripheral_interrupts::NvicIdx;
 use peripheral_registers::{RNG_BASE, RNG_REGS};
 
+pub static mut DMY: [u8; 4] = [0; 4];
+
 pub struct Trng<'a> {
     regs: *mut RNG_REGS,
     client: Cell<Option<&'a rng::Client>>,
+    done: Cell<u8>,
+    cnt: Cell<u8>,
 }
 
 pub static mut TRNG: Trng<'static> = Trng::new();
@@ -20,24 +24,44 @@ impl<'a> Trng<'a> {
         Trng {
             regs: RNG_BASE as *mut RNG_REGS,
             client: Cell::new(None),
+            done: Cell::new(0),
+            cnt: Cell::new(0),
         }
     }
 
+    #[inline(never)]
+    #[no_mangle]
     pub fn handle_interrupt(&self) {
         let regs: &mut RNG_REGS = unsafe { mem::transmute(self.regs) };
-        // panic!("random number: {:}\r\n", regs.VALUE.get());
-
         // ONLY VALRDY CAN TRIGGER THIS INTERRUPT
         self.disable_interrupts();
         self.disable_nvic();
         regs.STOP.set(1);
-        //
-        self.client.get().map(|client| {
-            let result = client.randomness_available(&mut TrngIter(self));
-            if Continue::Done != result {
-                self.start_rng();
+
+        self.cnt.set( self.cnt.get()+1);
+
+        match self.done.get() {
+            e @ 0...3 => {
+                unsafe {
+                    DMY[e as usize] = regs.VALUE.get() as u8;
+                }
+                self.done.set(e + 1);
+                self.start_rng()
             }
-        });
+            4 => {
+                self.client.get().map(|client| {
+                    let result = client.randomness_available(&mut TrngIter(self));
+                    if let Continue::Done = result {
+                        // do nothing WE ARE DONE REMOVE THIS LATER
+                        ()
+                    } else {
+                        self.done.set(0);
+                        self.start_rng();
+                    }
+                });
+            }
+            _ => panic!("invalid length of data\r\n"),
+        }
     }
 
     pub fn set_client(&self, client: &'a rng::Client) {
@@ -68,8 +92,8 @@ impl<'a> Trng<'a> {
     fn start_rng(&self) {
         let regs: &mut RNG_REGS = unsafe { mem::transmute(self.regs) };
         // clear registers
-        regs.START.set(0);
         regs.VALRDY.set(0);
+        regs.CONFIG.set(1);
 
         // enable interrupts
         self.enable_nvic();
@@ -78,12 +102,6 @@ impl<'a> Trng<'a> {
         // start rng
         regs.START.set(1);
     }
-
-    // fn stop_rng(&self) {
-    //     let regs: &mut RNG_REGS = unsafe { mem::transmute(self.regs) };
-    //     regs.VALRDY.set(0);
-    //     regs.START.set(0);
-    // }
 }
 
 struct TrngIter<'a, 'b: 'a>(&'a Trng<'b>);
@@ -91,10 +109,15 @@ struct TrngIter<'a, 'b: 'a>(&'a Trng<'b>);
 impl<'a, 'b> Iterator for TrngIter<'a, 'b> {
     type Item = u32;
 
+    #[inline(never)]
+    #[no_mangle]
     fn next(&mut self) -> Option<u32> {
-        let regs: &mut RNG_REGS = unsafe { mem::transmute(self.0.regs) };
-        if regs.VALRDY.get() != 0 {
-            Some(regs.VALUE.get())
+        // let regs: &mut RNG_REGS = unsafe { mem::transmute(self.0.regs) };
+        if self.0.done.get() == 4 {
+            unsafe {
+                let b = mem::transmute::<[u8; 4], u32>(DMY);
+                Some(b)
+            }
         } else {
             None
         }
@@ -103,12 +126,13 @@ impl<'a, 'b> Iterator for TrngIter<'a, 'b> {
 
 impl<'a> rng::RNG for Trng<'a> {
     fn get(&self) {
-        self.start_rng();
+        self.start_rng()
     }
 }
 
 #[inline(never)]
 #[no_mangle]
+#[allow(non_snake_case)]
 pub unsafe extern "C" fn RNG_Handler() {
     use kernel::common::Queue;
     nvic::disable(NvicIdx::RNG);
