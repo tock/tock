9bf614cc460d771f6ec253b7edbb9299e6d356ac
Fredrik Nilsson <frednils@student.chalmers.se>
2017-03-20 15:24:13 +0100

    working on virtual timers, removed some panics...

--- chips/nrf51/src/timer.rs
+++ chips/nrf51/src/timer.rs
@@ -222,7 +222,6 @@ impl Timer {
     /// created an event, and if so, add it to the bitmask of triggered
     /// events that is passed to the client.
     pub fn handle_interrupt(&self) {
-        panic!("handle int\r\n");
         nvic::clear_pending(self.nvic);
         self.client
             .get()
@@ -296,7 +295,6 @@ impl TimerAlarm {
 
     #[inline(never)]
     pub fn handle_interrupt(&self) {
-        panic!("handle int\r\n");
         self.clear_alarm();
         self.client.get().map(|client| { client.fired(); });
     }
