7b13071a34fa1ac95ec685e4449f5175fac2946a
niklasad1 <niklasadolfsson1@gmail.com>
2017-03-21 12:21:34 +0100

    re-named some stuff, added more comments, removed annotations

--- kernel/src/hil/symmetric_encryption.rs
+++ kernel/src/hil/symmetric_encryption.rs
@@ -1,16 +1,43 @@
+//! Interfaces for accessing encryption and decryption of symmetric ciphers
+//! (only AES-128-ctr for suppported nrf51dk "at the moment")
+//!
+//! The interface is supposed to work for hardware supported crypto but
+//! should work for software implemented crypto as well.
+//!
+//! State Machine:
+//!  1. init()
+//!  2. set_key()
+//!  3. aes128_crypt_ctr()   - can be used arbitary number of times
+//!
+
 use returncode::ReturnCode;
 
 pub trait SymmetricEncryptionDriver {
+    /// Initialization of the chip register
     fn init(&self);
+
+    /// Configure encryption/decryption key
+    /// assumes that key size is 16 bytes
     fn set_key(&self, key: &'static mut [u8]);
+
+    /// these may be used in the future but rename them then as "aes_128_encrypt_cbc etc"
     // fn encrypt(&self, plaintext: &'static mut [u8], len: u8);
     // fn decrypt(&self, ciphertext: &'static mut [u8], len: u8);
-    fn crypt_ctr(&self, data: &'static mut [u8], init_ctr: &'static mut [u8], len: u8);
+    /// encryption and decryption for aes in counter mode
+    /// because only the encryption-mode of the cipher is used and works like stream-cipher
+    /// rename method to "aes_128_crypt_ctr"?
+    /// this method is highly depedent on nrf51dk though but should work for
+    /// other chips perhaps only ignore "init_ctr" and assume all is performed in HW
+    fn aes128_crypt_ctr(&self, data: &'static mut [u8], init_ctr: &'static mut [u8], len: u8);
 }
 
 pub trait Client {
-    // fn encrypt_done(&self, ct: &'static mut [u8], len: u8) -> ReturnCode;
-    // fn decrypt_done(&self, pt: &'static mut [u8], len: u8) -> ReturnCode;
+    /// send back to result of the encryption/decryption to the capsule
+    /// this should be hardware independent if the cryptostate is used for all
+    /// implementations
     fn crypt_done(&self, data: &'static mut [u8], len: u8) -> ReturnCode;
+
+    /// once the key has been configure trigger call-back to indicate to the capsule
+    /// that now it's possible to begin to encrypt and decrypt data
     fn set_key_done(&self, key: &'static mut [u8], len: u8) -> ReturnCode;
 }
--- userland/examples/nrf51_aes/aes.c
+++ userland/examples/nrf51_aes/aes.c
@@ -1,10 +1,10 @@
 #include "aes.h"
 
-int aes_ecb_init(subscribe_cb callback, void *ud) {
+int aes128_init(subscribe_cb callback, void *ud) {
   return subscribe(DRIVER_ECB, 0, callback, ud);
 }
 
-int aes_ecb_configure_key(const char* key, unsigned char len) {
+int aes128_configure_key(const char* key, unsigned char len) {
   int err = allow(DRIVER_ECB, KEY, (void*)key, len);
   if (err < 0)  {
     return err;
@@ -12,16 +12,16 @@ int aes_ecb_configure_key(const char* key, unsigned char len) {
   return command(DRIVER_ECB, KEY, len);
 }
 
-int aes_ecb_encrypt(const char* msg, unsigned char len) {
-  int err = allow(DRIVER_ECB, ENC, (void*)msg, len);
+int aes128_encrypt_ctr(const char* buf, unsigned char len) {
+  int err = allow(DRIVER_ECB, ENC, (void*)buf, len);
   if (err < 0)  {
     return err;
   }
   return command(DRIVER_ECB, ENC, len);
 }
 
-int aes_ecb_decrypt(const char* ciphertext, unsigned char len) {
-  int err = allow(DRIVER_ECB, DEC, (void*)ciphertext, len);
+int aes128_decrypt_ctr(const char* buf, unsigned char len) {
+  int err = allow(DRIVER_ECB, DEC, (void*)buf, len);
   if (err < 0)  {
     return err;
   }
