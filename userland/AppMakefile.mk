e58a085444b290148e177ff2fb2bc8c1f9df9399
Pat Pannuto <pat.pannuto@gmail.com>
2017-03-28 23:24:04 -0400

    align libnrfserialization with documentation
      - use lib artifacts from the build/<arch> dir
      - also clean up create_libnrfserialization a bit

--- userland/AppMakefile.mk
+++ userland/AppMakefile.mk
@@ -62,7 +62,7 @@ ifneq "$$(wildcard $(1)/include)" ""
 endif
 
 # Add arch-specific rules for each library
-$$(foreach arch, $$(TOCK_ARCHS), $$(eval LIBS_$$(arch) += $(1)/$$(arch)/$(notdir $(1)).a))
+$$(foreach arch, $$(TOCK_ARCHS), $$(eval LIBS_$$(arch) += $(1)/build/$$(arch)/$(notdir $(1)).a))
 
 endef
 
--- userland/libnrfserialization/create_libnrfserialization.sh
+++ userland/libnrfserialization/create_libnrfserialization.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 
-rm -rf build/ headers/
-rm -f headers.tar.gz
-mkdir -p headers
+make clean
 
 # Commit hash of the https://github.com/lab11/nrf5x-base repository
 # to use to make the library.
@@ -20,17 +17,6 @@ if [ ! -d "nrf5x-base-$NRF5X_BASE_SHA" ]; then
 fi
 
 make -j NRF_BASE_PATH=nrf5x-base-$NRF5X_BASE_SHA
-mkdir -p cortex-m0 cortex-m4
-cp build/cortex-m0/libnrfserialization.a cortex-m0/libnrfserialization.a
-cp build/cortex-m4/libnrfserialization.a cortex-m4/libnrfserialization.a
-
-make -j headers NRF_BASE_PATH=nrf5x-base-$NRF5X_BASE_SHA
-
-
-cat build/headers/*.headers | awk '{$1=$1};1' | awk '{print $1}' | sort | grep '\.h' | grep -v libtock | uniq | xargs -IFOO cp FOO headers/
-tar czf headers.tar.gz headers
-
-set +x
 
 echo ""
 echo "Done."
