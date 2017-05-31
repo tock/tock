67c3f4d8cd387c73c394d9f7fdf4357eebd464f0
Pat Pannuto <pat.pannuto@gmail.com>
2017-05-31 16:29:49 -0400

    libtock: ignore warning from no newlib headers

--- userland/libtock/sys.c
+++ userland/libtock/sys.c
@@ -11,6 +11,7 @@
 // XXX Suppress missing prototype warnings for this file as the headers should
 // be in newlib internals, but first stab at including things didn't quite work
 // and the warnings are just noise
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wmissing-prototypes"
 #pragma GCC diagnostic ignored "-Wstrict-prototypes"
 
