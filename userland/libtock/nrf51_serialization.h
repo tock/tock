0afcca931d72204bb9423a47ba59ebf80ebaadd5
Pat Pannuto <pat.pannuto@gmail.com>
2017-06-01 17:19:40 -0400

    libtock: don't allow serialization calls to ignore returns

--- userland/libtock/nrf51_serialization.h
+++ userland/libtock/nrf51_serialization.h
@@ -8,15 +8,19 @@ extern "C" {
 
 // Give the BLE Serialization / UART layer a callback to call when
 // a packet is received and when a TX is finished.
+__attribute__ ((warn_unused_result))
 int nrf51_serialization_subscribe (subscribe_cb cb);
 
 // Pass a buffer for the driver to write received UART bytes to.
+__attribute__ ((warn_unused_result))
 int nrf51_serialization_setup_rx_buffer (char* rx, int rx_len);
 
 // Write a packet to the BLE Serialization connectivity processor.
+__attribute__ ((warn_unused_result))
 int nrf51_serialization_write (char* tx, int tx_len);
 
 // Generate an event to wake the app from a yield
+__attribute__ ((warn_unused_result))
 int nrf51_wakeup (void);
 
 #ifdef __cplusplus
