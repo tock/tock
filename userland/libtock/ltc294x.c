d1435edae3f336f274035b331c2e722a688098a6
Josh Adkins <adkinsjd@umich.edu>
2017-06-05 21:36:09 -0700

    added coulomb helper function for ltc294x

--- userland/libtock/ltc294x.c
+++ userland/libtock/ltc294x.c
@@ -237,6 +237,14 @@ int ltc294x_shutdown_sync(void) {
   return 0;
 }
 
+int ltc294x_convert_to_coulomb_uah(int c, int Rsense, uint16_t prescaler, ltc294x_model_e model) {
+    if(model == LTC2941 || model == LTC2942) {
+        return (int)(c*0.085*(50.0/Rsense)*(prescaler/128.0));
+    } else {
+        return (int)(c*0.340*(50.0/Rsense)*(prescaler/4096.0));
+    }
+}
+
 int ltc294x_convert_to_voltage_mv (int v) {
   return 23.6*(v/(float)0xFFFF)*1000;
 }
