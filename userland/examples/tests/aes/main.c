c4de680749fb783145a7bf14ab0db82a44cb3da1
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2017-06-07 22:18:09 +0200

    made the aes library syncronous and changed the test accordingly

--- userland/examples/tests/aes/main.c
+++ userland/examples/tests/aes/main.c
@@ -55,7 +55,7 @@
    Output Block   e89c399ff0f198c6d40a31db156cabfe
    Ciphertext     1e031dda2fbe03d1792170a0f3009cee
    Plaintext      f69f2445df4f9b17ad2b417be66c3710
- */
+*/
 
 int main(void)
 {
