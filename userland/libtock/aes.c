09e476308b593a956e399d1a0788f8122e993b1f
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2017-06-09 23:07:38 +0200

    changed name convention and callbacks are stack allocated

--- userland/libtock/aes.c
+++ userland/libtock/aes.c
@@ -1,23 +1,24 @@
 #include "aes.h"
 
-struct aes_data {
+
+// used for creating synchronous versions of functions
+//
+// fired -  set when the callback has been called
+// error - error received from the kernel less than zero indicates an error
+typedef struct {
   bool fired;
-  int type;
-};
-
-static struct aes_data result = { .fired = false, .type = -1};
+  int error;
+} aes_data_t;
 
 
-// 0 - set_key
-// 1 - encrypt
-// 2 - decrypt
 static void aes_cb(int cb,
     __attribute__ ((unused)) int len,
     __attribute__ ((unused)) int arg2,
-    __attribute__ ((unused)) void *ud) {
-  printf("cb %d\r\n", cb);
-  result.fired = true;
-  result.type = cb;
+    __attribute__ ((unused)) void *callback_args) {
+
+  aes_data_t* result = (aes_data_t*)callback_args;
+  result->fired = true;
+  result->error = cb;
 }
 
 
@@ -25,46 +26,70 @@ int aes128_set_callback(subscribe_cb callback, void *ud) {
   return subscribe(AES_DRIVER, 0, callback, ud);
 }
 
-int aes128_configure_key(const unsigned char* key, unsigned char len) {
-  int err = aes128_set_callback(aes_cb, NULL);
-  if (err < 0) return err;
+
+int aes128_set_key_sync(const unsigned char* key, unsigned char len) {
+  
+  int err;
+  aes_data_t result = { .fired = false, .error = SUCCESS };
+
+  err = aes128_set_callback(aes_cb, &result);
+  if (err < SUCCESS) return err;
 
   err = allow(AES_DRIVER, AES_KEY, (void*)key, len);
-  if (err < 0) return err;
-  
-  result.fired = false;
-  
+  if (err < SUCCESS) return err;
+
   err = command(AES_DRIVER, AES_KEY, 0);
-  if(err < 0) return err;
-  
+  if (err < SUCCESS) return err;
+
   yield_for(&result.fired);
-  return result.type;
+  
+  return result.error;
 }
 
-int aes128_encrypt_ctr(unsigned const char* buf, unsigned char buf_len, unsigned const char* ctr, unsigned char ctr_len) {
-  int err = allow(AES_DRIVER, AES_DATA, (void*)buf, buf_len);
-  if (err < 0) return err;
+
+int aes128_encrypt_ctr_sync(unsigned const char* buf, unsigned char buf_len, 
+    unsigned const char* ctr, unsigned char ctr_len) {
+  
+  int err;
+  aes_data_t result = { .fired = false, .error = SUCCESS };
+
+  err = aes128_set_callback(aes_cb, &result);
+  if (err < SUCCESS) return err;
+
+  err = allow(AES_DRIVER, AES_DATA, (void*)buf, buf_len);
+  if (err < SUCCESS) return err;
 
   err = allow(AES_DRIVER, AES_CTR, (void*)ctr, ctr_len);
-  if (err < 0) return err;
-  
-  result.fired = false;
+  if (err < SUCCESS) return err;
+
   err = command(AES_DRIVER, AES_ENC, 0);
+  if (err < SUCCESS) return err;
+
   yield_for(&result.fired);
-  return result.type;
+
+  return result.error;
 }
 
-int aes128_decrypt_ctr(const unsigned char* buf, unsigned char buf_len, const unsigned char* ctr, unsigned char ctr_len) {
-  int err = allow(AES_DRIVER, AES_DATA, (void*)buf, buf_len);
-  if (err < 0) return err;
+
+int aes128_decrypt_ctr_sync(const unsigned char* buf, unsigned char buf_len, 
+    const unsigned char* ctr, unsigned char ctr_len) {
+  
+  int err;
+  aes_data_t result = { .fired = false, .error = SUCCESS };
+
+  err = aes128_set_callback(aes_cb, &result);
+  if (err < SUCCESS) return err;
+
+  err = allow(AES_DRIVER, AES_DATA, (void*)buf, buf_len);
+  if (err < SUCCESS) return err;
 
   err = allow(AES_DRIVER, AES_CTR, (void*)ctr, ctr_len);
-  if (err < 0) return err;
+  if (err < SUCCESS) return err;
 
-  result.fired = false;
   err = command(AES_DRIVER, AES_DEC, 0);
-  if (err < 0) return err;
-  
+  if (err < SUCCESS) return err;
+
   yield_for(&result.fired);
-  return result.type;
+  
+  return result.error;
 }
