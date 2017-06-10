c549b01fb835a3f81d4ae76b567da5bfc1e25d44
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2017-06-10 20:35:36 +0200

    removed set_key_done from HIL makes no sense to have it async. tried to fix stuff according to @brghena comments

--- userland/libtock/aes.c
+++ userland/libtock/aes.c
@@ -11,39 +11,44 @@ typedef struct {
 } aes_data_t;
 
 
-static void aes_cb(int cb,
-    __attribute__ ((unused)) int len,
-    __attribute__ ((unused)) int arg2,
-    __attribute__ ((unused)) void *callback_args) {
+// Internal callback for creating synchronous functions
+//
+// callback_type - number indicating which type of callback occurred
+// currently 1(encryption) and 2(decryption)
+// callback_args - user data passed into the set_callback function
+//
+static void aes_cb(int callback_type,
+    __attribute__ ((unused)) int unused1,
+    __attribute__ ((unused)) int unused2,
+    void *callback_args) {
 
-  aes_data_t* result = (aes_data_t*)callback_args;
+  aes_data_t *result = (aes_data_t*)callback_args;
   result->fired = true;
-  result->error = cb;
+  result->error = callback_type;
 }
 
 
+// ***** System Call Interface *****
+
+// Internal callback for encryption and decryption
 int aes128_set_callback(subscribe_cb callback, void *ud) {
   return subscribe(AES_DRIVER, 0, callback, ud);
 }
 
+// ***** Synchronous Calls *****
 
+
+// Call to configure a buffer with an encryption key in the
+// kernel. No need to for a callback for this since it is syncronous in
+// the kernel as well.
 int aes128_set_key_sync(const unsigned char* key, unsigned char len) {
   
   int err;
-  aes_data_t result = { .fired = false, .error = SUCCESS };
-
-  err = aes128_set_callback(aes_cb, &result);
-  if (err < SUCCESS) return err;
 
   err = allow(AES_DRIVER, AES_KEY, (void*)key, len);
   if (err < SUCCESS) return err;
 
-  err = command(AES_DRIVER, AES_KEY, 0);
-  if (err < SUCCESS) return err;
-
-  yield_for(&result.fired);
-  
-  return result.error;
+  return command(AES_DRIVER, AES_KEY, 0);
 }
 
 
--- userland/libtock/aes.h
+++ userland/libtock/aes.h
@@ -13,9 +13,40 @@ extern "C" {
 #define AES_DEC    3
 #define AES_CTR    4
 
-int aes128_set_callback(subscribe_cb callback, void *ud);
+
+// set the function called by the encryption or decryption operations are 
+// complete
+//
+// callback - pointer to function to be called
+// callback_args - pointer to data provided to the callback
+int aes128_set_callback(subscribe_cb callback, void *callback_args);
+
+// configures an encryption key to be used for encryption and decryption
+//
+// key - a buffer containing the key (should be 16 bytes for aes128)
+// len - length of the buffer (should be 16 bytes for aes128)
 int aes128_set_key_sync(const unsigned char* key, unsigned char len);
+
+
+// encrypts a payload according to aes-128 counter-mode
+//
+// buf      - buffer to encrypt (currently max 128 bytes are supported)
+// buf_len  - length of the buffer to encrypt
+// ctr      - buffer with the initial counter
+// ctr_len  - length of buffer with the initial counter 
 int aes128_encrypt_ctr_sync(const unsigned char* buf, unsigned char buf_len, 
     const unsigned char* ctr, unsigned char ctr_len);
+
+
+// decrypts a payload according to aes-128 counter-mode
+//
+// buf      - buffer to decrypt (currently max 128 bytes are supported)
+// buf_len  - length of the buffer to decrypt
+// ctr      - buffer with the initial counter
+// ctr_len  - length of buffer with the initial counter 
 int aes128_decrypt_ctr_sync(const unsigned char* buf, unsigned char buf_len, 
     const unsigned char* ctr, unsigned char ctr_len);
+
+#ifdef __cplusplus
+}
+#endif
