76fd791b63864a9146ce4bd7d545dc5c3611b304
Amit Aryeh Levy <amit@amitlevy.com>
2017-06-21 18:27:58 -0400

    Nits in timer capsule

--- capsules/src/timer.rs
+++ capsules/src/timer.rs
@@ -84,7 +84,12 @@ impl<'a, A: Alarm> TimerDriver<'a, A> {
 }
 
 impl<'a, A: Alarm> Driver for TimerDriver<'a, A> {
-    fn subscribe(&self, _: usize, callback: Callback) -> ReturnCode {
+    /// Subscribe to timer expiration
+    ///
+    /// ### `_subscribe_num`
+    ///
+    /// - `0`: Subscribe to timer expieration
+    fn subscribe(&self, _subscribe_num: usize, callback: Callback) -> ReturnCode {
         self.app_timer
             .enter(callback.app_id(), |td, _allocator| {
                 td.callback = Some(callback);
@@ -97,107 +102,117 @@ impl<'a, A: Alarm> Driver for TimerDriver<'a, A> {
             })
     }
 
+    /// Setup and read the MAX17205.
+    ///
+    /// ### `command_num`
+    ///
+    /// - `0`: Driver check.
+    /// - `1`: Start a one-shot timer. `time` is used as the interval in milliseconds.
+    /// - `2`: Start a repeating timer. `time` is used as the interval in milliseconds.
+    /// - `3`: Stop the timer if it is outstanding
+    /// - `4`: Read the the current clock value
+    /// - `5`: Set an alarm to fire at a give clock value `time`.
+    /// - `6`: Return the clock frequency in Hz.
     fn command(&self, cmd_type: usize, time: usize, caller_id: AppId) -> ReturnCode {
-        // Returns the error code to return to the user (0 for success, negative
-        // otherwise) and whether we need to reset which is the next active
-        // alarm. We only _don't_ reset if we're disabling the underlying alarm
-        // anyway, if the underlying alarm is currently disabled and we're
-        // enabling the first alarm, or on an error (i.e. no change to the
-        // alarms).
+        // Returns the error code to return to the user and whether we need to
+        // reset which is the next active alarm. We only _don't_ reset if we're
+        // disabling the underlying alarm anyway, if the underlying alarm is
+        // currently disabled and we're enabling the first alarm, or on an error
+        // (i.e. no change to the alarms).
         let (return_code, reset) = self.app_timer
             .enter(caller_id, |td, _alloc| {
                 match cmd_type {
-                0 /* check if present */ => (ReturnCode::SUCCESS, false),
-                4 /* capture time */ => {
-                    let curr_time: u32 = self.alarm.now();
-                    (ReturnCode::SuccessWithValue { value: curr_time as usize },
-                     false)
-                },
-                3 /* Stop */ => {
-                    match td.expiration {
-                        Expiration::Disabled => {
-                            // Request to stop when already stopped
-                            (ReturnCode::EINVAL, false)
-                        },
-                        _ => {
-                            td.expiration = Expiration::Disabled;
-                            td.t0 = 0;
-                            let num_armed = self.num_armed.get();
-                            self.num_armed.set(num_armed - 1);
-                            if num_armed == 1 {
-                                self.alarm.disable();
-                                (ReturnCode::SUCCESS, false)
-                            } else {
-                                (ReturnCode::SUCCESS, true)
+                    0 /* check if present */ => (ReturnCode::SUCCESS, false),
+                    /* 1 for Oneshot, 2 for Repeat */
+                    1 | 2 => {
+                        // First, convert from milliseconds to native clock frequency
+                        let interval =
+                            (time as u32) * <A::Frequency>::frequency() / 1000;
+
+                        if interval == 0 {
+                            // Request for zero-length timer
+                            return (ReturnCode::EINVAL, false);
+                        }
+
+                        // if previously unarmed, but now will become armed
+                        if let Expiration::Disabled = td.expiration {
+                            self.num_armed.set(self.num_armed.get() + 1);
+                        }
+
+                        let now = self.alarm.now();
+                        let freq = <A::Frequency>::frequency() / 1000;
+
+                        // Round `now` to the nearest millisecond
+                        let now_mod = now % freq; // tics since last millisecond
+                        td.t0 = if now_mod >= freq / 2 {
+                            // round up
+                            now - now_mod + 1
+                        } else {
+                            // round down
+                            now - now_mod
+                        };
+
+                        if cmd_type == 1 {
+                            td.expiration = Expiration::Oneshot(interval);
+                        } else {
+                            td.expiration = Expiration::Repeating(interval);
+                        }
+
+                        if self.alarm.is_armed() {
+                            (ReturnCode::SUCCESS, true)
+                        } else {
+                            self.alarm.set_alarm(td.t0.wrapping_add(interval));
+                            (ReturnCode::SUCCESS, false)
+                        }
+                    },
+                    3 /* Stop */ => {
+                        match td.expiration {
+                            Expiration::Disabled => {
+                                // Request to stop when already stopped
+                                (ReturnCode::EINVAL, false)
+                            },
+                            _ => {
+                                td.expiration = Expiration::Disabled;
+                                td.t0 = 0;
+                                let new_num_armed = self.num_armed.get() - 1;
+                                self.num_armed.set(new_num_armed);
+                                if new_num_armed == 0 {
+                                    self.alarm.disable();
+                                    (ReturnCode::SUCCESS, false)
+                                } else {
+                                    (ReturnCode::SUCCESS, true)
+                                }
                             }
                         }
-                    }
-                },
-                5 /* Set absolute expiration */ => {
-                    // if previously unarmed, but now will become armed
-                    if let Expiration::Disabled = td.expiration {
-                        self.num_armed.set(self.num_armed.get() + 1);
-                    }
+                    },
+                    4 /* capture time */ => {
+                        let curr_time: u32 = self.alarm.now();
+                        (ReturnCode::SuccessWithValue { value: curr_time as usize },
+                         false)
+                    },
+                    5 /* Set absolute expiration */ => {
+                        // if previously unarmed, but now will become armed
+                        if let Expiration::Disabled = td.expiration {
+                            self.num_armed.set(self.num_armed.get() + 1);
+                        }
 
-                    let now = self.alarm.now();
-                    td.t0 = now;
-                    td.expiration = Expiration::Abs(time as u32);
+                        let now = self.alarm.now();
+                        td.t0 = now;
+                        td.expiration = Expiration::Abs(time as u32);
 
-                    if self.alarm.is_armed() {
-                        (ReturnCode::SUCCESS, true)
-                    } else {
-                        self.alarm.set_alarm(time as u32);
-                        (ReturnCode::SUCCESS, false)
-                    }
-                },
-                6 /* Get clock frequency */ => {
-                    let freq = <A::Frequency>::frequency() as usize;
-                    (ReturnCode::SuccessWithValue { value: freq }, true)
-                },
-                /* 1 for Oneshot, 2 for Repeat */
-                cmd_type if cmd_type <= 2 && cmd_type > 0 => {
-                    // First, convert from milliseconds to native clock frequency
-                    let interval =
-                        (time as u32) * <A::Frequency>::frequency() / 1000;
-
-                    if interval == 0 {
-                        // Request for zero-length timer
-                        return (ReturnCode::EINVAL, false);
-                    }
-
-                    // if previously unarmed, but now will become armed
-                    if let Expiration::Disabled = td.expiration {
-                        self.num_armed.set(self.num_armed.get() + 1);
-                    }
-
-                    let now = self.alarm.now();
-                    let freq = <A::Frequency>::frequency() / 1000;
-
-                    // Round `now` to the nearest millisecond
-                    let now_mod = now % freq; // tics since last millisecond
-                    td.t0 = if now_mod >= freq / 2 {
-                        // round up
-                        now - now_mod + 1
-                    } else {
-                        // round down
-                        now - now_mod
-                    };
-
-                    if cmd_type == 1 {
-                        td.expiration = Expiration::Oneshot(interval);
-                    } else {
-                        td.expiration = Expiration::Repeating(interval);
-                    }
-
-                    if self.alarm.is_armed() {
-                        (ReturnCode::SUCCESS, true)
-                    } else {
-                        self.alarm.set_alarm(td.t0.wrapping_add(interval));
-                        (ReturnCode::SUCCESS, false)
-                    }
-                },
-                _ => (ReturnCode::ENOSUPPORT, false)
-            }
+                        if self.alarm.is_armed() {
+                            (ReturnCode::SUCCESS, true)
+                        } else {
+                            self.alarm.set_alarm(time as u32);
+                            (ReturnCode::SUCCESS, false)
+                        }
+                    },
+                    6 /* Get clock frequency */ => {
+                        let freq = <A::Frequency>::frequency() as usize;
+                        (ReturnCode::SuccessWithValue { value: freq }, true)
+                    },
+                    _ => (ReturnCode::ENOSUPPORT, false)
+                }
             })
             .unwrap_or_else(|err| {
                 let e = match err {
