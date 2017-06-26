6bae41f7f85d6983e8dd83e81ef3e50c1931ad36
Paul Crews <ptcrews@stanford.edu>
2017-06-26 15:41:21 -0700

    Added ip structs

---
+++ capsules/src/net/ip.rs
@@ -0,0 +1,11 @@
+#[repr(C, packed)]
+pub struct IPv6Header {
+    version_class_flow: [u8, 4],
+    payload_len: u16, 
+    next_header: u8,
+    hop_limit: u8,
+    src_addr: [u8, 8],
+    dst_addr: [u8, 8],
+}
+
+type Addr16 = u16;
