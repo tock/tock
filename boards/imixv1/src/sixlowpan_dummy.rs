1c7f3ebd8eb0c8b5b94c7752f34bd25157f57c7a
Paul Crews <ptcrews@stanford.edu>
2017-06-30 15:17:12 -0700

    Small changes to test code

--- boards/imixv1/src/sixlowpan_dummy.rs
+++ boards/imixv1/src/sixlowpan_dummy.rs
@@ -10,7 +10,7 @@ pub const SRC_ADDR: IPAddr = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
                               0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f];
 pub const DST_ADDR: IPAddr = [0; 16];
 pub const SRC_MAC_ADDR: [u8; 8] = [0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17];
-pub const DST_MAC_ADDR: [u8; 8] = [0 as u8; 8];
+pub const DST_MAC_ADDR: [u8; 8] = [0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f];
 
 pub const IP6_HDR_SIZE: usize = 40;
 pub const PAYLOAD_LEN: usize = 10;
@@ -46,8 +46,8 @@ pub unsafe fn sixlowpan_dummy_test<R: radio::Radio>(rf233: &R) {
     }
     rf233.config_set_pan(0xABCD);
     rf233.config_set_address_long(SRC_MAC_ADDR);
-    rf233.config_commit();
-    while !rf233.is_on() {}
+    //rf233.config_commit();
+    //while !rf233.is_on() {}
     rf233.transmit_long(DST_MAC_ADDR,
                         &mut RF233_BUF,
                         (PAYLOAD_LEN + header_size) as u8
