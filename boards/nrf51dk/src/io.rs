a4120d30ccd69a52cf813d79488c027574d77e6d
Niklas <niklasadolfsson1@gmail.com>
2017-07-08 18:04:22 +0200

    ooops

--- boards/nrf51dk/src/io.rs
+++ boards/nrf51dk/src/io.rs
@@ -51,6 +51,7 @@ macro_rules! println {
 
 #[cfg(not(test))]
 #[lang="panic_fmt"]
+#[no_mangle]
 pub unsafe extern "C" fn rust_begin_unwind(_args: Arguments,
                                            _file: &'static str,
                                            _line: usize)
