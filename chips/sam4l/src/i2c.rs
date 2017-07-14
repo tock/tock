7fe18359c543c4366886a89e221ff8d8453548d1
Joshua Adkins <adkins@berkeley.edu>
2017-07-13 18:50:02 -0600

    Disable i2cslave Perhiperal for Master Transfer (#500)
    you can't just disable the clock - it causes the i2c to hang

--- chips/sam4l/src/i2c.rs
+++ chips/sam4l/src/i2c.rs
@@ -737,10 +737,8 @@ impl hil::i2c::I2CMaster for I2CHw {
             pm::enable_clock(self.master_clock);
         }
 
-        // If exists, disable slave clock
-        self.slave_clock.map(|slave_clock| unsafe {
-            pm::disable_clock(slave_clock);
-        });
+        //disable the i2c slave peripheral
+        hil::i2c::I2CSlave::disable(self);
 
         let regs: &mut TWIMRegisters = unsafe { mem::transmute(self.registers) };
 
