2025279a22fcfa6cef23107ca3677bf3bb8f9410
Amit Aryeh Levy <amit@amitlevy.com>
2017-08-09 11:36:13 -0700

    Revert moving led functionality to internal

--- userland/libtock/led.c
+++ userland/libtock/led.c
@@ -1,20 +1,17 @@
-#include "internal/led.h"
+#include "led.h"
 
-// For LEDs, these simply pass through to the syscall wrappers (the functions
-// in the internal folder).
+int led_count(void) {
+  return command(DRIVER_NUM_LEDS, 0, 0);
+}
 
 int led_on(int led_num) {
-  return led_internal_on(led_num);
+  return command(DRIVER_NUM_LEDS, 1, led_num);
 }
 
 int led_off(int led_num) {
-  return led_internal_off(led_num);
+  return command(DRIVER_NUM_LEDS, 2, led_num);
 }
 
 int led_toggle(int led_num) {
-  return led_internal_toggle(led_num);
-}
-
-int led_count(void) {
-  return led_internal_count();
+  return command(DRIVER_NUM_LEDS, 3, led_num);
 }
--- userland/libtock/led.h
+++ userland/libtock/led.h
@@ -6,6 +6,8 @@
 extern "C" {
 #endif
 
+#define DRIVER_NUM_LEDS 0x00000002
+
 int led_on(int led_num);
 int led_off(int led_num);
 int led_toggle(int led_num);
