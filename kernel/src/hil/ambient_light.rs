263303860d015ddfb8a535b88bbc83f74cf03391
Amit Aryeh Levy <amit@amitlevy.com>
2017-08-09 21:57:20 -0700

    Add an ambient light sensor HIL

---
+++ kernel/src/hil/ambient_light.rs
@@ -0,0 +1,24 @@
+//! Interface for ambient light sensors
+
+use returncode::ReturnCode;
+
+/// A basic interface for an ambient light sensor.
+pub trait AmbientLight {
+    /// Set the client to be notified when the capsule has data ready or has
+    /// finished some command.  This is likely called in a board's `main.rs`.
+    fn set_client(&self, client: &'static AmbientLightClient);
+
+    /// Get a single instantaneous reading of the acceleration in the
+    /// X,Y,Z directions.
+    fn read_light_intensity(&self) -> ReturnCode {
+        ReturnCode::ENODEVICE
+    }
+}
+
+/// Client for receiving light intensity readings.
+pub trait AmbientLightClient {
+    /// Called when an ambient light reading has completed.
+    ///
+    /// - `lux`: the most recently read ambient light reading in lux (lx).
+    fn callback(&self, lux: usize);
+}
