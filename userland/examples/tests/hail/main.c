ec2ef21f7763d8d7fd04aa56f6303ee08e044313
Amit Aryeh Levy <amit@amitlevy.com>
2017-08-14 22:57:15 -0700

    Userland: Remove no longer used si7021 driver

--- userland/examples/tests/hail/main.c
+++ userland/examples/tests/hail/main.c
@@ -15,7 +15,8 @@
 #include <led.h>
 #include <ninedof.h>
 #include <nrf51_serialization.h>
-#include <si7021.h>
+#include <temperature.h>
+#include <humidity.h>
 #include <timer.h>
 
 // Intervals for BLE advertising and connections
@@ -51,8 +52,9 @@ static void sample_sensors (void) {
 
   // Sensors: temperature/humidity, acceleration, light
   int temp;
+  temperature_read_sync(&temp);
   unsigned humi;
-  si7021_get_temperature_humidity_sync(&temp, &humi);
+  humidity_get_sync(&humi);
   uint32_t accel_mag = ninedof_read_accel_mag();
   int light          = ambient_light_read_intensity();
 
