c00df7ebb1e078d17dc80d60c308c4089789594d
Amit Aryeh Levy <amit@amitlevy.com>
2017-08-14 20:37:27 -0700

    Comment sensors

--- kernel/src/hil/sensors.rs
+++ kernel/src/hil/sensors.rs
@@ -1,5 +1,8 @@
+//! Interfaces for environment sensors
+
 use returncode::ReturnCode;
 
+/// A basic interface for a temperature sensor
 pub trait TemperatureDriver {
     fn set_client(&self, client: &'static TemperatureClient);
     fn read_ambient_temperature(&self) -> ReturnCode {
@@ -10,10 +13,16 @@ pub trait TemperatureDriver {
     }
 }
 
+/// Client for receiving temperature readings.
 pub trait TemperatureClient {
+    /// Called when a temperature reading has completed.
+    ///
+    /// - `value`: the most recently read temperature in hundredths of degrees
+    /// centigrate.
     fn callback(&self, value: usize);
 }
 
+/// A basic interface for a humidity sensor
 pub trait HumidityDriver {
     fn set_client(&self, client: &'static HumidityClient);
     fn read_humidity(&self) -> ReturnCode {
@@ -21,6 +30,10 @@ pub trait HumidityDriver {
     }
 }
 
+/// Client for receiving temperature readings.
 pub trait HumidityClient {
+    /// Called when a humidity reading has completed.
+    ///
+    /// - `value`: the most recently read temperature in hundredths of percent.
     fn callback(&self, value: usize);
 }
