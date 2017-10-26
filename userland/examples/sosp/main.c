e1106f529b9e6517542f830782149a82d027223a
Branden Ghena <brghena@umich.edu>
2017-10-26 13:57:58 -0700

    Fix what rebase somehow terribly screwed up

--- userland/examples/sosp/main.c
+++ userland/examples/sosp/main.c
@@ -1,24 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include <ambient_light.h>
-#include <console.h>
-#include <timer.h>
-
-char buf[300];
-
-static void print_complete(int a __attribute__((unused)),
-                           int b __attribute__((unused)),
-                           int c __attribute__((unused)),
-                           void* d __attribute__((unused)))
-{
-  // The message has been printed to the console
-}
-
-int main(void)
-{
-  int n = snprintf(buf, sizeof(buf), "From tock app: \"%s\"\n", "Hello, World!");
-  putnstr_async(buf, n, print_complete, NULL);
+int main (void) {
+  printf("Hello, World!\n");
 
   return 0;
 }
+
