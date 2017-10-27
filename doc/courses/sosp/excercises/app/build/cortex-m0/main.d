c5f6b4535bbe0c0ceb2105d794ece4a307c2f4a1
Amit Aryeh Levy <amit@amitlevy.com>
2017-10-28 07:37:46 +0800

    cleanup

--- doc/courses/sosp/excercises/app/build/cortex-m0/main.d
+++
@@ -1,4 +0,0 @@
-build/cortex-m0/main.d@ build/cortex-m0/main.o: main.c \
- ../../../../../userland/support/warning_header.h
-
-../../../../../userland/support/warning_header.h:
--- doc/courses/sosp/excercises/app/build/cortex-m4/main.d
+++
@@ -1,4 +0,0 @@
-build/cortex-m4/main.d@ build/cortex-m4/main.o: main.c \
- ../../../../../userland/support/warning_header.h
-
-../../../../../userland/support/warning_header.h:
