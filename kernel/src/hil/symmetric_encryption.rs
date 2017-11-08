08d63b5b5fc388e850097826df08d91542f319f7
Daniel B Giffin <daniel@beech-grove.net>
2017-11-07 21:54:19 -0800

    AES: pass bufs to crypt(), return at crypt_done()
    This changes the interface once more so that there is
    no opportunity for confusion over the state of buffer
    ownership.

--- kernel/src/hil/symmetric_encryption.rs
+++ kernel/src/hil/symmetric_encryption.rs
@@ -6,8 +6,8 @@ use returncode::ReturnCode;
 
 /// Implement this trait and use `set_client()` in order to receive callbacks from an `AES128`
 /// instance.
-pub trait Client {
-    fn crypt_done(&self);
+pub trait Client<'a> {
+    fn crypt_done(&'a self, source: Option<&'a mut [u8]>, dest: &'a mut [u8]);
 }
 
 /// The number of bytes used for AES block operations.  Keys and IVs must have this length,
@@ -23,7 +23,7 @@ pub trait AES128<'a> {
     fn disable(&self);
 
     /// Set the client instance which will receive `crypt_done()` callbacks
-    fn set_client(&'a self, client: &'a Client);
+    fn set_client(&'a self, client: &'a Client<'a>);
 
     /// Set the encryption key.
     /// Returns `EINVAL` if length is not `AES128_BLOCK_SIZE`
@@ -33,42 +33,10 @@ pub trait AES128<'a> {
     /// Returns `EINVAL` if length is not `AES128_BLOCK_SIZE`
     fn set_iv(&self, iv: &[u8]) -> ReturnCode;
 
-    /// Set the source buffer.  If this is full, the encryption input
-    /// will be this entire buffer.  If the source buffer is empty,
-    /// the destination buffer will be read to provide the input.
-    ///
-    /// If an encryption operation is in progress, this method instead
-    /// has no effect.
-    fn put_source(&'a self, buf: Option<&'a mut [u8]>);
-
-    /// Return the source buffer and set it to None, if one has
-    /// previously been set.
-    ///
-    /// If an encryption operation is in progress, this method instead
-    /// returns None.
-    fn take_source(&'a self) -> Option<&'a mut [u8]>;
-
-    /// Set the destination buffer.  If `put_source()` has not
-    /// been used to pass a source buffer, this buffer will also
-    /// provide the encryption input, which will be overwritten.
-    /// The option should be full whenever `crypt()` is called.
-    ///
-    /// If an encryption operation is in progress, this method instead
-    /// has no effect.
-    fn put_dest(&'a self, dest: Option<&'a mut [u8]>);
-
-    /// Return the destination buffer and set it to None, if one has
-    /// previously been set.
-    ///
-    /// If an encryption operation is in progress, this method instead
-    /// returns None.
-    fn take_dest(&'a self) -> Option<&'a mut [u8]>;
-
     /// Begin a new message (with the configured IV) when `crypt()` is
-    /// next called.  Multiple calls to `crypt()` (accompanied by
-    /// `put_source()` or `put_dest`) may be made between calls to
-    /// `start_message()`, allowing the encryption context to extend
-    /// over non-contiguous extents of data.
+    /// next called.  Multiple calls to `crypt()` may be made between
+    /// calls to `start_message()`, allowing the encryption context to
+    /// extend over non-contiguous extents of data.
     ///
     /// If an encryption operation is in progress, this method instead
     /// has no effect.
@@ -76,29 +44,41 @@ pub trait AES128<'a> {
 
     /// Request an encryption/decryption
     ///
-    /// The indices `start_index` and `stop_index` must be valid offsets in
-    /// a buffer previously passed in with `put_data`, and the length
-    /// `stop_index - start_index` must be a multiple of
-    /// `AES128_BLOCK_SIZE`.  Otherwise, EINVAL will be returned.
+    /// If the source buffer is not `None`, the encryption input
+    /// will be that entire buffer.  Otherwise the destination buffer
+    /// at indices between `start_index` and `stop_index` will
+    /// provide the input, which will be overwritten.
     ///
-    /// If a buffer has previously been passed in with `put_source`,
-    /// its length must be `stop_index - start_index`.  Otherwise,
-    /// EINVAL will be returned.
-    ///
-    /// If the source buffer is full, the encryption input will be
-    /// that entire buffer.  Otherwise the destination buffer will be
-    /// read to provide the input.
-    ///
-    /// If SUCCESS is returned, the client's `crypt_done` method will eventually
+    /// If `None` is returned, the client's `crypt_done` method will eventually
     /// be called, and the portion of the data buffer between `start_index`
     /// and `stop_index` will hold the result of the encryption/decryption.
+    ///
+    /// If `Some(result, source, dest)` is returned, `result` is the
+    /// error condition and `source` and `dest` are the buffers that
+    /// were passed to `crypt`.
+    ///
+    /// The indices `start_index` and `stop_index` must be valid
+    /// offsets in the destination buffer, and the length
+    /// `stop_index - start_index` must be a multiple of
+    /// `AES128_BLOCK_SIZE`.  Otherwise, `Some(EINVAL, ...)` will be
+    /// returned.
+    ///
+    /// If the source buffer is not `None`, its length must be
+    /// `stop_index - start_index`.  Otherwise, `Some(EINVAL, ...)`
+    /// will be returned.
+    ///
+    /// If an encryption operation is already in progress,
+    /// `Some(EBUSY, ...)` will be returned.
     ///
     /// For correct operation, the methods `set_key` and `set_iv` must have
     /// previously been called to set the buffers containing the
     /// key and the IV (or initial counter value), and a method `set_mode_*()`
     /// must have been called to set the desired mode.  These settings persist
     /// across calls to `crypt()`.
-    fn crypt(&self, start_index: usize, stop_index: usize) -> ReturnCode;
+    ///
+    fn crypt(&'a self, source: Option<&'a mut [u8]>, dest: &'a mut [u8],
+                 start_index: usize, stop_index: usize)
+        -> Option<(ReturnCode, Option<&'a mut [u8]>, &'a mut [u8])>;
 }
 
 pub trait AES128Ctr {
