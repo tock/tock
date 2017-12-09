f94e48b8a906d8d530cd9b37dcaf627fcf9c44fc
Pat Pannuto <pat.pannuto@gmail.com>
2017-12-09 13:55:06 -0800

    TEMP: keep a smaller faux peripheral around for tests

---
+++ chips/sam4l/src/test.rs
@@ -0,0 +1,77 @@
+//use core::cell::Cell;
+//use dma::{DMAChannel};
+use kernel::common::VolatileCell;
+//use kernel::common::take_cell::TakeCell;
+use kernel::ClockInterface;
+
+//use kernel::hil;
+use pm;
+
+
+
+#[repr(C, packed)]
+#[allow(dead_code)]
+struct TESTRegisters {
+    control: VolatileCell<u32>,
+    interrupt_mask: VolatileCell<u32>,
+}
+
+struct TESTRegisterManager <'a> {
+    registers: &'a TESTRegisters,
+    clock: &'a ClockInterface<PlatformClockType=pm::Clock>,
+}
+
+impl<'a> TESTRegisterManager <'a> {
+    fn new (hw: &'a TESTHw) -> TESTRegisterManager <'a> {
+        let clock = &hw.clock;
+        // If clock isn't enabled, lets enable it
+        if clock.is_enabled() == false {
+            debug!("TEST: Master clock on");
+            clock.enable();
+        }
+        TESTRegisterManager {
+            registers: unsafe { &*hw.registers },
+            clock: clock,
+        }
+    }
+}
+
+impl<'a> Drop for TESTRegisterManager <'a> {
+    fn drop(&mut self) {
+        let mask = self.registers.interrupt_mask.get();
+        if mask == 0 {
+            debug!("TEST: Master clock off");
+            self.clock.disable();
+        }
+        else {
+            debug!("TEST: Master clock left on");
+        }
+    }
+}
+
+pub struct TESTHw {
+    registers: *mut TESTRegisters,
+    clock: pm::Clock,
+    //dma: Cell<Option<&'static DMAChannel>>,
+}
+
+impl TESTHw {
+    const fn new(base_addr: *mut TESTRegisters,
+                 clock: pm::Clock,
+                 ) -> TESTHw {
+        TESTHw {
+            registers: base_addr as *mut TESTRegisters,
+            clock: clock,
+        }
+    }
+
+    pub fn do_thing(&self) {
+        let regs_manager = &TESTRegisterManager::new(&self);
+        regs_manager.registers.control.get();
+    }
+}
+
+const TEST_BASE_ADDR: *mut TESTRegisters = 0x40001000 as *mut TESTRegisters;
+pub static mut TEST0: TESTHw = TESTHw::new(TEST_BASE_ADDR,
+                                           pm::Clock::PBA(pm::PBAClock::TWIM0),
+                                           );
