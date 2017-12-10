43662f532801e13a7f3fb2101e3900cbb51bbd8b
Pat Pannuto <pat.pannuto@gmail.com>
2017-12-09 20:05:11 -0800

    refactor mmio

--- kernel/src/common/mod.rs
+++ kernel/src/common/mod.rs
@@ -8,8 +8,10 @@ pub mod volatile_cell;
 pub mod static_ref;
 pub mod list;
 pub mod math;
+pub mod mmio;
 
 pub use self::list::{List, ListLink, ListNode};
+pub use self::mmio::{MMIOInterface, MMIOManager};
 pub use self::queue::Queue;
 pub use self::ring_buffer::RingBuffer;
 pub use self::volatile_cell::VolatileCell;
