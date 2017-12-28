548726fb22ef15b2c8d498b01200c7e446c212e5
Amit Aryeh Levy <amit@amitlevy.com>
2017-12-28 18:27:54 -0500

    WIP - blink working
    - Removed all memory setup from kernel.
    
    - Implemeted GOT relocation and BSS zeroing in crt0.c
    
    This is enough for blink, but blink doesn't have a data section (that
    should be an easy fix), and doesn't have static relocations (the hard
    part).

--- userland/libtock/crt0.c
+++ userland/libtock/crt0.c
@@ -6,23 +6,57 @@ extern int main(void);
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wmissing-prototypes"
 
+struct hdr {
+  uint32_t got_sym_start;
+  uint32_t got_start;
+  int got_size;
+  uint32_t bss_start;
+  int bss_size;
+};
+
+
 __attribute__ ((section(".start"), used))
 __attribute__ ((weak))
 __attribute__ ((noreturn))
-__attribute__ ((naked))
 void _start(void* text_start __attribute__((unused)),
             void* mem_start __attribute__((unused)),
             void* memory_len __attribute__((unused)),
             void* app_heap_break __attribute__((unused))) {
-  /*
-   * 1. Setup r9 to point to the GOT (assumes the kernel places the GOT right
-   *    above the stack)
-   * 2. Call `main`
-   * 3. Loop on `yield` forever
-   */
-  asm volatile ("mov r9, sp; \
-                 bl main; \
-                 1: bl yield; \
-                 b 1b");
+
+  // Allocate stack. `brk` to 1024 from start of memory
+  {
+    int stacktop = (int)mem_start + 1024;
+    memop(0, stacktop + 1024);
+    asm volatile ("mov sp, %[stacktop]" :: [stacktop] "r" (stacktop));
+    asm volatile ("mov r9, sp");
+  }
+
+
+  struct hdr* myhdr = (struct hdr*)text_start;
+  int stacktop = (int)mem_start + 1024;
+
+  // fix up GOT
+  volatile uint32_t* got_start = (uint32_t*)(myhdr->got_start + stacktop);
+  volatile uint32_t* got_sym_start = (uint32_t*)(myhdr->got_sym_start + (uint32_t)text_start);
+  for (int i = 0; i < (myhdr->got_size / (int)sizeof(uint32_t)); i++) {
+    if ((got_sym_start[i] & 0x80000000) == 0) {
+      got_start[i] = got_sym_start[i] + stacktop + myhdr->got_size;
+    } else {
+      got_start[i] = (got_sym_start[i] ^ 0x80000000) + (uint32_t)text_start;
+    }
+  }
+
+  // zero BSS
+  volatile uint32_t* bss_start = (uint32_t*)(myhdr->bss_start + stacktop);
+  for (int i = 0; i < (myhdr->bss_size / (int)sizeof(uint32_t)); i++) {
+    bss_start[i] = 0;
+  }
+
+  // TODO: copy data section
+
+  main();
+  while(1) {
+    yield();
+  }
 }
 
