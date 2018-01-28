fc6dab51e2fafcf5e7ac92f4a47733f508c2b69a
niklasad1 <niklasadolfsson1@gmail.com>
2018-01-28 15:03:45 +0100

    Refactoring
    * Moved implementation to cpp files
    * Added "ifdef guards" (pragma)
    * More cppisch code
    * Still set/unordered map doesn't work

--- userland/examples/ble_passive_scanning/advertisement_list.h
+++ userland/examples/ble_passive_scanning/advertisement_list.h
@@ -1,56 +1,24 @@
+#pragma once
+
+#include <forward_list>
+#include "advertisement.h"
+
 // this is left outside the class because it doesn't work
 // code-generation bug?!
 const int MAX_SIZE = 10;
 
-struct AdvertisementList {
-  int currSize;
-  std::forward_list<Advertisement> list;
+class AdvertisementList {
+  private:
+    int currentSize_;
+    std::forward_list<Advertisement> list_;
+    bool containsDevice(const Advertisement& advertisement) const;
+  
+  public:
+    // Constructor
+    AdvertisementList();
 
-  AdvertisementList()
-      : currSize(0)
-  {
-    list.resize(MAX_SIZE);
-  }
-
-  bool add(const Advertisement& advertisement)
-  {
-    if (currSize < MAX_SIZE) {
-      list.push_front(advertisement);
-      currSize += 1;
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  bool containsDevice(const Advertisement& advertisement)
-  {
-    for (auto it = list.begin(); it != list.end(); ++it) {
-      if (it->device_detected(advertisement)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  bool newData(const Advertisement& advertisement)
-  {
-    for (auto it = list.begin(); it != list.end(); ++it) {
-      if (*it != advertisement) {
-         list.remove(*it);
-         list.push_front(advertisement);
-         return true;
-      }
-    }
-    return false;
-  }
-
-  void printList()
-  {
-    printf("--------------------------LIST-------------------------\r\n\r\n");
-    for (auto it = list.begin(); it != list.end(); ++it) {
-      it->print();
-    }
-    printf("--------------------------END---------------------------\r\n\r\n");
-  }
+    // Methods
+    bool tryAdd(const Advertisement& advertisement);
+    bool tryUpdateData(const Advertisement& advertisement);
+    void printList() const;
 };
--- userland/examples/ble_passive_scanning/main.cpp
+++ userland/examples/ble_passive_scanning/main.cpp
@@ -1,17 +1,12 @@
-#include <cstring>
-#include <forward_list>
-#include <simple_ble.h>
-#include <stdio.h>
-#include <string.h>
-
 #include "advertisement.h"
 #include "advertisement_list.h"
+#include <simple_ble.h>
+#include <stdio.h>
 
 /*
  * BLE Demo Application
  * Passive scanner for Bluetooth Low Energy advertisements
  */
-
 
 const int BUF_SIZE = 39;
 static unsigned char scan[BUF_SIZE];
@@ -22,19 +17,15 @@ static void callback(int result, int len, __attribute__((unused)) int unused2,
 {
   if (result == TOCK_SUCCESS) {
     if (Advertisement::validAdvertisement(scan, len)) {
-      Advertisement adv(scan, len);
+      Advertisement advertisement(scan, len);
 
-      // new device detected
-      if (!list.containsDevice(adv)) {
-        if (list.add(adv)) {
-          list.printList();
-        }
+      if (list.tryAdd(advertisement)) {
+        list.printList();
       }
-
       // FIXME: add this to get dynamic behavior i.e, update every time new advertisement is detected
       // but might it fill the print buffer, use at your own risk
-      // else {
-      //   list.newData(adv);
+      // else if (list.tryUpdateData(advertisement)) {
+      //   list.printList();
       // }
     }
   }
@@ -42,13 +33,13 @@ static void callback(int result, int len, __attribute__((unused)) int unused2,
 
 int main(void)
 {
-  printf("\rBLE Passive Scanner\r\n");
+  printf("[Tutorial] BLE Passive Scanner\r\n");
 
-  // using the pre-configured adv interval
+  // using the pre-configured advertisement interval
   int err = ble_start_passive_scan(scan, BUF_SIZE, callback);
 
   if (err < TOCK_SUCCESS) {
-    printf("ble_start_passive_scan_wip, error: %s\r\n", tock_strerror(err));
+    printf("ble_start_passive_scan, error: %s\r\n", tock_strerror(err));
   }
   return 0;
 }
