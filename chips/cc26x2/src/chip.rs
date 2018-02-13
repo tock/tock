37bafbdcf905d1d26a0eec775cd26c3a3c3cf177
sebastian <me@sebastianlund.se>
2018-02-13 10:14:31 +0100

    cc26x2: specify mpu in chip

--- chips/cc26x2/src/chip.rs
+++ chips/cc26x2/src/chip.rs
@@ -4,14 +4,14 @@ use kernel;
 use cc26xx::peripheral_interrupts::*;
 
 pub struct Cc26X2 {
-    mpu: (),
+    mpu: cortexm4::mpu::MPU,
     systick: cortexm4::systick::SysTick,
 }
 
 impl Cc26X2 {
     pub unsafe fn new() -> Cc26X2 {
         Cc26X2 {
-            mpu: (),
+            mpu: cortexm4::mpu::MPU::new(),
             // The systick clocks with 48MHz by default
             systick: cortexm4::systick::SysTick::new_with_calibration(48 * 1000000),
         }
@@ -19,7 +19,7 @@ impl Cc26X2 {
 }
 
 impl kernel::Chip for Cc26X2 {
-    type MPU = ();
+    type MPU = cortexm4::mpu::MPU;
     type SysTick = cortexm4::systick::SysTick;
 
     fn mpu(&self) -> &Self::MPU {
