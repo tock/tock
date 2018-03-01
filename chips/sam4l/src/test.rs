53dc6ffbd453898c536326d7fa8f7b9ecce6d9d4
Pat Pannuto <pat.pannuto@gmail.com>
2018-03-01 01:44:34 -0800

    cleanup example a bit

--- chips/sam4l/src/test.rs
+++ chips/sam4l/src/test.rs
@@ -1,81 +1,41 @@
 use kernel::common::VolatileCell;
 use kernel::{MMIOInterface, MMIOManager, NoClockControl};
 
-
-
-/*
-// NON GENERIC
-struct TESTRegisterManager <'a> {
-    registers: &'a TESTRegisters,
-    clock: &'a ClockInterface<PlatformClockType=pm::Clock>,
-}
-
-impl<'a> TESTRegisterManager <'a> {
-    fn new (hw: &'a TESTHw) -> TESTRegisterManager <'a> {
-        let clock = &hw.clock;
-        // If clock isn't enabled, lets enable it
-        if clock.is_enabled() == false {
-            debug!("TEST: Master clock on");
-            clock.enable();
-        }
-        TESTRegisterManager {
-            registers: unsafe { &*hw.registers },
-            clock: clock,
-        }
-    }
-}
-
-impl<'a> Drop for TESTRegisterManager <'a> {
-    fn drop(&mut self) {
-        let mask = self.registers.interrupt_mask.get();
-        if mask == 0 {
-            debug!("TEST: Master clock off");
-            self.clock.disable();
-        }
-        else {
-            debug!("TEST: Master clock left on");
-        }
-    }
-}
-/////////////////////////
-*/
-
-
-
-///// FAKE PERIPHERAL
-#[repr(C, packed)]
+/// The MMIO Structure
+#[repr(C)]
 #[allow(dead_code)]
-pub struct TESTRegisters {
+pub struct TestRegisters {
     control: VolatileCell<u32>,
     interrupt_mask: VolatileCell<u32>,
 }
 
+/// The Tock object that holds all information for this peripheral
 #[derive(NoClockControlMMIOHardware)]
-pub struct TESTHw {
-    registers: *mut TESTRegisters,
+pub struct TestHw {
+    registers: *mut TestRegisters,
 }
 
-impl TESTHw {
-    const fn new(base_addr: *mut TESTRegisters) -> TESTHw {
-        TESTHw { registers: base_addr as *mut TESTRegisters }
-    }
+/// Teaching the kernel how to create TestRegisters
+impl MMIOInterface<NoClockControl> for TestHw {
+    type MMIORegisterType = TestRegisters;
 
-    pub fn do_thing(&self) {
-        //let regs_manager = &TESTRegisterManager::new(&self); // use of non-gen
-        //regs_manager.registers.control.get();
-        let rm2 = &MMIOManager::new(self);                   // use of generic
-        rm2.registers.control.get();
-    }
-}
-///////////////////////////////
-
-impl MMIOInterface<NoClockControl> for TESTHw {
-    type MMIORegisterType = TESTRegisters;
-
-    fn get_hardware_address(&self) -> *mut TESTRegisters {
+    fn get_hardware_address(&self) -> *mut TestRegisters {
         self.registers
     }
 }
 
-const TEST_BASE_ADDR: *mut TESTRegisters = 0x40001000 as *mut TESTRegisters;
-pub static mut TEST0: TESTHw = TESTHw::new(TEST_BASE_ADDR);
+/// Mapping to actual hardware instance(s)
+const TEST_BASE_ADDR: *mut TestRegisters = 0x40001000 as *mut TestRegisters;
+pub static mut TEST0: TestHw = TestHw::new(TEST_BASE_ADDR);
+
+/// Methods this peripheral exports to the rest of the kernel
+impl TestHw {
+    const fn new(base_addr: *mut TestRegisters) -> TestHw {
+        TestHw { registers: base_addr as *mut TestRegisters }
+    }
+
+    pub fn do_thing(&self) {
+        let regs_manager = &MMIOManager::new(self);
+        regs_manager.registers.control.get();
+    }
+}
