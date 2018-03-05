6d3250277529a826435d901e05b743e99c644ebc
sebastian <me@sebastianlund.se>
2018-03-05 10:09:31 +0100

    launchxl: add blinking leds to panic

--- boards/launchxl/src/io.rs
+++ boards/launchxl/src/io.rs
@@ -1,4 +1,6 @@
 use core::fmt::Arguments;
+use kernel::hil::gpio::Pin;
+use cc26xx;
 
 #[cfg(not(test))]
 #[lang = "panic_fmt"]
@@ -8,5 +10,27 @@ pub unsafe extern "C" fn rust_begin_unwind(
     _file: &'static str,
     _line: usize,
 ) -> ! {
-    loop {}
+    let led0 = &cc26xx::gpio::PORT[6]; // Red led
+    let led1 = &cc26xx::gpio::PORT[7]; // Green led
+
+    led0.make_output();
+    led1.make_output();
+    loop {
+        for _ in 0..1000000 {
+            led0.clear();
+            led1.clear();
+        }
+        for _ in 0..100000 {
+            led0.set();
+            led1.set();
+        }
+        for _ in 0..1000000 {
+            led0.clear();
+            led1.clear();
+        }
+        for _ in 0..500000 {
+            led0.set();
+            led1.set();
+        }
+    }
 }
