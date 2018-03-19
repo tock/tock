906e3f3122c678d547a41ddb31aa6c64edffb99a
Brad Campbell <bradjc5@gmail.com>
2018-03-19 00:06:39 -0400

    Revert "sam4l: update usart to use RegManager"
    This reverts commit ee68206bc6889f651964a4bd2d7479f4eb441cf0.

--- chips/sam4l/src/dma.rs
+++ chips/sam4l/src/dma.rs
@@ -257,10 +257,6 @@ impl DMAChannel {
         }
     }
 
-    pub fn is_enabled(&self) -> bool {
-        self.enabled.get()
-    }
-
     pub fn handle_interrupt(&mut self) {
         let registers: &DMARegisters = unsafe { &*self.registers };
         registers
