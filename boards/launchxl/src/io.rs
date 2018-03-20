d5c9134d5d4984cce00d0c417d5634aa6355ea1a
Filip <filip.nilsson@blixtmail.se>
2018-03-20 10:10:31 +0100

    launchxl: Enabled debug and print macros

--- boards/launchxl/src/io.rs
+++ boards/launchxl/src/io.rs
@@ -2,15 +2,49 @@ use cc26xx;
 use core::fmt::{Arguments, Write};
 use kernel::debug;
 use kernel::hil::led;
+use kernel::hil::uart::{self, UART};
 
-struct Writer {}
+pub struct Writer {
+    initialized: bool,
+}
 
-static mut WRITER: Writer = Writer {};
+pub static mut WRITER: Writer = Writer { initialized: false };
 
 impl Write for Writer {
-    fn write_str(&mut self, _s: &str) -> ::core::fmt::Result {
+    fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
+        let uart = unsafe { &mut cc26xx::uart::UART0 };
+        if !self.initialized {
+            self.initialized = true;
+            uart.init(uart::UARTParams {
+                baud_rate: 115200,
+                stop_bits: uart::StopBits::One,
+                parity: uart::Parity::None,
+                hw_flow_control: false,
+            });
+        }
+        for c in s.bytes() {
+            uart.send_byte(c);
+            while !uart.tx_ready() {}
+        }
         Ok(())
     }
+}
+
+#[macro_export]
+macro_rules! print {
+        ($($arg:tt)*) => (
+            {
+                use core::fmt::write;
+                let writer = &mut $crate::io::WRITER;
+                let _ = write(writer, format_args!($($arg)*));
+            }
+        );
+}
+
+#[macro_export]
+macro_rules! println {
+        ($fmt:expr) => (print!(concat!($fmt, "\r\n")));
+            ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\r\n"), $($arg)*));
 }
 
 #[cfg(not(test))]
