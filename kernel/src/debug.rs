719f8b72ea77f3191ada8ec6ec5cc4bdbe5042ea
Paul Crews <ptcrews@stanford.edu>
2018-04-25 16:40:49 -0700

    Reduced size of debug buffer to standard default

--- kernel/src/debug.rs
+++ kernel/src/debug.rs
@@ -185,7 +185,7 @@ macro_rules! debug_gpio {
 // debug! and debug_verbose! support
 
 pub const APPID_IDX: usize = 255;
-const BUF_SIZE: usize = 20000;
+const BUF_SIZE: usize = 1024;
 
 pub struct DebugWriter {
     driver: Option<&'static Driver>,
