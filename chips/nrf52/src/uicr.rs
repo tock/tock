c90137d04591bb8f6d5fa0b3a711a324717cbd29
Brad Campbell <bradjc5@gmail.com>
2018-05-26 19:05:19 -0400

    nrf52: uicr: remove pub

--- chips/nrf52/src/uicr.rs
+++ chips/nrf52/src/uicr.rs
@@ -1,28 +1,30 @@
 //! User information configuration registers
-//! Minimal implementation to support activation of the reset button on nRF52-DK
+//!
+//! Minimal implementation to support activation of the reset button on
+//! nRF52-DK.
 
 use kernel::common::regs::ReadWrite;
 
 const UICR_BASE: usize = 0x10001200;
 
 #[repr(C)]
-pub struct UicrRegisters {
+struct UicrRegisters {
     /// Mapping of the nRESET function (see POWER chapter for details)
-    /// Address: 0x200 - 0x204
-    pub pselreset0: ReadWrite<u32, Pselreset::Register>,
+    /// - Address: 0x200 - 0x204
+    pselreset0: ReadWrite<u32, Pselreset::Register>,
     /// Mapping of the nRESET function (see POWER chapter for details)
-    /// Address: 0x204 - 0x208
-    pub pselreset1: ReadWrite<u32, Pselreset::Register>,
+    /// - Address: 0x204 - 0x208
+    pselreset1: ReadWrite<u32, Pselreset::Register>,
     /// Access Port protection
-    /// Address: 0x208 - 0x20c
-    pub approtect: ReadWrite<u32, ApProtect::Register>,
+    /// - Address: 0x208 - 0x20c
+    approtect: ReadWrite<u32, ApProtect::Register>,
     /// Setting of pins dedicated to NFC functionality: NFC antenna or GPIO
-    /// Address: 0x20c - 0x210
-    pub nfcpins: ReadWrite<u32, NfcPins::Register>,
+    /// - Address: 0x20c - 0x210
+    nfcpins: ReadWrite<u32, NfcPins::Register>,
 }
 
 register_bitfields! [u32,
-    /// Task register 
+    /// Task register
     Pselreset [
         /// GPIO number P0.n onto which Reset is exposed
         PIN OFFSET(0) NUMBITS(5) [],
