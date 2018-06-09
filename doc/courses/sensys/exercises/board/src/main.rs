1203437bff05667bb0636dc9ab69e1daca13c2a2
Brad Campbell <bradjc5@gmail.com>
2018-06-08 23:46:54 -0400

    kernel: move main
    main was in lib.rs, which was a little tricky to find. Also do_process
    in sched.rs had to be public, but is only called in one place. This
    merges the two, so all scheduling and main loop code is in the same
    place.
    
    It also renames the `main()` function to `kernel_loop()` to better
    reflect what it actually is. `main()` implies the start of a program
    with the normal argc/argv arguments, and this is neither.

--- doc/courses/sensys/exercises/board/src/main.rs
+++ doc/courses/sensys/exercises/board/src/main.rs
@@ -486,5 +486,5 @@ pub unsafe fn reset_handler() {
     );
 
     // Begin kernel main loop
-    kernel::main(&hail, &mut chip, &mut PROCESSES, Some(&hail.ipc));
+    kernel::kernel_loop(&hail, &mut chip, &mut PROCESSES, Some(&hail.ipc));
 }
