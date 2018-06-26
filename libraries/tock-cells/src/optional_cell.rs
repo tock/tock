3c08db34cff6e1d84e4ebeab1bfe60b192070953
Brad Campbell <bradjc5@gmail.com>
2018-06-26 12:23:42 -0400

    cells: add more common functions to OptionalCell

--- libraries/tock-cells/src/optional_cell.rs
+++ libraries/tock-cells/src/optional_cell.rs
@@ -24,6 +24,16 @@ impl<T: Copy> OptionalCell<T> {
         }
     }
 
+    /// Check if the cell is None.
+    pub fn is_none(&self) -> bool {
+        self.value.get().is_none()
+    }
+
+    /// Check if the cell contains something.
+    pub fn is_some(&self) -> bool {
+        self.value.get().is_some()
+    }
+
     /// Update the stored value.
     pub fn set(&self, val: T) {
         self.value.set(Some(val));
@@ -34,11 +44,25 @@ impl<T: Copy> OptionalCell<T> {
         self.value.set(None);
     }
 
+    /// Return the contained value and replace it with None.
+    pub fn take(&self) -> Option<T> {
+        self.value.take()
+    }
+
     /// Call a closure on the value if the value exists.
     pub fn map<F, R>(&self, closure: F) -> Option<R>
     where
         F: FnOnce(&mut T) -> R,
     {
         self.value.get().map(|mut val| closure(&mut val))
+    }
+
+    /// Call a closure on the value if the value exists, or return value
+    /// instead.
+    pub fn map_or<F, R>(&self, value: R, closure: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        self.value.get().map_or(value, |mut val| closure(&mut val))
     }
 }
