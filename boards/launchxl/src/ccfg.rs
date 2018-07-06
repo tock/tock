7323fa4cf3a99b57ba2a72bc2a5124ce1ac937ad
Amit Aryeh Levy <amit@amitlevy.com>
2018-07-06 16:34:57 -0400

    Make launchxl ccfg a separate binary
    We don't want to flash CCFG data for the launchxl as part of the kernel.
    It's dangerous, it makes the flah binary huge, and it makes it harder to
    make board-specific changes.
    
    Instead, pulls the ccfg into a separately compiled binary for the
    launchxl board with it's own Makefile target to flash it to the ccfg
    section. Other boards should do other board-specific things for CCFG
    (including potentially leaving it untouched).

---
+++ boards/launchxl/src/ccfg.rs
@@ -0,0 +1,39 @@
+#![no_std]
+#![no_main]
+#![feature(used, panic_implementation)]
+//! CCFG - Customer Configuration
+//!
+//! For details see p. 710 in the cc2650 technical reference manual.
+//!
+//! Currently setup to use the default settings.
+
+#[used]
+#[link_section = ".init"]
+pub static CCFG_CONF: [u32; 22] = [
+    0x01800000,
+    0xFF820010,
+    0x0058FFFD,
+    0xF3FFFF3A,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0x00FFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFF00,
+    0xFFC5C5C5,
+    0xFFC5C5C5,
+    0x00000000,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+];
+
+#[panic_implementation]
+#[no_mangle]
+pub unsafe extern "C" fn panic_fmt(_pi: &core::panic::PanicInfo) -> ! { loop {} }
