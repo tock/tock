863c58d1f7dcb60610ae76801b694015d30f1f3a
Brad Campbell <bradjc5@gmail.com>
2018-07-14 18:21:22 -0400

    update nightly to 2018-08-16
    Minor changes overall:
    
    - API to llvm-objcopy changed
    - Appended `rust-` to lld
    - llvm-tools is now llvm-tools-preview
    - moved where the tools end up
    - no more unique feature

--- chips/nrf51/src/lib.rs
+++ chips/nrf51/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(asm, concat_idents, const_fn, const_cell_new, try_from, used)]
+#![feature(asm, concat_idents, const_fn, try_from, used)]
 #![no_std]
 #![crate_name = "nrf51"]
 #![crate_type = "rlib"]
