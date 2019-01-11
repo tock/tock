ebb08054277ee7ddd561c54975f4fbf75b079534
Justin Restivo <jrestivo@mit.edu>
2019-01-11 07:42:13 -0500

    now compiles with nightly-2018-11-15

--- boards/hifive1/src/main.rs
+++ boards/hifive1/src/main.rs
@@ -4,7 +4,7 @@
 
 #![no_std]
 #![no_main]
-#![feature(panic_implementation, asm)]
+#![feature(asm)]
 
 extern crate capsules;
 #[allow(unused_imports)]
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -16,7 +16,10 @@
 extern crate tock_cells;
 extern crate tock_registers;
 
+//#[macro_use]
+//pub use tock_registers::macros;
 pub use tock_registers::{register_bitfields, register_bitmasks};
+
 
 pub mod capabilities;
 #[macro_use]
