14b577f3eaf636137f434499fbda65b8f9cc6bbf
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2019-04-09 17:45:42 +0200

    fix(bump rustc): nightly 2019-04-11

--- tools/netlify-build.sh
+++ tools/netlify-build.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 set -x
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2019-03-04
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2019-04-11
 
 export PATH="$PATH:$HOME/.cargo/bin"
 
