5b897b0622810f3b8ff03f7e119925ec6dd5109b
Alistair Francis <alistair.francis@wdc.com>
2019-10-15 16:12:15 -0700

    chips/e310x: Disable the interrupt source when required
    When we receive an interrupt let's quickly disable that interrupt source
    to avoid us getting interrupted again by the same interrupt until we
    have serviced that interrupt.
    
    This means that the interrupt source must be re-enabled when required.
    This fixes illegal instruction faults when running with a timer.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- chips/e310x/src/chip.rs
+++ chips/e310x/src/chip.rs
@@ -3,6 +3,7 @@
 use kernel;
 use kernel::debug;
 use rv32i;
+use rv32i::csr;
 use rv32i::machine_timer;
 use rv32i::plic;
 
@@ -174,6 +175,49 @@ pub unsafe fn handle_trap() {
     }
 }
 
+pub fn disable_interrupt_cause() {
+    let cause = rv32i::csr::CSR.mcause.extract();
+
+    match rv32i::csr::mcause::McauseHelpers::cause(&cause) {
+        rv32i::csr::mcause::Trap::Interrupt(interrupt) => match interrupt {
+            rv32i::csr::mcause::Interrupt::UserSoft => {
+                csr::CSR.mie.modify(csr::mie::mie::usoft::CLEAR);
+            }
+            rv32i::csr::mcause::Interrupt::SupervisorSoft => {
+                csr::CSR.mie.modify(csr::mie::mie::ssoft::CLEAR);
+            }
+            rv32i::csr::mcause::Interrupt::MachineSoft => {
+                csr::CSR.mie.modify(csr::mie::mie::msoft::CLEAR);
+            }
+
+            rv32i::csr::mcause::Interrupt::UserTimer => {
+                csr::CSR.mie.modify(csr::mie::mie::utimer::CLEAR);
+            }
+            rv32i::csr::mcause::Interrupt::SupervisorTimer => {
+                csr::CSR.mie.modify(csr::mie::mie::stimer::CLEAR);
+            }
+            rv32i::csr::mcause::Interrupt::MachineTimer => {
+                csr::CSR.mie.modify(csr::mie::mie::mtimer::CLEAR);
+            }
+
+            rv32i::csr::mcause::Interrupt::UserExternal => {
+                csr::CSR.mie.modify(csr::mie::mie::uext::CLEAR);
+            }
+            rv32i::csr::mcause::Interrupt::SupervisorExternal => {
+                csr::CSR.mie.modify(csr::mie::mie::sext::CLEAR);
+            }
+            rv32i::csr::mcause::Interrupt::MachineExternal => {
+                csr::CSR.mie.modify(csr::mie::mie::mext::CLEAR);
+            }
+
+            rv32i::csr::mcause::Interrupt::Unknown => {
+                debug!("interrupt of unknown cause");
+            }
+        },
+        rv32i::csr::mcause::Trap::Exception(_exception) => (),
+    }
+}
+
 /// Trap handler for board/chip specific code.
 ///
 /// For the e310 this gets called when an interrupt occurs while the chip is
@@ -181,6 +225,7 @@ pub unsafe fn handle_trap() {
 /// disable it.
 #[export_name = "_start_trap_rust"]
 pub unsafe extern "C" fn start_trap_rust() {
+    disable_interrupt_cause();
     handle_trap();
 }
 
@@ -189,7 +234,5 @@ pub unsafe extern "C" fn start_trap_rust() {
 /// interrupt that fired so that it does not trigger again.
 #[export_name = "_disable_interrupt_trap_handler"]
 pub extern "C" fn disable_interrupt_trap_handler(_mcause: u32) {
-    unsafe {
-        handle_trap();
-    }
+    disable_interrupt_cause();
 }
