13b66bf8c5e33990f5b052d7c287cceb33bfa730
DieracDelta <jrestivo@mit.edu>
2019-10-31 07:51:35 -0400

    Add hifive1 support for user application
    This allows for syscalls on the hifive1 board to not panic, and the hello_world program to run successfully.

--- arch/rv32i/src/syscall.rs
+++ arch/rv32i/src/syscall.rs
@@ -318,6 +318,12 @@ impl kernel::syscall::UserspaceKernelBoundary for SysCall {
           li   t1, 8          // 8 is the index of ECALL from U mode.
           beq  t0, t1, _ecall // Check if we did an ECALL and handle it
                               // correctly.
+            
+        _check_ecall_m_mode:
+          li   t1, 11          // 11 is the index of ECALL from M mode.
+          beq  t0, t1, _ecall  // analagous to _check_ecall_umode but included to support hifive1 board
+                              
+
 
         _check_exception:
           li   $0, 2          // If we get here, the only other option is an
