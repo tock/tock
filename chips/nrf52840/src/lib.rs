59a29b34daee7d4a7d9b36de853aea23c0c13348
Guillaume Endignoux <guillaumee@google.com>
2019-11-23 01:05:57 +0100

    Fix typo.

--- chips/nrf52840/src/lib.rs
+++ chips/nrf52840/src/lib.rs
@@ -2,6 +2,6 @@
 
 pub use nrf52::{
     adc, aes, ble_radio, chip, clock, constants, crt1, ficr, i2c, ieee802154_radio, init, nvmc,
-    peripheral_interrupts, pinmux, ppi, pwm, pwm, rtc, spi, temperature, timer, trng, uart, uicr,
+    peripheral_interrupts, pinmux, ppi, pwm, rtc, spi, temperature, timer, trng, uart, uicr,
 };
 pub mod gpio;
