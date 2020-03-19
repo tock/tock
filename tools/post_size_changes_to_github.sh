b95a1a410943112ccb8c14184e09c58e37c9316c
Hudson Ayers <hayers@stanford.edu>
2020-03-19 08:33:08 -0700

    bug fix

--- tools/post_size_changes_to_github.sh
+++ tools/post_size_changes_to_github.sh
@@ -22,7 +22,7 @@ if [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
     for elf in $(find boards -maxdepth 8 | grep 'release' | egrep '\.elf$' | grep -v 'riscv'); do
         tmp=${elf#*release/}
         b=${tmp%.elf}
-        ${TRAVIS_BUILD_DIR}/tools/print_tock_memory_usage.py ${elf} | tee ${TRAVIS_BUILD_DIR}/current-benchmark-${b}
+        ${TRAVIS_BUILD_DIR}/tools/print_tock_memory_usage.py -s ${elf} | tee ${TRAVIS_BUILD_DIR}/current-benchmark-${b}
     done
 
     # The Travis environment variables behave like so:
@@ -50,7 +50,7 @@ if [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
     for elf in $(find boards -maxdepth 8 | grep 'release' | egrep '\.elf$' | grep -v 'riscv'); do
         tmp=${elf#*release/}
         b=${tmp%.elf}
-        ${TRAVIS_BUILD_DIR}/tools/print_tock_memory_usage.py ${elf} | tee ${TRAVIS_BUILD_DIR}/previous-benchmark-${b}
+        ${TRAVIS_BUILD_DIR}/tools/print_tock_memory_usage.py -s ${elf} | tee ${TRAVIS_BUILD_DIR}/previous-benchmark-${b}
     done
 
     # now calculate diff for each board, and post status to github for each non-0 diff
@@ -58,7 +58,7 @@ if [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
     for elf in $(find boards -maxdepth 8 | grep 'release' | egrep '\.elf$' | grep -v 'riscv'); do
         tmp=${elf#*release/}
         b=${tmp%.elf}
-        ${TRAVIS_BUILD_DIR}/tools/diff_memory_usage.py -s previous-benchmark-${b} current-benchmark-${b} size-diffs-${b}.txt ${b}
+        ${TRAVIS_BUILD_DIR}/tools/diff_memory_usage.py previous-benchmark-${b} current-benchmark-${b} size-diffs-${b}.txt ${b}
         if [ -s "size-diffs-${b}.txt" ]; then
             RES="$( grep -hs ^ size-diffs-${b}.txt )" #grep instead of cat to prevent errors on no match
             curl -X POST -H "Content-Type: application/json" --header "Authorization: token ${TRAVIS_GITHUB_TOKEN}" --data '{"state": "success", "context": "'"${b}"'", "description": "'"${RES}"'"}' https://api.github.com/repos/tock/tock/statuses/${TRAVIS_PULL_REQUEST_SHA}
