9f62088bf290bdd7fb77154947a9544a302f391b
Hudson Ayers <hayers@stanford.edu>
2020-04-23 14:54:00 -0700

    import cfg_if, use for stm IRQS

--- kernel/src/common/mod.rs
+++ kernel/src/common/mod.rs
@@ -15,6 +15,7 @@ pub mod registers {
     pub use tock_registers::{register_bitfields, register_structs};
 }
 
+pub mod cfg_if;
 pub mod deferred_call;
 pub mod dynamic_deferred_call;
 pub mod leasable_buffer;
