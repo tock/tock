5545e9bfb2555d314136dcddfd9064ff66f997b1
Hudson Ayers <hayers@stanford.edu>
2020-04-28 06:11:21 -0700

    address gendx review

--- boards/nucleo_f429zi/src/main.rs
+++ boards/nucleo_f429zi/src/main.rs
@@ -292,6 +292,18 @@ pub unsafe fn reset_handler() {
 
     // GPIO
     let gpio = GpioComponent::new(board_kernel).finalize(components::gpio_component_helper!(
+        // ADC Pins
+        // Enable below to use the ADC pins as GPIO
+        // stm32f429zi::gpio::PIN[0][3].as_ref().unwrap(), //A0
+        // stm32f429zi::gpio::PIN[2][0].as_ref().unwrap(), //A1
+        // stm32f429zi::gpio::PIN[2][3].as_ref().unwrap(), //A2
+        // stm32f429zi::gpio::PIN[5][3].as_ref().unwrap(), //A3
+        // stm32f429zi::gpio::PIN[5][5].as_ref().unwrap(), //A4
+        // stm32f429zi::gpio::PIN[5][10].as_ref().unwrap(), //A5
+        // stm32f429zi::gpio::PIN[1][1].as_ref().unwrap(), //A6
+        // stm32f429zi::gpio::PIN[2][2].as_ref().unwrap(), //A7
+        // stm32f429zi::gpio::PIN[5][4].as_ref().unwrap(), //A8
+
         // Arduino like RX/TX
         stm32f429zi::gpio::PIN[6][9].as_ref().unwrap(), //D0
         stm32f429zi::gpio::PIN[6][14].as_ref().unwrap(), //D1
@@ -372,18 +384,6 @@ pub unsafe fn reset_handler() {
         stm32f429zi::gpio::PIN[5][1].as_ref().unwrap(), //D69
         stm32f429zi::gpio::PIN[5][2].as_ref().unwrap(), //D70
         stm32f429zi::gpio::PIN[0][7].as_ref().unwrap()  //D71
-
-                                                        // ADC Pins
-                                                        // Enable the to use the ADC pins as GPIO
-                                                        // stm32f429zi::gpio::PIN[0][3].as_ref().unwrap(), //A0
-                                                        // stm32f429zi::gpio::PIN[2][0].as_ref().unwrap(), //A1
-                                                        // stm32f429zi::gpio::PIN[2][3].as_ref().unwrap(), //A2
-                                                        // stm32f429zi::gpio::PIN[5][3].as_ref().unwrap(), //A3
-                                                        // stm32f429zi::gpio::PIN[5][5].as_ref().unwrap(), //A4
-                                                        // stm32f429zi::gpio::PIN[5][10].as_ref().unwrap(), //A5
-                                                        // stm32f429zi::gpio::PIN[1][1].as_ref().unwrap(), //A6
-                                                        // stm32f429zi::gpio::PIN[2][2].as_ref().unwrap(), //A7
-                                                        // stm32f429zi::gpio::PIN[5][4].as_ref().unwrap()  //A8
     ));
 
     let nucleo_f429zi = NucleoF429ZI {
