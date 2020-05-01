8c44a74d981f7788c10a4c873c608d550781439f
Leon Schuermann <leon@is.currently.online>
2020-05-01 22:43:33 +0200

    Print message on no size difference

--- tools/github_actions_size_changes.sh
+++ tools/github_actions_size_changes.sh
@@ -6,6 +6,9 @@
 # This script only reports updates for boards whose size have changed as a result of the PR being
 # tested, and does not currently support analyzing size differences in RISC-V boards.
 
+UPSTREAM_REMOTE_NAME="${UPSTREAM_REMOTE_NAME:-origin}"
+GITHUB_BASE_REF="${GITHUB_BASE_REF:-master}"
+
 set -e
 
 # Bench the current commit that was pushed. Requires navigating back to build directory
@@ -16,9 +19,9 @@ for elf in $(find . -maxdepth 8 | grep 'release' | egrep '\.elf$' | grep -v 'ris
     ./tools/print_tock_memory_usage.py -s ${elf} > current-benchmark-${b}
 done
 
-git remote set-branches origin ${GITHUB_BASE_REF}  > /dev/null 2>&1
-git fetch --depth 1 origin ${GITHUB_BASE_REF} > /dev/null 2>&1
-git checkout ${GITHUB_BASE_REF} > /dev/null 2>&1
+git remote set-branches "$UPSTREAM_REMOTE_NAME" "$GITHUB_BASE_REF"  > /dev/null 2>&1
+git fetch --depth 1 "$UPSTREAM_REMOTE_NAME" "$GITHUB_BASE_REF" > /dev/null 2>&1
+git checkout "$UPSTREAM_REMOTE_NAME"/"$GITHUB_BASE_REF" > /dev/null 2>&1
 make allboards > /dev/null 2>&1
 
 # Find elfs compiled for release (for use in analyzing binaries in CI),
@@ -29,6 +32,8 @@ for elf in $(find . -maxdepth 8 | grep 'release' | egrep '\.elf$' | grep -v 'ris
     ./tools/print_tock_memory_usage.py -s ${elf} > previous-benchmark-${b}
 done
 
+DIFF_DETECTED=0
+
 # now calculate diff for each board, and post status to github for each non-0 diff
 for elf in $(find . -maxdepth 8 | grep 'release' | egrep '\.elf$' | grep -v 'riscv'); do
     tmp=${elf#*release/}
@@ -36,6 +41,7 @@ for elf in $(find . -maxdepth 8 | grep 'release' | egrep '\.elf$' | grep -v 'ris
     # Compute a summary suitable for GitHub.
     ./tools/diff_memory_usage.py previous-benchmark-${b} current-benchmark-${b} size-diffs-${b}.txt ${b}
     if [ -s "size-diffs-${b}.txt" ]; then
+	DIFF_DETECTED=1
         RES="$( grep -hs ^ size-diffs-${b}.txt )" #grep instead of cat to prevent errors on no match
         echo "${b}: ${RES}"
     fi
@@ -43,3 +49,7 @@ for elf in $(find . -maxdepth 8 | grep 'release' | egrep '\.elf$' | grep -v 'ris
     # understand where the size differences come from.
     git diff --no-index previous-benchmark-${b} current-benchmark-${b} || true #Supress exit code
 done
+
+if [ $DIFF_DETECTED -eq 0 ]; then
+    echo "-> No size difference on any board detected"
+fi
