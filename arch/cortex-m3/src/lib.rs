3c29f9e7d9911263a1d1bf109165c96a3f5fdb33
cody <sebastian.kotulla@gmx.de>
2020-05-20 10:48:40 +0200

    Add comment for setting pending bit and apply fix to Cortex-M4, too.

--- arch/cortex-m3/src/lib.rs
+++ arch/cortex-m3/src/lib.rs
@@ -119,10 +119,14 @@ pub unsafe extern "C" fn generic_isr() {
      *  */
     str r0, [r3, r2, lsl #2]
 
+    /* The pending bit in ISPR might be reset by hardware for pulse interrupts
+     * at this point. So set it here again so the interrupt does not get lost
+     * in service_pending_interrupts()
+     * */
     /* r3 = &NVIC.ISPR */
     mov r3, #0xe200
     movt r3, #0xe000
-    /* set pending bit */
+    /* Set pending bit */
     str r0, [r3, r2, lsl #2]"
     : : : : "volatile" );
 }
