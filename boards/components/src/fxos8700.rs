b35219587dc4ceedd751df1b783ff596197cf08e
Vochescu Alexandru <alexvochescu@gmail.com>
2020-06-07 13:56:39 +0300

    Working tock
    (cherry picked from commit a84b4a69031b576979ef925e341e63b71feb4861)
    Change-Id: I2ca1b713ef2c1bd61aad3b91f8b08be6437cbeff

---
+++ boards/components/src/fxos8700.rs
@@ -0,0 +1,63 @@
+//! Components for the FXOS8700cq
+//!
+//! I2C Interface
+//!
+//! Usage
+//! -----
+//! ```rust
+//! let fxos8700 = components::fxos8700::Fxos8700Component::new(mux_i2c, PinId::AdB1_00.get_pin().as_ref().unwrap())
+//!    .finalize(());
+//!
+//! let ninedof = components::ninedof::NineDofComponent::new(board_kernel)
+//!    .finalize(components::ninedof_component_helper!(fxos8700));
+//! ```
+
+// Based on the component written for sam4l by:
+// Author: Philip Levis <pal@cs.stanford.edu>
+// Last modified: 6/03/2020
+
+#![allow(dead_code)] // Components are intended to be conditionally included
+#![allow(unused_imports)] // I2CDevice
+
+use capsules::fxos8700cq;
+use capsules::virtual_i2c::{I2CDevice, MuxI2C};
+use kernel::capabilities;
+use kernel::component::Component;
+use kernel::create_capability;
+use kernel::hil;
+use kernel::hil::gpio;
+use kernel::static_init;
+use kernel::Grant;
+
+pub struct Fxos8700Component {
+    i2c_mux: &'static MuxI2C<'static>,
+    gpio: &'static dyn gpio::InterruptPin,
+}
+
+impl Fxos8700Component {
+    pub fn new(
+        i2c: &'static MuxI2C<'static>,
+        gpio: &'static dyn hil::gpio::InterruptPin,
+    ) -> Fxos8700Component {
+        Fxos8700Component {
+            i2c_mux: i2c,
+            gpio: gpio,
+        }
+    }
+}
+
+impl Component for Fxos8700Component {
+    type StaticInput = ();
+    type Output = &'static fxos8700cq::Fxos8700cq<'static>;
+
+    unsafe fn finalize(self, _s: Self::StaticInput) -> Self::Output {
+        let fxos8700_i2c = static_init!(I2CDevice, I2CDevice::new(self.i2c_mux, 0x1f));
+        let fxos8700 = static_init!(
+            fxos8700cq::Fxos8700cq<'static>,
+            fxos8700cq::Fxos8700cq::new(fxos8700_i2c, self.gpio, &mut fxos8700cq::BUF)
+        );
+        fxos8700_i2c.set_client(fxos8700);
+        self.gpio.set_client(fxos8700);
+        fxos8700
+    }
+}
