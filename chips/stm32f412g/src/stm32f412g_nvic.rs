0837f53a8850919b4a5a493dd06dfd518f9df248
Alexandru Radovici <msg4alex@gmail.com>
2020-06-08 14:01:12 +0300

    added interrupts
    BUG=none
    TEST=make
    
    (cherry picked from commit b60fa1fde55241a18edaa603b87819996a19a026)
    Change-Id: I7071362002121e6188a5c1121b6adab9a6726318

--- chips/stm32f412g/src/stm32f412g_nvic.rs
+++ chips/stm32f412g/src/stm32f412g_nvic.rs
@@ -1,1 +1,9 @@
 //! Named constants for NVIC ids specific to this chip
+
+pub const RNG: u32 = 80;
+pub const FPU: u32 = 81;
+pub const SPI4: u32 = 84;
+pub const SPI5: u32 = 85;
+pub const SQPI: u32 = 92;
+pub const I2CFMP1_EV: u32 = 95;
+pub const I2CFMP1_ER: u32 = 96;
