92808fbbea85bfe6bade70f9b0eb7742cf542253
Brad Campbell <bradjc5@gmail.com>
2020-06-17 13:17:14 -0400

    usb: cdc: wait for usb reset to send data
    BUG=none
    TEST=make
    
    (cherry picked from commit 8db841b3f4c25d1ac227b0de5dbf3fea63d59907)
    Change-Id: Idd52798ae9b446ac3fce571d55ba14cdfb616d5a

--- capsules/src/usb/cdc.rs
+++ capsules/src/usb/cdc.rs
@@ -82,9 +82,8 @@ pub struct CdcAcm<'a, U: 'a> {
     /// 64 byte buffers for each endpoint.
     buffers: [Buffer64; N_ENDPOINTS],
 
-    /// Current state of the CDC driver. This helps us track if a CDC client is
-    /// connected and listening or not.
-    state: Cell<State>,
+    /// Flag to track if the underlying USB stack is initialized and ready.
+    initialized: Cell<bool>,
 
     /// A holder reference for the TX buffer we are transmitting from.
     tx_buffer: TakeCell<'static, [u8]>,
@@ -214,7 +213,7 @@ impl<'a, U: hil::usb::UsbController<'a>> CdcAcm<'a, U> {
                 Buffer64::default(),
                 Buffer64::default(),
             ],
-            state: Cell::new(State::Disabled),
+            initialized: Cell::new(false),
             tx_buffer: TakeCell::empty(),
             tx_len: Cell::new(0),
             tx_offset: Cell::new(0),
@@ -262,8 +261,13 @@ impl<'a, U: hil::usb::UsbController<'a>> hil::usb::Client<'a> for CdcAcm<'a, U> 
     }
 
     fn bus_reset(&'a self) {
-        // We take a bus reset to mean the enumeration has finished.
-        self.state.set(State::Enumerated);
+        // This signals enumeration has finished, and at this point we can ask
+        // for IN transfers if needed.
+        self.initialized.set(true);
+
+        if self.tx_buffer.is_some() {
+            self.controller().endpoint_resume_in(ENDPOINT_IN_NUM);
+        }
     }
 
     /// Handle a Control Setup transaction.
@@ -317,13 +321,6 @@ impl<'a, U: hil::usb::UsbController<'a>> hil::usb::Client<'a> for CdcAcm<'a, U> 
 
     /// Handle a Control Out transaction
     fn ctrl_out(&'a self, endpoint: usize, packet_bytes: u32) -> hil::usb::CtrlOutResult {
-        // Hack to make sure we ask to send data if we have a buffer queued. We
-        // expect control out messages when the host actually connects via CDC,
-        // so we use this to generate the data IN request.
-        if self.tx_buffer.is_some() {
-            self.controller().endpoint_resume_in(ENDPOINT_IN_NUM);
-        }
-
         self.client_ctrl.ctrl_out(endpoint, packet_bytes)
     }
 
@@ -522,10 +519,10 @@ impl<'a, U: hil::usb::UsbController<'a>> uart::Transmit<'a> for CdcAcm<'a, U> {
             self.tx_offset.set(0);
             self.tx_buffer.replace(tx_buffer);
 
-            // Don't try to send if there is no CDC client connected.
-            if self.state.get() == State::Connected {
-                // Then signal to the lower layer that we are ready to do a TX
-                // by putting data in the IN endpoint.
+            // Don't try to send if the USB stack isn't initialized yet.
+            if self.initialized.get() {
+                // Then signal to the lower layer that we are ready to do a TX by
+                // putting data in the IN endpoint.
                 self.controller().endpoint_resume_in(ENDPOINT_IN_NUM);
             }
             (ReturnCode::SUCCESS, None)
