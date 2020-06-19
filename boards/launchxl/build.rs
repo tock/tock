860a7a24a950a743bbfb784676b32f984baf98f0
Pat Pannuto <pat.pannuto@gmail.com>
2020-06-19 14:42:36 -0400

    deprecate launchxl, cc26x2
    Moved to tock-archive
    
    BUG=none
    TEST=make
    
    (cherry picked from commit 10d3c55450f3b536dc3f7838cac2916d20afc59a)
    Change-Id: Ia452456b55717e8031f0971f2c451f5c2c11bf04

--- boards/launchxl/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=layout.ld");
-    println!("cargo:rerun-if-changed=chip_layout.ld");
-    println!("cargo:rerun-if-changed=../kernel_layout.ld");
-}
