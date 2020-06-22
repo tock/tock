9242f8f9c8e2c86753938f567cbabeec6cd9d65e
Alexandru Radovici <msg4alex@gmail.com>
2020-06-22 19:25:30 +0300

    added i2c and initial touch support

--- capsules/src/ft6206.rs
+++ capsules/src/ft6206.rs
@@ -10,80 +10,25 @@
 //! -----
 //!
 //! ```rust
-//! let mux_i2c = components::i2c::I2CMuxComponent::new(&stm32f3xx::i2c::I2C1)
+//! let mux_i2c = components::i2c::I2CMuxComponent::new(&stm32f4xx::i2c::I2C1)
 //!     .finalize(components::i2c_mux_component_helper!());
 //!
-//! let lsm303dlhc = components::lsm303dlhc::Lsm303dlhcI2CComponent::new()
-//!    .finalize(components::lsm303dlhc_i2c_component_helper!(mux_i2c));
-//!
-//! lsm303dlhc.configure(
-//!    lsm303dlhc::Lsm303dlhcAccelDataRate::DataRate25Hz,
-//!    false,
-//!    lsm303dlhc::Lsm303dlhcScale::Scale2G,
-//!    false,
-//!    true,
-//!    lsm303dlhc::Lsm303dlhcMagnetoDataRate::DataRate3_0Hz,
-//!    lsm303dlhc::Lsm303dlhcRange::Range4_7G,
-//!);
-//! ```
-//!
-//! NideDof Example
-//!
-//! ```rust
-//! let grant_cap = create_capability!(capabilities::MemoryAllocationCapability);
-//! let grant_ninedof = board_kernel.create_grant(&grant_cap);
-//!
-//! // use as primary NineDof Sensor
-//! let ninedof = static_init!(
-//!    capsules::ninedof::NineDof<'static>,
-//!    capsules::ninedof::NineDof::new(lsm303dlhc, grant_ninedof)
-//! );
-//!
-//! hil::sensors::NineDof::set_client(lsm303dlhc, ninedof);
-//!
-//! // use as secondary NineDof Sensor
-//! let lsm303dlhc_secondary = static_init!(
-//!    capsules::ninedof::NineDofNode<'static, &'static dyn hil::sensors::NineDof>,
-//!    capsules::ninedof::NineDofNode::new(lsm303dlhc)
-//! );
-//! ninedof.add_secondary_driver(lsm303dlhc_secondary);
-//! hil::sensors::NineDof::set_client(lsm303dlhc, ninedof);
-//! ```
-//!
-//! Temperature Example
-//!
-//! ```rust
-//! let grant_cap = create_capability!(capabilities::MemoryAllocationCapability);
-//! let grant_temp = board_kernel.create_grant(&grant_cap);
-//!
-//! lsm303dlhc.configure(
-//!    lsm303dlhc::Lsm303dlhcAccelDataRate::DataRate25Hz,
-//!    false,
-//!    lsm303dlhc::Lsm303dlhcScale::Scale2G,
-//!    false,
-//!    true,
-//!    lsm303dlhc::Lsm303dlhcMagnetoDataRate::DataRate3_0Hz,
-//!    lsm303dlhc::Lsm303dlhcRange::Range4_7G,
-//!);
-//! let temp = static_init!(
-//! capsules::temperature::TemperatureSensor<'static>,
-//!     capsules::temperature::TemperatureSensor::new(lsm303dlhc, grant_temperature));
-//! kernel::hil::sensors::TemperatureDriver::set_client(lsm303dlhc, temp);
-//! ```
+//! let ft6206 = components::ft6206::Ft6206Component::new(
+//!     stm32f412g::gpio::PinId::PG05.get_pin().as_ref().unwrap(),
+//! )
+//! .finalize(components::ft6206_i2c_component_helper!(mux_i2c));
 //!
 //! Author: Alexandru Radovici <msg4alex@gmail.com>
-//!
 
 #![allow(non_camel_case_types)]
 
 use core::cell::Cell;
 use enum_primitive::cast::FromPrimitive;
 use enum_primitive::enum_from_primitive;
-use kernel::common::cells::{OptionalCell, TakeCell};
-use kernel::debug;
+use kernel::common::cells::TakeCell;
 use kernel::hil::gpio;
 use kernel::hil::i2c::{self, Error};
-use kernel::{AppId, Callback, Driver, ReturnCode};
+use kernel::{AppId, Driver, ReturnCode};
 
 use crate::driver;
 
@@ -108,7 +53,7 @@ enum_from_primitive! {
 pub struct Ft6206<'a> {
     i2c: &'a dyn i2c::I2CDevice,
     interrupt_pin: &'a dyn gpio::InterruptPin,
-    callback: OptionalCell<Callback>,
+    // callback: OptionalCell<Callback>,
     state: Cell<State>,
     buffer: TakeCell<'static, [u8]>,
 }
@@ -124,7 +69,7 @@ impl<'a> Ft6206<'a> {
         Ft6206 {
             i2c: i2c,
             interrupt_pin: interrupt_pin,
-            callback: OptionalCell::empty(),
+            // callback: OptionalCell::empty(),
             state: Cell::new(State::Idle),
             buffer: TakeCell::new(buffer),
         }
@@ -141,9 +86,8 @@ impl<'a> Ft6206<'a> {
 }
 
 impl i2c::I2CClient for Ft6206<'_> {
-    fn command_complete(&self, buffer: &'static mut [u8], error: Error) {
+    fn command_complete(&self, buffer: &'static mut [u8], _error: Error) {
         self.state.set(State::Idle);
-        debug!("{:?}", buffer);
         self.buffer.replace(buffer);
         self.interrupt_pin
             .enable_interrupts(gpio::InterruptEdge::FallingEdge);
@@ -164,7 +108,7 @@ impl gpio::Client for Ft6206<'_> {
 }
 
 impl Driver for Ft6206<'_> {
-    fn command(&self, command_num: usize, data: usize, _: usize, _: AppId) -> ReturnCode {
+    fn command(&self, command_num: usize, _: usize, _: usize, _: AppId) -> ReturnCode {
         match command_num {
             // is driver present
             0 => ReturnCode::SUCCESS,
