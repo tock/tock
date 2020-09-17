8dc182797432f10250f27946a8a34312b1773ddc
JOE1994 <joseph942010@gmail.com>
2020-09-17 11:14:07 -0400

    'update_rust_version.sh' - bump to 2020-09-16

--- tools/netlify-build.sh
+++ tools/netlify-build.sh
@@ -11,7 +11,7 @@ set -e
 set -u
 set -x
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-06-03
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-09-16
 
 export PATH="$PATH:$HOME/.cargo/bin"
 
