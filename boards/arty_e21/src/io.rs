b1c598306710f0854f40bb110a6164347862650d
Hudson Ayers <hayers@stanford.edu>
2020-10-12 09:14:04 -0700

    port arty_e21 to new peripheral creation approach

--- boards/arty_e21/src/io.rs
+++ boards/arty_e21/src/io.rs
@@ -7,6 +7,7 @@ use kernel::debug::IoWrite;
 use kernel::hil::gpio;
 use kernel::hil::led;
 use rv32i;
+use sifive;
 
 use crate::CHIP;
 use crate::PROCESSES;
@@ -24,9 +25,7 @@ impl Write for Writer {
 
 impl IoWrite for Writer {
     fn write(&mut self, buf: &[u8]) {
-        unsafe {
-            arty_e21_chip::uart::UART0.transmit_sync(buf);
-        }
+        sifive::uart::Uart::new(arty_e21_chip::uart::UART0_BASE, 32_000_000).transmit_sync(buf);
     }
 }
 
@@ -36,15 +35,32 @@ impl IoWrite for Writer {
 #[panic_handler]
 pub unsafe extern "C" fn panic_fmt(pi: &PanicInfo) -> ! {
     // turn off the non panic leds, just in case
-    let led_green = &arty_e21_chip::gpio::PORT[1];
+    let led_green = &sifive::gpio::GpioPin::new(
+        arty_e21_chip::gpio::GPIO0_BASE,
+        sifive::gpio::pins::pin1,
+        sifive::gpio::pins::pin1::SET,
+        sifive::gpio::pins::pin1::CLEAR,
+    );
     gpio::Pin::make_output(led_green);
     gpio::Pin::clear(led_green);
 
-    let led_blue = &arty_e21_chip::gpio::PORT[0];
+    let led_blue = &sifive::gpio::GpioPin::new(
+        arty_e21_chip::gpio::GPIO0_BASE,
+        sifive::gpio::pins::pin0,
+        sifive::gpio::pins::pin0::SET,
+        sifive::gpio::pins::pin0::CLEAR,
+    );
     gpio::Pin::make_output(led_blue);
     gpio::Pin::clear(led_blue);
 
-    let led_red = &mut led::LedHigh::new(&mut arty_e21_chip::gpio::PORT[2]);
+    let led_red_pin = &mut sifive::gpio::GpioPin::new(
+        arty_e21_chip::gpio::GPIO0_BASE,
+        sifive::gpio::pins::pin2,
+        sifive::gpio::pins::pin2::SET,
+        sifive::gpio::pins::pin2::CLEAR,
+    );
+
+    let led_red = &mut led::LedHigh::new(led_red_pin);
     let writer = &mut WRITER;
     debug::panic(
         &mut [led_red],
