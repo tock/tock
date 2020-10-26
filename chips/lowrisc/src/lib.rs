bae7660c5f35d460eb8d9e498e7457268c0cc6b9
Brad Campbell <bradjc5@gmail.com>
2020-10-26 12:35:04 -0400

    rust: update to 2020-10-25

--- chips/lowrisc/src/lib.rs
+++ chips/lowrisc/src/lib.rs
@@ -1,6 +1,8 @@
 //! Implementations for generic LowRISC peripherals.
 
 #![feature(const_fn)]
+// Feature required with newer versions of rustc (at least 2020-10-25).
+#![feature(const_mut_refs)]
 #![no_std]
 #![crate_name = "lowrisc"]
 #![crate_type = "rlib"]
