37e7a32800f959557fcff18ecad664377c3b4249
nirvikbaruah <baruah.nirvik@gmail.com>
2020-11-10 22:09:16 -0800

    Modified return values

--- capsules/src/virtual_rng.rs
+++ capsules/src/virtual_rng.rs
@@ -13,21 +13,12 @@ enum Op {
     GetRandom,
 }
 
-// The Mux struct manages multiple Rng clients. Each client may have
-// at most one outstanding Rng request.
+// Struct to manage multiple rng requests
 pub struct MuxRngMaster<'a, R: Random<'a> + Rng<'a>> {
     rng: &'a R,
     devices: List<'a, VirtualRngMasterDevice<'a, R>>,
-    // Additional data storage needed to implement virtualization logic
     inflight: OptionalCell<&'a VirtualRngMasterDevice<'a, R>>,
 }
-
-impl<'a, R: Random<'a> + Rng<'a>> ListNode<'a, VirtualRngMasterDevice<'a, R>> for VirtualRngMasterDevice<'a, R> {
-    fn next(&self) -> &'a ListLink<VirtualRngMasterDevice<'a, R>> {
-        &self.next
-    }
-}
-
 
 impl<'a, R: Random<'a> + Rng<'a>> MuxRngMaster<'a, R> {
     pub const fn new(rng: &'a R) -> MuxRngMaster<'a, R> {
@@ -38,46 +29,42 @@ impl<'a, R: Random<'a> + Rng<'a>> MuxRngMaster<'a, R> {
         }
     }
 
-    fn do_next_op(&self) {
+    fn do_next_op(&self) -> u32{
         if self.inflight.is_none() {
             let mnode = self
                 .devices
                 .iter()
                 .find(|node| node.operation.get() != Op::Idle);
             mnode.map(|node| {
-                // self.spi.specify_chip_select(node.chip_select.get());
                 let op = node.operation.get();
                 // Need to set idle here in case callback changes state
                 node.operation.set(Op::Idle);
                 match op {
+                    // TODO: return values are a hacky way of getting rng.random()
+                    //       back to callback. Is there a better way?
                     Op::SetClient => {
                         self.rng.set_client(node);
+                        return 0;
                     }
                     Op::Initialize => {
                         self.rng.initialize();
+                        return 0;
                     }
                     Op::GetRandom => {
                         self.inflight.set(node);
-                        self.rng.random();
+                        self.rng.random()
                     }
                     Op::Reseed(seed) => {
                         self.rng.reseed(seed);
+                        return 0;
                     }
-                    Op::Idle => {} // Can't get here...
+                    Op::Idle => {return 0;} // Can't get here...
                 }
             });
         }
+        // Return value indicating success
+        0
     }
-}
-
-pub struct VirtualRngMasterDevice<'a, R: Random<'a> + Rng<'a>> {
-    //reference to the mux
-    mux: &'a MuxRngMaster<'a, R>,
-
-    // Pointer to next element in the list of devices
-    next: ListLink<'a, VirtualRngMasterDevice<'a, R>>,
-    client: OptionalCell<&'a dyn Client>,
-    operation: Cell<Op>,
 }
 
 impl<'a, R: Random<'a> + Rng<'a>> Client for MuxRngMaster<'a, R> {
@@ -94,6 +81,24 @@ impl<'a, R: Random<'a> + Rng<'a>> Client for MuxRngMaster<'a, R> {
     }
 }
 
+// Struct for a single rng device
+pub struct VirtualRngMasterDevice<'a, R: Random<'a> + Rng<'a>> {
+    //reference to the mux
+    mux: &'a MuxRngMaster<'a, R>,
+
+    // Pointer to next element in the list of devices
+    next: ListLink<'a, VirtualRngMasterDevice<'a, R>>,
+    client: OptionalCell<&'a dyn Client>,
+    operation: Cell<Op>,
+}
+
+// Implement ListNode trait for virtual rng device
+impl<'a, R: Random<'a> + Rng<'a>> ListNode<'a, VirtualRngMasterDevice<'a, R>> for VirtualRngMasterDevice<'a, R> {
+    fn next(&self) -> &'a ListLink<VirtualRngMasterDevice<'a, R>> {
+        &self.next
+    }
+}
+
 impl<'a, R: Random<'a> + Rng<'a>> VirtualRngMasterDevice<'a, R> {
     pub const fn new(
         mux: &'a MuxRngMaster<'a, R>,
@@ -106,13 +111,11 @@ impl<'a, R: Random<'a> + Rng<'a>> VirtualRngMasterDevice<'a, R> {
         }
     }
 
-    // Most virtualizers will use a set_client method that looks exactly like this
     pub fn set_client(&'a self, client: &'a dyn Client) {
         self.mux.devices.push_head(self);
         self.client.set(client);
     }
 }
-
 
 impl<'a, R: Random<'a> + Rng<'a>> Rng<'a> for VirtualRngMasterDevice<'a, R> {
     fn get(&self) -> ReturnCode {
@@ -142,13 +145,10 @@ impl<'a, R: Random<'a> + Rng<'a>> Random<'a> for VirtualRngMasterDevice<'a, R> {
 
     fn random(&self) -> u32 {
         self.operation.set(Op::GetRandom);
-        self.mux.do_next_op();
-        //  TODO: return actual value
-        0
+        self.mux.do_next_op()
     }
 }
 
-//TODO: Clean up and call underlying hardware trait if possible
 impl<'a, R: Random<'a> + Rng<'a>> Client for VirtualRngMasterDevice<'a, R> {
     fn randomness_available(
         &self,
@@ -158,7 +158,7 @@ impl<'a, R: Random<'a> + Rng<'a>> Client for VirtualRngMasterDevice<'a, R> {
         self.client.map(move |client| {
             client.randomness_available(randomness, _error)
         });
-        // TODO: is this a valid failsafe?
+        // TODO: is this the valid default return value?
         Continue::Done
     }
 }
