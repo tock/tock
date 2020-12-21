20095ba39dfa63fa3a12fa9e16f19f4514d14895
Alexandru Radovici <msg4alex@gmail.com>
2020-12-21 19:18:15 +0200

    capsules/buzzer_driver: port to Tock 2.0 Driver interface
    Original message:
    
        update buzzer to Driver 2.0

--- boards/acd52832/src/main.rs
+++ boards/acd52832/src/main.rs
@@ -100,7 +100,7 @@ impl kernel::Platform for Platform {
             capsules::temperature::DRIVER_NUM => f(Some(Ok(self.temp))),
             capsules::gpio_async::DRIVER_NUM => f(Some(Ok(self.gpio_async))),
             capsules::ambient_light::DRIVER_NUM => f(Some(Ok(self.light))),
-            capsules::buzzer_driver::DRIVER_NUM => f(Some(Err(self.buzzer))),
+            capsules::buzzer_driver::DRIVER_NUM => f(Some(Ok(self.buzzer))),
             kernel::ipc::DRIVER_NUM => f(Some(Err(&self.ipc))),
             _ => f(None),
         }
