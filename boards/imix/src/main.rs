7a2e47df3298c21be027fc73c378133055465cdc
Philip Levis <pal@cs.stanford.edu>
2020-12-23 20:52:58 -0800

    Reinstate larger capsules.

--- boards/imix/src/main.rs
+++ boards/imix/src/main.rs
@@ -173,9 +173,9 @@ impl kernel::Platform for Imix {
             capsules::ninedof::DRIVER_NUM => f(Some(Ok(self.ninedof))),
             capsules::crc::DRIVER_NUM => f(Some(Ok(self.crc))),
             capsules::usb::usb_user::DRIVER_NUM => f(Some(Err(self.usb_driver))),
-//            capsules::ieee802154::DRIVER_NUM => f(Some(Ok(self.radio_driver))),
+            capsules::ieee802154::DRIVER_NUM => f(Some(Ok(self.radio_driver))),
             capsules::net::udp::DRIVER_NUM => f(Some(Ok(self.udp_driver))),
-//            capsules::nrf51822_serialization::DRIVER_NUM => f(Some(Err(self.nrf51822))),
+            capsules::nrf51822_serialization::DRIVER_NUM => f(Some(Err(self.nrf51822))),
             capsules::nonvolatile_storage_driver::DRIVER_NUM => {
                 f(Some(Err(self.nonvolatile_storage)))
             }
