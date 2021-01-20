341d6930f3374da9959e8d9db06993703e29a9ac
Alexandru Radovici <msg4alex@gmail.com>
2021-01-20 08:24:46 +0200

    delete unused driver code from ft6x06 and st77xx

--- capsules/src/driver.rs
+++ capsules/src/driver.rs
@@ -65,8 +65,6 @@ pub enum NUM {
     Pca9544a              = 0x80002,
     GpioAsync             = 0x80003,
     Nrf51822Serialization = 0x80004,
-    St7735                = 0x80006,
-    Ft6x06                = 0x80007,
 
     // Misc
     Buzzer                = 0x90000,
