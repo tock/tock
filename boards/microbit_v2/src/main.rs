1cbc1ef081b7593427c552e39fa0a82b1f6baf22
Alexandru Radovici <msg4alex@gmail.com>
2021-01-25 23:26:24 +0200

    update stack size for debug and release

--- boards/microbit_v2/src/main.rs
+++ boards/microbit_v2/src/main.rs
@@ -71,7 +71,9 @@ static mut CHIP: Option<&'static nrf52833::chip::NRF52<Nrf52833DefaultPeripheral
 /// Dummy buffer that causes the linker to reserve enough space for the stack.
 #[no_mangle]
 #[link_section = ".stack_buffer"]
-pub static mut STACK_MEMORY: [u8; 0x1500] = [0; 0x1500];
+pub static mut STACK_MEMORY: [u8; 0x1000] = [0; 0x1000];
+// debug mode requires more stack space
+// pub static mut STACK_MEMORY: [u8; 0x2000] = [0; 0x2000];
 
 /// Supported drivers by the platform
 pub struct Platform {
