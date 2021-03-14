tock/tock

commit 2baaf24e5d0665ef3d0d00d531687f44c888a9b3

shell.nix: remove deprecated stdenv.lib
As indicated in [1], upstream Nixpkgs has decided to deprecate
providing `lib` as part of the standard environments (`stdenv`), given
that those are independent of each other as documented in [2].

Accordingly, this changes usage of `pkgs.stdenv.lib` to `pkgs.lib`. A
remaining warning about usage of `stdenv.lib` might still be caused by
the Mozilla Rust overlay, which must be fixed upstream.

[1]: https://github.com/NixOS/nixpkgs/issues/108938
[2]: https://github.com/NixOS/nixpkgs/commit/c06b2b3d671da4847f950e7e9041920b40eca0bf

Signed-off-by: Leon Schuermann <leon@is.currently.online>

shell.nix
@@ -15,7 +15,7 @@
 with builtins;
 let
   inherit (pkgs) stdenv;
-  pythonPackages = stdenv.lib.fix' (self: with self; pkgs.python3Packages //
+  pythonPackages = lib.fix' (self: with self; pkgs.python3Packages //
   {
 
     tockloader = buildPythonPackage rec {

commit 0c68ffc5732c6350ef7306d21d8dd7a6ca796df0

shell.nix: fix missing lib import
As part of the transition from pkgs.stdenv.lib to pkgs.lib it seems
only a part of the required changes made it into the commit
2baaf24e5 ("shell.nix: remove deprecated stdenv.lib"). This adds the
required import such that the Nix expression evaluates again.

Fixes: 2baaf24e5 ("shell.nix: remove deprecated stdenv.lib")
Signed-off-by: Leon Schuermann <leon@is.currently.online>

shell.nix
@@ -14,7 +14,7 @@
 
 with builtins;
 let
-  inherit (pkgs) stdenv;
+  inherit (pkgs) stdenv lib;
   pythonPackages = lib.fix' (self: with self; pkgs.python3Packages //
   {
 