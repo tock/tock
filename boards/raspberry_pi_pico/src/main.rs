5eed5c3ff3ab48db399f67f2c2d4695d3565234b
Alexandru Radovici <msg4alex@gmail.com>
2021-03-21 21:53:29 +0200

    boots

--- boards/raspberry_pi_pico/src/main.rs
+++ boards/raspberry_pi_pico/src/main.rs
@@ -7,9 +7,40 @@
 // https://github.com/rust-lang/rust/issues/62184.
 #![cfg_attr(not(doc), no_main)]
 #![deny(missing_docs)]
+#![feature(asm, naked_functions)]
 
 use rp2040;
 mod io;
+
+mod flash_bootloader;
+
+extern "C" {
+    static _stext: *const u32;
+}
+
+/// Allocate memory for the stack
+#[no_mangle]
+#[link_section = ".stack_buffer"]
+pub static mut STACK_MEMORY: [u8; 0x1000] = [0; 0x1000];
+
+// Manually setting the boot header section that contains the FCB header
+#[used]
+#[link_section = ".flash_bootloader"]
+static FLASH_BOOTLOADER: [u8; 256] = flash_bootloader::FLASH_BOOTLOADER;
+
+/// Entry point used for debuger
+#[no_mangle]
+#[naked]
+pub unsafe extern "C" fn reset() {
+    asm! ("
+    ldr r0, =_stext
+    ldr r1, =(0xe0000000 + 0x0000ed08)
+    str r0, [r1]
+    ldmia r0!, {{r1, r2}}
+    msr msp, r1
+    bx r2
+    ", options (noreturn));   
+}
 
 /// Entry point in the vector table called on hard reset.
 #[no_mangle]
