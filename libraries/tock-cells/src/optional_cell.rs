tock/tock

commit a845aa8e27cfe0a630a4e5944b040476dd9e5e34

tock-cells/OptionalCell: add get-method for retrieving inner Option
Adds an OptionalCell::get() method which returns a copy of the
contained Option<T>, which is either None or Some. This allows a user
to treat an OptionalCell<T> as a Cell<Option<T>> in most cases, while
still providing the benefits of a regular OptionalCell<T>.

Signed-off-by: Leon Schuermann <leon@is.currently.online>

libraries/tock-cells/src/optional_cell.rs
@@ -148,6 +148,11 @@ impl<T> OptionalCell<T> {
 }
 
 impl<T: Copy> OptionalCell<T> {
+    /// Returns a copy the contained [`Option`].
+    pub fn get(&self) -> Option<T> {
+        self.value.get()
+    }
+
     /// Returns the contained value or panics if contents is `None`.
     pub fn expect(&self, msg: &str) -> T {
         self.value.get().expect(msg)

commit 3136f00a77173708625256011be54ee24778364f

tock-cells/OptionalCell: rename get-method to extract
As an OptionalCell is designed to look like a Cell which can also be
empty, the get method as currently implemented does not match the
expected behavior (as it will return an `Option<T>` instead of a `T`).
Furthermore, as `OptionalCell::set` takes simply a `T` (not wrapped in
an `Option`), `get` should not return an `Option<T>`.

Accordingly, this commit renames the `OptionalCell::get` method to
`extract`, to distinguish it from the `Cell::get` method.

Fixes: a845aa8e27cfe0 ("tock-cells/OptionalCell: add get-method...")
Suggested-by: Brad Campbell <bradjc5@gmail.com>
Signed-off-by: Leon Schuermann <leon@is.currently.online>

libraries/tock-cells/src/optional_cell.rs
@@ -148,8 +148,8 @@ impl<T> OptionalCell<T> {
 }
 
 impl<T: Copy> OptionalCell<T> {
-    /// Returns a copy the contained [`Option`].
-    pub fn get(&self) -> Option<T> {
+    /// Returns a copy of the contained [`Option`].
+    pub fn extract(&self) -> Option<T> {
         self.value.get()
     }
 