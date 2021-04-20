bb5faea2224b7e7a808b113c9ffee51b0f05733c
Alistair Francis <alistair.francis@wdc.com>
2021-04-20 17:21:01 +1000

    boards/esp32-c3: Initial commit
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

---
+++ boards/esp32-c3-devkitM-1/src/io.rs
@@ -0,0 +1,44 @@
+use core::fmt::Write;
+use core::panic::PanicInfo;
+use core::str;
+use kernel::debug;
+use kernel::debug::IoWrite;
+
+use crate::CHIP;
+use crate::PROCESSES;
+
+struct Writer {}
+
+static mut WRITER: Writer = Writer {};
+
+impl Write for Writer {
+    fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
+        self.write(s.as_bytes());
+        Ok(())
+    }
+}
+
+impl IoWrite for Writer {
+    fn write(&mut self, buf: &[u8]) {
+        let uart = esp32::uart::Uart::new(esp32::uart::UART0_BASE);
+        uart.disable_tx_interrupt();
+        uart.disable_rx_interrupt();
+        uart.transmit_sync(buf);
+    }
+}
+
+/// Panic handler.
+#[cfg(not(test))]
+#[no_mangle]
+#[panic_handler]
+pub unsafe extern "C" fn panic_fmt(pi: &PanicInfo) -> ! {
+    let writer = &mut WRITER;
+
+    debug::panic_banner(writer, pi);
+    debug::panic_cpu_state(&CHIP, writer);
+    debug::panic_process_info(&PROCESSES, writer);
+
+    loop {
+        rv32i::support::nop();
+    }
+}
