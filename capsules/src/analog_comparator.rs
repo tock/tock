tock/tock

commit e9621aa08b31748887e9bfe6ff4405d3a72bde75

capsules: analog comparator use grant

capsules/src/analog_comparator.rs
@@ -37,31 +37,38 @@
 use crate::driver;
 pub const DRIVER_NUM: usize = driver::NUM::AnalogComparator as usize;
 
-use core::cell::Cell;
+use core::mem;
+
+use kernel::common::cells::OptionalCell;
 use kernel::hil;
-use kernel::{AppId, CommandReturn, Driver, ErrorCode, Upcall};
+use kernel::{AppId, CommandReturn, Driver, ErrorCode, Grant, Upcall};
 
 pub struct AnalogComparator<'a, A: hil::analog_comparator::AnalogComparator<'a> + 'a> {
     // Analog Comparator driver
     analog_comparator: &'a A,
     channels: &'a [&'a <A as hil::analog_comparator::AnalogComparator<'a>>::Channel],
 
-    // App state
-    callback: Cell<Upcall>,
+    grants: Grant<App>,
+    current_process: OptionalCell<AppId>,
+}
+
+#[derive(Default)]
+pub struct App {
+    callback: Upcall,
 }
 
 impl<'a, A: hil::analog_comparator::AnalogComparator<'a>> AnalogComparator<'a, A> {
     pub fn new(
         analog_comparator: &'a A,
         channels: &'a [&'a <A as hil::analog_comparator::AnalogComparator<'a>>::Channel],
+        grant: Grant<App>,
     ) -> AnalogComparator<'a, A> {
         AnalogComparator {
             // Analog Comparator driver
             analog_comparator,
             channels,
-
-            // App state
-            callback: Cell::new(Upcall::default()),
+            grants: grant,
+            current_process: OptionalCell::empty(),
         }
     }
 
@@ -117,7 +124,24 @@ impl<'a, A: hil::analog_comparator::AnalogComparator<'a>> Driver for AnalogCompa
     /// - `3`: Stop interrupt-based comparisons.
     ///        Input x chooses the desired comparator ACx (e.g. 0 or 1 for
     ///        hail, 0-3 for imix)
-    fn command(&self, command_num: usize, channel: usize, _: usize, _: AppId) -> CommandReturn {
+    fn command(&self, command_num: usize, channel: usize, _: usize, appid: AppId) -> CommandReturn {
+        if command_num == 0 {
+            // Handle this first as it should be returned unconditionally.
+            return CommandReturn::success_u32(self.channels.len() as u32);
+        }
+
+        // Check if this driver is free, or already dedicated to this process.
+        let match_or_empty_or_nonexistant = self.current_process.map_or(true, |current_process| {
+            self.grants
+                .enter(*current_process, |_, _| current_process == &appid)
+                .unwrap_or(true)
+        });
+        if match_or_empty_or_nonexistant {
+            self.current_process.set(appid);
+        } else {
+            return CommandReturn::failure(ErrorCode::NOMEM);
+        }
+
         match command_num {
             0 => CommandReturn::success_u32(self.channels.len() as u32),
 
@@ -138,12 +162,23 @@ impl<'a, A: hil::analog_comparator::AnalogComparator<'a>> Driver for AnalogCompa
     fn subscribe(
         &self,
         subscribe_num: usize,
-        callback: Upcall,
-        _app_id: AppId,
+        mut callback: Upcall,
+        app_id: AppId,
     ) -> Result<Upcall, (Upcall, ErrorCode)> {
         match subscribe_num {
             // Subscribe to all interrupts
-            0 => Ok(self.callback.replace(callback)),
+            0 => {
+                let res = self
+                    .grants
+                    .enter(app_id, |app, _| {
+                        mem::swap(&mut app.callback, &mut callback);
+                    })
+                    .map_err(ErrorCode::from);
+                match res {
+                    Err(err) => Err((callback, err)),
+                    _ => Ok(callback),
+                }
+            }
             // Default
             _ => Err((callback, ErrorCode::NOSUPPORT)),
         }
@@ -155,6 +190,10 @@ impl<'a, A: hil::analog_comparator::AnalogComparator<'a>> hil::analog_comparator
 {
     /// Upcall to userland, signaling the application
     fn fired(&self, channel: usize) {
-        self.callback.get().schedule(channel, 0, 0);
+        self.current_process.take().map(|appid| {
+            let _ = self.grants.enter(appid, |app, _| {
+                app.callback.schedule(channel, 0, 0);
+            });
+        });
     }
 }

boards/components/src/analog_comparator.rs
@@ -19,11 +19,15 @@
 //! .finalize(components::acomp_component_buf!(sam4l::acifc::Acifc));
 //! ```
 
-use capsules::analog_comparator;
 use core::mem::MaybeUninit;
+
 use kernel;
+use kernel::capabilities;
 use kernel::component::Component;
+use kernel::create_capability;
 use kernel::static_init_half;
+
+use capsules::analog_comparator;
 
 #[macro_export]
 macro_rules! acomp_component_helper {
@@ -54,11 +58,20 @@ macro_rules! acomp_component_buf {
 pub struct AcComponent<AC: 'static + kernel::hil::analog_comparator::AnalogComparator<'static>> {
     comp: &'static AC,
     ac_channels: &'static [&'static AC::Channel],
+    board_kernel: &'static kernel::Kernel,
 }
 
 impl<AC: 'static + kernel::hil::analog_comparator::AnalogComparator<'static>> AcComponent<AC> {
-    pub fn new(comp: &'static AC, ac_channels: &'static [&'static AC::Channel]) -> Self {
-        Self { comp, ac_channels }
+    pub fn new(
+        comp: &'static AC,
+        ac_channels: &'static [&'static AC::Channel],
+        board_kernel: &'static kernel::Kernel,
+    ) -> Self {
+        Self {
+            comp,
+            ac_channels,
+            board_kernel,
+        }
     }
 }
 
@@ -69,10 +82,13 @@ impl<AC: 'static + kernel::hil::analog_comparator::AnalogComparator<'static>> Co
     type Output = &'static analog_comparator::AnalogComparator<'static, AC>;
 
     unsafe fn finalize(self, static_buffer: Self::StaticInput) -> Self::Output {
+        let grant_cap = create_capability!(capabilities::MemoryAllocationCapability);
+        let grant_ac = self.board_kernel.create_grant(&grant_cap);
+
         let analog_comparator = static_init_half!(
             static_buffer,
             analog_comparator::AnalogComparator<'static, AC>,
-            analog_comparator::AnalogComparator::new(self.comp, self.ac_channels)
+            analog_comparator::AnalogComparator::new(self.comp, self.ac_channels, grant_ac)
         );
         self.comp.set_client(analog_comparator);
 

boards/imix/src/main.rs
@@ -430,6 +430,7 @@ pub unsafe fn main() {
             ac_2,
             ac_3
         ),
+        board_kernel,
     )
     .finalize(components::acomp_component_buf!(sam4l::acifc::Acifc));
     let rng = RngComponent::new(board_kernel, &peripherals.trng).finalize(());

commit 90019748ab01a1929807366e2519df9f9d209e1a

capsules/analog_comparator: don't reset current_process on upcall
Fixes: e9621aa08b3174 ("capsules: analog comparator use grant")
Signed-off-by: Leon Schuermann <leon@is.currently.online>

capsules/src/analog_comparator.rs
@@ -196,8 +196,8 @@ impl<'a, A: hil::analog_comparator::AnalogComparator<'a>> hil::analog_comparator
 {
     /// Upcall to userland, signaling the application
     fn fired(&self, channel: usize) {
-        self.current_process.take().map(|appid| {
-            let _ = self.grants.enter(appid, |app| {
+        self.current_process.map(|appid| {
+            let _ = self.grants.enter(*appid, |app| {
                 app.callback.schedule(channel, 0, 0);
             });
         });