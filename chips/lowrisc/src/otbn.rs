a1d77d7f59294f37b250344df4e260ee8c60f5ce
Alistair Francis <alistair.francis@wdc.com>
2021-05-24 16:45:47 +1000

    chips/lowrisc: otbn: Add implementation
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- chips/lowrisc/src/otbn.rs
+++ chips/lowrisc/src/otbn.rs
@@ -1,7 +1,12 @@
 //! OTBN Control
 
-use kernel::common::cells::OptionalCell;
+use core::cell::Cell;
+use kernel::common::cells::{OptionalCell, TakeCell};
+use kernel::common::dynamic_deferred_call::{
+    DeferredCallHandle, DynamicDeferredCall, DynamicDeferredCallClient,
+};
 use kernel::common::leasable_buffer::LeasableBuffer;
+use kernel::common::registers::interfaces::{ReadWriteable, Readable, Writeable};
 use kernel::common::registers::{
     register_bitfields, register_structs, ReadOnly, ReadWrite, WriteOnly,
 };
@@ -64,20 +69,57 @@ register_bitfields![u32,
 ];
 
 pub struct Otbn<'a> {
-    _registers: StaticRef<OtbnRegisters>,
+    registers: StaticRef<OtbnRegisters>,
     client: OptionalCell<&'a dyn accel::Client<'a, 1024>>,
+
+    in_buffer: Cell<Option<LeasableBuffer<'static, u8>>>,
+    out_buffer: TakeCell<'static, [u8; 1024]>,
+
+    add_data_deferred_call: Cell<bool>,
+    deferred_caller: &'static DynamicDeferredCall,
+    deferred_handle: OptionalCell<DeferredCallHandle>,
 }
 
 impl<'a> Otbn<'a> {
-    pub const fn new(base: StaticRef<OtbnRegisters>) -> Self {
+    pub const fn new(
+        base: StaticRef<OtbnRegisters>,
+        deferred_caller: &'static DynamicDeferredCall,
+    ) -> Self {
         Otbn {
-            _registers: base,
+            registers: base,
             client: OptionalCell::empty(),
+            in_buffer: Cell::new(None),
+            out_buffer: TakeCell::empty(),
+
+            add_data_deferred_call: Cell::new(false),
+            deferred_caller,
+            deferred_handle: OptionalCell::empty(),
         }
     }
 
     pub fn handle_interrupt(&self) {
-        unimplemented!();
+        // Check if there is an error
+        if self.registers.err_bits.get() > 0 {
+            self.client.map(|client| {
+                self.out_buffer.take().map(|buf| {
+                    client.op_done(Err(ErrorCode::FAIL), buf);
+                })
+            });
+            return;
+        }
+
+        if !self.registers.status.is_set(STATUS::BUSY) {
+            self.client.map(|client| {
+                client.op_done(Ok(()), self.out_buffer.take().unwrap());
+            });
+        }
+
+        self.registers.intr_enable.set(0x00);
+        self.registers.intr_state.set(0xFFFF_FFFF);
+    }
+
+    pub fn initialise(&self, deferred_call_handle: DeferredCallHandle) {
+        self.deferred_handle.set(deferred_call_handle);
     }
 }
 
@@ -90,19 +132,84 @@ impl<'a> accel::Accel<'a, 1024> for Otbn<'a> {
         &self,
         input: LeasableBuffer<'static, u8>,
     ) -> Result<(), (ErrorCode, &'static mut [u8])> {
-        Err((ErrorCode::NOSUPPORT, input.take()))
+        if self.registers.status.is_set(STATUS::BUSY) {
+            // OTBN is performing an operation, we can't make any changes
+            return Err((ErrorCode::BUSY, input.take()));
+        }
+
+        for i in 0..(input.len() / 4) {
+            let idx = i * 4;
+
+            let mut d = (input[idx + 0] as u32) << 0;
+            d |= (input[idx + 1] as u32) << 8;
+            d |= (input[idx + 2] as u32) << 16;
+            d |= (input[idx + 3] as u32) << 24;
+
+            self.registers.imem[i].set(d);
+        }
+
+        self.in_buffer.set(Some(input));
+
+        // Schedule a deferred call as there are no interrupts to monitor
+        // the binary loading.
+        self.add_data_deferred_call.set(true);
+        self.deferred_handle
+            .map(|handle| self.deferred_caller.set(*handle));
+
+        Ok(())
     }
 
-    fn set_property(&self, _key: usize, _value: usize) -> Result<(), ErrorCode> {
-        Err(ErrorCode::NOSUPPORT)
+    /// Set the OTBN properties
+    /// key values:
+    ///  `0` -> Start Address, set the start address
+    fn set_property(&self, key: usize, value: usize) -> Result<(), ErrorCode> {
+        if self.registers.status.is_set(STATUS::BUSY) {
+            // OTBN is performing and operation
+            return Err(ErrorCode::BUSY);
+        }
+
+        match key {
+            0 => {
+                self.registers.start_addr.set(value as u32);
+                Ok(())
+            }
+            _ => Err(ErrorCode::NOSUPPORT),
+        }
     }
 
     fn run(
         &'a self,
         output: &'static mut [u8; 1024],
     ) -> Result<(), (ErrorCode, &'static mut [u8; 1024])> {
-        Err((ErrorCode::NOSUPPORT, output))
+        if self.registers.status.is_set(STATUS::BUSY) {
+            // OTBN is performing and operation
+            return Err((ErrorCode::BUSY, output));
+        }
+
+        // Clear and enable interrupts
+        self.registers.intr_state.modify(INTR::DONE::SET);
+        self.registers.intr_enable.modify(INTR::DONE::SET);
+
+        self.out_buffer.replace(output);
+
+        self.registers.cmd.modify(CMD::START::SET);
+
+        Ok(())
     }
 
     fn clear_data(&self) {}
+}
+
+impl<'a> DynamicDeferredCallClient for Otbn<'a> {
+    fn call(&self, _handle: DeferredCallHandle) {
+        if self.add_data_deferred_call.get() {
+            self.add_data_deferred_call.set(false);
+
+            self.client.map(|client| {
+                self.in_buffer.take().map(|buffer| {
+                    client.binary_load_done(Ok(()), buffer.take());
+                });
+            });
+        }
+    }
 }
