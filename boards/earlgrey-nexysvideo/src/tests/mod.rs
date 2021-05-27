971a2e8490358c9f19fcdec00d847434fc0d610d
Alistair Francis <alistair.francis@wdc.com>
2021-05-27 17:51:44 +1000

    boards/opentitan: Remove some flaky tests
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- boards/earlgrey-nexysvideo/src/tests/mod.rs
+++ boards/earlgrey-nexysvideo/src/tests/mod.rs
@@ -4,7 +4,7 @@ use crate::MAIN_CAP;
 use crate::NUM_PROCS;
 use crate::PLATFORM;
 use crate::SCHEDULER;
-use kernel::{debug, Chip};
+use kernel::debug;
 
 pub fn semihost_command_exit_success() -> ! {
     // Exit QEMU with a return code of 0
@@ -43,31 +43,6 @@ fn trivial_assertion() {
     run_kernel_op(10);
 
     assert_eq!(1, 1);
-
-    debug!("    [ok]");
-    run_kernel_op(10);
-}
-
-#[test_case]
-fn check_epmp_regions() {
-    debug!("check epmp regions... ");
-    run_kernel_op(10);
-    unsafe {
-        debug!("{}", CHIP.unwrap().pmp);
-    }
-    run_kernel_op(100);
-    debug!("    [ok]");
-    run_kernel_op(10);
-}
-
-#[test_case]
-fn check_pending_interrupts() {
-    debug!("check pending interrupts... ");
-    run_kernel_op(10);
-    unsafe {
-        assert_eq!(CHIP.unwrap().has_pending_interrupts(), false);
-    }
-    run_kernel_op(1000);
 
     debug!("    [ok]");
     run_kernel_op(10);
