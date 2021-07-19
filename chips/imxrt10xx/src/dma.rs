20884b6c4413dcc81a15b84635573fa2559c36fc
Alexandru Radovici <msg4alex@gmail.com>
2021-07-19 09:27:19 +0300

    update imxrt to use new kernel exports

--- chips/imxrt10xx/src/dma.rs
+++ chips/imxrt10xx/src/dma.rs
@@ -26,7 +26,7 @@
 //! - Channel priorities may come into play when preferring DMA channels. See the reference manual for more
 //!   information on channel priorities, and how the DMA controller use priorities for scheduling.
 
-use kernel::common::{
+use kernel::utilities::{
     cells::OptionalCell,
     registers::{
         self,
