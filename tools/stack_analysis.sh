01086c2338b4cb306dd86f97941485baec7823d7
Brad Campbell <bradjc5@gmail.com>
2021-07-30 13:14:44 -0400

    make: update stack-analysis format

--- tools/stack_analysis.sh
+++ tools/stack_analysis.sh
@@ -4,13 +4,28 @@
 # size information, and is thus most easily called using the `make stack-analysis`
 # rule.
 
-# Print the stack frame size of `main`
-printf "main stack frame: \n"
-$(find $(rustc --print sysroot) -name llvm-readobj) --elf-output-style GNU --stack-sizes $1 | grep 'main'
+bold=$(tput bold)
+normal=$(tput sgr0)
 
+# Get a list of all stack frames and their sizes.
+frames=`$(find $(rustc --print sysroot) -name llvm-readobj) --elf-output-style GNU --stack-sizes $1`
+
+# Print the stack frame size of `main`
+printf "   main stack frame: \n"
+echo "$frames" | grep ' main' # Use a space before main to avoid functions with main in the name.
 printf "\n"
-printf "5 largest stack frames: \n"
 
 # Print the 5 largest stack frames
-$(find $(rustc --print sysroot) -name llvm-readobj) --elf-output-style GNU --stack-sizes $1 | sort -n -r | head -5
+printf "   5 largest stack frames: \n"
+echo "$frames" | sort -n -r | head -5
 printf "\n"
+
+# Check if main is the largest stack frame
+largest=`echo "$frames" | sort -n -r | head -1 | grep ' main'`
+largest_ret_val=$?
+
+# If it is, print a warning.
+if [ $largest_ret_val -eq 0 ]; then
+    echo "   ${bold}WARNING! main is the largest stack frame!${normal}"
+    printf "\n"
+fi
