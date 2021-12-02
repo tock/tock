7176e4613799442f238272fd6101d383c8a21e36
Alistair Francis <alistair.francis@wdc.com>
2021-12-02 13:27:53 +1000

    capsules/test: aes_ccm: Panic if the test fails
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- capsules/src/test/aes_ccm.rs
+++ capsules/src/test/aes_ccm.rs
@@ -150,6 +150,7 @@ impl<'a, A: AES128CCM<'a>> Test<'a, A> {
                 {
                     debug!("{:x} vs {:x}", *a, *b);
                 }
+                panic!("aes_ccm_test failed");
             }
         } else {
             let a_matches = buf[a_off..m_off]
@@ -168,7 +169,7 @@ impl<'a, A: AES128CCM<'a>> Test<'a, A> {
                     tag_is_valid
                 );
             } else {
-                debug!("aes_ccm_test failed: a_matches={}, m_matches={}, (current_test={}, encrypting={}, tag_is_valid={}",
+                panic!("aes_ccm_test failed: a_matches={}, m_matches={}, (current_test={}, encrypting={}, tag_is_valid={}",
                        a_matches,
                        m_matches,
                        self.current_test.get(),
@@ -185,7 +186,7 @@ impl<'a, A: AES128CCM<'a>> CCMClient for Test<'a, A> {
     fn crypt_done(&self, buf: &'static mut [u8], res: Result<(), ErrorCode>, tag_is_valid: bool) {
         self.buf.replace(buf);
         if res != Ok(()) {
-            debug!("aes_ccm_test failed: crypt_done returned {:?}", res);
+            panic!("aes_ccm_test failed: crypt_done returned {:?}", res);
         } else {
             self.check_test(tag_is_valid);
             if self.next_test() {
