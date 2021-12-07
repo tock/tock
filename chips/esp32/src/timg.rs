e7dc5f9dc5e92cb9e970ab3dc3dbcafd9532a913
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2021-12-07 16:01:24 +0200

    now working

--- chips/esp32/src/timg.rs
+++ chips/esp32/src/timg.rs
@@ -14,6 +14,15 @@ pub const TIMG0_BASE: StaticRef<TimgRegisters> =
 pub const TIMG1_BASE: StaticRef<TimgRegisters> =
     unsafe { StaticRef::new(0x6002_0000 as *const TimgRegisters) };
 
+/// 20MHz `Frequency`
+#[derive(Debug)]
+pub struct Freq20MHz;
+impl time::Frequency for Freq20MHz {
+    fn frequency() -> u32 {
+        20_000_000
+    }
+}
+
 /// 80MHz `Frequency`
 #[derive(Debug)]
 pub struct Freq80MHz;
@@ -32,7 +41,7 @@ register_structs! {
         (0x010 => t0alarmlo: ReadWrite<u32>),
         (0x014 => t0alarmhi: ReadWrite<u32>),
         (0x018 => t0loadlo: ReadWrite<u32>),
-        (0x01C => _reserved0),
+        (0x01C => t0loadhi: ReadWrite<u32>),
         (0x020 => t0load: ReadWrite<u32>),
 
         (0x024 => t1config: ReadWrite<u32, CONFIG::Register>),
@@ -57,7 +66,13 @@ register_structs! {
         (0x068 => rtccalicfg: ReadWrite<u32, RTCCALICFG::Register>),
         (0x06C => rtccalicfg1: ReadWrite<u32, RTCCALICFG1::Register>),
 
-        (0x070 => _reserved2),
+        (0x070 => int_c3_ena: ReadWrite<u32, INT_C3::Register>),
+        (0x074 => int_c3_raw: ReadWrite<u32, INT_C3::Register>),
+        (0x078 => int_c3_st: ReadWrite<u32, INT_C3::Register>),
+        (0x07C => int_c3_clr: ReadWrite<u32, INT_C3::Register>),
+
+        (0x080 => _reserved2),
+
         (0x098 => int_ena: ReadWrite<u32, INT::Register>),
         (0x09C => int_raw: ReadWrite<u32, INT::Register>),
         (0x0A0 => int_st: ReadWrite<u32, INT::Register>),
@@ -68,20 +83,23 @@ register_structs! {
 
 register_bitfields![u32,
     CONFIG [
+        USE_XTAL OFFSET(9) NUMBITS(1) [],
         ALARM_EN OFFSET(10) NUMBITS(1) [],
         LEVEL_INT_EN OFFSET(11) NUMBITS(1) [],
-        EDGE_INT_EN OFFSET(12) NUMBITS(1) [],
+        EDGE_INT_EN_OR_DIVIDER_RST OFFSET(12) NUMBITS(1) [],
         DIVIDER OFFSET(13) NUMBITS(16) [],
         AUTORELOAD OFFSET(29) NUMBITS(1) [],
-        INCREATE OFFSET(30) NUMBITS(1) [],
+        INCREASE OFFSET(30) NUMBITS(1) [],
         EN OFFSET(31) NUMBITS(1) [],
     ],
     WDTCONFIG0 [
+        APP_CPU_RESET_EN OFFSET(12) NUMBITS(1) [],
+        PROC_CPU_RESET_EN OFFSET(13) NUMBITS(1) [],
         FLASHBOOT_MOD_EN OFFSET(14) NUMBITS(1) [],
         SYS_RESET_LENGTH OFFSET(15) NUMBITS(3) [],
         CPU_RESET_LENGTH OFFSET(18) NUMBITS(3) [],
-        LEVEL_INT_EN OFFSET(21) NUMBITS(1) [],
-        EDGE_INT_EN OFFSET(22) NUMBITS(1) [],
+        LEVEL_INT_EN_OR_USE_XTAL OFFSET(21) NUMBITS(1) [],
+        EDGE_INT_EN_OR_UPDATE_EN OFFSET(22) NUMBITS(1) [],
         STG3 OFFSET(23) NUMBITS(2) [],
         STG2 OFFSET(25) NUMBITS(2) [],
         STG1 OFFSET(27) NUMBITS(2) [],
@@ -89,9 +107,10 @@ register_bitfields![u32,
         EN OFFSET(31) NUMBITS(1) [],
     ],
     WDTCONFIG1 [
+        DIVCNT_RST OFFSET(0) NUMBITS(1) [],
         CLK_PRESCALE OFFSET(16) NUMBITS(16) [],
     ],
-    pub RTCCALICFG [
+    RTCCALICFG [
         START_CYCLING OFFSET(12) NUMBITS(1) [],
         CLK_SEL OFFSET(13) NUMBITS(2) [],
         RDY OFFSET(15) NUMBITS(1) [],
@@ -99,7 +118,12 @@ register_bitfields![u32,
         START OFFSET(31) NUMBITS(1) [],
     ],
     RTCCALICFG1 [
+        CYCLING_DATA_VLD OFFSET(0) NUMBITS(1) [],
         VALUE OFFSET(7) NUMBITS(25) [],
+    ],
+    INT_C3 [
+        T0 OFFSET(0) NUMBITS(1) [],
+        WDT OFFSET(1) NUMBITS(1) [],
     ],
     INT [
         T0 OFFSET(0) NUMBITS(1) [],
@@ -108,15 +132,23 @@ register_bitfields![u32,
     ],
 ];
 
-pub struct TimG<'a> {
+#[derive(Copy, Clone)]
+pub enum ClockSource {
+    Pll = 0,
+    Xtal = 1,
+}
+
+pub struct TimG<'a, const C3: bool> {
     registers: StaticRef<TimgRegisters>,
+    clocksource: ClockSource,
     alarm_client: OptionalCell<&'a dyn time::AlarmClient>,
 }
 
-impl TimG<'_> {
-    pub const fn new(base: StaticRef<TimgRegisters>) -> Self {
+impl<const C3: bool> TimG<'_, C3> {
+    pub const fn new(base: StaticRef<TimgRegisters>, clocksource: ClockSource) -> Self {
         TimG {
             registers: base,
+            clocksource,
             alarm_client: OptionalCell::empty(),
         }
     }
@@ -144,7 +176,19 @@ impl TimG<'_> {
     }
 }
 
-impl time::Time for TimG<'_> {
+impl time::Time for TimG<'_, true> {
+    type Frequency = Freq20MHz;
+    type Ticks = Ticks64;
+
+    fn now(&self) -> Self::Ticks {
+        self.registers.t0update.set(0xABC);
+        Self::Ticks::from(
+            self.registers.t0lo.get() as u64 + ((self.registers.t0hi.get() as u64) << 32),
+        )
+    }
+}
+
+impl time::Time for TimG<'_, false> {
     type Frequency = Freq80MHz;
     type Ticks = Ticks64;
 
@@ -156,7 +200,7 @@ impl time::Time for TimG<'_> {
     }
 }
 
-impl<'a> Counter<'a> for TimG<'a> {
+impl<'a, const C3: bool> Counter<'a> for TimG<'a, C3> {
     fn set_overflow_client(&self, _client: &'a dyn time::OverflowClient) {
         // We have no way to know when this happens
     }
@@ -182,7 +226,7 @@ impl<'a> Counter<'a> for TimG<'a> {
     }
 }
 
-impl<'a> Alarm<'a> for TimG<'a> {
+impl<'a, const C3: bool> Alarm<'a> for TimG<'a, C3> {
     fn set_alarm_client(&self, client: &'a dyn time::AlarmClient) {
         self.alarm_client.set(client);
     }
@@ -192,6 +236,22 @@ impl<'a> Alarm<'a> for TimG<'a> {
         let mut expire = reference.wrapping_add(dt);
         if !now.within_range(reference, expire) {
             expire = now;
+        }
+
+        self.registers
+            .t0config
+            .modify(CONFIG::ALARM_EN::CLEAR + CONFIG::EN::CLEAR);
+
+        self.registers.t0config.modify(
+            CONFIG::USE_XTAL.val(self.clocksource as u32)
+                + CONFIG::INCREASE::SET
+                + CONFIG::DIVIDER.val(2 * (2 - self.clocksource as u32)),
+        );
+
+        if C3 {
+            self.registers
+                .t0config
+                .modify(CONFIG::EDGE_INT_EN_OR_DIVIDER_RST::SET);
         }
 
         let val = expire.into_u64();
@@ -204,7 +264,9 @@ impl<'a> Alarm<'a> for TimG<'a> {
 
         self.registers.int_ena.modify(INT::T0::SET);
 
-        self.registers.t0config.modify(CONFIG::ALARM_EN::SET);
+        self.registers
+            .t0config
+            .modify(CONFIG::ALARM_EN::SET + CONFIG::ALARM_EN::SET);
     }
 
     fn get_alarm(&self) -> Self::Ticks {
