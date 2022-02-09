32adefa0c11f689ec83d32236fb6fe2406657429
Brad Campbell <bradjc5@gmail.com>
2022-02-09 18:33:34 -0500

    make: move -L to build.rs
    Per the comment for `RUSTC_FLAGS_FOR_BIN`:
    
    > Ultimately, this should move to the Cargo.toml, for example when
    > https://github.com/rust-lang/cargo/pull/7811 is merged into Cargo.
    
    this moves setting the linker script search path to build.rs, which is
    enabled by the addition of the `cargo:rustc-link-arg=FLAG` instruction
    for build scripts.
    
    This removes the need to special case the -L flag so that it is only
    used when creating a binary and not when compiling dependencies, since
    that is exactly what the new build script instruction handles.

--- boards/imix/build.rs
+++ boards/imix/build.rs
@@ -1,4 +1,8 @@
 fn main() {
     println!("cargo:rerun-if-changed=layout.ld");
     println!("cargo:rerun-if-changed=../kernel_layout.ld");
+    println!(
+        "cargo:rustc-link-arg-bins=-L{}",
+        std::env::var("CARGO_MANIFEST_DIR").unwrap_or(".".to_string())
+    );
 }
