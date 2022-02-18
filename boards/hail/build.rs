e2077605d993e2e768c9908fe8fe8c1b94109351
Brad Campbell <bradjc5@gmail.com>
2022-02-18 12:34:30 -0500

    hail: update to common build.rs
    This moves some flags from make to build.rs.

--- boards/hail/build.rs
+++ boards/hail/build.rs
@@ -1,5 +1,10 @@
+#[path = "../common_build.rs"]
+mod common_build;
+
 fn main() {
     println!("cargo:rerun-if-changed=layout.ld");
     println!("cargo:rerun-if-changed=chip_layout.ld");
     println!("cargo:rerun-if-changed=../kernel_layout.ld");
+
+    common_build::tock_default_linker_args();
 }
