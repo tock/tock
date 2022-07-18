1d4cc1393935a1e81b7d45deb696a11a6895ef8d
teomiu <teodora.miu01@gmail.com>
2022-07-18 16:34:35 +0300

    fix unused imports

--- capsules/src/led_matrix.rs
+++ capsules/src/led_matrix.rs
@@ -81,9 +81,8 @@
 
 use core::cell::Cell;
 
-use kernel::syscall::{CommandReturn, SyscallDriver};
 use kernel::utilities::cells::TakeCell;
-use kernel::{ErrorCode, ProcessId};
+use kernel::ErrorCode;
 
 use kernel::hil::gpio::{ActivationMode, Pin};
 use kernel::hil::led::Led;
@@ -260,42 +259,6 @@ impl<'a, L: Pin, A: Alarm<'a>> AlarmClient for LedMatrixDriver<'a, L, A> {
     }
 }
 
-// impl<'a, L: Pin, A: Alarm<'a>> SyscallDriver for LedMatrixDriver<'a, L, A> {
-//     /// Control the LEDs.
-//     ///
-//     /// ### `command_num`
-//     ///
-//     /// - `0`: Returns the number of LEDs on the board. This will always be 0 or
-//     ///        greater, and therefore also allows for checking for this driver.
-//     /// - `1`: Turn the LED at index specified by `data` on. Returns `INVAL` if
-//     ///        the LED index is not valid.
-//     /// - `2`: Turn the LED at index specified by `data` off. Returns `INVAL`
-//     ///        if the LED index is not valid.
-//     /// - `3`: Toggle the LED at index specified by `data` on or off. Returns
-//     ///        `INVAL` if the LED index is not valid.
-//     fn command(&self, command_num: usize, data: usize, _: usize, _: ProcessId) -> CommandReturn {
-//         match command_num {
-//             // get number of LEDs
-//             0 => CommandReturn::success_u32((self.cols.len() * self.rows.len()) as u32),
-
-//             // on
-//             1 => CommandReturn::from(self.on_index(data)),
-
-//             // off
-//             2 => CommandReturn::from(self.off_index(data)),
-
-//             // toggle
-//             3 => CommandReturn::from(self.toggle_index(data)),
-
-//             // default
-//             _ => CommandReturn::failure(ErrorCode::NOSUPPORT),
-//         }
-//     }
-
-//     fn allocate_grant(&self, _processid: ProcessId) -> Result<(), kernel::process::Error> {
-//         Ok(())
-//     }
-// }
 // one Led from the matrix
 pub struct LedMatrixLed<'a, L: Pin, A: Alarm<'a>> {
     matrix: &'a LedMatrixDriver<'a, L, A>,
