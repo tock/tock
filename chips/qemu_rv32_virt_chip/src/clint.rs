lschuermann/hsm-tock

Add qemu-system-riscv32 virt-machine support
This adds support for the qemu-system-riscv32 "virt" machine type, a
generic emulated/virtualized target, featuring paravirtualized
peripherals. Compared to the already supported hifive1 target, this
does not try to mimic any real hardware, but provides a generic target
machine with a flexible memory layout.

Furthermore, it can use VirtIO peripherals to enable paravirtualized
network cards, block storage devices, random number generators and
other devices.

Significant parts of this have been inspired by or copied from the
e310x / hifive1 crates, given that the QEMU virt target implementation
reuses some mechanisms of the hifive1 / sifive_e machine
implementations (for instance the PLIC and CLINT).

Signed-off-by: Leon Schuermann <leon@is.currently.online>

chips/qemu_rv32_virt_chip/src/clint.rs
@@ -1,0 +1,7 @@
+//! Machine Timer instantiation.
+
+use kernel::common::StaticRef;
+use sifive::clint::ClintRegisters;
+
+pub const CLINT_BASE: StaticRef<ClintRegisters> =
+    unsafe { StaticRef::new(0x0200_0000 as *const ClintRegisters) };

chips/qemu_rv32_virt_chip/src/plic.rs
@@ -1,0 +1,9 @@
+//! Instantiation of the sifive Platform Level Interrupt Controller
+
+use kernel::common::StaticRef;
+use sifive::plic::{Plic, PlicRegisters};
+
+pub const PLIC_BASE: StaticRef<PlicRegisters> =
+    unsafe { StaticRef::new(0x0c00_0000 as *const PlicRegisters) };
+
+pub static mut PLIC: Plic = Plic::new(PLIC_BASE);

tock/tock

chips/qemu_rv32_virt_chip/src/clint.rs
@@ -1,0 +1,7 @@
+//! Machine Timer instantiation.
+
+use kernel::utilities::StaticRef;
+use sifive::clint::ClintRegisters;
+
+pub const CLINT_BASE: StaticRef<ClintRegisters> =
+    unsafe { StaticRef::new(0x0200_0000 as *const ClintRegisters) };

chips/qemu_rv32_virt_chip/src/plic.rs
@@ -1,0 +1,9 @@
+//! Instantiation of the sifive Platform Level Interrupt Controller
+
+use kernel::utilities::StaticRef;
+use sifive::plic::{Plic, PlicRegisters};
+
+pub const PLIC_BASE: StaticRef<PlicRegisters> =
+    unsafe { StaticRef::new(0x0c00_0000 as *const PlicRegisters) };
+
+pub static mut PLIC: Plic = Plic::new(PLIC_BASE);