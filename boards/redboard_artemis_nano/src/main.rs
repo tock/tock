tock/tock

commit e822de1742f24e24d3ef48dada9066f432150dd1

boards/redboard_artemis_nano: Setup initial tests
Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

boards/redboard_artemis_nano/src/main.rs
@@ -7,8 +7,12 @@
 // https://github.com/rust-lang/rust/issues/62184.
 #![cfg_attr(not(doc), no_main)]
 #![deny(missing_docs)]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use apollo3::chip::Apollo3DefaultPeripherals;
+use capsules::virtual_alarm::MuxAlarm;
 use capsules::virtual_alarm::VirtualMuxAlarm;
 use kernel::capabilities;
 use kernel::component::Component;
@@ -25,6 +29,9 @@ pub mod ble;
 /// Support routines for debugging I/O.
 pub mod io;
 
+#[cfg(test)]
+mod tests;
+
 // Number of concurrent processes this platform supports.
 const NUM_PROCS: usize = 4;
 
@@ -38,6 +45,21 @@ static mut PROCESS_PRINTER: Option<&'static kernel::process::ProcessPrinterText>
 
 // How should the kernel respond when a process faults.
 const FAULT_RESPONSE: kernel::process::PanicFaultPolicy = kernel::process::PanicFaultPolicy {};
+
+// Test access to the peripherals
+#[cfg(test)]
+static mut PERIPHERALS: Option<&'static Apollo3DefaultPeripherals> = None;
+// Test access to board
+#[cfg(test)]
+static mut BOARD: Option<&'static kernel::Kernel> = None;
+// Test access to platform
+#[cfg(test)]
+static mut PLATFORM: Option<&'static RedboardArtemisNano> = None;
+// Test access to main loop capability
+#[cfg(test)]
+static mut MAIN_CAP: Option<&dyn kernel::capabilities::MainLoopCapability> = None;
+// Test access to alarm
+static mut ALARM: Option<&'static MuxAlarm<'static, apollo3::stimer::STimer<'static>>> = None;
 
 /// Dummy buffer that causes the linker to reserve enough space for the stack.
 #[no_mangle]
@@ -118,11 +140,12 @@ impl KernelResources<apollo3::chip::Apollo3<Apollo3DefaultPeripherals>> for Redb
     }
 }
 
-/// Main function.
-///
-/// This is called after RAM initialization is complete.
-#[no_mangle]
-pub unsafe fn main() {
+unsafe fn setup() -> (
+    &'static kernel::Kernel,
+    &'static RedboardArtemisNano,
+    &'static apollo3::chip::Apollo3<Apollo3DefaultPeripherals>,
+    &'static Apollo3DefaultPeripherals,
+) {
     apollo3::init();
 
     let peripherals = static_init!(Apollo3DefaultPeripherals, Apollo3DefaultPeripherals::new());
@@ -136,7 +159,6 @@ pub unsafe fn main() {
 
     // initialize capabilities
     let process_mgmt_cap = create_capability!(capabilities::ProcessManagementCapability);
-    let main_loop_cap = create_capability!(capabilities::MainLoopCapability);
     let memory_allocation_cap = create_capability!(capabilities::MemoryAllocationCapability);
 
     let dynamic_deferred_call_clients =
@@ -221,6 +243,7 @@ pub unsafe fn main() {
         mux_alarm,
     )
     .finalize(components::alarm_component_helper!(apollo3::stimer::STimer));
+    ALARM = Some(mux_alarm);
 
     // Create a process printer for panic.
     let process_printer =
@@ -318,10 +341,54 @@ pub unsafe fn main() {
         debug!("{:?}", err);
     });
 
-    board_kernel.kernel_loop(
-        artemis_nano,
-        chip,
-        None::<&kernel::ipc::IPC<NUM_PROCS>>,
-        &main_loop_cap,
-    );
+    (board_kernel, artemis_nano, chip, peripherals)
+}
+
+/// Main function.
+///
+/// This function is called from the arch crate after some very basic RISC-V
+/// setup and RAM initialization.
+#[no_mangle]
+pub unsafe fn main() {
+    #[cfg(test)]
+    test_main();
+
+    #[cfg(not(test))]
+    {
+        let (board_kernel, esp32_c3_board, chip, _peripherals) = setup();
+
+        let main_loop_cap = create_capability!(capabilities::MainLoopCapability);
+
+        board_kernel.kernel_loop(
+            esp32_c3_board,
+            chip,
+            None::<&kernel::ipc::IPC<NUM_PROCS>>,
+            &main_loop_cap,
+        );
+    }
+}
+
+#[cfg(test)]
+use kernel::platform::watchdog::WatchDog;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    unsafe {
+        let (board_kernel, esp32_c3_board, _chip, peripherals) = setup();
+
+        BOARD = Some(board_kernel);
+        PLATFORM = Some(&esp32_c3_board);
+        PERIPHERALS = Some(peripherals);
+        MAIN_CAP = Some(&create_capability!(capabilities::MainLoopCapability));
+
+        PLATFORM.map(|p| {
+            p.watchdog().setup();
+        });
+
+        for test in tests {
+            test();
+        }
+    }
+
+    loop {}
 }

.gitignore
@@ -77,3 +77,6 @@ boards/swervolf/fusesoc_libraries/
 
 # ESP C3 temp hex
 boards/esp32-c3-devkitM-1/binary.hex
+
+# Redboard Artemis Nano test binary
+boards/redboard_artemis_nano/redboard-artemis-nano-tests.bin

boards/redboard_artemis_nano/.cargo/config
@@ -1,0 +1,2 @@
+[target.'cfg(target_arch = "arm")']
+runner = "./run.sh"

boards/redboard_artemis_nano/Makefile
@@ -24,3 +24,10 @@ flash-debug: $(TOCK_ROOT_DIRECTORY)target/$(TARGET)/debug/$(PLATFORM).bin
 .PHONY: flash-app
 flash-app:
 	python ambiq/ambiq_bin2board.py --bin $(APP) --load-address-blob 0x20000 -b 115200 -port $(PORT) -r 2 -v --magic-num 0xCB --version 0x0 --load-address-wired 0x40000 -i 6 --options 0x1
+
+.PHONY: test
+test:
+	mkdir -p $(TOCK_ROOT_DIRECTORY)target/$(TARGET)/release/deps/
+	$(Q)cp layout.ld $(TOCK_ROOT_DIRECTORY)target/$(TARGET)/release/deps/
+	$(Q)cp ../kernel_layout.ld $(TOCK_ROOT_DIRECTORY)target/$(TARGET)/release/
+	$(Q)RUSTFLAGS="$(RUSTC_FLAGS_TOCK)" OBJCOPY=${OBJCOPY} PORT=$(PORT) $(CARGO) test $(CARGO_FLAGS_TOCK_NO_BUILD_STD) $(NO_RUN) --bin $(PLATFORM) --release

boards/redboard_artemis_nano/run.sh
@@ -1,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+${OBJCOPY} --output-target=binary ${OBJCOPY_FLAGS} ${1} redboard-artemis-nano-tests.bin
+python ambiq/ambiq_bin2board.py --bin redboard-artemis-nano-tests.bin --load-address-blob 0x20000 -b 115200 -port ${PORT} -r 2 -v --magic-num 0xCB --version 0x0 --load-address-wired 0xc000 -i 6 --options 0x1
+
+# If we connect too quickly the UART doesn't work, so add a small delay
+sleep 1
+screen ${PORT} 115200

boards/redboard_artemis_nano/src/tests/mod.rs
@@ -1,0 +1,33 @@
+use crate::BOARD;
+use crate::CHIP;
+use crate::MAIN_CAP;
+use crate::NUM_PROCS;
+use crate::PLATFORM;
+use kernel::debug;
+
+fn run_kernel_op(loops: usize) {
+    unsafe {
+        for _i in 0..loops {
+            BOARD.unwrap().kernel_loop_operation(
+                PLATFORM.unwrap(),
+                CHIP.unwrap(),
+                None::<&kernel::ipc::IPC<NUM_PROCS>>,
+                true,
+                MAIN_CAP.unwrap(),
+            );
+        }
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    debug!("trivial assertion... ");
+    run_kernel_op(10000);
+
+    assert_eq!(1, 1);
+
+    debug!("    [ok]");
+    run_kernel_op(10000);
+}
+
+mod multi_alarm;

boards/redboard_artemis_nano/src/tests/multi_alarm.rs
@@ -1,0 +1,88 @@
+//! Test the behavior of a single alarm.
+//! To add this test, include the line
+//! ```
+//!    multi_alarm_test::run_alarm(alarm_mux);
+//! ```
+//! to the OpenTitan boot sequence, where `alarm_mux` is a
+//! `capsules::virtual_alarm::MuxAlarm`. The test sets up 3
+//! different virtualized alarms of random durations and prints
+//! out when they fire. The durations are uniformly random with
+//! one caveat, that 1 in 11 is of duration 0; this is to test
+//! that alarms whose expiration was in the past due to the
+//! latency of software work correctly.
+
+use crate::tests::run_kernel_op;
+use crate::ALARM;
+use apollo3::stimer::STimer;
+use capsules::test::random_alarm::TestRandomAlarm;
+use capsules::virtual_alarm::{MuxAlarm, VirtualMuxAlarm};
+use kernel::debug;
+use kernel::hil::time::Alarm;
+use kernel::static_init;
+
+static mut TESTS: Option<
+    [&'static TestRandomAlarm<'static, VirtualMuxAlarm<'static, STimer<'static>>>; 3],
+> = None;
+
+#[test_case]
+pub fn run_multi_alarm() {
+    debug!("start multi alarm test...");
+    unsafe {
+        TESTS = Some(static_init_multi_alarm_test(ALARM.unwrap()));
+        TESTS.unwrap()[0].run();
+        TESTS.unwrap()[1].run();
+        TESTS.unwrap()[2].run();
+    }
+
+    run_kernel_op(10000);
+
+    unsafe {
+        assert!(TESTS.unwrap()[0].counter.get() > 15);
+        assert!(TESTS.unwrap()[1].counter.get() > 30);
+        assert!(TESTS.unwrap()[2].counter.get() > 80);
+    }
+
+    debug!("    [ok]");
+    run_kernel_op(10000);
+}
+
+unsafe fn static_init_multi_alarm_test(
+    mux: &'static MuxAlarm<'static, STimer<'static>>,
+) -> [&'static TestRandomAlarm<'static, VirtualMuxAlarm<'static, STimer<'static>>>; 3] {
+    let virtual_alarm1 = static_init!(
+        VirtualMuxAlarm<'static, STimer<'static>>,
+        VirtualMuxAlarm::new(mux)
+    );
+    virtual_alarm1.setup();
+
+    let test1 = static_init!(
+        TestRandomAlarm<'static, VirtualMuxAlarm<'static, STimer<'static>>>,
+        TestRandomAlarm::new(virtual_alarm1, 19, 'A', false)
+    );
+    virtual_alarm1.set_alarm_client(test1);
+
+    let virtual_alarm2 = static_init!(
+        VirtualMuxAlarm<'static, STimer<'static>>,
+        VirtualMuxAlarm::new(mux)
+    );
+    virtual_alarm2.setup();
+
+    let test2 = static_init!(
+        TestRandomAlarm<'static, VirtualMuxAlarm<'static, STimer<'static>>>,
+        TestRandomAlarm::new(virtual_alarm2, 37, 'B', false)
+    );
+    virtual_alarm2.set_alarm_client(test2);
+
+    let virtual_alarm3 = static_init!(
+        VirtualMuxAlarm<'static, STimer<'static>>,
+        VirtualMuxAlarm::new(mux)
+    );
+    virtual_alarm3.setup();
+
+    let test3 = static_init!(
+        TestRandomAlarm<'static, VirtualMuxAlarm<'static, STimer<'static>>>,
+        TestRandomAlarm::new(virtual_alarm3, 89, 'C', false)
+    );
+    virtual_alarm3.set_alarm_client(test3);
+    [&*test1, &*test2, &*test3]
+}

commit 0b88e6ada208b72a945cf29d6c147e2bfc4580ae

boards/redboard_artemis: Fixup Hard Fault exception
Fixup a hardfaul exception that is seen on every non-test boot of Tock.

This change ensures that the `svc` call in `apollo3::init()` is called
inline with `main()`. I'm not clear why, but if the `svc` is called
after a function call (a `push` op) we end up with a corrupted call
stack.

We need to call the `svc` op to ensure the FPU is correctly disabled (as
the boot loader enables it).

While we are chaning the inline let's also never inline `setup()` to
reduce the stack size usage (see
https://github.com/tock/tock/issues/2425).

Without this change, I see faults like this on the board:

panicked at 'Kernel HardFault.
        Kernel version release-2.1-rc1
        r0  0x10001ab8
        r1  0x10001da0
        r2  0x10000fe8
        r3  0x10001e08
        r12 0x10001acc
        lr  0x10001d70
        pc  0x0
        prs 0x0 [ N 0 Z 0 C 0 V 0 Q 0 GE 0000 ; ICI.IT 0 T false ; Exc 0-Thread Mode ]
        sp  0x10000f80
        top of stack     0x40008000
        bottom of stack  0x10000000
        SHCSR 0x0
        CFSR  0x20000
        HSFR  0x40000000
        Instruction Access Violation:       false
        Data Access Violation:              false
        Memory Management Unstacking Fault: false
        Memory Management Stacking Fault:   false
        Memory Management Lazy FP Fault:    false
        Instruction Bus Error:              false
        Precise Data Bus Error:             false
        Imprecise Data Bus Error:           false
        Bus Unstacking Fault:               false
        Bus Stacking Fault:                 false
        Bus Lazy FP Fault:                  false
        Undefined Instruction Usage Fault:  false
        Invalid State Usage Fault:          true
        Invalid PC Load Usage Fault:        false
        No Coprocessor Usage Fault:         false
        Unaligned Access Usage Fault:       false
        Divide By Zero:                     false
        Bus Fault on Vector Table Read:     false
        Forced Hard Fault:                  true
        Faulting Memory Address: (valid: false) 0xE000ED34
        Bus Fault Address:       (valid: false) 0xE000ED38
', arch/cortex-m/src/lib.rs:496:5
        Kernel version release-2.1-rc1

Fixes: e822de1742 "boards/redboard_artemis_nano: Setup initial tests"

boards/redboard_artemis_nano/src/main.rs
@@ -151,14 +151,13 @@ impl KernelResources<apollo3::chip::Apollo3<Apollo3DefaultPeripherals>> for Redb
     }
 }
 
+#[inline(never)]
 unsafe fn setup() -> (
     &'static kernel::Kernel,
     &'static RedboardArtemisNano,
     &'static apollo3::chip::Apollo3<Apollo3DefaultPeripherals>,
     &'static Apollo3DefaultPeripherals,
 ) {
-    apollo3::init();
-
     let peripherals = static_init!(Apollo3DefaultPeripherals, Apollo3DefaultPeripherals::new());
 
     // No need to statically allocate mcu/pwr/clk_ctrl because they are only used in main!
@@ -394,6 +393,8 @@ unsafe fn setup() -> (
 /// setup and RAM initialization.
 #[no_mangle]
 pub unsafe fn main() {
+    apollo3::init();
+
     #[cfg(test)]
     test_main();
 

chips/apollo3/src/lib.rs
@@ -71,6 +71,7 @@ pub static IRQS: [unsafe extern "C" fn(); 32] = [CortexM4::GENERIC_ISR; 32];
 pub static PATCH: [unsafe extern "C" fn(); 16] = [unhandled_interrupt; 16];
 
 #[cfg(all(target_arch = "arm", target_os = "none"))]
+#[inline(always)]
 pub unsafe fn init() {
     use core::arch::asm;
     let cache_ctrl = crate::cachectrl::CacheCtrl::new();