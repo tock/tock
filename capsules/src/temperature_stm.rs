28e548270694e3faa735c400f849f425e2357988
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2022-08-25 00:58:10 +0300

    fix temperature_stm driver to use 16 bits

--- capsules/src/temperature_stm.rs
+++ capsules/src/temperature_stm.rs
@@ -42,7 +42,7 @@ impl<'a> adc::Client for TemperatureSTM<'a> {
         self.status.set(Status::Idle);
         self.temperature_client.map(|client| {
             client.callback(
-                ((((self.v_25 - (sample as f32 * 3.3 / 4095.0)) * 1000.0 / self.slope) + 25.0)
+                ((((self.v_25 - (sample as f32 * 3.3 / 65535.0)) * 1000.0 / self.slope) + 25.0)
                     * 100.0) as usize,
             );
         });
