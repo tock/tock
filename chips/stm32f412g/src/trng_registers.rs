424257c1d5151c8b609f7a0cfe9e30aae5b8fb95
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2022-08-31 22:03:49 +0300

    use pub crate

--- chips/stm32f412g/src/trng_registers.rs
+++ chips/stm32f412g/src/trng_registers.rs
@@ -3,5 +3,5 @@
 use kernel::utilities::StaticRef;
 use stm32f4xx::trng::RngRegisters;
 
-pub const RNG_BASE: StaticRef<RngRegisters> =
+pub(crate) const RNG_BASE: StaticRef<RngRegisters> =
     unsafe { StaticRef::new(0x5006_0800 as *const RngRegisters) };
