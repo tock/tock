026361c7e830586c3eb25f2345b0449618c16078
teonaseverin <teona.severin9@gmail.com>
2022-11-10 18:44:46 +0200

    modified license

--- chips/stm32f4xx/src/can.rs
+++ chips/stm32f4xx/src/can.rs
@@ -1,5 +1,6 @@
-// Copyright 2022 OxidOS Automotive SRL
-// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright OxidOS Automotive SRL 2022.
 //
 // Author: Teona Severin <teona.severin@oxidos.io>
 
