5fa97269b993b8e0c4ca9e4c075d74acbfb6614d
Ioan-Cristian CÎRSTEA <ioan-cristian.cirstea@tutanota.com>
2022-12-24 17:28:03 +0200

    Added bitfields for the CSR register

--- chips/rp2040/src/pwm.rs
+++ chips/rp2040/src/pwm.rs
@@ -1,7 +1,32 @@
 //! PWM driver for RP2040.
 
 //use kernel::hil;
-use kernel::utilities::registers::{ReadWrite, ReadOnly, WriteOnly};
+use kernel::utilities::registers::{register_bitfields, ReadWrite, ReadOnly, WriteOnly};
+
+register_bitfields![u32,
+    CSR [
+        EN OFFSET(0) NUMBITS(1) [], // Enable PWM channel
+        PH_CORRECT OFFSET(1) NUMBITS(1) [], // Enable phase-correct modulation
+        A_INV OFFSET(2) NUMBITS(1) [], // Invert output A
+        B_INV OFFSET(3) NUMBITS(1) [], // Invert output B
+        // PWM slice event selection for fractional clock divider
+        // Default value = FREE_RUNNING (always on)
+        // If the event is different from FREE_RUNNING, then pin B becomes
+        // an input pin
+        DIVMOD OFFSET(4) NUMBITS(2) [
+            FREE_RUNNING = 0, // Free-running counting at rate dictated by fractional divider
+            B_HIGH = 1, // Fractional divider operation is gated by the PWM B pin
+            B_RISING = 2, // Counter advances with each rising edge of the PWM B pin
+            B_FALLING = 3 // Counter advances with each falling edge of the PWM B pin
+        ],
+        // Retard the phase of the counter by 1 count, while it is running
+        // Self-clearing. Write a 1, and poll until low. Counter must be running.
+        PH_RET OFFSET(6) NUMBITS(1) [],
+        // Advance the phase of the counter by 1 count, while it is running
+        // Self clearing. Write a 1, and poll until low. Counter must be running.
+        PH_ADV OFFSET(7) NUMBITS(1) []
+    ]
+];
 
 #[repr(C)]
 struct Ch {
