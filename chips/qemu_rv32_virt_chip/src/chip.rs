lschuermann/hsm-tock

qemu_rv32_virt_chip: add memory-mapped VirtIO support
The QEMU riscv32 "virt" machine type has 8 memory-mapped VirtIO
transports (VirtIO over MMIO) built in, starting from address
0x10001000.

This wires up these transports with VirtIOMMIODevices, such that the
corresponding board can utilize devices attached to these
transports. It also routes interrupts to the peripherals.

Signed-off-by: Leon Schuermann <leon@is.currently.online>

chips/qemu_rv32_virt_chip/src/chip.rs
@@ -32,12 +32,23 @@ pub struct QemuRv32VirtChip<'a, A: 'static + Alarm<'static>, I: InterruptService
 
 pub struct QemuRv32VirtDefaultPeripherals<'a> {
     pub uart0: crate::uart::Uart16550<'a>,
+    pub virtio_mmio: [VirtIOMMIODevice; 8],
 }
 
 impl<'a> QemuRv32VirtDefaultPeripherals<'a> {
     pub fn new() -> Self {
         Self {
             uart0: crate::uart::Uart16550::new(crate::uart::UART0_BASE),
+            virtio_mmio: [
+                VirtIOMMIODevice::new(crate::virtio::VIRTIO_MMIO_0_BASE),
+                VirtIOMMIODevice::new(crate::virtio::VIRTIO_MMIO_1_BASE),
+                VirtIOMMIODevice::new(crate::virtio::VIRTIO_MMIO_2_BASE),
+                VirtIOMMIODevice::new(crate::virtio::VIRTIO_MMIO_3_BASE),
+                VirtIOMMIODevice::new(crate::virtio::VIRTIO_MMIO_4_BASE),
+                VirtIOMMIODevice::new(crate::virtio::VIRTIO_MMIO_5_BASE),
+                VirtIOMMIODevice::new(crate::virtio::VIRTIO_MMIO_6_BASE),
+                VirtIOMMIODevice::new(crate::virtio::VIRTIO_MMIO_7_BASE),
+            ],
         }
     }
 }
@@ -45,9 +56,15 @@ impl<'a> QemuRv32VirtDefaultPeripherals<'a> {
 impl<'a> InterruptService<()> for QemuRv32VirtDefaultPeripherals<'a> {
     unsafe fn service_interrupt(&self, interrupt: u32) -> bool {
         match interrupt {
-            interrupts::UART0 => {
-                self.uart0.handle_interrupt();
-            }
+            interrupts::UART0 => self.uart0.handle_interrupt(),
+            interrupts::VIRTIO_MMIO_0 => self.virtio_mmio[0].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_1 => self.virtio_mmio[1].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_2 => self.virtio_mmio[2].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_3 => self.virtio_mmio[3].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_4 => self.virtio_mmio[4].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_5 => self.virtio_mmio[5].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_6 => self.virtio_mmio[6].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_7 => self.virtio_mmio[7].handle_interrupt(),
             _ => return false,
         }
         true

chips/qemu_rv32_virt_chip/src/lib.rs
@@ -10,3 +10,4 @@ pub mod chip;
 pub mod clint;
 pub mod plic;
 pub mod uart;
+pub mod virtio;

tock/tock

chips/qemu_rv32_virt_chip/src/chip.rs
@@ -18,6 +18,8 @@ use sifive::plic::Plic;
 
 use crate::interrupts;
 
+use virtio::transports::mmio::VirtIOMMIODevice;
+
 type QemuRv32VirtPMP = PMP<8>;
 
 pub type QemuRv32VirtClint<'a> = sifive::clint::Clint<'a, Freq10MHz>;
@@ -32,12 +34,23 @@ pub struct QemuRv32VirtChip<'a, I: InterruptService<()> + 'a> {
 
 pub struct QemuRv32VirtDefaultPeripherals<'a> {
     pub uart0: crate::uart::Uart16550<'a>,
+    pub virtio_mmio: [VirtIOMMIODevice; 8],
 }
 
 impl<'a> QemuRv32VirtDefaultPeripherals<'a> {
     pub fn new() -> Self {
         Self {
             uart0: crate::uart::Uart16550::new(crate::uart::UART0_BASE),
+            virtio_mmio: [
+                VirtIOMMIODevice::new(crate::virtio_mmio::VIRTIO_MMIO_0_BASE),
+                VirtIOMMIODevice::new(crate::virtio_mmio::VIRTIO_MMIO_1_BASE),
+                VirtIOMMIODevice::new(crate::virtio_mmio::VIRTIO_MMIO_2_BASE),
+                VirtIOMMIODevice::new(crate::virtio_mmio::VIRTIO_MMIO_3_BASE),
+                VirtIOMMIODevice::new(crate::virtio_mmio::VIRTIO_MMIO_4_BASE),
+                VirtIOMMIODevice::new(crate::virtio_mmio::VIRTIO_MMIO_5_BASE),
+                VirtIOMMIODevice::new(crate::virtio_mmio::VIRTIO_MMIO_6_BASE),
+                VirtIOMMIODevice::new(crate::virtio_mmio::VIRTIO_MMIO_7_BASE),
+            ],
         }
     }
 }
@@ -45,9 +58,15 @@ impl<'a> QemuRv32VirtDefaultPeripherals<'a> {
 impl<'a> InterruptService<()> for QemuRv32VirtDefaultPeripherals<'a> {
     unsafe fn service_interrupt(&self, interrupt: u32) -> bool {
         match interrupt {
-            interrupts::UART0 => {
-                self.uart0.handle_interrupt();
-            }
+            interrupts::UART0 => self.uart0.handle_interrupt(),
+            interrupts::VIRTIO_MMIO_0 => self.virtio_mmio[0].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_1 => self.virtio_mmio[1].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_2 => self.virtio_mmio[2].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_3 => self.virtio_mmio[3].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_4 => self.virtio_mmio[4].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_5 => self.virtio_mmio[5].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_6 => self.virtio_mmio[6].handle_interrupt(),
+            interrupts::VIRTIO_MMIO_7 => self.virtio_mmio[7].handle_interrupt(),
             _ => return false,
         }
         true

chips/qemu_rv32_virt_chip/src/lib.rs
@@ -4,7 +4,10 @@
 #![crate_name = "qemu_rv32_virt_chip"]
 #![crate_type = "rlib"]
 
+pub use virtio;
+
 mod interrupts;
+pub mod virtio_mmio;
 
 pub mod chip;
 pub mod clint;