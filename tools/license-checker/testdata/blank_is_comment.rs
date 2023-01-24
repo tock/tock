b004a36461dc4dbe84dede30a7505df9bdc273d1
Johnathan Van Why <jrvanwhy@google.com>
2023-01-23 16:34:22 -0800

    `licensecheck`: Allow an empty comment line after the header.
    `trd-legal.md` says:
    
    > License and copyright information SHOULD have at least one (1) blank line separating it from any other content in the file.
    
    When I implemented the license checker, I interpreted that as disallowing an empty comment. However, a few files in this repository have an empty comment line after the header instead. This changes the license checker to accept either a truly blank line or an empty comment line after the license header.

---
+++ tools/license-checker/testdata/blank_is_comment.rs
@@ -0,0 +1,8 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2023.
+// Copyright Google LLC 2023.
+//
+// This file has an empty comment line rather than a truly blank line after the
+// header. This should be accepted as well, as it makes sense when the Tock
+// license header is followed by an "Author: " comment.
