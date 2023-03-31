7d44a7bd69b14034bc5046f4a5821a34923ed42b
Ioan-Cristian CÎRSTEA <ioan-cristian.cirstea@tutanota.com>
2023-03-31 23:04:17 +0300

    Added flash import in every stm32f4 chip

--- chips/stm32f401cc/src/lib.rs
+++ chips/stm32f401cc/src/lib.rs
@@ -2,7 +2,7 @@
 
 use cortexm4::{unhandled_interrupt, CortexM4, CortexMVariant};
 
-pub use stm32f4xx::{adc, chip, dbg, dma, exti, gpio, nvic, rcc, spi, syscfg, tim2, usart};
+pub use stm32f4xx::{adc, chip, dbg, dma, exti, flash, gpio, nvic, rcc, spi, syscfg, tim2, usart};
 
 pub mod interrupt_service;
 
