807d1ba8919bc03e85059fae48dd5753ec6d7f4e
Ioan-Cristian CÎRSTEA <ioan-cristian.cirstea@tutanota.com>
2023-05-12 15:53:46 +0300

    Updated Ethernet HIL

--- kernel/src/hil/ethernet.rs
+++ kernel/src/hil/ethernet.rs
@@ -1,3 +1,13 @@
+use crate::ErrorCode;
+
 pub trait TransmitClient {
     fn transmitted_frame(&self, transmit_status: Result<(), ErrorCode>);
+}
+
+pub trait ReceiveClient {
+    fn received_frame(&self,
+        receive_status: Result<(), ErrorCode>,
+        received_frame: &mut [u8],
+        received_frame_length: usize
+    );
 }
