8b89c4343dc4d1689f44dae779fcd32481e040ab
Michał Mazurek <maz@semihalf.com>
2023-05-29 16:52:13 +0200

    chip/earlgrey: Replacing peripheral addresses with autogenerated consts
    Replacing values of addresses in device driver initialization with
    auto-generated const form OT chip definitions.
    
    Signed-off-by: Michał Mazurek <maz@semihalf.com>

--- chips/earlgrey/src/usbdev.rs
+++ chips/earlgrey/src/usbdev.rs
@@ -2,9 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // Copyright Tock Contributors 2022.
 
+use crate::registers::top_earlgrey::TOP_EARLGREY_USBDEV_BASE_ADDR;
 use kernel::utilities::StaticRef;
 pub use lowrisc::usbdev::Usb;
 use lowrisc::usbdev::UsbRegisters;
 
 pub const USB0_BASE: StaticRef<UsbRegisters> =
-    unsafe { StaticRef::new(0x4032_0000 as *const UsbRegisters) };
+    unsafe { StaticRef::new(TOP_EARLGREY_USBDEV_BASE_ADDR as *const UsbRegisters) };
