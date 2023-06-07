a1e14413311427daf952f1be5b2c62d69fdccfb7
Brad Campbell <bradjc5@gmail.com>
2023-06-07 19:35:52 -0400

    siphash24: add test

--- boards/nordic/nrf52840dk/src/test/mod.rs
+++ boards/nordic/nrf52840dk/src/test/mod.rs
@@ -2,11 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // Copyright Tock Contributors 2023.
 
-pub mod aes;
-pub mod aes_ccm;
-pub mod aes_gcm;
-pub mod crc;
-pub mod kv_system;
-pub mod sha256;
-pub mod siphash24;
-pub mod udp;
+pub(crate) mod siphash24_test;
