6abe94dbc0ed3f60c2b9fc1b3977e2136aa409eb
Jakub Kowalski <32602977+j-kk@users.noreply.github.com>
2023-07-18 01:36:17 +0200

    Added impl using the NonvolatileToPages, docs, error handling, tested

--- capsules/extra/src/eeprom.rs
+++ capsules/extra/src/eeprom.rs
@@ -1,23 +1,80 @@
+//! Driver for the AT24C32/64 EEPROM memory. Built on top of the I2C interface.
+//! Provides interface for the NonvolatileToPages driver.
+//!
+//! Datasheet:
+//! <https://ww1.microchip.com/downloads/en/devicedoc/doc0336.pdf>
+//!
+//! > The AT24C32/64 provides 32,768/65,536 bits of serial electrically erasable and programmable
+//! > read only memory (EEPROM) organized as 4096/8192 words of 8 bits each. The device’s cascadable
+//! > feature allows up to 8 devices to share a common 2- wire bus. The device is optimized for use
+//! > in many industrial and commercial applica- tions where low power and low voltage operation are
+//! > essential. The AT24C32/64 is available in space saving 8-pin JEDEC PDIP, 8-pin JEDEC SOIC,
+//! > 8-pin EIAJ SOIC, and 8-pin TSSOP (AT24C64) packages and is accessed via a 2-wire serial
+//! > interface.
+//!
+//! Usage
+//! -----
+//!
+//! ```rust
+//! let i2cmux = I2CMuxComponent::new(i2c0, None).finalize(components::i2c_mux_component_static!());
+//!
+//! let eeprom_buffer = static_init!([u8; 34], [0; 34]);
+//!
+//! let eeprom_i2c_device = static_init!(I2CDevice, I2CDevice::new(i2cmux, 0x50));
+//! let eeprom_capsule = static_init!(capsules_extra::eeprom::EEPROM,capsules_extra::eeprom::EEPROM::new(
+//!             eeprom_i2c_device,
+//!             eeprom_buffer,
+//!         ) );
+//! eeprom_i2c_device.set_client(eeprom_capsule);
+//!
+//! let nonvolatile_storage = components::nonvolatile_storage::NonvolatileStorageComponent::new(
+//!         board_kernel,
+//!         capsules_extra::nonvolatile_storage_driver::DRIVER_NUM,
+//!         eeprom_capsule,
+//!         0x0,
+//!         0x10000,
+//!         0x0,
+//!         0x0,
+//!     ).finalize(components::nonvolatile_storage_component_static!(capsules_extra::eeprom::EEPROM));
+//! ```
+
 use core::cell::Cell;
 use core::cmp;
-use kernel::hil::nonvolatile_storage::{NonvolatileStorage, NonvolatileStorageClient};
 
-use kernel::ErrorCode;
+use kernel::{ErrorCode, hil};
 use kernel::hil::i2c::{Error, I2CClient, I2CDevice};
 use kernel::utilities::cells::{OptionalCell, TakeCell};
+
+
+const PAGE_SIZE: usize = 32;
+
+pub struct EEPROMPage(pub [u8; PAGE_SIZE]);
+
+impl Default for EEPROMPage {
+    fn default() -> Self {
+        Self([0; PAGE_SIZE])
+    }
+}
+
+impl AsMut<[u8]> for EEPROMPage {
+    fn as_mut(&mut self) -> &mut [u8] {
+        &mut self.0
+    }
+}
 
 #[derive(Copy, Clone, Debug)]
 enum State {
     Idle,
-    Reading(usize),
-    Writing(usize),
+    Reading,
+    Writing,
+    Erasing,
 }
 
 pub struct EEPROM<'a> {
     i2c: &'a dyn I2CDevice,
     buffer: TakeCell<'static, [u8]>,
-    client_buffer: TakeCell<'a, [u8]>,
-    client: OptionalCell<&'a dyn NonvolatileStorageClient<'a>>,
+    client_page: TakeCell<'a, EEPROMPage>,
+    flash_client: OptionalCell<&'a dyn hil::flash::Client<EEPROM<'a>>>,
     state: Cell<State>,
 }
 
@@ -27,47 +84,76 @@ impl<'a> EEPROM<'a> {
         Self {
             i2c,
             buffer: TakeCell::new(buffer),
-            client_buffer: TakeCell::empty(),
-            client: OptionalCell::empty(),
+            client_page: TakeCell::empty(),
+            flash_client: OptionalCell::empty(),
             state: Cell::new(State::Idle),
         }
     }
 
-    fn read(&self, buffer: &'a mut [u8], address: usize, length: usize) -> Result<(), ErrorCode> {
+    fn read_sector(&self, page_number: usize, buf: &'static mut EEPROMPage) -> Result<(), (ErrorCode, &'static mut EEPROMPage)> {
         self.i2c.enable();
-        self.buffer.take().map_or(Err(ErrorCode::RESERVE), move |local_buffer| {
-            local_buffer[0] = ((address >> 8) & 0x00ff) as u8;
-            local_buffer[1] = (address & 0x00ff) as u8;
+        let address = page_number * PAGE_SIZE;
+        if let Some(rxbuffer) = self.buffer.take() {
+            rxbuffer[0] = ((address >> 8) & 0x00ff) as u8;
+            rxbuffer[1] = (address & 0x00ff) as u8;
 
-            let read_len = cmp::min(buffer.len(), length);
-
-            self.client_buffer.replace(buffer);
-
-            self.state.set(State::Reading(length));
-            if let Err((error, local_buffer)) = self.i2c.write_read(local_buffer, 2, read_len) {
+            self.state.set(State::Reading);
+            if let Err((error, local_buffer)) = self.i2c.write_read(rxbuffer, 2, PAGE_SIZE) {
                 self.buffer.replace(local_buffer);
                 self.i2c.disable();
-                Err(error.into())
+                Err((error.into(), buf))
             } else {
+                self.client_page.replace(buf);
                 Ok(())
             }
-        })
+        } else {
+            Err((ErrorCode::RESERVE, buf))
+        }
     }
 
-    fn write(&self, buffer: &'a mut [u8], address: usize, length: usize) -> Result<(), ErrorCode> {
-        self.i2c.enable();
-        self.buffer.take().map_or(Err(ErrorCode::RESERVE), move |txbuffer| {
+    fn write_sector(&self, page_number: usize, buf: &'static mut EEPROMPage) -> Result<(), (ErrorCode, &'static mut EEPROMPage)> {
+        let address = page_number * PAGE_SIZE;
+        let length = buf.0.len();
+        // Schedule page write and do first
+        if let Some(txbuffer) = self.buffer.take() {
             txbuffer[0] = ((address >> 8) & 0x00ff) as u8;
             txbuffer[1] = (address & 0x00ff) as u8;
 
             let write_len = cmp::min(txbuffer.len() - 2, length);
+
             for i in 0..write_len {
-                txbuffer[(i + 2)] = buffer[i];
+                txbuffer[(i + 2)] = buf.0[i];
             }
 
-            self.client_buffer.replace(buffer);
+            self.state.set(State::Writing);
+            if let Err((error, txbuffer)) = self.i2c.write(txbuffer, write_len + 2) {
+                self.buffer.replace(txbuffer);
+                self.i2c.disable();
+                Err((error.into(), buf))
+            } else {
+                self.client_page.replace(buf);
+                Ok(())
+            }
+        } else {
+            Err((ErrorCode::RESERVE, buf))
+        }
+    }
 
-            self.state.set(State::Writing(length));
+    fn erase_sector(&self, page_number: usize) -> Result<(), ErrorCode> {
+        self.i2c.enable();
+        let address = page_number * PAGE_SIZE;
+        // Schedule page write and do first
+        if let Some(txbuffer) = self.buffer.take() {
+            txbuffer[0] = ((address >> 8) & 0x00ff) as u8;
+            txbuffer[1] = (address & 0x00ff) as u8;
+
+            let write_len = cmp::min(txbuffer.len() - 2, PAGE_SIZE);
+
+            for i in 0..write_len {
+                txbuffer[(i + 2)] = 0;
+            }
+
+            self.state.set(State::Erasing);
             if let Err((error, txbuffer)) = self.i2c.write(txbuffer, write_len + 2) {
                 self.buffer.replace(txbuffer);
                 self.i2c.disable();
@@ -75,56 +161,87 @@ impl<'a> EEPROM<'a> {
             } else {
                 Ok(())
             }
-        })
+        } else {
+            Err(ErrorCode::RESERVE)
+        }
     }
 }
 
-impl<'a> I2CClient for EEPROM<'a> {
+impl I2CClient for EEPROM<'static> {
     fn command_complete(&self, buffer: &'static mut [u8], status: Result<(), Error>) {
         match self.state.get() {
-            State::Reading(read_len) => {
+            State::Reading => {
                 self.state.set(State::Idle);
-                if status.is_err() {
-                    self.buffer.replace(buffer);
-                    // TODO
-                    return;
-                }
-                self.client_buffer.take().map(|client_buffer| {
-                    for i in 0..read_len {
-                        client_buffer[i] = buffer[i];
+                self.i2c.disable();
+                if let Some(client_page) = self.client_page.take() {
+                    for i in 0..PAGE_SIZE {
+                        client_page.0[i] = buffer[i];
                     }
-                    self.client.map(move |client| client.read_done(client_buffer, read_len));
-                });
-                self.buffer.replace(buffer);
-                self.i2c.disable();
+                    self.buffer.replace(buffer);
+                    self.flash_client.map(|client| {
+                        if status.is_err() {
+                            client.read_complete(client_page, hil::flash::Error::FlashError);
+                        } else {
+                            client.read_complete(client_page, hil::flash::Error::CommandComplete);
+                        }
+                    });
+                } else {
+                    self.buffer.replace(buffer);
+                }
             }
-            State::Writing(write_len) => {
+            State::Writing => {
                 self.state.set(State::Idle);
                 self.buffer.replace(buffer);
-                if status.is_err() {
-                    // TODO
-                    return;
-                }
-                self.client_buffer.take().map(move |client_buffer| {
-                    self.client.map(move |client| client.write_done(client_buffer, write_len))
-                });
                 self.i2c.disable();
+                self.flash_client.map(|client| {
+                    if let Some(client_page) = self.client_page.take() {
+                        if status.is_err() {
+                            client.write_complete(client_page, hil::flash::Error::FlashError);
+                        } else {
+                            client.write_complete(client_page, hil::flash::Error::CommandComplete);
+                        }
+                    } else {
+                        panic!("EEPROM Writing op completed without client_page");
+                    }
+                });
+            }
+            State::Erasing => {
+                self.state.set(State::Idle);
+                self.buffer.replace(buffer);
+                self.i2c.disable();
+                self.flash_client.map(move |client| {
+                    if status.is_err() {
+                        client.erase_complete(hil::flash::Error::FlashError);
+                    } else {
+                        client.erase_complete(hil::flash::Error::CommandComplete);
+                    }
+                });
             }
             State::Idle => {}
         }
     }
 }
 
-impl<'a> NonvolatileStorage<'a> for EEPROM<'a> {
-    fn set_client(&self, client: &'a dyn NonvolatileStorageClient<'a>) {
-        self.client.set(client)
+
+impl<'a> hil::flash::Flash for EEPROM<'a> {
+    type Page = EEPROMPage;
+
+    fn read_page(&self, page_number: usize, buf: &'static mut Self::Page) -> Result<(), (ErrorCode, &'static mut Self::Page)> {
+        self.read_sector(page_number, buf)
     }
 
-    fn read(&self, buffer: &'a mut [u8], address: usize, length: usize) -> Result<(), ErrorCode> {
-        self.read(buffer, address, length)
+    fn write_page(&self, page_number: usize, buf: &'static mut Self::Page) -> Result<(), (ErrorCode, &'static mut Self::Page)> {
+        self.write_sector(page_number, buf)
     }
 
-    fn write(&self, buffer: &'a mut [u8], address: usize, length: usize) -> Result<(), ErrorCode> {
-        self.write(buffer, address, length)
+    fn erase_page(&self, page_number: usize) -> Result<(), ErrorCode> {
+        self.erase_sector(page_number)
+    }
+}
+
+impl<'a, C: hil::flash::Client<Self>> hil::flash::HasClient<'a, C> for EEPROM<'a>
+{
+    fn set_client(&'a self, client: &'a C) {
+        self.flash_client.set(client);
     }
 }
