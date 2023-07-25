f7af5cd21e53bd1d58d5892c12cd142c8eb9b17b
Leon Schuermann <leon@is.currently.online>
2023-07-24 22:30:57 -0400

    Copy `boards/nordic/nrf52840dk` to `boards/nordic/nrf52840dk_demo`

---
+++ boards/nordic/nrf52840dk_demo/jtag/jdbserver_pca10040.sh
@@ -0,0 +1,5 @@
+# Licensed under the Apache License, Version 2.0 or the MIT License.
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+# Copyright Tock Contributors 2023.
+
+JLinkGDBServer -device nrf52 -speed 1200 -if swd -AutoConnect 1 -port 2331
