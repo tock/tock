757c6efe8e66353e88e4c804a20f9d179c855592
Chris Frantz <cfrantz@google.com>
2023-08-04 09:03:32 -0700

    Give more stack for OpenTitan init
    The tests overflow the stack; give some more space so they can run.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- boards/opentitan/src/main.rs
+++ boards/opentitan/src/main.rs
@@ -102,7 +102,7 @@ const FAULT_RESPONSE: kernel::process::PanicFaultPolicy = kernel::process::Panic
 /// Dummy buffer that causes the linker to reserve enough space for the stack.
 #[no_mangle]
 #[link_section = ".stack_buffer"]
-pub static mut STACK_MEMORY: [u8; 0x1000] = [0; 0x1000];
+pub static mut STACK_MEMORY: [u8; 0x1400] = [0; 0x1400];
 
 /// A structure representing this platform that holds references to all
 /// capsules for this platform. We've included an alarm and console.
