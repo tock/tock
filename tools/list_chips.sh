b0cfc0653220c528aaf3e98bf01ba89eeaccbcc2
Ioan-Cristian CÎRSTEA <ioan.cirstea@oxidos.io>
2023-09-16 14:02:56 +0300

    Don't exclude stm32f4xx crate from list_chips.sh output

--- tools/list_chips.sh
+++ tools/list_chips.sh
@@ -8,8 +8,4 @@
 for b in $(find chips -maxdepth 4 -name 'Cargo.toml'); do
     b1=${b#chips/}
     b2=${b1%/*}
-    # `stm32f4xx` crate is excluded in order to pass CI
-    # This is due to the use of Rust features and the lack of support from
-    # `cargo test` and `cargo clippy`
-    echo $b2 | grep --invert-match "stm32f4xx"
 done
