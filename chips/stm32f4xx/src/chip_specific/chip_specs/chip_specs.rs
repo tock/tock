11ad60b6af1dfc2dc432e2952dd3c8b6650d26a9
Ioan-Cristian CÎRSTEA <ioan.cirstea@oxidos.io>
2023-09-19 18:35:57 +0300

    Added comment to better explain the intent of the file

--- chips/stm32f4xx/src/chip_specific/chip_specs/chip_specs.rs
+++ chips/stm32f4xx/src/chip_specific/chip_specs/chip_specs.rs
@@ -5,6 +5,9 @@
 // Author: Ioan-Cristian CÎRSTEA <ioan.cirstea@oxidos.io>
 
 //! Trait that encompasses chip specifications
+//!
+//! The main use of this trait is to be passed as a bound for the type parameter for chip
+//! peripherals in crates such as `stm32f429zi`.
 
 use crate::chip_specific::clock_constants::{ClockConstants};
 use crate::chip_specific::flash::{FlashChipSpecific};
