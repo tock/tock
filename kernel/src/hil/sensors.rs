705d40b99f14e52e75a7f1be9ba5d3731ebb99dd
TheButterMineCutter <TheButterMineCutter@gmail.com>
2023-10-05 23:27:13 -0400

    Added Documentation

--- kernel/src/hil/sensors.rs
+++ kernel/src/hil/sensors.rs
@@ -214,7 +214,15 @@ pub trait SoundPressureClient {
 
 /// A Basic interface for a barometer sensor.
 pub trait PressureDriver<'a> {
+    /// Used to initialize a atmospheric pressure reading
+    /// 
+    /// This function might return the following errors:
+    /// - `BUSY`: Indicates that the hardware is busy with an existing
+    ///           operation or initialisation/calibration.
+    /// - `FAIL`: Failed to correctly communicate over communication protocol.
+    /// - `NOSUPPORT`: Indicates that this data type isn't supported.
     fn read_atmospheric_pressure(&self) -> Result<(), ErrorCode>;
+
     /// Set the client
     fn set_client(&self, client: &'a dyn PressureClient);
 }
