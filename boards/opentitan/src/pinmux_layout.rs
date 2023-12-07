tock/tock

commit 7523a538100544bec5ada6799a795f97d6ff1485

board/earlgrey: Add board sepcific pinmux setup
Following commit introduce new trait that allow to configure
Pinmux during board initialization. The new interface allows to
configure Pinmux for specific board layout. When new board that
uses Ealgrey chip is added user should also implement this trait
to provide support for board specific I/O layout.

* Add new trait EarlGreyPinmuxConfig for passing baord I/O layout to chip drivers
* Add CW310 pinmux layout configuration, compatible with OpenTitan Hyperdebug
* Initialize GPIO port based on currently board I/O layout.

Signed-off-by: Michał Mazurek <mazurekm@google.com>

boards/opentitan/src/pinmux_layout.rs
@@ -1,0 +1,140 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2023.
+
+use earlgrey::pinmux_config::{EarlGreyPinmuxConfig, INPUT_NUM, OUTPUT_NUM};
+use earlgrey::registers::top_earlgrey::{PinmuxInsel, PinmuxOutsel};
+
+type In = PinmuxInsel;
+type Out = PinmuxOutsel;
+
+pub enum BoardPinmuxLayout {}
+
+/// Implementations of Pinmux initial board configurations.
+/// Defined Pinmux layout is designed for CW310 FPGA board
+/// and is compatible with Hyperdebug test board IO layout.
+/// In feature we should add layouts compatible with other
+/// OpenTitan boards.
+/// Source of true:
+/// <OPENTITAN_TREE/hw/top_earlgrey/data/pins_cw310_hyperdebug.xdc>
+impl EarlGreyPinmuxConfig for BoardPinmuxLayout {
+    /// Array of input selector initial configurations
+    #[rustfmt::skip]
+    const INPUT: &'static [PinmuxInsel; INPUT_NUM] = &[
+        In::Ioa2,         // GpioGpio0
+        In::Ioa3,         // GpioGpio1
+        In::Ioa6,         // GpioGpio2
+        In::Iob0,         // GpioGpio3
+        In::Iob1,         // GpioGpio4
+        In::Iob2,         // GpioGpio5
+        In::Iob3,         // GpioGpio6
+        In::Iob6,         // GpioGpio7
+        In::Iob7,         // GpioGpio8
+        In::Iob8,         // GpioGpio9
+        In::Ioc0,         // GpioGpio10
+        In::Ioc1,         // GpioGpio11
+        In::Ioc2,         // GpioGpio12
+        In::Ioc5,         // GpioGpio13
+        In::Ioc6,         // GpioGpio14
+        In::Ioc7,         // GpioGpio15
+        In::Ioc8,         // GpioGpio16
+        In::Ioc9,         // GpioGpio17
+        In::Ioc10,        // GpioGpio18
+        In::Ioc11,        // GpioGpio19
+        In::Ioc12,        // GpioGpio20
+        In::Ior0,         // GpioGpio21
+        In::Ior1,         // GpioGpio22
+        In::Ior2,         // GpioGpio23
+        In::Ior3,         // GpioGpio24
+        In::Ior4,         // GpioGpio25
+        In::Ior5,         // GpioGpio26
+        In::Ior6,         // GpioGpio27
+        In::Ior7,         // GpioGpio28
+        In::Ior10,        // GpioGpio29
+        In::Ior11,        // GpioGpio30
+        In::Ior12,        // GpioGpio31
+        In::Ioa7,         // I2c0Sda
+        In::Ioa8,         // I2c0Scl
+        In::Iob10,        // I2c1Sda
+        In::Iob9,         // I2c1Scl
+        In::Iob11,        // I2c2Sda
+        In::Iob12,        // I2c2Scl
+        In::ConstantZero, // SpiHost1Sd0
+        In::ConstantZero, // SpiHost1Sd1
+        In::ConstantZero, // SpiHost1Sd2
+        In::ConstantZero, // SpiHost1Sd3
+        In::Ioa0,         // Uart0Rx
+        In::Ioa4,         // Uart1Rx
+        In::Iob4,         // Uart2Rx
+        In::Ioc3,         // Uart3Rx
+        In::ConstantZero, // SpiDeviceTpmCsb
+        In::ConstantZero, // FlashCtrlTck
+        In::ConstantZero, // FlashCtrlTms
+        In::ConstantZero, // FlashCtrlTdi
+        In::ConstantZero, // SysrstCtrlAonAcPresent
+        In::ConstantZero, // SysrstCtrlAonKey0In
+        In::ConstantZero, // SysrstCtrlAonKey1In
+        In::ConstantZero, // SysrstCtrlAonKey2In
+        In::ConstantZero, // SysrstCtrlAonPwrbIn
+        In::ConstantZero, // SysrstCtrlAonLidOpen
+        In::ConstantZero, // UsbdevSense
+    ];
+
+    /// Array representing configgurations of pinmux output selector
+    #[rustfmt::skip]
+    const OUTPUT: &'static [PinmuxOutsel; OUTPUT_NUM] = &[
+        // __________  BANK IOA __________
+        Out::ConstantHighZ, // Ioa0 (CW310Hyp Uart_RX / CW310 SAM3X)
+        Out::Uart0Tx,       // Ioa1 (CW310Hyp Uart_Tx / CW310 SAM3x)
+        Out::GpioGpio0,     // Ioa2
+        Out::GpioGpio1,     // Ioa3
+        Out::ConstantHighZ, // Ioa4
+        Out::Uart1Tx,       // Ioa5
+        Out::GpioGpio2,     // Ioa6
+        Out::I2c0Sda,       // Ioa7 I2C0_TPM_SDA
+        Out::I2c0Scl,       // Ioa8 I2C0_TPM_SCL
+        // __________ BANK IOB __________
+        Out::GpioGpio3,     // Iob0 SPI_HOST_CS
+        Out::GpioGpio4,     // Iob1 SPI_HOST_DI
+        Out::GpioGpio5,     // Iob2 SPI_HOST_DO
+        Out::GpioGpio6,     // Iob3 SPI_HOST_CLK
+        Out::ConstantHighZ, // Iob4 UART2_RX
+        Out::Uart2Tx,       // Iob5 UART2_TX
+        Out::GpioGpio7,     // Iob6
+        Out::GpioGpio8,     // Iob7
+        Out::GpioGpio9,     // Iob8
+        Out::I2c1Scl,       // Iob9  I2C1_SCL
+        Out::I2c1Sda,       // Iob10 I2C1_SDA
+        Out::I2c2Sda,       // Iob11 I2C2_SDA
+        Out::I2c2Scl,       // Iob12 I2C2_SCL
+        // __________ BANK IOC __________
+        Out::GpioGpio10,    // Ioc0
+        Out::GpioGpio11,    // Ioc1
+        Out::GpioGpio12,    // Ioc2
+        Out::ConstantHighZ, // Ioc3 UART3_RX
+        Out::Uart3Tx,       // Ioc4 UART3_TX
+        Out::GpioGpio13,    // Ioc5
+        Out::GpioGpio14,    // Ioc6
+        Out::GpioGpio15,    // Ioc7
+        Out::GpioGpio16,    // Ioc8
+        Out::GpioGpio17,    // Ioc9
+        Out::GpioGpio18,    // Ioc10
+        Out::GpioGpio19,    // Ioc11
+        Out::GpioGpio20,    // Ioc12
+        // __________ BANK IOR __________
+        Out::GpioGpio21,    // Ior0
+        Out::GpioGpio22,    // Ior1
+        Out::GpioGpio23,    // Ior2
+        Out::GpioGpio24,    // Ior3
+        Out::GpioGpio25,    // Ior4
+        Out::GpioGpio26,    // Ior5
+        Out::GpioGpio27,    // Ior6
+        Out::GpioGpio28,    // Ior7
+        // DIO CW310_hyp       Ior8
+        // DIO CW310_hyp       Ior9
+        Out::GpioGpio29,    // Ior10
+        Out::GpioGpio30,    // Ior11
+        Out::GpioGpio31,    // Ior12
+        Out::ConstantHighZ, // Ior13
+    ];
+}

boards/opentitan/src/io.rs
@@ -50,8 +50,8 @@ pub unsafe extern "C" fn panic_fmt(pi: &PanicInfo) -> ! {
     let first_led_pin = &mut earlgrey::gpio::GpioPin::new(
         earlgrey::gpio::GPIO_BASE,
         earlgrey::pinmux::PadConfig::Output(
-            earlgrey::registers::top_earlgrey::MuxedPads::Ioa0,
-            earlgrey::registers::top_earlgrey::PinmuxOutsel::GpioGpio0,
+            earlgrey::registers::top_earlgrey::MuxedPads::Ioa6,
+            earlgrey::registers::top_earlgrey::PinmuxOutsel::GpioGpio7,
         ),
         earlgrey::gpio::pins::pin7,
     );

boards/opentitan/src/main.rs
@@ -16,11 +16,13 @@
 
 use crate::hil::symmetric_encryption::AES128_BLOCK_SIZE;
 use crate::otbn::OtbnComponent;
+use crate::pinmux_layout::BoardPinmuxLayout;
 use capsules_aes_gcm::aes_gcm;
 use capsules_core::virtualizers::virtual_aes_ccm;
 use capsules_core::virtualizers::virtual_alarm::{MuxAlarm, VirtualMuxAlarm};
 use earlgrey::chip::EarlGreyDefaultPeripherals;
 use earlgrey::chip_config::EarlGreyConfig;
+use earlgrey::pinmux_config::EarlGreyPinmuxConfig;
 use kernel::capabilities;
 use kernel::component::Component;
 use kernel::hil;
@@ -42,6 +44,7 @@ use rv32i::csr;
 
 pub mod io;
 mod otbn;
+pub mod pinmux_layout;
 #[cfg(test)]
 mod tests;
 
@@ -84,7 +87,8 @@ static mut PROCESSES: [Option<&'static dyn kernel::process::Process>; 4] = [None
 
 // Test access to the peripherals
 #[cfg(test)]
-static mut PERIPHERALS: Option<&'static EarlGreyDefaultPeripherals<ChipConfig>> = None;
+static mut PERIPHERALS: Option<&'static EarlGreyDefaultPeripherals<ChipConfig, BoardPinmuxLayout>> =
+    None;
 // Test access to board
 #[cfg(test)]
 static mut BOARD: Option<&'static kernel::Kernel> = None;
@@ -127,7 +131,11 @@ static mut RSA_HARDWARE: Option<&lowrisc::rsa::OtbnRsa<'static>> = None;
 static mut SHA256SOFT: Option<&capsules_extra::sha256::Sha256Software<'static>> = None;
 
 static mut CHIP: Option<
-    &'static earlgrey::chip::EarlGrey<EarlGreyDefaultPeripherals<ChipConfig>, ChipConfig>,
+    &'static earlgrey::chip::EarlGrey<
+        EarlGreyDefaultPeripherals<ChipConfig, BoardPinmuxLayout>,
+        ChipConfig,
+        BoardPinmuxLayout,
+    >,
 > = None;
 static mut PROCESS_PRINTER: Option<&'static kernel::process::ProcessPrinterText> = None;
 
@@ -235,8 +243,9 @@ impl
     KernelResources<
         earlgrey::chip::EarlGrey<
             'static,
-            EarlGreyDefaultPeripherals<'static, ChipConfig>,
+            EarlGreyDefaultPeripherals<'static, ChipConfig, BoardPinmuxLayout>,
             ChipConfig,
+            BoardPinmuxLayout,
         >,
     > for EarlGrey
 {
@@ -282,13 +291,17 @@ unsafe fn setup() -> (
     &'static EarlGrey,
     &'static earlgrey::chip::EarlGrey<
         'static,
-        EarlGreyDefaultPeripherals<'static, ChipConfig>,
+        EarlGreyDefaultPeripherals<'static, ChipConfig, BoardPinmuxLayout>,
         ChipConfig,
+        BoardPinmuxLayout,
     >,
-    &'static EarlGreyDefaultPeripherals<'static, ChipConfig>,
+    &'static EarlGreyDefaultPeripherals<'static, ChipConfig, BoardPinmuxLayout>,
 ) {
     // Ibex-specific handler
     earlgrey::chip::configure_trap_handler();
+
+    // Configure board layout in pinmux
+    BoardPinmuxLayout::setup();
 
     // initialize capabilities
     let process_mgmt_cap = create_capability!(capabilities::ProcessManagementCapability);
@@ -297,7 +310,7 @@ unsafe fn setup() -> (
     let board_kernel = static_init!(kernel::Kernel, kernel::Kernel::new(&PROCESSES));
 
     let peripherals = static_init!(
-        EarlGreyDefaultPeripherals<ChipConfig>,
+        EarlGreyDefaultPeripherals<ChipConfig, BoardPinmuxLayout>,
         EarlGreyDefaultPeripherals::new()
     );
     peripherals.init();
@@ -396,7 +409,11 @@ unsafe fn setup() -> (
     );
 
     let chip = static_init!(
-        earlgrey::chip::EarlGrey<EarlGreyDefaultPeripherals<ChipConfig>, ChipConfig>,
+        earlgrey::chip::EarlGrey<
+            EarlGreyDefaultPeripherals<ChipConfig, BoardPinmuxLayout>,
+            ChipConfig,
+            BoardPinmuxLayout,
+        >,
         earlgrey::chip::EarlGrey::new(peripherals, hardware_alarm)
     );
     CHIP = Some(chip);

chips/earlgrey/src/chip.rs
@@ -15,10 +15,16 @@ use rv32i::syscall::SysCall;
 
 use crate::chip_config::EarlGreyConfig;
 use crate::interrupts;
+use crate::pinmux_config::EarlGreyPinmuxConfig;
 use crate::plic::Plic;
 use crate::plic::PLIC;
 
-pub struct EarlGrey<'a, I: InterruptService + 'a, CFG: EarlGreyConfig + 'static> {
+pub struct EarlGrey<
+    'a,
+    I: InterruptService + 'a,
+    CFG: EarlGreyConfig + 'static,
+    PINMUX: EarlGreyPinmuxConfig,
+> {
     userspace_kernel_boundary: SysCall,
     pub pmp: PMP<8>,
     plic: &'a Plic,
@@ -26,9 +32,10 @@ pub struct EarlGrey<'a, I: InterruptService + 'a, CFG: EarlGreyConfig + 'static>
     pwrmgr: lowrisc::pwrmgr::PwrMgr,
     plic_interrupt_service: &'a I,
     _cfg: PhantomData<CFG>,
+    _pinmux: PhantomData<PINMUX>,
 }
 
-pub struct EarlGreyDefaultPeripherals<'a, CFG: EarlGreyConfig> {
+pub struct EarlGreyDefaultPeripherals<'a, CFG: EarlGreyConfig, PINMUX: EarlGreyPinmuxConfig> {
     pub aes: crate::aes::Aes<'a>,
     pub hmac: lowrisc::hmac::Hmac<'a>,
     pub usb: lowrisc::usbdev::Usb<'a>,
@@ -42,9 +49,12 @@ pub struct EarlGreyDefaultPeripherals<'a, CFG: EarlGreyConfig> {
     pub rng: lowrisc::csrng::CsRng<'a>,
     pub watchdog: lowrisc::aon_timer::AonTimer,
     _cfg: PhantomData<CFG>,
+    _pinmux: PhantomData<PINMUX>,
 }
 
-impl<'a, CFG: EarlGreyConfig> EarlGreyDefaultPeripherals<'a, CFG> {
+impl<'a, CFG: EarlGreyConfig, PINMUX: EarlGreyPinmuxConfig>
+    EarlGreyDefaultPeripherals<'a, CFG, PINMUX>
+{
     pub fn new() -> Self {
         Self {
             aes: crate::aes::Aes::new(),
@@ -52,7 +62,7 @@ impl<'a, CFG: EarlGreyConfig> EarlGreyDefaultPeripherals<'a, CFG> {
             usb: lowrisc::usbdev::Usb::new(crate::usbdev::USB0_BASE),
             uart0: lowrisc::uart::Uart::new(crate::uart::UART0_BASE, CFG::PERIPHERAL_FREQ),
             otbn: lowrisc::otbn::Otbn::new(crate::otbn::OTBN_BASE),
-            gpio_port: crate::gpio::Port::new(),
+            gpio_port: crate::gpio::Port::new::<PINMUX>(),
             i2c0: lowrisc::i2c::I2c::new(crate::i2c::I2C0_BASE, (1 / CFG::CPU_FREQ) * 1000 * 1000),
             spi_host0: lowrisc::spi_host::SpiHost::new(
                 crate::spi_host::SPIHOST0_BASE,
@@ -73,6 +83,7 @@ impl<'a, CFG: EarlGreyConfig> EarlGreyDefaultPeripherals<'a, CFG> {
                 CFG::CPU_FREQ,
             ),
             _cfg: PhantomData,
+            _pinmux: PhantomData,
         }
     }
 
@@ -81,7 +92,9 @@ impl<'a, CFG: EarlGreyConfig> EarlGreyDefaultPeripherals<'a, CFG> {
     }
 }
 
-impl<'a, CFG: EarlGreyConfig> InterruptService for EarlGreyDefaultPeripherals<'a, CFG> {
+impl<'a, CFG: EarlGreyConfig, PINMUX: EarlGreyPinmuxConfig> InterruptService
+    for EarlGreyDefaultPeripherals<'a, CFG, PINMUX>
+{
     unsafe fn service_interrupt(&self, interrupt: u32) -> bool {
         match interrupt {
             interrupts::UART0_TX_WATERMARK..=interrupts::UART0_RX_PARITYERR => {
@@ -121,7 +134,9 @@ impl<'a, CFG: EarlGreyConfig> InterruptService for EarlGreyDefaultPeripherals<'a
     }
 }
 
-impl<'a, I: InterruptService + 'a, CFG: EarlGreyConfig> EarlGrey<'a, I, CFG> {
+impl<'a, I: InterruptService + 'a, CFG: EarlGreyConfig, PINMUX: EarlGreyPinmuxConfig>
+    EarlGrey<'a, I, CFG, PINMUX>
+{
     pub unsafe fn new(
         plic_interrupt_service: &'a I,
         timer: &'static crate::timer::RvTimer<'_, CFG>,
@@ -134,6 +149,7 @@ impl<'a, I: InterruptService + 'a, CFG: EarlGreyConfig> EarlGrey<'a, I, CFG> {
             timer,
             plic_interrupt_service,
             _cfg: PhantomData,
+            _pinmux: PhantomData,
         }
     }
 
@@ -227,8 +243,8 @@ impl<'a, I: InterruptService + 'a, CFG: EarlGreyConfig> EarlGrey<'a, I, CFG> {
     }
 }
 
-impl<'a, I: InterruptService + 'a, CFG: EarlGreyConfig> kernel::platform::chip::Chip
-    for EarlGrey<'a, I, CFG>
+impl<'a, I: InterruptService + 'a, CFG: EarlGreyConfig, PINMUX: EarlGreyPinmuxConfig>
+    kernel::platform::chip::Chip for EarlGrey<'a, I, CFG, PINMUX>
 {
     type MPU = PMP<8>;
     type UserspaceKernelBoundary = SysCall;

chips/earlgrey/src/gpio.rs
@@ -11,9 +11,11 @@ use lowrisc::gpio::GpioRegisters;
 pub use lowrisc::gpio::{pins, GpioBitfield, GpioPin};
 
 use crate::pinmux::PadConfig;
+use crate::pinmux_config::EarlGreyPinmuxConfig;
 use crate::registers::top_earlgrey::GPIO_BASE_ADDR;
 use crate::registers::top_earlgrey::{
-    MuxedPads, PinmuxOutsel, PinmuxPeripheralIn, PINMUX_PERIPH_OUTSEL_IDX_OFFSET,
+    MuxedPads, PinmuxInsel, PinmuxOutsel, PinmuxPeripheralIn, PINMUX_MIO_PERIPH_INSEL_IDX_OFFSET,
+    PINMUX_PERIPH_OUTSEL_IDX_OFFSET,
 };
 
 pub const GPIO_BASE: StaticRef<GpioRegisters> =
@@ -40,49 +42,71 @@ impl From<GpioBitfield> for PinmuxOutsel {
     }
 }
 
-pub fn gpio_pad_config(pad: MuxedPads, pin: GpioBitfield) -> PadConfig {
-    PadConfig::InOut(pad, PinmuxPeripheralIn::from(pin), PinmuxOutsel::from(pin))
+// This function use extract GPIO mapping from initial pinmux configurations
+pub fn gpio_pad_config<Layout: EarlGreyPinmuxConfig>(pin: GpioBitfield) -> PadConfig {
+    let inp: PinmuxPeripheralIn = PinmuxPeripheralIn::from(pin);
+    match Layout::INPUT[inp as usize] {
+        // Current implementation don't support Output only GPIO
+        PinmuxInsel::ConstantZero | PinmuxInsel::ConstantOne => PadConfig::Unconnected,
+        input_selector => {
+            if let Ok(pad) = MuxedPads::try_from(
+                input_selector as u32 - PINMUX_MIO_PERIPH_INSEL_IDX_OFFSET as u32,
+            ) {
+                let out: PinmuxOutsel = Layout::OUTPUT[pad as usize];
+                // Checking for bi-directional I/O
+                if out == PinmuxOutsel::from(pin) {
+                    PadConfig::InOut(pad, inp, out)
+                } else {
+                    PadConfig::Input(pad, inp)
+                }
+            } else {
+                // Upper match checked for unconnected pad so in this
+                // place we probably have some invalid value in INPUT array.
+                PadConfig::Unconnected
+            }
+        }
+    }
 }
 
-// Configuring first 32 pad as GPIO
+// Configuring first all GPIO based on board layout
 impl<'a> Port<'a> {
-    pub const fn new() -> Self {
+    pub fn new<Layout: EarlGreyPinmuxConfig>() -> Self {
         Self {
-            // Intentionally prevent splitting GpioPin to multi line definition
+            // Intentionally prevent splitting GpioPin to multiple line
             #[rustfmt::skip]
             pins: [
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioa0, PinmuxPeripheralIn::GpioGpio0, PinmuxOutsel::GpioGpio0), pins::pin0),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioa1, PinmuxPeripheralIn::GpioGpio1, PinmuxOutsel::GpioGpio1), pins::pin1),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioa2, PinmuxPeripheralIn::GpioGpio2, PinmuxOutsel::GpioGpio2), pins::pin2),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioa3, PinmuxPeripheralIn::GpioGpio3, PinmuxOutsel::GpioGpio3), pins::pin3),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioa4, PinmuxPeripheralIn::GpioGpio4, PinmuxOutsel::GpioGpio4), pins::pin4),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioa5, PinmuxPeripheralIn::GpioGpio5, PinmuxOutsel::GpioGpio5), pins::pin5),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioa6, PinmuxPeripheralIn::GpioGpio6, PinmuxOutsel::GpioGpio6), pins::pin6),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioa7, PinmuxPeripheralIn::GpioGpio7, PinmuxOutsel::GpioGpio7), pins::pin7),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioa8, PinmuxPeripheralIn::GpioGpio8, PinmuxOutsel::GpioGpio8), pins::pin8),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob0, PinmuxPeripheralIn::GpioGpio9, PinmuxOutsel::GpioGpio9), pins::pin9),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob1, PinmuxPeripheralIn::GpioGpio10, PinmuxOutsel::GpioGpio10), pins::pin10),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob2, PinmuxPeripheralIn::GpioGpio11, PinmuxOutsel::GpioGpio11), pins::pin11),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob3, PinmuxPeripheralIn::GpioGpio12, PinmuxOutsel::GpioGpio12), pins::pin12),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob4, PinmuxPeripheralIn::GpioGpio13, PinmuxOutsel::GpioGpio13), pins::pin13),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob5, PinmuxPeripheralIn::GpioGpio14, PinmuxOutsel::GpioGpio14), pins::pin14),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob6, PinmuxPeripheralIn::GpioGpio15, PinmuxOutsel::GpioGpio15), pins::pin15),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob7, PinmuxPeripheralIn::GpioGpio16, PinmuxOutsel::GpioGpio16), pins::pin16),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob8, PinmuxPeripheralIn::GpioGpio17, PinmuxOutsel::GpioGpio17), pins::pin17),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob9, PinmuxPeripheralIn::GpioGpio18, PinmuxOutsel::GpioGpio18), pins::pin18),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob10, PinmuxPeripheralIn::GpioGpio19, PinmuxOutsel::GpioGpio19), pins::pin19),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob11, PinmuxPeripheralIn::GpioGpio20, PinmuxOutsel::GpioGpio20), pins::pin20),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Iob12, PinmuxPeripheralIn::GpioGpio21, PinmuxOutsel::GpioGpio21), pins::pin21),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc0, PinmuxPeripheralIn::GpioGpio22, PinmuxOutsel::GpioGpio22), pins::pin22),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc1, PinmuxPeripheralIn::GpioGpio23, PinmuxOutsel::GpioGpio23), pins::pin23),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc2, PinmuxPeripheralIn::GpioGpio24, PinmuxOutsel::GpioGpio24), pins::pin24),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc3, PinmuxPeripheralIn::GpioGpio25, PinmuxOutsel::GpioGpio25), pins::pin25),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc4, PinmuxPeripheralIn::GpioGpio26, PinmuxOutsel::GpioGpio26), pins::pin26),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc5, PinmuxPeripheralIn::GpioGpio27, PinmuxOutsel::GpioGpio27), pins::pin27),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc6, PinmuxPeripheralIn::GpioGpio28, PinmuxOutsel::GpioGpio28), pins::pin28),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc7, PinmuxPeripheralIn::GpioGpio29, PinmuxOutsel::GpioGpio29), pins::pin29),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc8, PinmuxPeripheralIn::GpioGpio30, PinmuxOutsel::GpioGpio30), pins::pin30),
-                GpioPin::new(GPIO_BASE, PadConfig::InOut(MuxedPads::Ioc9, PinmuxPeripheralIn::GpioGpio31, PinmuxOutsel::GpioGpio31), pins::pin31),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin0), pins::pin0),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin1), pins::pin1),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin2), pins::pin2),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin3), pins::pin3),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin4), pins::pin4),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin5), pins::pin5),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin6), pins::pin6),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin7), pins::pin7),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin8), pins::pin8),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin9), pins::pin9),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin10), pins::pin10),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin11), pins::pin11),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin12), pins::pin12),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin13), pins::pin13),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin14), pins::pin14),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin15), pins::pin15),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin16), pins::pin16),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin17), pins::pin17),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin18), pins::pin18),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin19), pins::pin19),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin20), pins::pin20),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin21), pins::pin21),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin22), pins::pin22),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin23), pins::pin23),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin24), pins::pin24),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin25), pins::pin25),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin26), pins::pin26),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin27), pins::pin27),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin28), pins::pin28),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin29), pins::pin29),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin30), pins::pin30),
+                GpioPin::new(GPIO_BASE, gpio_pad_config::<Layout>(pins::pin31), pins::pin31),
             ],
         }
     }

chips/earlgrey/src/lib.rs
@@ -13,6 +13,8 @@
 #![recursion_limit = "256"]
 
 pub mod chip_config;
+pub mod pinmux_config;
+
 mod interrupts;
 
 pub mod aes;

commit 34f3c8b77e852283a003abf128d0fd5509d088e0

opentitan: Fixup UART output
Commit 7523a5381 "board/earlgrey: Add board sepcific pinmux setup" broke
Tock on OpenTitan as the user was unable to see any UART output.

Let's fix this up by correcting the PinMux output setup to match the
setup we are using. This also matches what the lowrisc repo does for
their tests.

Fixes: 7523a5381 "board/earlgrey: Add board sepcific pinmux setup"
Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

boards/opentitan/src/pinmux_layout.rs
@@ -85,7 +85,7 @@ impl EarlGreyPinmuxConfig for BoardPinmuxLayout {
     const OUTPUT: &'static [PinmuxOutsel; OUTPUT_NUM] = &[
         // __________  BANK IOA __________
         Out::ConstantHighZ, // Ioa0 (CW310Hyp Uart_RX / CW310 SAM3X)
-        Out::Uart0Tx,       // Ioa1 (CW310Hyp Uart_Tx / CW310 SAM3x)
+        Out::Uart3Tx,       // Ioa1 (CW310Hyp Uart_Tx / CW310 SAM3x)
         Out::GpioGpio0,     // Ioa2
         Out::GpioGpio1,     // Ioa3
         Out::ConstantHighZ, // Ioa4
@@ -112,7 +112,7 @@ impl EarlGreyPinmuxConfig for BoardPinmuxLayout {
         Out::GpioGpio11,    // Ioc1
         Out::GpioGpio12,    // Ioc2
         Out::ConstantHighZ, // Ioc3 UART3_RX
-        Out::Uart3Tx,       // Ioc4 UART3_TX
+        Out::Uart0Tx,       // Ioc4 UART3_TX
         Out::GpioGpio13,    // Ioc5
         Out::GpioGpio14,    // Ioc6
         Out::GpioGpio15,    // Ioc7