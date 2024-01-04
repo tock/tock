cd96019d83f2d7ed209991c404be792b09a9d269
Brad Campbell <bradjc5@gmail.com>
2024-01-04 18:37:37 -0500

    hil: pkc: sig: add error types

--- kernel/src/hil/public_key_crypto/signature.rs
+++ kernel/src/hil/public_key_crypto/signature.rs
@@ -1,6 +1,6 @@
 // Licensed under the Apache License, Version 2.0 or the MIT License.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// Copyright Tock Contributors 2022.
+// Copyright Tock Contributors 2024.
 
 //! Interface for verifying signatures.
 
@@ -16,7 +16,10 @@ pub trait ClientVerify<const HL: usize, const SL: usize> {
     /// will be `Ok(false)`.
     ///
     /// If verification operation did encounter errors `result` will be `Err()`
-    /// with an appropriate `ErrorCode`.
+    /// with an appropriate `ErrorCode`. Valid `ErrorCode`s include:
+    ///
+    /// - `CANCEL`: the operation was cancelled.
+    /// - `FAIL`: an internal failure.
     fn verification_done(
         &self,
         result: Result<bool, ErrorCode>,
@@ -27,6 +30,9 @@ pub trait ClientVerify<const HL: usize, const SL: usize> {
 
 /// Verify a signature.
 ///
+/// This is a generic interface, and it is up to the implementation as to the
+/// signature verification algorithm being used.
+///
 /// - `HL`: The length in bytes of the hash.
 /// - `SL`: The length in bytes of the signature.
 pub trait SignatureVerify<'a, const HL: usize, const SL: usize> {
@@ -34,7 +40,17 @@ pub trait SignatureVerify<'a, const HL: usize, const SL: usize> {
     /// callback.
     fn set_verify_client(&self, client: &'a dyn ClientVerify<HL, SL>);
 
-    /// Verify the signature.
+    /// Verify the signature matches the given hash.
+    ///
+    /// If this returns `Ok(())`, then the `verification_done()` callback will
+    /// be called. If this returns `Err()`, no callback will be called.
+    ///
+    /// The valid `ErrorCode`s that can occur are:
+    ///
+    /// - `OFF`: the underlying digest engine is powered down and cannot be
+    ///   used.
+    /// - `BUSY`: there is an outstanding operation already in process, and the
+    ///   verification engine cannot accept another request.
     fn verify(
         &self,
         hash: &'static mut [u8; HL],
