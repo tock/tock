3bd66c346009cbced09eafd6e87fd9268057bb9b
James Wainwright <james.wainwright@lowrisc.org>
2024-01-10 14:34:48 +0000

    Remove unused hdr.s from imxrt1050-evkb
    This file is unused as far as I can tell.

--- boards/imxrt1050-evkb/build.rs
+++ boards/imxrt1050-evkb/build.rs
@@ -6,7 +6,4 @@ fn main() {
     println!("cargo:rerun-if-changed=layout.ld");
     println!("cargo:rerun-if-changed=chip_layout.ld");
     println!("cargo:rerun-if-changed=../kernel_layout.ld");
-
-    // rebuild if `asm.s` changed
-    println!("cargo:rerun-if-changed=hdr.s"); // <- NEW!
 }
