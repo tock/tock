f90e5e7ebe63f620625edfcbb07fd681034ebad9
Brad Campbell <bradjc5@gmail.com>
2024-02-16 09:19:05 -0500

    boards: update to ieee802154 template

--- boards/nano33ble/src/main.rs
+++ boards/nano33ble/src/main.rs
@@ -119,6 +119,15 @@ type HTS221Sensor = components::hts221::Hts221ComponentType<
 >;
 type TemperatureDriver = components::temperature::TemperatureComponentType<HTS221Sensor>;
 type HumidityDriver = components::humidity::HumidityComponentType<HTS221Sensor>;
+type MuxAes = components::ieee802154::MuxAes128ccmComponentType<nrf52840::aes::AesECB<'static>>;
+type Ieee802154MuxMac = components::ieee802154::Ieee802154ComponentMuxMacType<
+    nrf52840::ieee802154_radio::Radio<'static>,
+    MuxAes,
+>;
+type Ieee802154Driver = components::ieee802154::Ieee802154ComponentType<
+    nrf52840::ieee802154_radio::Radio<'static>,
+    MuxAes,
+>;
 
 /// Supported drivers by the platform
 pub struct Platform {
@@ -130,7 +139,7 @@ pub struct Platform {
             nrf52::rtc::Rtc<'static>,
         >,
     >,
-    ieee802154_radio: &'static capsules_extra::ieee802154::RadioDriver<'static>,
+    ieee802154_radio: &'static Ieee802154Driver,
     console: &'static capsules_core::console::Console<'static>,
     pconsole: &'static capsules_core::process_console::ProcessConsole<
         'static,
@@ -577,7 +586,10 @@ pub unsafe fn start() -> (
         local_ip_ifaces,
         mux_alarm,
     )
-    .finalize(components::udp_mux_component_static!(nrf52840::rtc::Rtc));
+    .finalize(components::udp_mux_component_static!(
+        nrf52840::rtc::Rtc,
+        Ieee802154MuxMac
+    ));
 
     // UDP driver initialization happens here
     let udp_driver = components::udp_driver::UDPDriverComponent::new(
