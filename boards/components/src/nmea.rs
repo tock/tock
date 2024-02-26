25b7bf598df6697cf3b86b4bacd611eccc36a1a6
Alistair Francis <alistair@alistair23.me>
2024-02-26 21:03:40 +1000

    capsules: Add a NMEA userspace driver
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- boards/components/src/nmea.rs
+++ boards/components/src/nmea.rs
@@ -2,10 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // Copyright Tock Contributors 2022.
 
+//! Component for any NMEA data.
+
 use capsules_core::virtualizers::virtual_i2c::{I2CDevice, MuxI2C};
+use capsules_extra::nmea::Nmea;
+use capsules_extra::nmea_i2c::NMEA_BUFFER_LEN;
 use capsules_extra::nmea_i2c::{I2cNmea, I2C_BUFFER_LEN};
 use core::mem::MaybeUninit;
+use kernel::capabilities;
 use kernel::component::Component;
+use kernel::create_capability;
+use kernel::hil;
 use kernel::hil::i2c;
 
 // Setup static space for the objects.
@@ -56,5 +63,60 @@ impl<I: 'static + i2c::I2CMaster<'static>> Component for I2cNmeaComponent<I> {
 
         i2c_device.set_client(nmea_i2c);
         nmea_i2c
+    }
+}
+
+#[macro_export]
+macro_rules! nmea_component_static {
+    () => {{
+        (
+            kernel::static_buf!(capsules_extra::nmea::Nmea<'static>),
+            kernel::static_buf!([u8; capsules_extra::nmea_i2c::NMEA_BUFFER_LEN]),
+        )
+    };};
+}
+
+pub type NmeaComponentType = Nmea<'static>;
+
+pub struct NmeaComponent<T: 'static + hil::sensors::NmeaDriver<'static>> {
+    board_kernel: &'static kernel::Kernel,
+    driver_num: usize,
+    driver: &'static T,
+}
+
+impl<T: 'static + hil::sensors::NmeaDriver<'static>> NmeaComponent<T> {
+    pub fn new(
+        board_kernel: &'static kernel::Kernel,
+        driver_num: usize,
+        driver: &'static T,
+    ) -> NmeaComponent<T> {
+        NmeaComponent {
+            board_kernel,
+            driver_num,
+            driver,
+        }
+    }
+}
+
+impl<T: 'static + hil::sensors::NmeaDriver<'static>> Component for NmeaComponent<T> {
+    type StaticInput = (
+        &'static mut MaybeUninit<Nmea<'static>>,
+        &'static mut MaybeUninit<[u8; NMEA_BUFFER_LEN]>,
+    );
+    type Output = &'static Nmea<'static>;
+
+    fn finalize(self, s: Self::StaticInput) -> Self::Output {
+        let grant_cap = create_capability!(capabilities::MemoryAllocationCapability);
+
+        let buffer = s.1.write([0; NMEA_BUFFER_LEN]);
+
+        let nmea = s.0.write(Nmea::new(
+            self.driver,
+            self.board_kernel.create_grant(self.driver_num, &grant_cap),
+            buffer,
+        ));
+
+        hil::sensors::NmeaDriver::set_client(self.driver, nmea);
+        nmea
     }
 }
