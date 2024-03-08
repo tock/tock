0fe05bbaccfbb78355616bb4a27f9816e3ef15dd
Brad Campbell <bradjc5@gmail.com>
2024-03-08 13:08:11 -0500

    tools: format check_capsule_readme

--- tools/check_capsule_readme.py
+++ tools/check_capsule_readme.py
@@ -4,47 +4,43 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 # Copyright Tock Contributors 2023.
 
-'''
+"""
 Check if all of the available capsules are documented in the README.
-'''
+"""
 
 import os
 import re
 
-SKIP = ['/mod.rs',
-        'src/lib.rs',
-        '/test',
-        'src/driver.rs',
-        'src/rf233_const.rs']
+SKIP = ["/mod.rs", "src/lib.rs", "/test", "src/driver.rs", "src/rf233_const.rs"]
 
 
 documented_capsules = []
 implemented_capsules = []
 
 # Find all documented capsules
-with open('capsules/README.md') as f:
-	for l in f:
-		items = re.findall(r".*\((src/.*?)\).*", l)
-		if len(items) > 0:
-			for item in items:
-				documented_capsules.append('capsules/{}'.format(item))
+with open("capsules/README.md") as f:
+    for l in f:
+        items = re.findall(r".*\((src/.*?)\).*", l)
+        if len(items) > 0:
+            for item in items:
+                documented_capsules.append("capsules/{}".format(item))
 
 
 # Find all capsule source files.
-for subdir, dirs, files in os.walk(os.fsencode('capsules/src/')):
-	for file in files:
-		filepath = os.fsdecode(os.path.join(subdir, file))
+for subdir, dirs, files in os.walk(os.fsencode("capsules/src/")):
+    for file in files:
+        filepath = os.fsdecode(os.path.join(subdir, file))
 
-		# Get just the part after /src, effectively.
-		folders = filepath.split('/')
-		filepath = '/'.join(folders[0:3])
+        # Get just the part after /src, effectively.
+        folders = filepath.split("/")
+        filepath = "/".join(folders[0:3])
 
-		# Skip some noise.
-		for skip in SKIP:
-			if skip in filepath:
-				break
-		else:
-			implemented_capsules.append(filepath)
+        # Skip some noise.
+        for skip in SKIP:
+            if skip in filepath:
+                break
+        else:
+            implemented_capsules.append(filepath)
 
 
 # Calculate what doesn't seem to be documented.
@@ -54,12 +50,11 @@ missing = list(set(implemented_capsules) - set(documented_capsules))
 removed = list(set(documented_capsules) - set(implemented_capsules))
 
 
-print('The following capsules do not seem to be documented:')
+print("The following capsules do not seem to be documented:")
 for m in sorted(missing):
-	print(' - {}'.format(m))
+    print(" - {}".format(m))
 
 
-print('The following capsules seem to have been removed:')
+print("The following capsules seem to have been removed:")
 for m in sorted(removed):
-	print(' - {}'.format(m))
-
+    print(" - {}".format(m))
