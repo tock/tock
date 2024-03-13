da222627a4bfbb78702cd44b168dfb8033505f6f
Brad Campbell <bradjc5@gmail.com>
2024-03-12 21:57:26 -0400

    arch: riscv: csr: remove unused code

--- arch/riscv/src/csr/stvec.rs
+++ arch/riscv/src/csr/stvec.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // Copyright Tock Contributors 2022.
 
-use kernel::utilities::registers::{register_bitfields, LocalRegisterCopy};
+use kernel::utilities::registers::register_bitfields;
 
 // stvec contains the address(es) of the trap handler
 register_bitfields![usize,
@@ -14,13 +14,3 @@ register_bitfields![usize,
         ]
     ]
 ];
-
-trait StvecHelpers {
-    fn get_trap_address(&self) -> usize;
-}
-
-impl StvecHelpers for LocalRegisterCopy<usize, stvec::Register> {
-    fn get_trap_address(&self) -> usize {
-        self.read(stvec::trap_addr) << 2
-    }
-}
