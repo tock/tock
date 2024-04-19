5a111e198a30fb8f55077e106ea5b5de70e06d2a
Brad Campbell <bradjc5@gmail.com>
2024-04-19 11:57:48 -0400

    nrf5x: trng: _ variables not public

--- chips/nrf5x/src/trng.rs
+++ chips/nrf5x/src/trng.rs
@@ -44,23 +44,23 @@ pub struct RngRegisters {
     /// Address: 0x004 - 0x008
     pub task_stop: WriteOnly<u32, Task::Register>,
     /// Reserved
-    pub _reserved1: [u32; 62],
+    _reserved1: [u32; 62],
     /// Event being generated for every new random number written to the VALUE register
     /// Address: 0x100 - 0x104
     pub event_valrdy: ReadWrite<u32, Event::Register>,
     /// Reserved
-    pub _reserved2: [u32; 63],
+    _reserved2: [u32; 63],
     /// Shortcut register
     /// Address: 0x200 - 0x204
     pub shorts: ReadWrite<u32, Shorts::Register>,
-    pub _reserved3: [u32; 64],
+    _reserved3: [u32; 64],
     /// Enable interrupt
     /// Address: 0x304 - 0x308
     pub intenset: ReadWrite<u32, Intenset::Register>,
     /// Disable interrupt
     /// Address: 0x308 - 0x30c
     pub intenclr: ReadWrite<u32, Intenclr::Register>,
-    pub _reserved4: [u32; 126],
+    _reserved4: [u32; 126],
     /// Configuration register
     /// Address: 0x504 - 0x508
     pub config: ReadWrite<u32, Config::Register>,
