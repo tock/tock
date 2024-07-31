17a23e0e9cc97e20dd9c189d16f41e8284e4d0fc
inesmaria08 <inesmaria.moraru@gmail.com>
2024-07-31 16:48:27 +0300

    format update

--- capsules/extra/src/sg90.rs
+++ capsules/extra/src/sg90.rs
@@ -20,15 +20,15 @@ impl<'a, P: hil::pwm::PwmPin> kernel::hil::servo::Servo<'a> for Sg90<'a, P> {
         if angle <= 180 {
             // The frequency used for sg90 servo is always 50hz.
             let frequency_hz = 50;
-            // This calculates the pulse width in microseconds for a specific angle. 
-            // We substract 50 from angle to adapt how the servo uses angles to the 
+            // This calculates the pulse width in microseconds for a specific angle.
+            // We substract 50 from angle to adapt how the servo uses angles to the
             // trigonometric system.
             let pulse_width_us = 1000 + 1000 / 90 * (angle - 50);
-            // The duty_cycle formula is (pulse_width/period)*100. 
+            // The duty_cycle formula is (pulse_width/period)*100.
             // The period is usually 20 000 miliseconds.
-            // If we simplify we're left with pulse_width/20. 
+            // If we simplify we're left with pulse_width/20.
             // We also need to scale this to the maximum duty_cycle suported by the pin.
-            // We do this by multiplying the value we get from the 
+            // We do this by multiplying the value we get from the
             // get_maximum_duty_cycle() function with pulse_width/20 and divide it by 100.
             // This leaves us with the below formula:
             let duty_cycle = pulse_width_us * self.pwm_pin.get_maximum_duty_cycle() / 20000;
