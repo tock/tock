d01c7436d1163a968bda13cc0e01542f3ef83f08
Evan Johnson <e5johnso@eng.ucsd.edu>
2024-08-29 12:24:03 -0700

    implement PartialOrd for FluxPtr

--- flux_support/src/lib.rs
+++ flux_support/src/lib.rs
@@ -5,20 +5,32 @@ pub use flux_register_interface::*;
 
 #[allow(dead_code)]
 #[flux_rs::sig(fn(x: bool[true]))]
-pub fn assert(_x: bool) {}
+pub const fn assert(_x: bool) {}
 
 #[flux_rs::sig(fn(b:bool) ensures b)]
-pub fn assume(b: bool) {
+pub const fn assume(b: bool) {
     if !b {
         panic!("assume fails")
     }
 }
 
+// #[flux_rs::extern_spec]
+// #[flux_rs::refined_by(val: int)]
+// #[flux_rs::invariant(val == -1 || val == 0 || val == 1)]
+// enum Ordering {
+//     #[flux::variant(Ordering[-1])]
+//     Less,
+//     #[flux::variant(Ordering[0])]
+//     Equal,
+//     #[flux::variant(Ordering[1])]
+//     Greater,
+// }
+
 #[flux_rs::opaque]
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Ord)]
 #[flux_rs::refined_by(ptr: int)]
 pub struct FluxPtr {
-    _inner: *mut u8,
+    inner: *mut u8,
 }
 
 // VTOCK-TODO: fill in these functions with obvious implementations
@@ -68,6 +80,40 @@ impl FluxPtr {
         unimplemented!()
     }
 }
+
+#[flux_rs::trusted]
+impl PartialOrd for FluxPtr {
+    fn partial_cmp(&self, _other: &Self) -> Option<core::cmp::Ordering>{todo!()}
+
+    // Provided methods
+    #[flux_rs::sig(fn(self: &Self[@lhs], other: &Self[@rhs]) -> bool[lhs < rhs])]
+    fn lt(&self, _other: &Self) -> bool { todo!() }
+    #[flux_rs::sig(fn(self: &Self[@lhs], other: &Self[@rhs]) -> bool[lhs <= rhs])]
+    fn le(&self, _other: &Self) -> bool { todo!() }
+    #[flux_rs::sig(fn(self: &Self[@lhs], other: &Self[@rhs]) -> bool[lhs > rhs])]
+    fn gt(&self, _other: &Self) -> bool { todo!() }
+    #[flux_rs::sig(fn(self: &Self[@lhs], other: &Self[@rhs]) -> bool[lhs >= rhs])]
+    fn ge(&self, _other: &Self) -> bool { todo!() }
+}
+
+// #[flux_rs::trusted]
+// impl Ord for FluxPtr {
+//     #[flux_rs::sig(fn(self: &Self[@lhs], other: &Self[@rhs]) -> Ordering {
+//         order: order == -1 => lhs < rhs &&
+//                order == 0 => lhs == rhs && 
+//                order == 1 => lhs > rhs
+//     })]
+//     fn cmp(&self, other: &Self) -> Ordering {
+//         self.inner.cmp(&other.inner)
+//     }
+// }
+
+
+// #[flux_rs::sig(
+//     fn(self: &Wrapper[@lhs], other: &Wrapper[@rhs]) -> Ordering{val: val == -1 => lhs < rhs && 
+//                                                                      val == 0 => lhs == rhs && 
+//                                                                      val == 1 => lhs > rhs}
+// )]
 
 #[flux_rs::alias(type FluxPtrU8[n: int] = FluxPtr[n])]
 pub type FluxPtrU8 = FluxPtr;
@@ -146,3 +192,13 @@ pub fn max_usize(lhs: usize, rhs: usize) -> usize {
         rhs
     }
 }
+
+#[flux_rs::sig(fn(self: FluxPtr[@lhs], other: FluxPtr[@rhs]) -> FluxPtr {r: (lhs >= rhs => r == lhs) && (rhs > lhs => r == rhs)})]
+pub fn max_ptr(lhs: FluxPtr, rhs: FluxPtr) -> FluxPtr {
+    if lhs >= rhs {
+        lhs
+    } else {
+        rhs
+    }
+}
+
