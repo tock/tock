2bc7242f606002bfc08ca9db10b9e72b61920f34
inesmaria08 <inesmaria.moraru@gmail.com>
2024-09-13 13:22:19 +0300

    example of usage and comments update

--- capsules/extra/src/servo.rs
+++ capsules/extra/src/servo.rs
@@ -7,35 +7,31 @@
 //! Usage
 //! -----
 //!
-//! ```rust,ignore
-//! # use kernel::static_init;
-//!     let mux_pwm = components::pwm::PwmMuxComponent::new(&peripherals.pwm)
-//!         .finalize(components::pwm_mux_component_static!(rp2040::pwm::Pwm));
+//! use kernel::static_init;
+//! let mux_pwm = components::pwm::PwmMuxComponent::new(&peripherals.pwm)
+//! .finalize(components::pwm_mux_component_static!(rp2040::pwm::Pwm));
 //!
-//!     let virtual_pwm_servo =
-//!        components::pwm::PwmPinUserComponent::new(mux_pwm, rp2040::gpio::RPGpio::GPIO4)
-//!             .finalize(components::pwm_pin_user_component_static!(rp2040::pwm::Pwm));
+//! let virtual_pwm_servo: &PwmPinUser<'static, rp2040::pwm::Pwm<'static>> =
+//! components::pwm::PwmPinUserComponent::new(mux_pwm, rp2040::gpio::RPGpio::GPIO4)
+//!     .finalize(components::pwm_pin_user_component_static!(rp2040::pwm::Pwm));
 //!
-//!     let pwm_servo = static_init!(
-//!         capsules_extra::sg90::Sg90<
-//!             'static,
-//!             capsules_core::virtualizers::virtual_pwm::PwmPinUser<'static, rp2040::pwm::Pwm>,
-//!         >,
-//!         capsules_extra::sg90::Sg90::new(
-//!             virtual_pwm_servo,
-//!         )
-//!     );
+//! let sg90_servo = static_init!(
+//! capsules_extra::sg90::Sg90<
+//!    'static,
+//!    capsules_core::virtualizers::virtual_pwm::PwmPinUser<'static, rp2040::pwm::Pwm>,
+//! >,
+//! capsules_extra::sg90::Sg90::new(virtual_pwm_servo)
+//! );
 //!
-//!     let servo_driver = static_init!(
-//!         capsules_extra::servo::Servo<
-//!             'static,
-//!             capsules_extra::sg90::Sg90<
-//!                 'static,
-//!                 capsules_core::virtualizers::virtual_pwm::PwmPinUser<'static, rp2040::pwm::Pwm>,
-//!             >,
-//!         >,
-//!         capsules_extra::servo::Servo::new(pwm_servo,)
-//!     );
+//! // Here, we initialize an array of two SG90 servomotors as an example.
+//! let multi_servo = static_init!(
+//! [&'static dyn hil::servo::Servo<'static>; 2],
+//! [sg90_servo, sg90_servo]
+//! );
+//! let servo = static_init!(
+//! capsules_extra::servo::Servo<'static, 2>,
+//! capsules_extra::servo::Servo::new(multi_servo)
+//! );
 
 use kernel::hil;
 use kernel::syscall::{CommandReturn, SyscallDriver};
@@ -65,8 +61,10 @@ impl<'a, const NUM_SERVO: usize> SyscallDriver for Servo<'a, NUM_SERVO> {
     /// ### `command_num`
     ///
     /// - `0`: Return Ok(()) if this driver is included on the platform.
-    /// - `1`: Changing the angle immediatelly. `data1` is used for the angle (0-180).
-    /// - `2`: Returning the current angle.
+    /// - `1`: Changing the angle immediatelly.`servo_index` receives the index
+    /// corresponding to the servo whose angle we want to adjust
+    /// `angle` is used to receive a value between 0 and 180.
+    /// - `2`: Returning the current angle for a specific index.
     fn command(
         &self,
         command_num: usize,
@@ -79,11 +77,12 @@ impl<'a, const NUM_SERVO: usize> SyscallDriver for Servo<'a, NUM_SERVO> {
             0 => CommandReturn::success(),
             // Change the angle immediately.
             1 => {
-                if servo_index >= NUM_SERVO {
-                    CommandReturn::failure(ErrorCode::INVAL)
+                // We use `servo_index` - 1 to adjust the indexing so that the API displays it starting from 1.
+                if servo_index - 1 >= NUM_SERVO {
+                    CommandReturn::failure(ErrorCode::NODEVICE)
                 } else {
                     match angle.try_into() {
-                        Ok(angle) => match self.servo[servo_index].set_angle(angle) {
+                        Ok(angle) => match self.servo[servo_index - 1].set_angle(angle) {
                             Ok(()) => CommandReturn::success(),
                             Err(err) => CommandReturn::failure(err),
                         },
@@ -92,10 +91,16 @@ impl<'a, const NUM_SERVO: usize> SyscallDriver for Servo<'a, NUM_SERVO> {
                 }
             }
             // Return the current angle.
-            2 => match self.servo[servo_index].get_angle() {
-                Ok(angle) => CommandReturn::success_u32(angle as u32),
-                Err(err) => CommandReturn::failure(err),
-            },
+            2 => {
+                if servo_index - 1 >= NUM_SERVO {
+                    CommandReturn::failure(ErrorCode::NODEVICE)
+                } else {
+                    match self.servo[servo_index - 1].get_angle() {
+                        Ok(angle) => CommandReturn::success_u32(angle as u32),
+                        Err(err) => CommandReturn::failure(err),
+                    }
+                }
+            }
             _ => CommandReturn::failure(ErrorCode::NOSUPPORT),
         }
     }
