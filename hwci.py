25ceb447fd42cd6382185336732b5b28b34de212
charles37 <benprevor@gmail.com>
2024-09-30 11:49:37 -0400

    WIP

--- hwci.py
+++ hwci.py
@@ -8,7 +8,7 @@ import asyncio
 import time
 import re
 
-### HW CI TEST multi_alarm_simple default test WIP
+### HW CI TEST multi_alarm_simple default test wip
 
 logging.basicConfig(
     level=logging.DEBUG,
