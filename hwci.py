73398838d402fca79dc5af5ebc97c27906d69af4
charles37 <benprevor@gmail.com>
2024-10-02 13:54:33 -0400

    WIP

--- hwci.py
+++ hwci.py
@@ -7,6 +7,8 @@ import serial.tools.list_ports
 import asyncio
 import time
 import re
+from contextlib import contextmanager
+
 
 ### HW CI TEST multi_alarm_simple default test wip
 
@@ -17,16 +19,14 @@ logging.basicConfig(
 
 
 def run_command(command, cwd=None):
-    logging.info(f"Running command: {command}")
+    logging.info(f"Running command: {' '.join(command)}")
     process = subprocess.Popen(
         command,
         cwd=cwd,
-        shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
         bufsize=1,
-        universal_newlines=True,
     )
     for line in process.stdout:
         print(line, end="")
@@ -37,17 +37,31 @@ def run_command(command, cwd=None):
         raise subprocess.CalledProcessError(process.returncode, command)
 
 
+@contextmanager
+def change_directory(new_dir):
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    logging.info(f"Changed directory to: {os.getcwd()}")
+    try:
+        yield
+    finally:
+        os.chdir(previous_dir)
+        logging.info(f"Reverted to directory: {os.getcwd()}")
+
+
 def flash_kernel():
     if not os.path.exists("tock"):
-        run_command("git clone https://github.com/tock/tock")
+        run_command(["git", "clone", "https://github.com/tock/tock"])
 
-    os.chdir("tock/boards/nordic/nrf52840dk")
-    logging.info(f"Changed directory to: {os.getcwd()}")
-    run_command(
-        "openocd -c 'interface jlink; transport select swd; source [find target/nrf52.cfg]; init; nrf52_recover; exit'"
-    )
-    run_command("make flash-openocd")
-    os.chdir("../../../../")
+    with change_directory("tock/boards/nordic/nrf52840dk"):
+        run_command(
+            [
+                "openocd",
+                "-c",
+                "interface jlink; transport select swd; source [find target/nrf52.cfg]; init; nrf52_recover; exit",
+            ]
+        )
+        run_command(["make", "flash-openocd"])
 
 
 def install_apps(apps, target, port):
@@ -80,7 +94,7 @@ def get_serial_ports():
     return list(serial.tools.list_ports.comports())
 
 
-async def listen_for_output(command, search_text="Hello World!", timeout=60):
+async def listen_for_output(command, analysis_func=None, timeout=60):
     process = await asyncio.create_subprocess_shell(
         command,
         stdout=asyncio.subprocess.PIPE,
@@ -89,69 +103,59 @@ async def listen_for_output(command, search_text="Hello World!", timeout=60):
 
     logging.info(f"Listening for output from command: {command}")
 
+    output_lines = []
+
     try:
-        # No need to write to stdin since we're specifying the port
+
         async def read_stream(stream, name):
-            found = False
             while True:
                 line = await stream.readline()
                 if not line:
                     break  # EOF
                 decoded_line = line.decode().strip()
                 logging.info(f"{name}: {decoded_line}")
-                if search_text in decoded_line:
-                    return True
-            return False
+                output_lines.append(decoded_line)
 
         stdout_task = asyncio.create_task(read_stream(process.stdout, "STDOUT"))
         stderr_task = asyncio.create_task(read_stream(process.stderr, "STDERR"))
 
-        done, pending = await asyncio.wait(
+        await asyncio.wait(
             [stdout_task, stderr_task],
             timeout=timeout,
-            return_when=asyncio.FIRST_COMPLETED,
+            return_when=asyncio.ALL_COMPLETED,
         )
 
-        if done:
-            for task in done:
-                if task.result():
-                    logging.info(f"Found '{search_text}' in output")
-                    break
-            else:
-                logging.error(f"'{search_text}' not found in output")
+        if analysis_func:
+            return analysis_func(output_lines)
         else:
-            logging.error(f"Timeout expired after {timeout} seconds")
+            return output_lines
 
     except asyncio.TimeoutError:
         logging.error(f"Timeout expired after {timeout} seconds")
+        return None
     finally:
-        for task in pending:
-            task.cancel()
-        # Check if process is still running before killing
-        if process.returncode is None:
-            process.kill()
-            await process.wait()
+        process.kill()
+        await process.wait()
 
 
 def analyze_multi_alarm_output(output_lines):
-    pattern = re.compile(r"^(\d) (\d+) (\d+)$")
-    alarm_1_count = 0
-    alarm_2_count = 0
-    last_time = 0
+    pattern = re.compile(r"^Alarm (\d): Time (\d+), Expiration (\d+)$")
+    alarm_counts = {1: 0, 2: 0}
+    last_time = -1
 
     for line in output_lines:
         match = pattern.match(line)
         if match:
             alarm_id, current_time, expiration = map(int, match.groups())
-            if alarm_id == 1:
-                alarm_1_count += 1
-            elif alarm_id == 2:
-                alarm_2_count += 1
+            alarm_counts[alarm_id] += 1
 
-            if int(current_time) < last_time:
+            if current_time < last_time:
                 logging.error("Time went backwards")
                 return False
-            last_time = int(current_time)
+            last_time = current_time
+
+    alarm_1_count = alarm_counts[1]
+    alarm_2_count = alarm_counts[2]
 
     if alarm_1_count == 0 or alarm_2_count == 0:
         logging.error("One or both alarms did not fire")
@@ -200,8 +204,11 @@ async def main():
 
         if args.test == "hello_world":
             apps = ["c_hello"]
+            search_text = "Hello World!"
+            analysis_func = lambda output: search_text in "\n".join(output)
         elif args.test == "multi_alarm_simple_test":
             apps = ["multi_alarm_simple_test"]
+            analysis_func = analyze_multi_alarm_output
         else:
             logging.error(f"Unknown test type: {args.test}")
             return
@@ -210,22 +217,25 @@ async def main():
 
         await asyncio.sleep(1)
 
-        try:
-            test_result = await listen_for_output(
-                f"tockloader listen --port {port} --no-terminal",
-                search_text="Hello World!",
-                timeout=60,
-            )
-            if test_result:
-                logging.info("Test completed successfully")
-            else:
-                logging.error("Test failed")
-        except asyncio.TimeoutError:
-            logging.error("Test timed out waiting for result")
+        output = await listen_for_output(
+            f"tockloader listen --port {port} --no-terminal",
+            analysis_func=analysis_func,
+            timeout=60,
+        )
+
+        if output is True:
+            logging.info("Test completed successfully")
+        else:
+            logging.error("Test failed")
 
     except Exception as e:
         logging.exception("An error occurred during script execution")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+        sys.exit(0)
+    except Exception as e:
+        logging.exception("An error occurred during script execution")
+        sys.exit(1)
