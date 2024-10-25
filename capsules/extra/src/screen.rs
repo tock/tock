ad60c3a6f48e18583282510d3d290736c737b1a3
Amit Aryeh Levy <amit@amitlevy.com>
2024-10-25 12:07:52 -0700

    screen driver: fix error in copy length reporting
    If the target kernel buffer is smaller than the userspace buffer this
    commit correctly reports the amount of data _actually_ copied rather
    than the amount attempted from the user.

--- capsules/extra/src/screen.rs
+++ capsules/extra/src/screen.rs
@@ -353,17 +353,19 @@ impl<'a> Screen<'a> {
                                     .get_readonly_processbuffer(ro_allow::SHARED)
                                     .and_then(|shared| {
                                         shared.enter(|s| {
+                                            let mut count = 0;
                                             let mut chunks = s.chunks(buffer_size);
                                             if let Some(chunk) = chunks.nth(chunk_number) {
                                                 for (i, byte) in chunk.iter().enumerate() {
                                                     if pos < len {
                                                         buffer[i] = byte.get();
-                                                        pos += 1
+                                                        count += 1;
+                                                        pos += 1;
                                                     } else {
                                                         break;
                                                     }
                                                 }
-                                                app.write_len - initial_pos
+                                                count
                                             } else {
                                                 // stop writing
                                                 0
