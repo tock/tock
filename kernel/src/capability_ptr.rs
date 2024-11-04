7ec6bdd51152cc054c2f68dcab3425928ad11876
Amit Aryeh Levy <amit@amitlevy.com>
2024-11-04 14:35:04 -0800

    capability_ptr: update doc to reflect meaning

--- kernel/src/capability_ptr.rs
+++ kernel/src/capability_ptr.rs
@@ -7,26 +7,23 @@
 use core::fmt::{Formatter, LowerHex, UpperHex};
 use core::ops::AddAssign;
 
-/// A pointer with target specific metadata concerning validity or access rights.
+/// A pointer to userspace memory.
 ///
-/// This should be used any time the kernel wishes to grant authority to the user, or any time
-/// the user should be required to prove validity of a pointer.
+/// A [CapabilityPtr] points to memory a userspace processes may be
+/// permitted to read, write, or execute. It is sized exacly to a
+/// register that can pass values between userspace and kernel and at
+/// least the size of a word ([usize]) [^note1]. Operations on the
+/// pointer may affect permissions, e.g. offsetting the pointer beyond
+/// the bounds of the memory object invalidates it. Like a `*const
+/// ()`, a [CapabilityPtr] may also "hide" information by storing a
+/// word of data with no memory access permissions.
 ///
-/// Values that are just raw addresses but imply nothing about a Rust object at that location
-/// should be `usize`.
-/// Values that are references, but do not cross the boundary between the user and the
-/// kernel (or do cross the boundary but are merely informative and do not imply any rights)
-/// can be `*const T` (or `&T` if the kernel knows they are valid).
-/// Values that are references, and do need to cross the boundary, should be this type.
+/// [CapabilityPtr] should be used to store or pass between kernel and
+/// userspace a value that may represent a valid userspace reference.
 ///
-/// For example, `allow` grants authority to the kernel to access a buffer, so passes [CapabilityPtr]s.
-/// Conversely, when a process communicates its stack location to the kernel it need not be
-/// passed as a [CapabilityPtr], as the kernel does not access it.
-///
-/// [CapabilityPtr] is also assumed to be wide enough that it could contain a raw pointer (`*const ()`) or
-/// A `usize`, possibly podding with extra bits. It is therefore an appropriate choice for the type
-/// of a register that may contain any one of these in the syscall ABI at a point where it is not
-/// yet clear which of these it is yet.
+/// [^note1]: Depending on the architecutre, this the size of a
+/// [CapabilityPtr] may be a word size or larger, e.g., if registers
+/// can store metadata such as access permissions.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 #[repr(transparent)]
 pub struct CapabilityPtr {
