a990dc1664a99e712924f1df5e1849c0a634d2c5
Alexandru Radovici <msg4alex@gmail.com>
2024-12-13 19:05:12 +0200

    Update arch/cortex-m0/src/lib.rs
    Co-authored-by: Leon Schuermann <leon@is.currently.online>

--- arch/cortex-m0/src/lib.rs
+++ arch/cortex-m0/src/lib.rs
@@ -247,7 +247,7 @@ svc_handler:
   movs r0, #0
   msr CONTROL, r0
   /* CONTROL writes must be followed by ISB */
-  /* http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dai0321a/BIHFJCAC.html */
+    /* https://developer.arm.com/documentation/dui0662/b/The-Cortex-M0--Processor/Programmers-model/Core-registers */
   isb
   ldr r0, =SYSCALL_FIRED
   movs r1, #1
