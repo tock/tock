4285faad35fcf787fbdfae106f44a061243c4e82
Siddharth Kumar Jha <siddharth.k.jha@proton.me>
2024-12-14 15:04:25 +0530

    Improv(docs): Fixed doc errors

--- chips/sifive/src/plic.rs
+++ chips/sifive/src/plic.rs
@@ -15,7 +15,7 @@ use kernel::utilities::StaticRef;
 /// appropriate bounds set
 
 ///    The generic SiFive PLIC specification:
-///    https://github.com/riscv/riscv-plic-spec/blob/master/riscv-plic.adoc
+///    <https://github.com/riscv/riscv-plic-spec/blob/master/riscv-plic.adoc>
 ///    is defining maximum of 1023 interrupt sources
 
 // TODO: replace with const generic for `priority` and `_reserved1` field
@@ -127,7 +127,7 @@ impl<const TOTAL_INTS: usize> Plic<TOTAL_INTS> {
     /// Clear all pending interrupts. The [`PLIC specification`] section 7:
     /// > A successful claim will also atomically clear the corresponding pending bit on the interrupt source..
     /// Note that this function will only clear the enabled interrupt sources, as only those can be claimed.
-    /// [`PLIC specification`]: https://github.com/riscv/riscv-plic-spec/blob/master/riscv-plic.adoc
+    /// [`PLIC specification`]: <https://github.com/riscv/riscv-plic-spec/blob/master/riscv-plic.adoc>
     pub fn clear_all_pending(&self) {
         let claim = self.registers.get_claim_reg();
         loop {
