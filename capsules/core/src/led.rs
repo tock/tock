9e3144b65b30409bf464c3aeb5b80665dc9c6657
Anthony Alvarez <anthonyalvarez2026@u.northwestern.edu>
2025-01-04 19:52:09 -0600

    added debug statement to led syscall driver

--- capsules/core/src/led.rs
+++ capsules/core/src/led.rs
@@ -57,6 +57,7 @@
 use kernel::hil::led;
 use kernel::syscall::{CommandReturn, SyscallDriver};
 use kernel::{ErrorCode, ProcessId};
+use kernel::debug;
 
 /// Syscall driver number.
 use crate::driver;
@@ -94,6 +95,12 @@ impl<L: led::Led, const NUM_LEDS: usize> SyscallDriver for LedDriver<'_, L, NUM_
     /// - `3`: Toggle the LED at index specified by `data` on or off. Returns
     ///        `INVAL` if the LED index is not valid.
     fn command(&self, command_num: usize, data: usize, _: usize, _: ProcessId) -> CommandReturn {
+
+        debug!(
+            "LED syscall with command {} and data {}\n",
+            command_num, data
+        );
+
         match command_num {
             // get number of LEDs
             // TODO(Tock 3.0): TRD104 specifies that Command 0 should return Success, not SuccessU32,
