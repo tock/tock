9f502d920c040b9b1864a7c7a89d942443ff69af
Brad Campbell <bradjc5@gmail.com>
2025-01-08 11:48:25 -0500

    kernel: machine register: implement as struct

--- kernel/src/utilities/machine_register.rs
+++ kernel/src/utilities/machine_register.rs
@@ -16,19 +16,65 @@
 //! ISA-level memory protection (e.g., CHERI), a register is larger than
 //! `usize`.
 
-use crate::utilities::capability_ptr::CapabilityPtr;
+use core::fmt::{Formatter, LowerHex, UpperHex};
 
 /// [`MachineRegister`] is a datatype that can hold exactly the contents of a
 /// register with no additional semantic information.
-///
-/// We can define this as a [`CapabilityPtr`] as [`CapabilityPtr`] has the same
-/// size.
 ///
 /// [`MachineRegister`] is useful for identifying when data within the Tock
 /// kernel has no semantic meaning other than being the size of a register. In
 /// the future it may be possible, useful, or necessary to change the
 /// implementation of [`MachineRegister`], however, the semantics will remain.
-/// To reduce implementation complexity we simply use a type alias, but this
-/// should not be construed as to suggest that [`MachineRegister`] will always
-/// be implemented as a [`CapabilityPtr`].
-pub type MachineRegister = CapabilityPtr;
+/// No use of [`MachineRegister`] should assume a particular Rust implementation
+/// or any semantics other this description.
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+#[repr(transparent)]
+pub struct MachineRegister {
+    /// We store the actual data as a rust pointer as a convenient way to hold
+    /// an architecture-specific register worth of data.
+    ///
+    /// While represented as a pointer type, `value` has no semantic meaning as
+    /// a pointer. Any uses of a [`MachineRegister`] as a pointer must be based
+    /// other semantic understanding within Tock, and not from this type.
+    value: *const (),
+}
+
+impl Default for MachineRegister {
+    fn default() -> Self {
+        Self {
+            value: core::ptr::null(),
+        }
+    }
+}
+
+impl From<MachineRegister> for usize {
+    /// Returns the contents of the register.
+    #[inline]
+    fn from(from: MachineRegister) -> Self {
+        from.value as usize
+    }
+}
+
+impl From<usize> for MachineRegister {
+    /// Create a [`MachineRegister`] representation of a `usize`.
+    #[inline]
+    fn from(from: usize) -> Self {
+        Self {
+            value: from as *const (),
+        }
+    }
+}
+
+impl UpperHex for MachineRegister {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        UpperHex::fmt(&(self.value as usize), f)
+    }
+}
+
+impl LowerHex for MachineRegister {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        LowerHex::fmt(&(self.value as usize), f)
+    }
+}
