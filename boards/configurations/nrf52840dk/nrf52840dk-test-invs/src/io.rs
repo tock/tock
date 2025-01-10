4423d85348fc354b8709060e2f96ea30387acea2
Brad Campbell <bradjc5@gmail.com>
2025-01-10 13:23:36 -0500

    boards: config: add invs config board for testing

---
+++ boards/configurations/nrf52840dk/nrf52840dk-test-invs/src/io.rs
@@ -0,0 +1,17 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2024.
+
+use core::panic::PanicInfo;
+use nrf52840::gpio::Pin;
+
+#[cfg(not(test))]
+#[no_mangle]
+#[panic_handler]
+/// Panic handler
+pub unsafe fn panic_fmt(_pi: &PanicInfo) -> ! {
+    // The nRF52840DK LEDs (see back of board)
+    let led_kernel_pin = &nrf52840::gpio::GPIOPin::new(Pin::P0_13);
+    let led = &mut kernel::hil::led::LedLow::new(led_kernel_pin);
+    kernel::debug::panic_blink_forever(&mut [led])
+}
