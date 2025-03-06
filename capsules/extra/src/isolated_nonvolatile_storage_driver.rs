76f597e98c2e1d6f83aab1b343fa92d58719b27c
Brad Campbell <bradjc5@gmail.com>
2025-03-06 10:52:41 -0500

    capsules: invs: add usize32s_to_usize helper fn

--- capsules/extra/src/isolated_nonvolatile_storage_driver.rs
+++ capsules/extra/src/isolated_nonvolatile_storage_driver.rs
@@ -332,6 +332,38 @@ pub struct App {
     pending_operation: Option<NvmCommand>,
 }
 
+/// Helper function to convert create a full, single usize value from two 32-bit
+/// values stored in usizes.
+///
+/// In C this would look like:
+///
+/// ```c
+/// size_t v = (hi << 32) | (uint32_t) lo;
+/// ```
+///
+/// This is useful when passing a machine-sized value (i.e. a `size_t`) via the
+/// system call interface in two 32-bit usize values. On a 32-bit machine this
+/// essentially has no effect; the full value is stored in the `lo` usize. On a
+/// 64-bit machine, this creates a usize by concatenating the hi and lo 32-bit
+/// values.
+///
+/// TODO
+/// ----
+///
+/// This can be more succinctly implemented using
+/// [`unbounded_shl()`](https://doc.rust-lang.org/stable/std/primitive.usize.html#method.unbounded_shl).
+/// However, that method is currently a nightly-only feature.
+#[inline]
+pub const fn usize32s_to_usize(lo: usize, hi: usize) -> usize {
+    if usize::BITS <= 32 {
+        // Just return the lo value since it has the bits we need.
+        lo
+    } else {
+        // Create a 64-bit value.
+        (lo & 0xFFFFFFFF) | (hi << 32)
+    }
+}
+
 pub struct IsolatedNonvolatileStorage<'a, const APP_REGION_SIZE: usize> {
     /// The underlying physical storage device.
     driver: &'a dyn hil::nonvolatile_storage::NonvolatileStorage<'a>,
@@ -1081,11 +1113,7 @@ impl<const APP_REGION_SIZE: usize> SyscallDriver
             1 | 2 | 3 => {
                 // We want to handle both 64-bit and 32-bit platforms, but on
                 // 32-bit platforms shifting `offset_hi` doesn't make sense.
-                let offset: usize = if usize::BITS <= 32 {
-                    offset_lo
-                } else {
-                    (offset_lo & 0xFFFFFFFF) | (offset_hi << 32)
-                };
+                let offset: usize = usize32s_to_usize(offset_lo, offset_hi);
                 let nvm_command = match command_num {
                     1 => NvmCommand::GetSize,
                     2 => NvmCommand::Read { offset },
