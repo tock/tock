ca6ca9d933da115b56d47c95abae53daa60e4cfd
viswajith-g <gyx4bw@virginia.edu>
2025-05-01 11:26:10 -0400

    revert no_mangle removal from main.rs and remove in io.rs

--- boards/tutorials/nrf52840dk-dynamic-apps-and-policies/src/main.rs
+++ boards/tutorials/nrf52840dk-dynamic-apps-and-policies/src/main.rs
@@ -61,6 +61,7 @@ static mut PROCESS_PRINTER: Option<&'static capsules_system::process_printer::Pr
     None;
 
 /// Dummy buffer that causes the linker to reserve enough space for the stack.
+#[no_mangle]
 #[link_section = ".stack_buffer"]
 pub static mut STACK_MEMORY: [u8; 0x2000] = [0; 0x2000];
 
