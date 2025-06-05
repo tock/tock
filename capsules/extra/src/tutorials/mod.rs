9208f3ba693668e96459d2b2eac5de958a5f4470
Tyler Potyondy <potyondyt@gmail.com>
2025-06-05 13:13:14 -0700

    Reorganize tutorial capsule folder per tutorial
    seperate into:
    - hotp tutorial

--- capsules/extra/src/tutorials/mod.rs
+++ capsules/extra/src/tutorials/mod.rs
@@ -1,15 +1,5 @@
 // Licensed under the Apache License, Version 2.0 or the MIT License.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// Copyright Tock Contributors 2023.
+// Copyright Tock Contributors 2025.
 
-pub mod encryption_oracle_chkpt0;
-#[allow(dead_code)]
-pub mod encryption_oracle_chkpt1;
-#[allow(dead_code)]
-pub mod encryption_oracle_chkpt2;
-#[allow(dead_code)]
-pub mod encryption_oracle_chkpt3;
-#[allow(dead_code, unused_variables, unused_mut)]
-pub mod encryption_oracle_chkpt4;
-#[allow(dead_code)]
-pub mod encryption_oracle_chkpt5;
+mod hotp_tutorial;
