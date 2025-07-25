1fc57fb1be7c41458e8af3549726c7cbd5d27d66
Ioan-Cristian CÎRSTEA <ioan.cirstea@oxidos.io>
2025-07-25 19:48:14 +0300

    boards/nucleo_f429zi: Fixed stack size (again...)
    Signed-off-by: Ioan-Cristian CÎRSTEA <ioan.cirstea@oxidos.io>

--- boards/nucleo_f429zi/src/main.rs
+++ boards/nucleo_f429zi/src/main.rs
@@ -50,7 +50,7 @@ const FAULT_RESPONSE: capsules_system::process_policies::PanicFaultPolicy =
 /// Dummy buffer that causes the linker to reserve enough space for the stack.
 #[no_mangle]
 #[link_section = ".stack_buffer"]
-static mut STACK_MEMORY: [u8; 0x2000] = [0; 0x2000];
+static mut STACK_MEMORY: [u8; 0x3000] = [0; 0x3000];
 
 //------------------------------------------------------------------------------
 // SYSCALL DRIVER TYPE DEFINITIONS
