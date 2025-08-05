fccd7dfa03971a496520d0ac6faa30dfea70e412
Brad Campbell <bradjc5@gmail.com>
2025-08-05 09:46:27 -0400

    hil: keyboard

--- kernel/src/hil/keyboard.rs
+++ kernel/src/hil/keyboard.rs
@@ -6,10 +6,20 @@
 
 use crate::ErrorCode;
 
-pub trait Client {
-    fn keys_pressed(&self, keys: &[u16], result: Result<(), ErrorCode>);
+/// Receiver for keyboard key presses.
+pub trait KeyboardClient {
+    /// Called when one or more keys are pressed or un-pressed.
+    ///
+    /// `keys` is an array of `(key_code, is_pressed)` tuples. `key_code` is the
+    /// same as the codes used by Linux to identify different keys.
+    /// `is_pressed` is true if the key was pressed, and false if the key was
+    /// un-pressed.
+    ///
+    /// `result` is `Ok(())` if the keys were received correctly.
+    fn keys_pressed(&self, keys: &[(u16, bool)], result: Result<(), ErrorCode>);
 }
 
+/// Represents a keyboard that can generate button presses.
 pub trait Keyboard<'a> {
-    fn set_client(&self, client: &'a dyn Client);
+    fn set_client(&self, client: &'a dyn KeyboardClient);
 }
