bd9c2ec5a04e7ba21015883c578b6d30cde0887f
Brad Campbell <bradjc5@gmail.com>
2025-08-05 09:46:38 -0400

    virtio input

--- chips/virtio/src/devices/virtio_input.rs
+++ chips/virtio/src/devices/virtio_input.rs
@@ -6,24 +6,32 @@
 //!
 //! <https://docs.oasis-open.org/virtio/virtio/v1.2/csd01/virtio-v1.2-csd01.html#x1-3850008>
 //!
-//! This implementation assumes the input device is a keyboard.
+//! This implementation assumes the input device is a keyboard that follows the
+//! [event code](https://www.kernel.org/doc/Documentation/input/event-codes.txt)
+//! format that Linux uses.
 
 use kernel::utilities::cells::OptionalCell;
 
 use crate::devices::{VirtIODeviceDriver, VirtIODeviceType};
 use crate::queues::split_queue::{SplitVirtqueue, SplitVirtqueueClient, VirtqueueBuffer};
 
+/// Event: separate events.
 const EV_SYN: u16 = 0;
+/// Event: state change of keyboard.
 const EV_KEY: u16 = 1;
 
+/// VirtIO for input devices (e.g., keyboards).
 pub struct VirtIOInput<'a> {
+    /// Queue of events from the device (e.g., keyboard).
     eventq: &'a SplitVirtqueue<'static, 'static, 3>,
+    /// Queue of status updates from this driver (e.g., turn on LED).
     statusq: &'a SplitVirtqueue<'static, 'static, 1>,
+    /// Buffer to hold status updates.
     status_buffer: OptionalCell<&'static mut [u8]>,
-
-    keys: [OptionalCell<u16>; 2],
-
-    client: OptionalCell<&'a dyn kernel::hil::keyboard::Client>,
+    /// Store keys sent across multiple events.
+    keys: [OptionalCell<(u16, bool)>; 2],
+    /// Keyboard callback client.
+    client: OptionalCell<&'a dyn kernel::hil::keyboard::KeyboardClient>,
 }
 
 impl<'a> VirtIOInput<'a> {
@@ -38,7 +46,6 @@ impl<'a> VirtIOInput<'a> {
             eventq,
             statusq,
             status_buffer: OptionalCell::new(status_buffer),
-
             keys: [const { OptionalCell::empty() }; 2],
             client: OptionalCell::empty(),
         }
@@ -50,6 +57,8 @@ impl<'a> VirtIOInput<'a> {
         event_buffer2: &'static mut [u8],
         event_buffer3: &'static mut [u8],
     ) {
+        // Provide the device three buffers to hold up to two keys and a sync
+        // event.
         let event_buffer_len = event_buffer1.len();
         let mut buffer_chain = [Some(VirtqueueBuffer {
             buf: event_buffer1,
@@ -83,43 +92,37 @@ impl SplitVirtqueueClient<'static> for VirtIOInput<'_> {
         buffer_chain: &mut [Option<VirtqueueBuffer<'static>>],
         _bytes_used: usize,
     ) {
+        fn parse_event(buf: &[u8]) -> Result<(u16, u16, u32), ()> {
+            let event_type = u16::from_le_bytes(buf.get(0..2).ok_or(())?.try_into().or(Err(()))?);
+            let event_code = u16::from_le_bytes(buf.get(2..4).ok_or(())?.try_into().or(Err(()))?);
+            let event_value = u32::from_le_bytes(buf.get(4..8).ok_or(())?.try_into().or(Err(()))?);
+            Ok((event_type, event_code, event_value))
+        }
+
         if queue_number == self.eventq.queue_number().unwrap() {
             // Received an input device event
-            kernel::debug!("bcr input event");
 
             // Process the incoming key. If this is the SYN_REPORT then our key
             // press is finished and we can call the client.
             let end = if let Some(event_buffer) = &buffer_chain[0] {
-                let event_type = u16::from_le_bytes([event_buffer.buf[0], event_buffer.buf[1]]);
-                let event_code = u16::from_le_bytes([event_buffer.buf[2], event_buffer.buf[3]]);
-                let event_value = u32::from_le_bytes([
-                    event_buffer.buf[4],
-                    event_buffer.buf[5],
-                    event_buffer.buf[6],
-                    event_buffer.buf[7],
-                ]);
-
-                kernel::debug!(
-                    "VirtIO Input Event: t:{}, c:{}, v:{}",
-                    event_type,
-                    event_code,
-                    event_value
-                );
-
-                if event_type == EV_KEY && event_value == 1 {
-                    // This is a key down press. Save in the next available
-                    // slot.
-                    if self.keys[0].is_none() {
-                        self.keys[0].set(event_code);
-                    } else {
-                        if self.keys[1].is_none() {
-                            self.keys[1].set(event_code);
+                if let Ok((event_type, event_code, event_value)) = parse_event(event_buffer.buf) {
+                    if event_type == EV_KEY {
+                        // This is a key down press. Save in the next available
+                        // slot.
+                        if self.keys[0].is_none() {
+                            self.keys[0].set((event_code, event_value == 1));
+                        } else {
+                            if self.keys[1].is_none() {
+                                self.keys[1].set((event_code, event_value == 1));
+                            }
                         }
                     }
-                }
 
-                // If this is a SYN_REPORT return true
-                event_type == EV_SYN && event_code == 0 && event_value == 0
+                    // If this is a SYN_REPORT return true
+                    event_type == EV_SYN && event_code == 0 && event_value == 0
+                } else {
+                    false
+                }
             } else {
                 false
             };
@@ -128,7 +131,7 @@ impl SplitVirtqueueClient<'static> for VirtIOInput<'_> {
 
             if end {
                 // Signal to client that we got key presses.
-                let mut key_presses: [u16; 2] = [0; 2];
+                let mut key_presses: [(u16, bool); 2] = [(0, false); 2];
                 let mut length = 0;
                 self.keys[0].take().map(|key| {
                     key_presses[length] = key;
@@ -138,7 +141,6 @@ impl SplitVirtqueueClient<'static> for VirtIOInput<'_> {
                     key_presses[length] = key;
                     length += 1;
                 });
-                kernel::debug!("KEYS PRESSED {:?}", key_presses);
                 self.client.map(|client| {
                     client.keys_pressed(&key_presses[0..length], Ok(()));
                 });
@@ -166,7 +168,7 @@ impl VirtIODeviceDriver for VirtIOInput<'_> {
 }
 
 impl<'a> kernel::hil::keyboard::Keyboard<'a> for VirtIOInput<'a> {
-    fn set_client(&self, client: &'a dyn kernel::hil::keyboard::Client) {
+    fn set_client(&self, client: &'a dyn kernel::hil::keyboard::KeyboardClient) {
         self.client.set(client);
     }
 }
