e237ec76997d57fe8e51e22f0e23db17ca3a711d
Leon Schuermann <leon@is.currently.online>
2025-08-08 16:31:15 -0400

    kernel/utilities/SubSlice: add `active_range` method for subslicing

--- kernel/src/utilities/leasable_buffer.rs
+++ kernel/src/utilities/leasable_buffer.rs
@@ -333,6 +333,44 @@ impl<'a, T> SubSliceMut<'a, T> {
         self.as_mut_slice().as_mut_ptr()
     }
 
+    /// Returns the bounds of the currently accessible `SubSliceMut` window,
+    /// relative to the underlying, internal buffer.
+    ///
+    /// This method can be used to re-construct a `SubSliceMut`, retaining its
+    /// active window, after `take()`ing its internal buffer. This is useful for
+    /// performing nested sub-slicing and interoperability with interfaces that take
+    /// a `&[u8]` buffer with an offset and length.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use kernel::utilities::leasable_buffer::SubSliceMut;
+    /// let mut buffer: [u8; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
+    ///
+    /// // Construct a SubSliceMut and activate a window:
+    /// let mut original_subslicemut = SubSliceMut::new(&mut buffer[..]);
+    /// original_subslicemut.slice(3..5);
+    /// assert!(original_subslicemut.as_slice() == &[3, 4]);
+    ///
+    /// // Destruct the SubSliceMut, extracting its underlying buffer, but
+    /// // remembering its active range:
+    /// let remembered_range = original_subslicemut.active_range();
+    /// let extracted_buffer = original_subslicemut.take();
+    /// assert!(remembered_range == (3..5));
+    ///
+    /// // Construct a new SubSliceMut, over the original buffer, with identical
+    /// // bounds:
+    /// let mut reconstructed_subslicemut = SubSliceMut::new(extracted_buffer);
+    /// reconstructed_subslicemut.slice(remembered_range);
+    ///
+    /// // The new, reconstructed SubSliceMut's window is identical to the
+    /// // original one's:
+    /// assert!(reconstructed_subslicemut.as_slice() == &[3, 4]);
+    /// ```
+    pub fn active_range(&self) -> Range<usize> {
+        self.active_range.clone()
+    }
+
     /// Returns a slice of the currently accessible portion of the
     /// LeasableBuffer.
     pub fn as_slice(&self) -> &[T] {
@@ -467,6 +505,44 @@ impl<'a, T> SubSlice<'a, T> {
     /// to try to use a sliced LeasableBuffer.
     pub fn is_sliced(&self) -> bool {
         self.internal.len() != self.len()
+    }
+
+    /// Returns the bounds of the currently accessible `SubSlice` window,
+    /// relative to the underlying, internal buffer.
+    ///
+    /// This method can be used to re-construct a `SubSlice`, retaining its
+    /// active window, after `take()`ing its internal buffer. This is useful for
+    /// performing nested sub-slicing and interoperability with interfaces that take
+    /// a `&[u8]` buffer with an offset and length.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// use kernel::utilities::leasable_buffer::SubSlice;
+    /// let mut buffer: [u8; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
+    ///
+    /// // Construct a SubSlice and activate a window:
+    /// let mut original_subslicemut = SubSlice::new(&mut buffer[..]);
+    /// original_subslicemut.slice(3..5);
+    /// assert!(original_subslicemut.as_slice() == &[3, 4]);
+    ///
+    /// // Destruct the SubSlice, extracting its underlying buffer, but
+    /// // remembering its active range:
+    /// let remembered_range = original_subslicemut.active_range();
+    /// let extracted_buffer = original_subslicemut.take();
+    /// assert!(remembered_range == (3..5));
+    ///
+    /// // Construct a new SubSlice, over the original buffer, with identical
+    /// // bounds:
+    /// let mut reconstructed_subslicemut = SubSlice::new(extracted_buffer);
+    /// reconstructed_subslicemut.slice(remembered_range);
+    ///
+    /// // The new, reconstructed SubSlice's window is identical to the
+    /// // original one's:
+    /// assert!(reconstructed_subslicemut.as_slice() == &[3, 4]);
+    /// ```
+    pub fn active_range(&self) -> Range<usize> {
+        self.active_range.clone()
     }
 
     /// Reduces the range of the SubSlice that is accessible.
