f10eb661e280e03fc13c1f6977b331a98f049d30
Brad Campbell <bradjc5@gmail.com>
2025-10-03 11:24:17 -0400

    kernel: debug: update debugwriter trait
    - Lie about available_len if there is no buffer (and therefore no
      available buffer exists which cannot have a length)
    - Clarify flush vs publish
    - Add APIs in example sync debug writer

--- kernel/src/debug.rs
+++ kernel/src/debug.rs
@@ -43,7 +43,20 @@
 //!        }
 //!        buf.len()
 //!    }
-//! }
+//!
+//!    fn available_len(&self) -> usize {
+//!        usize::MAX
+//!    }
+//!
+//!    fn to_write_len(&self) -> usize {
+//!        0
+//!    }
+//!
+//!    fn publish(&self) -> usize {
+//!        0
+//!    }
+//!
+//!    fn flush(&self, _writer: &mut dyn IoWrite) { }
 //! ```
 //! And instantiate it in the main board file:
 //!
@@ -351,8 +364,12 @@ pub trait DebugWriter {
 
     /// Available length of the internal buffer if limited.
     ///
-    /// If the buffer can support a write of any size, this returns `None`.
-    fn available_len(&self) -> Option<usize>;
+    /// If the buffer can support a write of any size, it should lie and return
+    /// `usize::MAX`.
+    ///
+    /// Across subsequent calls to this function, without invoking `write()` in
+    /// between, this returned value may only increase, but never decrease.
+    fn available_len(&self) -> usize;
 
     /// How many bytes are buffered and not yet written.
     fn to_write_len(&self) -> usize;
@@ -362,7 +379,10 @@ pub trait DebugWriter {
     /// Returns how many bytes were written.
     fn publish(&self) -> usize;
 
-    /// Flush any buffered bytes to the output writer.
+    /// Flush any buffered bytes to the provided output writer.
+    ///
+    /// `flush()` should be used to write an buffered bytes to a new `writer`
+    /// instead of the internal writer that `publish()` would use.
     fn flush(&self, writer: &mut dyn IoWrite);
 }
 
@@ -518,8 +538,8 @@ impl DebugWriter for UartDebugWriter {
         });
     }
 
-    fn available_len(&self) -> Option<usize> {
-        Some(self.internal_buffer.map_or(0, |rb| rb.available_len()))
+    fn available_len(&self) -> usize {
+        self.internal_buffer.map_or(0, |rb| rb.available_len())
     }
 
     fn to_write_len(&self) -> usize {
