3a43dd1d04b7d057528791c4e07e288a3412eb9f
Leon Schuermann <leon@is.currently.online>
2025-11-26 17:09:45 -0500

    boards: wm1110dev: panic resources

--- boards/wm1110dev/src/io.rs
+++ boards/wm1110dev/src/io.rs
@@ -12,10 +12,6 @@ use kernel::hil::uart;
 use nrf52840::gpio::Pin;
 use nrf52840::uart::UARTE0_BASE;
 
-use crate::CHIP;
-use crate::PROCESSES;
-use crate::PROCESS_PRINTER;
-
 /// Writer is used by kernel::debug to panic message to the serial port.
 pub struct Writer {
     initialized: bool,
@@ -73,17 +69,15 @@ impl IoWrite for Writer {
 pub unsafe fn panic_fmt(pi: &PanicInfo) -> ! {
     // Red Led
 
-    use core::ptr::{addr_of, addr_of_mut};
+    use core::ptr::addr_of_mut;
     let led_red_pin = &nrf52840::gpio::GPIOPin::new(Pin::P0_14);
     let led = &mut led::LedHigh::new(led_red_pin);
     let writer = &mut *addr_of_mut!(WRITER);
-    debug::panic_old(
+    debug::panic(
         &mut [led],
         writer,
         pi,
         &cortexm4::support::nop,
-        PROCESSES.unwrap().as_slice(),
-        &*addr_of!(CHIP),
-        &*addr_of!(PROCESS_PRINTER),
+        crate::PANIC_RESOURCES.get(),
     )
 }
