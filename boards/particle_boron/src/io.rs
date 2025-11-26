9794c48b6b29cd43cdc6a6fe0fe15767da58eaf9
Brad Campbell <bradjc5@gmail.com>
2025-11-26 17:17:12 -0500

    boards: particle_boron: panic resources

--- boards/particle_boron/src/io.rs
+++ boards/particle_boron/src/io.rs
@@ -12,10 +12,6 @@ use kernel::hil::uart::Configure;
 use nrf52840::gpio::Pin;
 use nrf52840::uart::{Uarte, UARTE0_BASE};
 
-use crate::CHIP;
-use crate::PROCESSES;
-use crate::PROCESS_PRINTER;
-
 // Expand here with more writing methods as required (rtt/cdc etc...)
 enum Writer {
     WriterUart(/* initialized */ bool),
@@ -66,17 +62,15 @@ const LED2_R_PIN: Pin = Pin::P0_13;
 pub unsafe fn panic_fmt(pi: &PanicInfo) -> ! {
     // The nRF52840DK LEDs (see back of board)
 
-    use core::ptr::{addr_of, addr_of_mut};
+    use core::ptr::addr_of_mut;
     let led_kernel_pin = &nrf52840::gpio::GPIOPin::new(LED2_R_PIN);
     let led = &mut led::LedLow::new(led_kernel_pin);
     let writer = &mut *addr_of_mut!(WRITER);
-    debug::panic_old(
+    debug::panic(
         &mut [led],
         writer,
         pi,
         &cortexm4::support::nop,
-        PROCESSES.unwrap().as_slice(),
-        &*addr_of!(CHIP),
-        &*addr_of!(PROCESS_PRINTER),
+        crate::PANIC_RESOURCES.get(),
     )
 }
