4feedb3201ea19ed96281a8ee422182bba19973f
Brad Campbell <bradjc5@gmail.com>
2025-12-02 14:29:32 -0500

    chips: nrf52: implement init()
    Move the errata setup, vector table, and interrupt enable to functions,
    document safety, and call everything from the new init() trait function.

--- chips/nrf52840/src/lib.rs
+++ chips/nrf52840/src/lib.rs
@@ -4,8 +4,8 @@
 
 #![no_std]
 pub use nrf52::{
-    acomp, adc, aes, ble_radio, chip, clock, constants, crt1, ficr, i2c, ieee802154_radio, init,
-    nvmc, peripheral_interrupts as base_interrupts, pinmux, power, ppi, pwm, rtc, spi, temperature,
+    acomp, adc, aes, ble_radio, chip, clock, constants, crt1, ficr, i2c, ieee802154_radio, nvmc,
+    peripheral_interrupts as base_interrupts, pinmux, power, ppi, pwm, rtc, spi, temperature,
     timer, trng, uart, uicr, usbd,
 };
 pub mod gpio;
