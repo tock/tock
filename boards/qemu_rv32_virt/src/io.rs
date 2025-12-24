18c102ecbfda56d042e721125a18141c49864f1b
Brad Campbell <bradjc5@gmail.com>
2025-12-24 10:56:37 -0500

    boards: qemu rv32: use panic writer
    tested with the panic command in the process console

--- boards/qemu_rv32_virt/src/io.rs
+++ boards/qemu_rv32_virt/src/io.rs
@@ -2,42 +2,25 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // Copyright Tock Contributors 2022.
 
-use core::fmt::Write;
 use core::panic::PanicInfo;
-use core::str;
 
 use kernel::debug;
-use kernel::utilities::io_write::IoWrite;
-
-struct Writer {}
-
-static mut WRITER: Writer = Writer {};
-
-impl Write for Writer {
-    fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
-        self.write(s.as_bytes());
-        Ok(())
-    }
-}
-
-impl IoWrite for Writer {
-    fn write(&mut self, buf: &[u8]) -> usize {
-        let uart = qemu_rv32_virt_chip::uart::Uart16550::new(qemu_rv32_virt_chip::uart::UART0_BASE);
-        uart.transmit_sync(buf);
-        buf.len()
-    }
-}
+use kernel::hil::uart;
 
 /// Panic handler.
 #[cfg(not(test))]
 #[panic_handler]
 pub unsafe fn panic_fmt(pi: &PanicInfo) -> ! {
-    use core::ptr::addr_of_mut;
-
-    let writer = &mut *addr_of_mut!(WRITER);
-
-    debug::panic_print_old::<_, _, _>(
-        writer,
+    debug::panic_print::<qemu_rv32_virt_chip::uart::Uart16550, _, _>(
+        qemu_rv32_virt_chip::uart::UartPanicWriterConfig {
+            params: uart::Parameters {
+                baud_rate: 115200,
+                stop_bits: uart::StopBits::One,
+                parity: uart::Parity::None,
+                hw_flow_control: false,
+                width: uart::Width::Eight,
+            },
+        },
         pi,
         &rv32i::support::nop,
         crate::PANIC_RESOURCES.get(),
