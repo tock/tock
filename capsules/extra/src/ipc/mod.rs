a1f10d85f327fc0658d92be8ff7d47bb5b5b5f8a
Branden Ghena <brghena@berkeley.edu>
2025-12-28 12:07:32 -0500

    Initial IPC Registry Mechanisms
    String Name mechanism has passed basic testing

---
+++ capsules/extra/src/ipc/mod.rs
@@ -0,0 +1,8 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2026.
+
+//! Provides capsules for interprocess communication (IPC)
+
+pub mod ipc_registry_package_name;
+pub mod ipc_registry_string_name;
