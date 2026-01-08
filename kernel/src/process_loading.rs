310edcb54018c99f552b4b1b811d44c43ebcaaa6
viswajith-g <gyx4bw@virginia.edu>
2026-01-07 22:24:03 -0500

    reclaim ram

--- kernel/src/process_loading.rs
+++ kernel/src/process_loading.rs
@@ -1230,6 +1230,11 @@ impl<'a, C: Chip, D: ProcessStandardDebug> SequentialProcessLoaderMachine<'a, C,
             )),
         }
     }
+
+    /// Function to terminate process and remove it from process array
+    pub fn reclaim_memory(&self, shortid: ShortId) {
+        self.kernel.reclaim_app_memory(shortid)
+    }
 }
 
 impl<'a, C: Chip, D: ProcessStandardDebug> ProcessLoadingAsync<'a>
