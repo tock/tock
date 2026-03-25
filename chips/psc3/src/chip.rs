4f5c8f8f2f357c20bba104b64ca29a449851237a
Niklas Sirch <niklas.sirch@infineon.com>
2026-03-25 09:07:18 +0100

    migrate chip

---
+++ chips/psc3/src/chip.rs
@@ -0,0 +1,123 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright OxidOS Automotive 2025.
+
+//! Chip trait setup.
+
+use core::fmt::Write;
+use kernel::platform::chip::Chip;
+use kernel::platform::chip::InterruptService;
+
+use crate::interrupts;
+use crate::scb::Scb;
+use crate::tcpwm::Tcpwm0;
+use cortexm33::{CortexM33, CortexMVariant};
+
+pub struct Psc3<'a, I: InterruptService + 'a> {
+    mpu: cortexm33::mpu::MPU<8>,
+    userspace_kernel_boundary: cortexm33::syscall::SysCall,
+    interrupt_service: &'a I,
+}
+
+impl<'a, I: InterruptService> Psc3<'a, I> {
+    pub unsafe fn new(interrupt_service: &'a I) -> Self {
+        Self {
+            mpu: cortexm33::mpu::new(),
+            userspace_kernel_boundary: cortexm33::syscall::SysCall::new(),
+            interrupt_service,
+        }
+    }
+}
+
+impl<I: InterruptService> Chip for Psc3<'_, I> {
+    type MPU = cortexm33::mpu::MPU<8>;
+    type UserspaceKernelBoundary = cortexm33::syscall::SysCall;
+    type ThreadIdProvider = cortexm33::thread_id::CortexMThreadIdProvider;
+
+    fn service_pending_interrupts(&self) {
+        unsafe {
+            while let Some(interrupt) = cortexm33::nvic::next_pending() {
+                // ignore PROC1_IRQ_CTI as it is intended for processor 1
+                // not able to unset its pending status
+                // probably only processor 1 can unset the pending by reading the fifo
+                if !self.interrupt_service.service_interrupt(interrupt) {
+                    panic!("unhandled interrupt {}", interrupt);
+                }
+                let n = cortexm33::nvic::Nvic::new(interrupt);
+                n.clear_pending();
+                n.enable();
+            }
+        }
+    }
+
+    fn has_pending_interrupts(&self) -> bool {
+        unsafe { cortexm33::nvic::has_pending() }
+    }
+
+    fn mpu(&self) -> &Self::MPU {
+        &self.mpu
+    }
+
+    fn userspace_kernel_boundary(&self) -> &Self::UserspaceKernelBoundary {
+        &self.userspace_kernel_boundary
+    }
+
+    fn sleep(&self) {
+        unsafe {
+            cortexm33::support::wfi();
+        }
+    }
+
+    unsafe fn with_interrupts_disabled<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce() -> R,
+    {
+        cortexm33::support::with_interrupts_disabled(f)
+    }
+
+    unsafe fn print_state(_this: Option<&Self>, writer: &mut dyn Write) {
+        CortexM33::print_cortexm_state(writer);
+    }
+}
+
+pub struct Psc3DefaultPeripherals<'a> {
+    // pub cpuss: cpuss::Cpuss,
+    // pub gpio: gpio::PsocPins<'a>,
+    // pub hsiom: hsiom::Hsiom,
+    // pub peri: peri::Peri,
+    pub scb: Scb<'a>,
+    // pub srss: srss::Srss,
+    pub tcpwm: Tcpwm0<'a>,
+}
+
+impl Psc3DefaultPeripherals<'_> {
+    pub fn new() -> Self {
+        Self {
+            scb: Scb::new(),
+            tcpwm: Tcpwm0::new(),
+        }
+    }
+
+    pub fn resolve_dependencies(&'static self) {
+        // self.uart0.set_clocks(&self.clocks);
+        // self.ticks.set_timer0_generator();
+        // self.ticks.set_timer1_generator();
+        // kernel::deferred_call::DeferredCallClient::register(&self.scb);
+    }
+}
+
+impl InterruptService for Psc3DefaultPeripherals<'_> {
+    unsafe fn service_interrupt(&self, interrupt: u32) -> bool {
+        match interrupt {
+            interrupts::TCPWM_0_INTERRUPTS_0 => {
+                self.tcpwm.handle_interrupt();
+                true
+            }
+            // interrupts::UART0_IRQ => {
+            //     self.uart0.handle_interrupt();
+            //     true
+            // }
+            _ => false,
+        }
+    }
+}
---
+++ chips/psc3/src/lib.rs
@@ -0,0 +1,55 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright OxidOS Automotive 2025 SRL.
+
+#![no_std]
+// `registers/rv_plic_regs` has many register definitions in `register_structs()!`
+// and requires a deeper recursion limit than the default to fully expand.
+#![recursion_limit = "512"]
+
+use cortexm33::{initialize_ram_jump_to_main, unhandled_interrupt, CortexM33, CortexMVariant};
+
+extern "C" {
+    // _estack is not really a function, but it makes the types work
+    // You should never actually invoke it!!
+    fn _estack();
+}
+
+#[cfg_attr(
+    all(target_arch = "arm", target_os = "none"),
+    link_section = ".vectors"
+)]
+// used Ensures that the symbol is kept until the final binary
+#[cfg_attr(all(target_arch = "arm", target_os = "none"), used)]
+pub static BASE_VECTORS: [unsafe extern "C" fn(); 16] = [
+    _estack,
+    initialize_ram_jump_to_main,
+    unhandled_interrupt,           // NMI
+    CortexM33::HARD_FAULT_HANDLER, // Hard Fault
+    unhandled_interrupt,           // MemManage
+    unhandled_interrupt,           // BusFault
+    unhandled_interrupt,           // UsageFault
+    unhandled_interrupt,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    unhandled_interrupt,
+    CortexM33::SVC_HANDLER, // SVC
+    unhandled_interrupt,    // DebugMon
+    unhandled_interrupt,
+    unhandled_interrupt,        // PendSV
+    CortexM33::SYSTICK_HANDLER, // SysTick
+];
+
+#[cfg_attr(
+    all(target_arch = "arm", target_os = "none"),
+    link_section = ".vectors"
+)]
+// used Ensures that the symbol is kept until the final binary
+#[cfg_attr(all(target_arch = "arm", target_os = "none"), used)]
+pub static IRQS: [unsafe extern "C" fn(); 8] = [CortexM33::GENERIC_ISR; 8];
+
+pub mod chip;
+pub mod gpio;
+pub mod interrupts;
+pub mod scb;
+pub mod tcpwm;
