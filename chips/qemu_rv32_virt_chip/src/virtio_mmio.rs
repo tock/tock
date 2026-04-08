09fa1f1e0b260d835c5bc94dbbd8c91f0cb6b68f
Johnathan Van Why <jrvanwhy@betterbytes.org>
2026-04-07 19:53:30 -0700

    Rename update: Mmio*::with_addr -> Mmio*::from_addr.

--- chips/qemu_rv32_virt_chip/src/virtio_mmio.rs
+++ chips/qemu_rv32_virt_chip/src/virtio_mmio.rs
@@ -8,11 +8,11 @@ use tock_registers::Mmio32;
 
 type Registers = virtio::transports::mmio::virtio_mmio_device_registers::Real<Mmio32>;
 
-pub const VIRTIO_MMIO_0_BASE: Registers = unsafe { Registers::new(Mmio32::with_addr(0x1000_1000)) };
-pub const VIRTIO_MMIO_1_BASE: Registers = unsafe { Registers::new(Mmio32::with_addr(0x1000_2000)) };
-pub const VIRTIO_MMIO_2_BASE: Registers = unsafe { Registers::new(Mmio32::with_addr(0x1000_3000)) };
-pub const VIRTIO_MMIO_3_BASE: Registers = unsafe { Registers::new(Mmio32::with_addr(0x1000_4000)) };
-pub const VIRTIO_MMIO_4_BASE: Registers = unsafe { Registers::new(Mmio32::with_addr(0x1000_5000)) };
-pub const VIRTIO_MMIO_5_BASE: Registers = unsafe { Registers::new(Mmio32::with_addr(0x1000_6000)) };
-pub const VIRTIO_MMIO_6_BASE: Registers = unsafe { Registers::new(Mmio32::with_addr(0x1000_7000)) };
-pub const VIRTIO_MMIO_7_BASE: Registers = unsafe { Registers::new(Mmio32::with_addr(0x1000_8000)) };
+pub const VIRTIO_MMIO_0_BASE: Registers = unsafe { Registers::new(Mmio32::from_addr(0x1000_1000)) };
+pub const VIRTIO_MMIO_1_BASE: Registers = unsafe { Registers::new(Mmio32::from_addr(0x1000_2000)) };
+pub const VIRTIO_MMIO_2_BASE: Registers = unsafe { Registers::new(Mmio32::from_addr(0x1000_3000)) };
+pub const VIRTIO_MMIO_3_BASE: Registers = unsafe { Registers::new(Mmio32::from_addr(0x1000_4000)) };
+pub const VIRTIO_MMIO_4_BASE: Registers = unsafe { Registers::new(Mmio32::from_addr(0x1000_5000)) };
+pub const VIRTIO_MMIO_5_BASE: Registers = unsafe { Registers::new(Mmio32::from_addr(0x1000_6000)) };
+pub const VIRTIO_MMIO_6_BASE: Registers = unsafe { Registers::new(Mmio32::from_addr(0x1000_7000)) };
+pub const VIRTIO_MMIO_7_BASE: Registers = unsafe { Registers::new(Mmio32::from_addr(0x1000_8000)) };
