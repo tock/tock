f5a41ca39ccf8298a9196129a5a9d33083751166
FarhadGUL06 <farhadgul2001@gmail.com>
2026-04-09 11:41:44 +0300

    chips: replaced numbers with const

--- chips/stm32u5xx/src/chip.rs
+++ chips/stm32u5xx/src/chip.rs
@@ -7,6 +7,13 @@ use core::fmt::Write;
 use kernel::platform::chip::Chip;
 use kernel::platform::chip::InterruptService;
 
+// IRQ Numbers
+const EXTI13_IRQ: u32 = 24;
+const GPDMA1_CH0_IRQ: u32 = 29;
+const GPDMA1_CH1_IRQ: u32 = 30;
+const TIM2_IRQ: u32 = 45;
+const USART1_IRQ: u32 = 61;
+
 pub struct Stm32u5xx<'a, I: InterruptService + 'a> {
     mpu: cortexm33::mpu::MPU<8>,
     userspace_kernel_boundary: cortexm33::syscall::SysCall,
@@ -33,27 +40,27 @@ impl<'a> Stm32u5xxDefaultPeripherals<'a> {
 impl InterruptService for Stm32u5xxDefaultPeripherals<'_> {
     unsafe fn service_interrupt(&self, interrupt: u32) -> bool {
         match interrupt {
-            45 => {
+            TIM2_IRQ => {
                 // TIM2
                 self.tim2.handle_interrupt();
                 true
             }
-            61 => {
+            USART1_IRQ => {
                 // USART1
                 self.usart1.handle_interrupt();
                 true
             }
-            24 => {
+            EXTI13_IRQ => {
                 // EXTI13 (Button)
                 self.exti.handle_interrupt(13);
                 true
             }
-            29 => {
+            GPDMA1_CH0_IRQ => {
                 // GPDMA1 Channel 0 (USART1 TX Complete)
                 self.usart1.handle_dma_interrupt(true);
                 true
             }
-            30 => {
+            GPDMA1_CH1_IRQ => {
                 // GPDMA1 Channel 1 (USART1 RX Complete)
                 self.usart1.handle_dma_interrupt(false);
                 true
