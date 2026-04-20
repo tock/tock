d4aa1c7cebf9556035785dd77ae2958fd1dc4b3e
FarhadGUL06 <farhadgul2001@gmail.com>
2026-04-20 12:56:11 +0300

    nvic: Added all interrupts

--- chips/stm32u545/src/lib.rs
+++ chips/stm32u545/src/lib.rs
@@ -17,6 +17,6 @@ pub unsafe fn init() {
 // Link to the STM32U5 series reference manual (RM0456):
 // Table 186 "STM32U5 series vector table"
 // https://www.st.com/resource/en/reference_manual/rm0456-stm32u5-series-armbased-32bit-mcus-stmicroelectronics.pdf
-// Link to the STM32U545RE datasheet confirming 114 maskable interrupt channels:
+// Link to the STM32U545RE datasheet confirming 141 maskable interrupt channels:
 // https://www.st.com/resource/en/datasheet/stm32u545re.pdf (Section 3.19.1)
-pub static IRQS: [unsafe extern "C" fn(); 114] = [<CortexM33 as CortexMVariant>::GENERIC_ISR; 114];
+pub static IRQS: [unsafe extern "C" fn(); 141] = [<CortexM33 as CortexMVariant>::GENERIC_ISR; 141];
