7dd22b9a38d8897a945f78df8be0c44df5e5eb5d
Brad Campbell <bradjc5@gmail.com>
2026-05-21 21:27:35 -0400

    arch: riscv: use macro to make pseudo instruction

--- arch/riscv/src/pseudo_instructions.rs
+++ arch/riscv/src/pseudo_instructions.rs
@@ -2,48 +2,35 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // Copyright Tock Contributors 2026.
 
-//! Define missing RISC-V pseudo instructions using macros.
+//! Define missing RISC-V pseudo instructions using assembly macros.
 //!
 //! The RISC-V spec includes numerous pseudo instructions to help with writing
 //! RISC-V assembly. However, somewhat bafflingly, there are no pseudo
 //! instructions for making operations XLEN bits long. This makes it difficult
 //! to write general assembly code that works on both RV32 and RV64 systems,
-//! where the assembly only needs to operate on different register sizes.
+//! where the assembly only needs to operate on different XLENs.
 //!
-//! We use the pseudo instructions `lx` and `sx` for loading and storing an
-//! entire register, respectively.
+//! `xlen_macros!` defines macros `lx` and `sx`, which function as
+//! pseudoinstructions for loading and storing XLEN-sized values.
 
-/// Loads an XLEN size register.
-#[cfg(any(doc, target_arch = "riscv32"))]
+#[cfg(target_arch = "riscv32")]
 #[macro_export]
-macro_rules! lx {
-    () => {
-        "lw "
-    };
-}
+macro_rules! xlen_macros[() => [r"
+    .macro sx src, dest
+        sw \src, \dest
+    .endm
+    .macro lx dest, src
+        lw \dest, \src
+    .endm
+"]];
 
-/// Loads an XLEN size register.
 #[cfg(target_arch = "riscv64")]
 #[macro_export]
-macro_rules! lx {
-    () => {
-        "ld "
-    };
-}
-
-/// Stores a XLEN size register.
-#[cfg(any(doc, target_arch = "riscv32"))]
-#[macro_export]
-macro_rules! sx {
-    () => {
-        "sw "
-    };
-}
-/// Stores a XLEN size register.
-#[cfg(target_arch = "riscv64")]
-#[macro_export]
-macro_rules! sx {
-    () => {
-        "sd "
-    };
-}
+macro_rules! xlen_macros[() => [r"
+    .macro sx src, dest
+        sd \src, \dest
+    .endm
+    .macro lx dest, src
+        ld \dest, \src
+    .endm
+"]];
