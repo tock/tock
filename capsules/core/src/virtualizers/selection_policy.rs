ee8293746128a5b8462c10f1a95eac3a7e00ac68
Brad Campbell <bradjc5@gmail.com>
2026-06-09 14:40:42 -0400

    capsules: virt: selection: require Default, no insert order
    Only provide RoundRobin.
    
    Require Default to make it possible to construct automatically.

--- capsules/core/src/virtualizers/selection_policy.rs
+++ capsules/core/src/virtualizers/selection_policy.rs
@@ -6,29 +6,20 @@
 
 use core::cell::Cell;
 
-/// Trait that determines which device to choose first
+/// Trait that determines which device to choose next
 /// from the list of devices in the virtualizer.
-pub trait SelectionPolicy<I> {
+pub trait SelectionPolicy<I>: Default {
     /// Function that selects the device from the list based on the predicate.
     fn select<It>(&self, it: It, ready: impl Fn(&I) -> bool) -> Option<I>
     where
         It: Iterator<Item = I> + Clone;
 }
 
-/// Default policy which selects the first ready device starting from the beginning of the list.
-pub struct InsertionFirstPolicy;
-
-impl<I> SelectionPolicy<I> for InsertionFirstPolicy {
-    fn select<It>(&self, mut it: It, ready: impl Fn(&I) -> bool) -> Option<I>
-    where
-        It: Iterator<Item = I>,
-    {
-        it.find(|node| ready(node))
-    }
-}
-
-/// Round Robin policy which selects the first ready device based on the last access position.
-/// It stores the position of the last selected device to introduce fairness.
+/// Round-robin virtualizer selection policy.
+///
+/// Selects the first ready device based on the last access position. It stores
+/// the position of the last selected device to introduce fairness.
+#[derive(Default)]
 pub struct RoundRobinPolicy {
     last_access_position: Cell<usize>,
 }
@@ -47,13 +38,5 @@ impl<I> SelectionPolicy<I> for RoundRobinPolicy {
                 self.last_access_position.set(index);
                 node
             })
-    }
-}
-
-impl Default for RoundRobinPolicy {
-    fn default() -> Self {
-        Self {
-            last_access_position: Cell::new(0),
-        }
     }
 }
