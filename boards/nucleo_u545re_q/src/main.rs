615442b5a5d307c8349f03025a9836d7ec64916f
theonlytruealex <alexiova264@gmail.com>
2026-06-12 13:32:05 +0300

    finished TRNG implementation in low power mode

--- boards/nucleo_u545re_q/src/main.rs
+++ boards/nucleo_u545re_q/src/main.rs
@@ -12,8 +12,6 @@ use kernel::deferred_call::{DeferredCall, DeferredCallClient};
 use kernel::hil::entropy::Entropy32;
 use kernel::platform::chip::Chip;
 use kernel::platform::{KernelResources, SyscallDriverLookup};
-use kernel::utilities::registers::interfaces::Readable;
-use kernel::utilities::registers::interfaces::Writeable;
 use kernel::utilities::single_thread_value::SingleThreadValue;
 use kernel::{capabilities, debug};
 use kernel::{create_capability, static_init};
@@ -197,7 +195,7 @@ unsafe fn start() -> (
     usart1.register();
     let trng = static_init!(
         stm32u545::entropy::Trng<'static>,
-        stm32u545::entropy::Trng::new(stm32u545::entropy::RNG_BASE, DeferredCall::new())
+        stm32u545::entropy::Trng::new(stm32u545::entropy::RNG_BASE)
     );
 
     // Load Peripherals Bundle
@@ -417,8 +415,8 @@ unsafe fn start() -> (
     );
 
     // 3. Register the test as the driver's client
+    rng_test.register();
     trng.set_client(rng_test);
-    trng.register();
     // 4. Kick it off — call after the kernel loop is about to start
     // // temporary diagnostic
     rng_test.run();
--- chips/stm32u5xx/src/chip.rs
+++ chips/stm32u5xx/src/chip.rs
@@ -89,7 +89,6 @@ impl<'a> Stm32u5xxDefaultPeripherals<'a> {
         self.rcc.enable_pwr();
         self.rcc.enable_adc1();
         self.rcc.enable_trng();
-        self.trng.register();
         self.rcc.set_usart1_source_pclk();
 
         // ADC
@@ -108,6 +107,7 @@ impl<'a> Stm32u5xxDefaultPeripherals<'a> {
         if let (Some(tx), Some(rx)) = (usart1_channel_tx, usart1_channel_rx) {
             usart::Usart::set_dma(self.usart1, self.dma1, tx, rx);
         }
+        self.trng.init();
     }
 }
 
@@ -257,10 +257,6 @@ impl InterruptService for Stm32u5xxDefaultPeripherals<'_> {
             }
             GPDMA1_CH15_IRQ => {
                 self.dma1.handle_interrupt(ChannelId::Channel15);
-                true
-            }
-            RNG_IRQ => {
-                self.trng.handle_interrupt();
                 true
             }
             _ => false,
