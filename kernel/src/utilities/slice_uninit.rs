2cdae69fe661fc71ff78e5e195f82ec78cec8a7a
Brad Campbell <bradjc5@gmail.com>
2026-07-30 12:20:27 -0400

    kernel: slice uninit: format safety comments

--- kernel/src/utilities/slice_uninit.rs
+++ kernel/src/utilities/slice_uninit.rs
@@ -7,9 +7,7 @@
 /// This is a safe operation, but as of June 2026, there is no safe API in the
 /// standard library to do this. So, we provide our own.
 pub fn mut_slice_as_maybeuninit<T>(buffer: &mut [T]) -> &mut [core::mem::MaybeUninit<T>] {
-    // # Safety
-    //
-    // `MaybeUninit<T>` has the same size and alignment as `T`.
+    // SAFETY: `MaybeUninit<T>` has the same size and alignment as `T`.
     let maybeuninit_buf: &mut [core::mem::MaybeUninit<T>] =
         unsafe { core::slice::from_raw_parts_mut(buffer.as_mut_ptr().cast(), buffer.len()) };
     maybeuninit_buf
