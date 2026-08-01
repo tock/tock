e63d739c68cfaa9fd892a5e9f83967b65bdeef8f
Branden Ghena <branden@northwestern.edu>
2026-08-01 16:13:01 -0500

    Components for IPC capsules

---
+++ boards/components/src/ipc/mod.rs
@@ -0,0 +1,7 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright Tock Contributors 2026.
+
+pub mod ipc_registry_package_name;
+pub mod ipc_registry_string_name;
+pub mod ipc_relay_request;
