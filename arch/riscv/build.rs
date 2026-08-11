b3fd5ac04f754c73f626fa01bc531eb50a67d9de
Brad Campbell <bradjc5@gmail.com>
2026-08-11 09:43:41 -0700

    arch: riscv: build.rs re-run when env. var changes
    Co-authored-by: Johnathan Van Why <github@jrvanwhy.net>

--- arch/riscv/build.rs
+++ arch/riscv/build.rs
@@ -74,6 +74,8 @@ fn main() {
     // Get build variables to determine how we are building the crate.
     let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
     let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    println!("cargo::rerun-if-env-changed=CARGO_CFG_TARGET_ARCH");
+    println!("cargo::rerun-if-env-changed=CARGO_CFG_TARGET_OS");
 
     // `riscv_bare_metal`
     println!("cargo::rustc-check-cfg=cfg(riscv_bare_metal)");
