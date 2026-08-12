30ee35e66394f21e251c57a8619d886693801924
Brad Campbell <bradjc5@gmail.com>
2026-08-12 12:08:34 -0700

    kernel: chip: make create_panic_writer safe
    Rather than have the requirement that the constructor is only called in
    a panic context be a safety requirement, enforce that invariant by
    requiring the function to need a `PanicInfo` reference. The `PanicInfo`
    type has no public constructor and can only be created by the language
    at panic.

--- chips/nrf52/src/uart.rs
+++ chips/nrf52/src/uart.rs
@@ -837,7 +837,10 @@ pub struct UartPanicWriterConfig {
 impl kernel::platform::chip::PanicWriter for Uarte<'_> {
     type Config = UartPanicWriterConfig;
 
-    unsafe fn create_panic_writer(config: Self::Config) -> impl IoWrite + core::fmt::Write {
+    fn create_panic_writer(
+        config: Self::Config,
+        _panic: &core::panic::PanicInfo,
+    ) -> impl IoWrite + core::fmt::Write {
         use uart::Configure as _;
 
         let inner = Uarte::new(UARTE0_BASE);
