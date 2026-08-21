5069f489a1e0b57fe2ed9632750719ee1ac69869
Brad Campbell <bradjc5@gmail.com>
2026-08-21 15:56:49 -0400

    boards: qemu-rv32: provide peripherals from lib

--- boards/qemu_rv32_virt/src/main.rs
+++ boards/qemu_rv32_virt/src/main.rs
@@ -92,7 +92,7 @@ impl KernelResources<qemu_rv32_virt_lib::ChipHw> for Platform {
 pub unsafe fn main() {
     let main_loop_capability = create_capability!(capabilities::MainLoopCapability);
 
-    let (board_kernel, base_platform, chip) = qemu_rv32_virt_lib::start();
+    let (board_kernel, base_platform, chip, _peripherals) = qemu_rv32_virt_lib::start();
 
     let screen = base_platform.virtio_gpu_screen.map(|screen| {
         components::screen::ScreenComponent::new(
