746b891a5dbe48fb154a0c77c0f3b932dd6e9fc6
Brad Campbell <bradjc5@gmail.com>
2026-09-10 10:38:19 -0400

    chips: nrf52840: move usbd registers to nrf52840 crate

--- chips/nrf52840/src/interrupt_service.rs
+++ chips/nrf52840/src/interrupt_service.rs
@@ -5,7 +5,17 @@
 //! Handle interrupts for nRF52840-specific peripherals
 
 use kernel::hil::time::Alarm;
+use kernel::utilities::StaticRef;
 use nrf52::chip::Nrf52DefaultPeripherals;
+
+const USBD_BASE: StaticRef<crate::usbd::UsbdRegisters<'static>> =
+    unsafe { StaticRef::new(0x40027000 as *const crate::usbd::UsbdRegisters<'static>) };
+
+const USBD_CHIPINFO_BASE: StaticRef<crate::usbd::ChipInfoRegisters> =
+    unsafe { StaticRef::new(0x10000130 as *const crate::usbd::ChipInfoRegisters) };
+
+const USBD_USBERRATA_BASE: StaticRef<crate::usbd::UsbErrataRegisters> =
+    unsafe { StaticRef::new(0x4006E000 as *const crate::usbd::UsbErrataRegisters) };
 
 /// This struct, when initialized, instantiates all peripheral drivers for the nrf52840.
 ///
@@ -43,7 +53,7 @@ impl Nrf52840DefaultPeripherals<'_> {
         Self {
             nrf52: nrf52_peripherals,
             ieee802154_radio: crate::ieee802154_radio::Radio::new(ieee802154_radio_ack_buf),
-            usbd: crate::usbd::Usbd::new(),
+            usbd: crate::usbd::Usbd::new(USBD_BASE, USBD_CHIPINFO_BASE, USBD_USBERRATA_BASE),
             gpio_port: crate::gpio::nrf52840_gpio_create(),
         }
     }
