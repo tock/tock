25ed0f2a43e5879811e0e5aae9652e31bf7ef5c7
Alexandru RADOVICI <alexandru.radovici@oxidos.io>
2026-09-21 18:21:31 +0300

    add panic resources for the nucleo_u545re_q board and print cortex-m33 state

--- boards/nucleo_u545re_q/src/main.rs
+++ boards/nucleo_u545re_q/src/main.rs
@@ -9,6 +9,7 @@
 use components::hmac_component_static;
 use kernel::capabilities::{self, MemoryAllocationCapability};
 use kernel::component::Component;
+use kernel::debug;
 use kernel::debug::PanicResources;
 use kernel::hil::gpio::{Configure, Output};
 use kernel::hil::symmetric_encryption::AES256;
@@ -271,6 +272,11 @@ unsafe fn start() -> (
         <ChipHw as kernel::platform::chip::Chip>::ThreadIdProvider,
     >();
 
+    let _ = PANIC_RESOURCES
+        .bind_to_thread::<<ChipHw as kernel::platform::chip::Chip>::ThreadIdProvider>(
+            PanicResources::new(),
+        );
+
     // Create individual drivers
     let exti = static_init!(
         stm32u545::exti::Exti<'static>,
@@ -320,6 +326,11 @@ unsafe fn start() -> (
     // Kernel and Muxes
     let processes = components::process_array::ProcessArrayComponent::new()
         .finalize(components::process_array_component_static!(NUM_PROCS));
+
+    PANIC_RESOURCES.get().map(|resources| {
+        resources.processes.put(processes.as_slice());
+    });
+
     let board_kernel = static_init!(kernel::Kernel, kernel::Kernel::new(processes.as_slice()));
 
     let uart_mux = components::console::UartMuxComponent::new(&periphs.usart1, 115200)
@@ -364,12 +375,18 @@ unsafe fn start() -> (
         AES256
     ));
 
+    let process_printer = components::process_printer::ProcessPrinterTextComponent::new()
+        .finalize(components::process_printer_text_component_static!());
+
+    PANIC_RESOURCES.get().map(|resources| {
+        resources.printer.put(process_printer);
+    });
+
     let process_console = components::process_console::ProcessConsoleComponent::new(
         board_kernel,
         uart_mux,
         alarm_mux,
-        components::process_printer::ProcessPrinterTextComponent::new()
-            .finalize(components::process_printer_text_component_static!()),
+        process_printer,
         None,
         process_console_cap,
     )
@@ -509,15 +526,17 @@ unsafe fn start() -> (
             11 => periphs.gpio_a.pin(PinId::Pin07), // D11
             // 12 => D12/PA6 is used by the PWM capsule
             // 13 => D13/PA5 is used by the LD2 LED capsule
-            // D14-D15 require GPIOB
+            // Pins 14 and 15 are used by I2C
+            // 14 => periphs.gpio_b.pin(PinId::Pin07), // D14
+            // 15 => periphs.gpio_b.pin(PinId::Pin06), // D15
 
-            // Analog pins exposed as GPIO
-            16 => periphs.gpio_a.pin(PinId::Pin00), // A0
-            17 => periphs.gpio_a.pin(PinId::Pin01), // A1
-            18 => periphs.gpio_a.pin(PinId::Pin04), // A2
-            // 19 => A3 requires GPIOB
-            20 => periphs.gpio_c.pin(PinId::Pin01), // A4
-            21 => periphs.gpio_c.pin(PinId::Pin00), // A5
+            // Analog pins are used by ADC
+            // 16 => periphs.gpio_a.pin(PinId::Pin00), // A0
+            // 17 => periphs.gpio_a.pin(PinId::Pin01), // A1
+            // 18 => periphs.gpio_a.pin(PinId::Pin04), // A2
+            // 19 => periphs.gpio_b.pin(PinId::Pin00), // A3
+            // 20 => periphs.gpio_c.pin(PinId::Pin01), // A4
+            // 21 => periphs.gpio_c.pin(PinId::Pin00), // A5
 
             // ST Morpho-only GPIO pins (no D/A aliases)
             22 => periphs.gpio_c.pin(PinId::Pin10), // CN7 pin 1
@@ -590,6 +609,10 @@ unsafe fn start() -> (
         stm32u545::chip::Stm32u5xx::new(periphs)
     );
 
+    PANIC_RESOURCES.get().map(|resources| {
+        resources.chip.put(chip);
+    });
+
     // Symbols for linker
     extern "C" {
         /// Beginning of the ROM region containing app images.
@@ -630,6 +653,9 @@ pub unsafe fn main() {
     let main_loop_capability = create_capability!(capabilities::MainLoopCapability);
 
     let (board_kernel, platform, chip) = start();
+
+    debug!("Initialization complete. Entering main loop");
+
     // Hand over control to the Tock Kernel Loop
     board_kernel.kernel_loop::<NucleoU545RE, ChipHw, { NUM_PROCS as u8 }>(
         platform,
