c0345e8ea7c16352a9e907250090fd03e09a2e80
Brad Campbell <bradjc5@gmail.com>
2026-09-24 13:19:50 -0400

    arch: cortex-m: make Syscall construction safe
    Just having a syscall implementation is fine.

--- chips/psoc62xa/src/chip.rs
+++ chips/psoc62xa/src/chip.rs
@@ -18,7 +18,7 @@ impl<'a, I: InterruptService> Psoc62xa<'a, I> {
     pub fn new(interrupt_service: &'a I) -> Self {
         Self {
             mpu: unsafe { cortexm0p::mpu::new() },
-            userspace_kernel_boundary: unsafe { cortexm0p::syscall::SysCall::new() },
+            userspace_kernel_boundary: cortexm0p::syscall::SysCall::new(),
             interrupt_service,
         }
     }
